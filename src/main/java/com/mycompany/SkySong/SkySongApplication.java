f6be92097bdafd0f56c7bf1656dced9793a65caf
Champel Mariusz <mariuszchampel@gmail.com>
2023-08-13 15:14:07 +0200

    Initial commit

---
+++ src/main/java/com/mycompany/SkySong/SkySongApplication.java
@@ -0,0 +1,13 @@
+package com.mycompany.SkySong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkySongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkySongApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mycompany/SkySong/SkySongApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mycompany.SkySong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkySongApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
