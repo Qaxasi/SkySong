25c12094d034056f9d107bd8c62a83cdef512512
Champel Mariusz <champelmariusz@gmail.com>
2023-08-20 22:02:01 +0200

    feature/add-class-WeatherConstants

---
+++ src/main/java/com/mycompany/SkySong/constants/WeatherConstants.java
@@ -0,0 +1,8 @@
+package com.mycompany.SkySong.constants;
+
+public class WeatherConstants {
+    public static final int MAX_RETRIES = 3;
+    public static final int RETRY_SLEEP_MILLIS = 5000;
+
+
+}
