7c0247ded7bc8a2c8b90a3f34b1a54b480e8f1fb
Champel Mariusz <champelmariusz@gmail.com>
2023-08-23 12:28:13 +0200

    feature/adding-column-names-to-Track-entity

--- src/main/java/com/mycompany/SkySong/entity/Track.java
+++ src/main/java/com/mycompany/SkySong/entity/Track.java
@@ -1,7 +1,6 @@
 package com.mycompany.SkySong.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -17,13 +16,30 @@ import java.util.Date;
 @Table(name = "track")
 public class Track {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Integer id;
+
+    @Column(name = "title", nullable = false)
     private String title;
+
+    @Column(name = "artist", nullable = false)
     private String artist;
+
+    @Column(name = "album", nullable = false)
     private String album;
+
+    @Column(name = "duration", nullable = false)
     private Integer duration;
+
+    @Column(name = "release_date", nullable = false)
     private Date release_date;
+
+    @Column(name = "genre", nullable = false)
     private String genre;
+
+    @Column(name = "mood", nullable = false)
     private String mood;
 
 
