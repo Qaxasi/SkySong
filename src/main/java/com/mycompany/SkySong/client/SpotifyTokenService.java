17d7bc4df9a33c3299b42ec898a235e7b7a0a635
Champel Mariusz <champelmariusz@gmail.com>
2023-08-23 16:44:58 +0200

    refactor: Adding a function to save the token expiration date and chceck if the token has already expired in method getAccessToken()

--- src/main/java/com/mycompany/SkySong/client/SpotifyTokenService.java
+++ src/main/java/com/mycompany/SkySong/client/SpotifyTokenService.java
@@ -1,16 +1,28 @@
 package com.mycompany.SkySong.client;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import okhttp3.*;
 import org.json.JSONObject;
+import org.springframework.stereotype.Component;
 
-public class MusicApiClient {
+import java.io.IOException;
+import java.time.Instant;
+
+@Component
+public class SpotifyTokenService {
 
     private static final String SPOTIFY_CLIENT_ID = System.getenv("SPOTIFY_CLIENT_ID");
     private static final String SPOTIFY_CLIENT_SECRET = System.getenv("SPOTIFY_CLIENT_SECRET");
     private static final String SPOTIFY_TOKEN_API_URL_TEMPLATE = "https://accounts.spotify.com/api/token";
+    private final OkHttpClient client = new OkHttpClient();
+    private String cachedToken;
+    private Instant tokenExpiryTime;
 
-    public static String getAccessToken() throws Exception {
-        OkHttpClient client = new OkHttpClient();
+    public String getAccessToken() throws Exception {
+        if (cachedToken != null && Instant.now().isBefore(tokenExpiryTime)) {
+            return cachedToken;
+        }
 
         RequestBody fromBody = new FormBody.Builder()
                 .add("grant_type", "client_credentials")
@@ -31,7 +43,11 @@ public class MusicApiClient {
 
             String responseData = response.body().string();
             JSONObject jsonResponse = new JSONObject(responseData);
-            return jsonResponse.getString("access_token");
+
+            cachedToken = jsonResponse.getString("access_token");
+
+            tokenExpiryTime = Instant.now().plusSeconds(jsonResponse.getInt("expires_int") - 10);
+            return cachedToken;
         }
     }
 }
