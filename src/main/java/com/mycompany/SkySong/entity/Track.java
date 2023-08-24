a0fba6b17db8636efbb09db1bba70ebc44b84f73
Champel Mariusz <champelmariusz@gmail.com>
2023-08-24 18:56:08 +0200

    refactor: removing fields - artist, album, genre, mood from class Track. Addition of field spotifyApiTrackId

--- src/main/java/com/mycompany/SkySong/entity/Track.java
+++ src/main/java/com/mycompany/SkySong/entity/Track.java
@@ -21,14 +21,11 @@ public class Track {
     @Column(name = "id")
     private Integer id;
 
+    @Column(name = "spotify_track_id")
+    private Integer spotifyTrackId;
+
     @Column(name = "title", nullable = false)
     private String title;
-
-    @Column(name = "artist", nullable = false)
-    private String artist;
-
-    @Column(name = "album", nullable = false)
-    private String album;
 
     @Column(name = "duration", nullable = false)
     private Integer duration;
@@ -36,11 +33,8 @@ public class Track {
     @Column(name = "release_date", nullable = false)
     private Date release_date;
 
-    @Column(name = "genre", nullable = false)
-    private String genre;
-
-    @Column(name = "mood", nullable = false)
-    private String mood;
+    @Column(name = "uri", nullable = false)
+    private String uri;
 
 
 
