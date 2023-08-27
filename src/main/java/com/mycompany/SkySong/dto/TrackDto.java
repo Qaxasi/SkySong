69feed82a10affb25a91322930bbf32cd55310b3
Champel Mariusz <champelmariusz@gmail.com>
2023-08-27 17:43:19 +0200

    feat: addition of TrackDto class

--- src/main/java/com/mycompany/SkySong/dto/TrackDto.java
+++ src/main/java/com/mycompany/SkySong/dto/TrackDto.java
@@ -1,2 +1,17 @@
-package com.mycompany.SkySong.dto;public class TrackDto {
+package com.mycompany.SkySong.dto;
+
+import lombok.Data;
+
+@Data
+public class TrackDto {
+
+    private Integer id;
+    private String spotifyTrackId;
+    private String title;
+    private Integer duration;
+    private String url;
+    private Integer artistId;
+    private Integer albumId;
+
+
 }
