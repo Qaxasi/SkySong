26b6052d97650c899bc83a311cca7496d86f00ba
Champel Mariusz <champelmariusz@gmail.com>
2023-08-30 22:16:15 +0200

    feat: addition of WebClient configuration class

--- src/main/java/com/mycompany/SkySong/config/WebConfig.java
+++ src/main/java/com/mycompany/SkySong/config/WebConfig.java
@@ -1,2 +1,13 @@
-package com.mycompany.SkySong.config;public class WebConfig {
+package com.mycompany.SkySong.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public WebClient webClient() {
+        return WebClient.builder().build();
+    }
 }
