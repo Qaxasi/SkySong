760c3cfeda84b1f03a08d218ec2ef561f6747770
Champel Mariusz <champelmariusz@gmail.com>
2023-08-30 22:14:30 +0200

    refactor: addition of @Repository annotation and remove unused imports

--- src/main/java/com/mycompany/SkySong/repository/LocationDAO.java
+++ src/main/java/com/mycompany/SkySong/repository/LocationDAO.java
@@ -2,7 +2,9 @@ package com.mycompany.SkySong.repository;
 
 import com.mycompany.SkySong.entity.Location;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface LocationDAO extends JpaRepository<Location, Integer> {
     Location findByLocationName(String locationName);
 
