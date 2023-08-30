ec78345fb2f513c3e6db29d3f3bc507361fa48c0
Champel Mariusz <champelmariusz@gmail.com>
2023-08-30 22:07:29 +0200

    refactor: remove unused imports

--- src/main/java/com/mycompany/SkySong/SkySongApplication.java
+++ src/main/java/com/mycompany/SkySong/SkySongApplication.java
@@ -11,5 +11,4 @@ public class SkySongApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SkySongApplication.class, args);
 	}
-
 }
---
+++ src/main/java/com/mycompany/SkySong/config/WebConfig.java
@@ -0,0 +1,2 @@
+package com.mycompany.SkySong.config;public class WebConfig {
+}
