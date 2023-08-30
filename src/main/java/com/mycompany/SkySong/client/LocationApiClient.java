38201df455495c97749a3a81fca49c5083b94344
Champel Mariusz <champelmariusz@gmail.com>
2023-08-30 22:12:30 +0200

    refactor: change Okhttp to WebClient

--- src/main/java/com/mycompany/SkySong/client/LocationApiClient.java
+++ src/main/java/com/mycompany/SkySong/client/LocationApiClient.java
@@ -1,49 +1,50 @@
 package com.mycompany.SkySong.client;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.mycompany.SkySong.exception.GeocodingException;
+import com.mycompany.SkySong.entity.Location;
+import com.mycompany.SkySong.entity.LocationRequest;
 import com.mycompany.SkySong.exception.ValidationException;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.io.IOException;
+import java.time.Duration;
 
 
 @Component
+@Slf4j
 public class LocationApiClient {
 
     private static final String GEOCODING_API_URL_TEMPLATE =
-            "http://api.positionstack.com/v1/forward?access_key=%s&query=%s";
-    private final String API_KEY = System.getenv("GEOCODING_API_KEY");
-    private final OkHttpClient client = new OkHttpClient();
-    private final ObjectMapper objectMapper = new ObjectMapper();
+            "http://api.openweathermap.org/geo/1.0/direct";
+    private final String API_KEY = System.getenv("WEATHER_API_KEY");
+    private final Duration timeout = Duration.ofSeconds(5);
+    private final WebClient webClient;
 
-    public JsonNode fetchGeocodingData(String locationName) throws IOException {
+    public LocationApiClient(WebClient webClient) {
+        this.webClient = WebClient.builder()
+                .baseUrl(GEOCODING_API_URL_TEMPLATE)
+                .build();
+    }
+    public LocationRequest fetchGeocodingData(String locationName) throws IOException {
+        validateLocationName(locationName);
+
+        return webClient.get()
+                .uri(uriBuilder -> uriBuilder
+                        .queryParam("q", locationName)
+                        .queryParam("appid", API_KEY)
+                        .build())
+                .retrieve()
+                .bodyToFlux(LocationRequest.class)
+                .next()
+                .timeout(timeout)
+                .doOnError(e -> log.error("An error occurred while fetching geocoding data", e)).block();
+    }
+    private void validateLocationName(String locationName) {
         if (locationName == null || locationName.trim().isEmpty()) {
             throw new ValidationException(
                     "Location name cannot be null or empty. First you need to specify your location.");
         }
-
-        String apiUrl = String.format(GEOCODING_API_URL_TEMPLATE, API_KEY, locationName);
-        Request request = new Request.Builder()
-                .url(apiUrl)
-                .build();
-
-        try (Response response = client.newCall(request).execute()) {
-            if (response.isSuccessful() && response.body() != null) {
-                JsonNode jsonNode = objectMapper.readTree(response.body().string());
-                if (jsonNode.has("error")) {
-                    throw new GeocodingException("API returned error: " + jsonNode.get("error").asText());
-                }
-                return jsonNode;
-            } else {
-                throw new GeocodingException("Failed to fetch geocoding data. Response code: " + response.code());
-            }
-        } catch (IOException e) {
-            throw new GeocodingException("Error communicating with the geocoding API.", e);
-        }
     }
 }
+
