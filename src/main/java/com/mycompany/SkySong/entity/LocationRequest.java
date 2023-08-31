d9bfdc599b0cfc57e813650fcd983f2e37c76458
Champel Mariusz <champelmariusz@gmail.com>
2023-08-31 12:05:25 +0200

    feat: addition of record LocationRequest

--- src/main/java/com/mycompany/SkySong/entity/LocationRequest.java
+++ src/main/java/com/mycompany/SkySong/entity/LocationRequest.java
@@ -1,2 +1,11 @@
-package com.mycompany.SkySong.entity;public class LocationRequest {
+package com.mycompany.SkySong.entity;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public record LocationRequest(@JsonProperty("name") String locationName,
+                              @JsonProperty("lat") Double latitude,
+                              @JsonProperty("lon") Double longitude,
+                              @JsonProperty("country") String country,
+                              @JsonProperty("state") String state)
+{
 }
