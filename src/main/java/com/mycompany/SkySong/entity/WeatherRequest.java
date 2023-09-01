adb83c112a5aa0b79d62581e39c5784955ddc3d4
Champel Mariusz <champelmariusz@gmail.com>
2023-09-01 17:48:52 +0200

    feat: addition of record WeatherRequest

--- src/main/java/com/mycompany/SkySong/entity/WeatherRequest.java
+++ src/main/java/com/mycompany/SkySong/entity/WeatherRequest.java
@@ -1,2 +1,8 @@
-package com.mycompany.SkySong.entity;public record WeatherRequest() {
+package com.mycompany.SkySong.entity;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.List;
+
+public record WeatherRequest(@JsonProperty List<WeatherInfo> weather) {
 }
