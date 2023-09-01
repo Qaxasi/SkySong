e1fcf6f991d6fc3be04ad0e9e6f39a54d3ea7a15
Champel Mariusz <champelmariusz@gmail.com>
2023-09-01 17:33:20 +0200

    refactor: switch OkHttp to WebClient in WeatherApiClient

--- src/main/java/com/mycompany/SkySong/client/WeatherApiClient.java
+++ src/main/java/com/mycompany/SkySong/client/WeatherApiClient.java
@@ -3,47 +3,51 @@ package com.mycompany.SkySong.client;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mycompany.SkySong.constants.GeocodingConstants;
+import com.mycompany.SkySong.entity.WeatherRequest;
 import com.mycompany.SkySong.exception.ValidationException;
 import com.mycompany.SkySong.exception.WeatherException;
+import lombok.extern.slf4j.Slf4j;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.io.IOException;
 
 @Component
+@Slf4j
 public class WeatherApiClient {
-
     private static final String WEATHER_API_URL_TEMPLATE =
-            "https://api.openweathermap.org/data/2.5/weather?lat=%f&lon=%f&appid=%s";
+            "https://api.openweathermap.org/data/2.5/weather";
     private final String API_KEY = System.getenv("WEATHER_API_KEY");
-    private final OkHttpClient client = new OkHttpClient();
-    private final ObjectMapper objectMapper = new ObjectMapper();
+    private final WebClient webClient;
+    public WeatherApiClient(WebClient webClient) {
+        this.webClient = WebClient.builder()
+                .baseUrl(WEATHER_API_URL_TEMPLATE)
+                .build();
+    }
 
-    public JsonNode fetchWeatherData(double latitude, double longitude) throws IOException {
+    public WeatherRequest fetchWeatherData(double latitude, double longitude) throws IOException {
         if (latitude < GeocodingConstants.MINIMUM_VALUES_FOR_LATITUDE ||
                 latitude > GeocodingConstants.MAXIMUM_VALUES_FOR_LATITUDE ||
                 longitude < GeocodingConstants.MINIMUM_VALUES_FOR_LONGITUDE
                 || longitude > GeocodingConstants.MAXIMUM_VALUES_FOR_LONGITUDE) {
             throw new ValidationException("Invalid latitude or longitude values.");
         }
-        String apiUrl = String.format(WEATHER_API_URL_TEMPLATE, latitude, longitude, API_KEY);
-        Request request = new Request.Builder().url(apiUrl).build();
 
-        try(Response response = client.newCall(request).execute()) {
-            if (response.isSuccessful() && response.body() != null) {
-                JsonNode jsonNode = objectMapper.readTree(response.body().string());
-                if (jsonNode.has("error")) {
-                    throw new WeatherException("API returned error: " + jsonNode.get("error").asText());
-                }
-                return jsonNode;
-            } else {
-                throw new WeatherException("Failed to fetch weather data. Response code: " + response.code());
-            }
-        } catch (IOException e) {
-            throw new WeatherException("Error communicating with the weather API.", e);
-        }
+        return webClient.get()
+                .uri(uriBuilder -> uriBuilder
+                        .queryParam("lat", latitude)
+                        .queryParam("lon", longitude)
+                        .queryParam("appid", API_KEY)
+                        .build())
+                .retrieve()
+                .bodyToFlux(WeatherRequest.class)
+                .next()
+                .doOnError(e -> log.error("An error occurred while fetching weather data", e))
+                .block();
+
 
     }
 }
