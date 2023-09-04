172cd614620eba5e87e3e0b57702601c33c29d1f
Champel Mariusz <champelmariusz@gmail.com>
2023-09-04 17:24:15 +0200

    feat: addition of class TokenController and maps HTTP GET requests to 'api/auth/token'

--- src/main/java/com/mycompany/SkySong/controller/TokenController.java
+++ src/main/java/com/mycompany/SkySong/controller/TokenController.java
@@ -1,2 +1,24 @@
-package com.mycompany.SkySong.controller;public class TokenController {
+package com.mycompany.SkySong.controller;
+
+import com.mycompany.SkySong.client.SpotifyTokenService;
+import com.mycompany.SkySong.entity.SpotifyAccessToken;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/auth")
+public class TokenController {
+    private SpotifyTokenService spotifyTokenService;
+
+    @Autowired
+    public TokenController(SpotifyTokenService spotifyTokenService) {
+        this.spotifyTokenService = spotifyTokenService;
+    }
+
+    @GetMapping("/token")
+    public SpotifyAccessToken fetchToken() throws Exception {
+        return spotifyTokenService.getAccessToken();
+    }
 }
