c8e4ffc81f7c0f0eb6c0408719eeb055c2339f26
Champel Mariusz <champelmariusz@gmail.com>
2023-09-04 22:41:00 +0200

    feat: addition of TokenRequest record and toMultiValueMap method

--- src/main/java/com/mycompany/SkySong/entity/TokenRequest.java
+++ src/main/java/com/mycompany/SkySong/entity/TokenRequest.java
@@ -1,2 +1,20 @@
-package com.mycompany.SkySong.entity;public class TokenRequest {
+package com.mycompany.SkySong.entity;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+
+import java.util.List;
+
+public record TokenRequest(@JsonProperty("grant_type") String grantType,
+                           @JsonProperty("code") String code,
+                           @JsonProperty("redirect_uri") String redirectUri) {
+
+    public MultiValueMap<String, String> toMultiValueMap() {
+        var bodyValue = new LinkedMultiValueMap<String, String>();
+        bodyValue.put("grant_type", List.of(this.grantType));
+        bodyValue.put("code", List.of(this.code));
+        bodyValue.put("redirect_uri", List.of(this.redirectUri));
+        return bodyValue;
+    }
 }
