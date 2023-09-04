05a4cfa6c5ed95c7212fb67bc86bd7463ebdfac0
Champel Mariusz <champelmariusz@gmail.com>
2023-09-04 22:39:35 +0200

    feat: addition of SpotifyAccessToken record

---
+++ src/main/java/com/mycompany/SkySong/entity/SpotifyAccessToken.java
@@ -0,0 +1,11 @@
+package com.mycompany.SkySong.entity;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public record SpotifyAccessToken (
+        @JsonProperty("access_token") String token,
+        @JsonProperty("token_type") String tokenType,
+        @JsonProperty("expires_in") int expirationTimeInSec
+
+        ) {
+}
