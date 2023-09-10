3b18e250307d56a70c6d4b838f4a967c2937182a
Champel Mariusz <champelmariusz@gmail.com>
2023-09-10 23:43:35 +0200

    refactor: Remove unused imports

--- src/test/java/com/mycompany/SkySong/location/client/LocationApiClientTest.java
+++ src/test/java/com/mycompany/SkySong/location/client/LocationApiClientTest.java
@@ -1,21 +1,17 @@
 package com.mycompany.SkySong.location.client;
 
 import com.mycompany.SkySong.exception.ValidationException;
-import com.mycompany.SkySong.music.authorization.service.SpotifyAuthorizationService;
 import okhttp3.mockwebserver.MockResponse;
 import okhttp3.mockwebserver.MockWebServer;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
 import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.client.WebClient;
-
 import java.io.IOException;
-
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static reactor.core.publisher.Mono.when;
+
 
 class LocationApiClientTest {
 
