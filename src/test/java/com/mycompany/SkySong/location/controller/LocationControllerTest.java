2ea65661d52e499761fa2496b7c10f694a9e2611
Champel Mariusz <champelmariusz@gmail.com>
2023-09-10 23:40:24 +0200

    test: Add test for BadRequest on empty location name

--- src/test/java/com/mycompany/SkySong/location/controller/LocationControllerTest.java
+++ src/test/java/com/mycompany/SkySong/location/controller/LocationControllerTest.java
@@ -1,7 +1,10 @@
 package com.mycompany.SkySong.location.controller;
 
+import com.mycompany.SkySong.exception.ValidationException;
 import com.mycompany.SkySong.location.exception.LocationException;
+import com.mycompany.SkySong.location.exception.LocationNotGiven;
 import com.mycompany.SkySong.location.service.LocationService;
+import org.junit.Before;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,13 +12,14 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import java.io.IOException;
 
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(LocationController.class)
@@ -25,7 +29,6 @@ class LocationControllerTest {
     private MockMvc mockMvc;
     @MockBean
     private LocationService locationService;
-
 
     @Test
     void shouldReturnLocationExceptionDuringFetchAndSaveCoordinates() throws Exception {
@@ -48,5 +51,15 @@ class LocationControllerTest {
         verify(locationService).fetchAndSaveCoordinates(locationName);
     }
 
-    
+    @Test
+    void shouldReturnBadRequestWhenLocationNameIsEmpty() throws Exception {
+        String locationName = "";
+
+        when(locationService.fetchAndSaveCoordinates(locationName))
+                .thenThrow(new LocationNotGiven("Location name cannot be empty"));
+
+        mockMvc.perform(get("/api/location/coordinates/" + locationName)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNotFound());
+    }
 }
