d675631e1fb1e36da33a9bedc89c64430caa918b
Champel Mariusz <champelmariusz@gmail.com>
2023-09-11 20:04:46 +0200

    refactory: refactor method TooManyRequestException

--- src/main/java/com/mycompany/SkySong/location/exception/TooManyRequestException.java
+++ src/main/java/com/mycompany/SkySong/location/exception/TooManyRequestException.java
@@ -1,7 +1,7 @@
 package com.mycompany.SkySong.location.exception;
 
 public class TooManyRequestException extends RuntimeException {
-    public TooManyRequestException() {
-        super();
+    public TooManyRequestException(String message) {
+        super(message);
     }
 }
