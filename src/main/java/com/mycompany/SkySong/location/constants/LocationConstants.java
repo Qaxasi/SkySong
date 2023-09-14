bd5dca6fa3e63e8d00a9fd2dd6ae86d9d13cbc1c
Champel Mariusz <champelmariusz@gmail.com>
2023-09-14 20:41:23 +0200

    refactor: Change class name

--- src/main/java/com/mycompany/SkySong/location/constants/LocationConstants.java
+++ src/main/java/com/mycompany/SkySong/location/constants/LocationConstants.java
@@ -1,6 +1,6 @@
 package com.mycompany.SkySong.location.constants;
 
-public class GeocodingConstants {
+public class LocationConstants {
 
     public static final int MAXIMUM_VALUES_FOR_LATITUDE = 90;
     public static final int MINIMUM_VALUES_FOR_LATITUDE = -90;
