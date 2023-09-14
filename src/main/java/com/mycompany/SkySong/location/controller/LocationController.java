54e91e90a2598cd5c1178c22bf573c51e6550d86
Champel Mariusz <champelmariusz@gmail.com>
2023-09-14 22:46:36 +0200

    feat: Add endpoint to handle missing location errors

--- src/main/java/com/mycompany/SkySong/location/controller/LocationController.java
+++ src/main/java/com/mycompany/SkySong/location/controller/LocationController.java
@@ -1,6 +1,8 @@
 package com.mycompany.SkySong.location.controller;
 
+import com.mycompany.SkySong.config.ErrorResponse;
 import com.mycompany.SkySong.location.entity.LocationRequest;
+import com.mycompany.SkySong.location.exception.LocationNotGiven;
 import com.mycompany.SkySong.location.service.LocationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -10,16 +12,20 @@ import java.io.IOException;
 @RestController
 @RequestMapping("/api/location/coordinates/")
 public class LocationController {
-    @Autowired
     private LocationService locationService;
+
+    @Autowired
     public LocationController(LocationService locationService) {
         this.locationService = locationService;
     }
 
+    @GetMapping
+    public void handleErrorWhenLocationIsNotGiven() {
+        throw new LocationNotGiven();
+    }
     @GetMapping("{locationName}")
-    public LocationRequest getLocationCoordinates(
+    public LocationRequest fetchLocationCoordinates(
             @PathVariable String locationName) throws IOException {
         return locationService.fetchAndSaveCoordinates(locationName);
-
     }
 }
