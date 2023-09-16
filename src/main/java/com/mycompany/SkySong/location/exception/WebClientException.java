88c7b49b581d04a73010a5a98e75f0b236ff4a61
Champel Mariusz <champelmariusz@gmail.com>
2023-09-16 18:25:12 +0200

    feat: Add WebClientException for WebClient error handling

---
+++ src/main/java/com/mycompany/SkySong/location/exception/WebClientException.java
@@ -0,0 +1,7 @@
+package com.mycompany.SkySong.location.exception;
+
+public class WebClientException extends RuntimeException {
+    public WebClientException(String message) {
+        super(message);
+    }
+}
