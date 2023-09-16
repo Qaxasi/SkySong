042d0e75ff707b7ecf57fdceb1ed365926bf05d5
Champel Mariusz <champelmariusz@gmail.com>
2023-09-16 18:23:02 +0200

    refactor: Simplify null-checks in LocationApiClient constructor

--- src/main/java/com/mycompany/SkySong/location/client/LocationApiClient.java
+++ src/main/java/com/mycompany/SkySong/location/client/LocationApiClient.java
@@ -13,28 +13,25 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
 import reactor.core.publisher.Flux;
 
 import java.time.Duration;
+import java.util.Objects;
 
 
 @Component
 @Slf4j
 public class LocationApiClient {
     private final String API_KEY;
-    private final Duration timeout = Duration.ofSeconds(5);
+    private final Duration timeout;
     private final WebClient webClient;
     @Autowired
     public LocationApiClient(@Qualifier("location") WebClient webClient,
                              @Value("${WEATHER_API_KEY}") String API_KEY) {
-        if (webClient == null) {
-            throw new IllegalArgumentException("WebClient cannot be null");
-        }
-        if (API_KEY == null || API_KEY.isEmpty()) {
-            throw new IllegalArgumentException("API_KEY cannot be null or empty");
-        }
-        this.webClient = webClient;
-        this.API_KEY = API_KEY;
+        this.webClient = Objects.requireNonNull(webClient, "WebClient cannot be null");
+        this.API_KEY = Objects.requireNonNull(API_KEY, "API_KEY cannot be null");
+        this.timeout = Duration.ofSeconds(5);
     }
 
     public LocationRequest fetchGeocodingData(String locationName) {
@@ -64,7 +61,7 @@ public class LocationApiClient {
                     .next()
                     .timeout(timeout)
                     .block();
-        } catch (Exception e) {
+        } catch (WebClientResponseException e) {
             log.error("An error occurred while fetching geocoding data: {}", e.getMessage());
             throw new LocationClientException("An error occurred while fetching geocoding data", e);
         }
