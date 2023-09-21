9f87b7244bf94b4d5af20ab8c647e7cb47432a37
Champel Mariusz <champelmariusz@gmail.com>
2023-09-21 16:53:54 +0200

    feat: Define Genres Entity with Many-to-Many User Relationships

---
+++ src/main/java/com/mycompany/SkySong/entity/Genres.java
@@ -0,0 +1,32 @@
+package com.mycompany.SkySong.entity;
+
+import com.mycompany.SkySong.user.entity.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Genres {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @ManyToMany(mappedBy = "preferredGenres")
+    private Set<User> usersWhoPrefer = new HashSet<>();
+
+    @ManyToMany(mappedBy = "dislikedGenres")
+    private Set<User> usersWhoDislike;
+
+}
