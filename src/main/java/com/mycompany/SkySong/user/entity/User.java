2d0acc660990f4fa71fbae0fbfd49e245f37a6bc
Champel Mariusz <champelmariusz@gmail.com>
2023-09-21 17:22:05 +0200

    feat: Add Many-to-Many mapping between User and Role Entities

--- src/main/java/com/mycompany/SkySong/user/entity/User.java
+++ src/main/java/com/mycompany/SkySong/user/entity/User.java
@@ -38,4 +38,10 @@ public class User {
     @OneToOne(mappedBy = "user", cascade = CascadeType.ALL)
     private Location location;
 
+
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinTable(name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id"))
+    private Set<Role> roles;
 }
