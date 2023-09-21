dd21761464f32c194f3eecd9aaa7f54bbe4d6163
Champel Mariusz <champelmariusz@gmail.com>
2023-09-21 17:14:46 +0200

    feat: Create User Entity

--- src/main/java/com/mycompany/SkySong/user/entity/User.java
+++ src/main/java/com/mycompany/SkySong/user/entity/User.java
@@ -1,6 +1,36 @@
 package com.mycompany.SkySong.user.entity;
 
+import com.mycompany.SkySong.entity.Artist;
+import com.mycompany.SkySong.entity.Genres;
+import com.mycompany.SkySong.role.entity.Role;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Setter
+@Getter
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "user")
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
 
 }
