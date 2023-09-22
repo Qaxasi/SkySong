849bb6a7039b01c0a6a367a1607065d86ca093e7
Champel Mariusz <champelmariusz@gmail.com>
2023-09-22 17:27:15 +0200

    feat: Add repository method to find user by username or email

--- src/main/java/com/mycompany/SkySong/user/repository/UserDAO.java
+++ src/main/java/com/mycompany/SkySong/user/repository/UserDAO.java
@@ -7,5 +7,6 @@ import java.util.Optional;
 
 public interface UserDAO extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
+    Optional<User> findByUsernameOrEmail(String username, String email);
 
  }
