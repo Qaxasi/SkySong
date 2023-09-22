1e0d7585cd0c7bcb50a2b5fa8f24767c18bb2a3c
Champel Mariusz <champelmariusz@gmail.com>
2023-09-22 16:18:59 +0200

    refactor: Extend JWTAuthResponse record with fields and annotations

--- src/main/java/com/mycompany/SkySong/user/entity/JWTAuthResponse.java
+++ src/main/java/com/mycompany/SkySong/user/entity/JWTAuthResponse.java
@@ -1,2 +1,7 @@
-package com.mycompany.SkySong.user.entity;public record JWTAuthResponse() {
+package com.mycompany.SkySong.user.entity;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public record JWTAuthResponse(@JsonProperty("accessToken") String accessToken,
+                              @JsonProperty("tokenType") String tokenType) {
 }
