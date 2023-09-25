4f4eb7560e61440682c5b1225636cc575f447143
Champel Mariusz <champelmariusz@gmail.com>
2023-09-25 20:15:26 +0200

    feat: Add Many-to-Many mapping between User and Role Entities

--- src/main/java/com/mycompany/SkySong/user/entity/User.java
+++ src/main/java/com/mycompany/SkySong/user/entity/User.java
@@ -1,10 +1,13 @@
 package com.mycompany.SkySong.user.entity;
 
+import com.mycompany.SkySong.role.entity.Role;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.util.Set;
 
 @Setter
 @Getter
@@ -26,5 +29,11 @@ public class User {
 
     @Column(nullable = false)
     private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinTable(name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id"))
+    private Set<Role> roles;
 
 }
