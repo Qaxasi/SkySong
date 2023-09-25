775d43b97378d4f5a7a264ff5fa7136c492e25ee
Champel Mariusz <champelmariusz@gmail.com>
2023-09-25 21:51:47 +0200

    feat: Extends RoleDAO interface and add findByName method to RoleDAO

--- src/main/java/com/mycompany/SkySong/role/repository/RoleDAO.java
+++ src/main/java/com/mycompany/SkySong/role/repository/RoleDAO.java
@@ -1,4 +1,10 @@
 package com.mycompany.SkySong.role.repository;
 
-public interface RoleDAO {
+import com.mycompany.SkySong.role.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RoleDAO extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(String name);
 }
