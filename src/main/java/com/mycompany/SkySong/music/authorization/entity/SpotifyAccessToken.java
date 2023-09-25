fc4a84ea15bd8afc88e09818bb9b0d14a95398fd
Champel Mariusz <champelmariusz@gmail.com>
2023-09-25 18:05:19 +0200

    feat: Add scope field to SpotifyAccessToken record

--- src/main/java/com/mycompany/SkySong/music/authorization/entity/SpotifyAccessToken.java
+++ src/main/java/com/mycompany/SkySong/music/authorization/entity/SpotifyAccessToken.java
@@ -5,7 +5,8 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 public record SpotifyAccessToken (
         @JsonProperty("access_token") String token,
         @JsonProperty("token_type") String tokenType,
-        @JsonProperty("expires_in") int expirationTimeInSec
+        @JsonProperty("expires_in") int expirationTimeInSec,
+        String scope
 
         ) {
 }
