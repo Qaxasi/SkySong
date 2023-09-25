75ea0c07455259d9c178624e37984d6590b769dd
Champel Mariusz <champelmariusz@gmail.com>
2023-09-25 20:17:05 +0200

    feat: Add Many-to-Many User-Role mappings

--- src/main/java/com/mycompany/SkySong/role/entity/Role.java
+++ src/main/java/com/mycompany/SkySong/role/entity/Role.java
@@ -1,10 +1,13 @@
 package com.mycompany.SkySong.role.entity;
 
+import com.mycompany.SkySong.user.entity.User;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.util.Set;
 
 @Getter
 @Setter
@@ -18,4 +21,7 @@ public class Role {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+
+    @ManyToMany(mappedBy = "roles")
+    private Set<User> users;
 }
