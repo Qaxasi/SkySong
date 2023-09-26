59784d0dc54edf3395bb5911b44115aca93132a4
Champel Mariusz <champelmariusz@gmail.com>
2023-09-26 18:02:03 +0200

    feat: Inject UserDetailsService to SecurityConfig

--- src/main/java/com/mycompany/SkySong/config/SecurityConfig.java
+++ src/main/java/com/mycompany/SkySong/config/SecurityConfig.java
@@ -2,7 +2,14 @@ package com.mycompany.SkySong.config;
 
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
 @Configuration
 public class SecurityConfig {
+
+    private UserDetailsService userDetailsService;
+
+    public SecurityConfig(UserDetailsService userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
 }
