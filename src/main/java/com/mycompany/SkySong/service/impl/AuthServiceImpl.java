d2552cdc87c8ec30cf17ad345b3dfcc0223ef2a7
Champel Mariusz <champelmariusz@gmail.com>
2023-09-26 22:01:10 +0200

    feat: Implement login method in AuthServiceImpl

--- src/main/java/com/mycompany/SkySong/service/impl/AuthServiceImpl.java
+++ src/main/java/com/mycompany/SkySong/service/impl/AuthServiceImpl.java
@@ -3,6 +3,9 @@ package com.mycompany.SkySong.service.impl;
 import com.mycompany.SkySong.service.AuthService;
 import com.mycompany.SkySong.user.entity.LoginResponse;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -15,6 +18,11 @@ public class AuthServiceImpl implements AuthService {
 
     @Override
     public String login(LoginResponse loginResponse) {
-        return null;
+        Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
+                loginResponse.usernameOrEmail(), loginResponse.password()));
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        return "User Logged-in successfully";
     }
 }
