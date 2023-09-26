1f4c484505238afeadfee78b53da878fab255ac3
Champel Mariusz <champelmariusz@gmail.com>
2023-09-26 18:58:21 +0200

    refactor: Rename method

--- src/main/java/com/mycompany/SkySong/user/repository/UserDAO.java
+++ src/main/java/com/mycompany/SkySong/user/repository/UserDAO.java
@@ -8,6 +8,6 @@ import java.util.Optional;
 public interface UserDAO extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
     Optional<User> findByUsernameOrEmail(String username, String email);
-    Boolean existByUsername(String username);
-    Boolean existByEmail(String email);
+    Boolean existsByUsername(String username);
+    Boolean existsByEmail(String email);
 }
