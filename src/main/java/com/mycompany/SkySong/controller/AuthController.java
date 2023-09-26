548daf579a5703de3a1f4a11d8d01998c1f94978
Champel Mariusz <champelmariusz@gmail.com>
2023-09-26 22:25:08 +0200

    feat: Inject AuthService

--- src/main/java/com/mycompany/SkySong/controller/AuthController.java
+++ src/main/java/com/mycompany/SkySong/controller/AuthController.java
@@ -1,7 +1,14 @@
 package com.mycompany.SkySong.controller;
 
+import com.mycompany.SkySong.service.AuthService;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AuthController {
+
+    private AuthService authService;
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
 }
