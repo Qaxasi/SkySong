712a223cbe87d232a783e6f66e9545c711a04a74
Champel Mariusz <champelmariusz@gmail.com>
2023-09-26 18:29:37 +0200

    feat: Add AuthenticationManager bean

--- src/main/java/com/mycompany/SkySong/config/SecurityConfig.java
+++ src/main/java/com/mycompany/SkySong/config/SecurityConfig.java
@@ -3,6 +3,8 @@ package com.mycompany.SkySong.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -22,4 +24,11 @@ public class SecurityConfig {
     public static PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
+
+
+
 }
