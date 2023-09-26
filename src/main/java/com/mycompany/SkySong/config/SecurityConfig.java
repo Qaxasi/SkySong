e3c06630625db0593775f8ac90bb127d7cca8d53
Champel Mariusz <champelmariusz@gmail.com>
2023-09-26 18:04:27 +0200

    feat: Add @EnableMethodSecurity annotation to SecurityConfig class

--- src/main/java/com/mycompany/SkySong/config/SecurityConfig.java
+++ src/main/java/com/mycompany/SkySong/config/SecurityConfig.java
@@ -2,9 +2,11 @@ package com.mycompany.SkySong.config;
 
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 
 @Configuration
+@EnableMethodSecurity
 public class SecurityConfig {
 
     private UserDetailsService userDetailsService;
