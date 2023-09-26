ff64eb605bc81310e2c92f48f89fcc15deadb798
Champel Mariusz <champelmariusz@gmail.com>
2023-09-26 22:30:20 +0200

    feat: Add @RequestMapping annotation

--- src/main/java/com/mycompany/SkySong/controller/AuthController.java
+++ src/main/java/com/mycompany/SkySong/controller/AuthController.java
@@ -1,9 +1,11 @@
 package com.mycompany.SkySong.controller;
 
 import com.mycompany.SkySong.service.AuthService;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/v1/auth")
 public class AuthController {
 
     private AuthService authService;
@@ -11,4 +13,5 @@ public class AuthController {
     public AuthController(AuthService authService) {
         this.authService = authService;
     }
+
 }
