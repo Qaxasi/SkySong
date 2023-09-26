65d1518f0de9170b0955583bd718f1c086db2733
Champel Mariusz <champelmariusz@gmail.com>
2023-09-26 18:05:44 +0200

    feat: Add BCryptPasswordEncoder bean

--- src/main/java/com/mycompany/SkySong/config/SecurityConfig.java
+++ src/main/java/com/mycompany/SkySong/config/SecurityConfig.java
@@ -1,9 +1,12 @@
 package com.mycompany.SkySong.config;
 
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableMethodSecurity
@@ -13,5 +16,10 @@ public class SecurityConfig {
 
     public SecurityConfig(UserDetailsService userDetailsService) {
         this.userDetailsService = userDetailsService;
+    }
+
+    @Bean
+    public static PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
