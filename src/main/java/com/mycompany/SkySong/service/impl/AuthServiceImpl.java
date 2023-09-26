8191a50c6c95694c8994575b7657be2a620a82d2
Champel Mariusz <champelmariusz@gmail.com>
2023-09-26 20:43:12 +0200

    feat: Add @Service annotation

--- src/main/java/com/mycompany/SkySong/service/impl/AuthServiceImpl.java
+++ src/main/java/com/mycompany/SkySong/service/impl/AuthServiceImpl.java
@@ -1,2 +1,8 @@
-package com.mycompany.SkySong.service.impl;public class AuthServiceImpl {
+package com.mycompany.SkySong.service.impl;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthServiceImpl {
+
 }
