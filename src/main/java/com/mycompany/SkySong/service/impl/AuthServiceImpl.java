53aabf38227af4202126e7593ccdb5c935d79be6
Champel Mariusz <champelmariusz@gmail.com>
2023-09-26 21:57:21 +0200

    feat: Implement AuthService in AuthServiceImpl and inject AuthenticationManager

--- src/main/java/com/mycompany/SkySong/service/impl/AuthServiceImpl.java
+++ src/main/java/com/mycompany/SkySong/service/impl/AuthServiceImpl.java
@@ -1,8 +1,20 @@
 package com.mycompany.SkySong.service.impl;
 
+import com.mycompany.SkySong.service.AuthService;
+import com.mycompany.SkySong.user.entity.LoginResponse;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.stereotype.Service;
 
 @Service
-public class AuthServiceImpl {
+public class AuthServiceImpl implements AuthService {
+    private AuthenticationManager authenticationManager;
 
+    public AuthServiceImpl(AuthenticationManager authenticationManager) {
+        this.authenticationManager = authenticationManager;
+    }
+
+    @Override
+    public String login(LoginResponse loginResponse) {
+        return null;
+    }
 }
