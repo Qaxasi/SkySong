37f62994176d29601da0946b5a3a18352420d6bd
Champel Mariusz <champelmariusz@gmail.com>
2023-09-28 20:54:43 +0200

    feat: Add login endpoint to AuthController

--- src/main/java/com/mycompany/SkySong/controller/AuthController.java
+++ src/main/java/com/mycompany/SkySong/controller/AuthController.java
@@ -1,6 +1,10 @@
 package com.mycompany.SkySong.controller;
 
 import com.mycompany.SkySong.service.AuthService;
+import com.mycompany.SkySong.user.entity.LoginRequest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +16,13 @@ public class AuthController {
 
     public AuthController(AuthService authService) {
         this.authService = authService;
+    }
+
+    @PostMapping({"/login", "/signing"})
+    public ResponseEntity<String> login(@RequestBody LoginRequest loginRequest) {
+        String response = authService.login(loginRequest);
+        return ResponseEntity.ok(response);
+
     }
 
 }
