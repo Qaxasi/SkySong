6579c687066486d1afd3027afe5f392a6bbd22b8
Champel Mariusz <champelmariusz@gmail.com>
2023-09-28 22:56:18 +0200

    feat: Add register method to AuthService interface

--- src/main/java/com/mycompany/SkySong/service/AuthService.java
+++ src/main/java/com/mycompany/SkySong/service/AuthService.java
@@ -1,7 +1,9 @@
 package com.mycompany.SkySong.service;
 
 import com.mycompany.SkySong.user.entity.LoginRequest;
+import com.mycompany.SkySong.user.entity.RegisterRequest;
 
 public interface AuthService {
     String login(LoginRequest loginRequest);
+    String register(RegisterRequest registerRequest);
 }
