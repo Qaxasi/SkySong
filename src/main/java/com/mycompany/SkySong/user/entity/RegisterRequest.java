1ee664c72a0d8f3e38173e146d4cc70343941f2b
Champel Mariusz <champelmariusz@gmail.com>
2023-09-30 22:24:02 +0200

    feat: Add validation annotations to RegisterRequest fields

--- src/main/java/com/mycompany/SkySong/user/entity/RegisterRequest.java
+++ src/main/java/com/mycompany/SkySong/user/entity/RegisterRequest.java
@@ -1,8 +1,15 @@
 package com.mycompany.SkySong.user.entity;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
 
-public record RegisterRequest(@JsonProperty("username") String username,
-                               @JsonProperty("email") String email,
-                               @JsonProperty("password") String password) {
+public record RegisterRequest(@Pattern(regexp = "^[a-zA-Z0-9]*$",
+                              message = "The username can contain only letter and numbers.")
+                              String username,
+                              @Email(message = "Invalid email address format.")
+                              String email,
+                              @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}$",
+                                      message = "The password must contain an least 8 characters, including " +
+                                              "uppercase letters, lowercase letters, numbers, and special characters.")
+                              String password) {
 }
