48e6914d6ccfb76bb46fc04a0f3112da8b81db84
Champel Mariusz <champelmariusz@gmail.com>
2023-10-01 20:28:03 +0200

    feat: Implement token generation in JwtTokenProvider

--- src/main/java/com/mycompany/SkySong/secutiry/JwtTokenProvider.java
+++ src/main/java/com/mycompany/SkySong/secutiry/JwtTokenProvider.java
@@ -1,2 +1,36 @@
-package com.mycompany.SkySong.secutiry;public class JwtTokenProvider {
+package com.mycompany.SkySong.secutiry;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+
+@Component
+public class JwtTokenProvider {
+    @Value("${app.jwt-secret}")
+    private String jwtSecret;
+    @Value("${app-jwt-expiration-milliseconds}")
+    private long jwtExpirationDate;
+
+    public String generateToken(Authentication authentication) {
+        String username = authentication.getName();
+
+        Date currentDate = new Date();
+
+        Date expireDate = new Date(currentDate.getTime() + jwtExpirationDate);
+
+        String token = Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(expireDate)
+                .signWith(key())
+                .compact();
+
+        return token;
+    }
 }
