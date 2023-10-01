94b1dd67b1d9e7e23e365f56bec147a034086223
Champel Mariusz <champelmariusz@gmail.com>
2023-10-01 17:19:00 +0200

    feat: Add handler for MethodArgumentNotValidException

--- src/main/java/com/mycompany/SkySong/ex/GlobalExHandler.java
+++ src/main/java/com/mycompany/SkySong/ex/GlobalExHandler.java
@@ -2,9 +2,14 @@ package com.mycompany.SkySong.ex;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @ControllerAdvice
 public class GlobalExHandler {
@@ -13,6 +18,15 @@ public class GlobalExHandler {
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ResponseEntity<String> handleRegisterException(final RegisterException ex) {
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<Map<String, String>> handleMethodArgumentNotValid(MethodArgumentNotValidException exception) {
+
+        Map<String, String> errorMap = exception.getBindingResult().getFieldErrors().stream()
+                .collect(Collectors.toMap(FieldError::getField, FieldError::getDefaultMessage));
+
+        return new ResponseEntity<>(errorMap, HttpStatus.BAD_REQUEST);
     }
 
 }
