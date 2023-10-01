c763fd50729b603bf745a20d6f6e1e6216b1206d
Champel Mariusz <champelmariusz@gmail.com>
2023-10-01 16:05:21 +0200

    feat: Add method to create ErrorResponse from general exceptions

--- src/main/java/com/mycompany/SkySong/config/ErrorResponseBuilder.java
+++ src/main/java/com/mycompany/SkySong/config/ErrorResponseBuilder.java
@@ -1,2 +1,8 @@
-package com.mycompany.SkySong.config;public class ErrorResponseBuilder {
+package com.mycompany.SkySong.config;
+
+public class ErrorResponseBuilder {
+    public static ErrorResponse createFromGeneralException(Exception exception) {
+        return new ErrorResponse(exception.getMessage());
+    }
+
 }
