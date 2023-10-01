293e1d7f90f2b047de886ac0ca0983896822b23f
Champel Mariusz <champelmariusz@gmail.com>
2023-10-01 20:51:25 +0200

    feat: Extend TokenException from RuntimeException

--- src/main/java/com/mycompany/SkySong/ex/TokenException.java
+++ src/main/java/com/mycompany/SkySong/ex/TokenException.java
@@ -1,4 +1,8 @@
 package com.mycompany.SkySong.ex;
 
-public class TokenException {
+public class TokenException extends RuntimeException {
+    public TokenException(String message) {
+        super(message);
+    }
+
 }
