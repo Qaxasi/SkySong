311c42d74e02f0a59432a2314df96e29bdb71db1
Champel Mariusz <champelmariusz@gmail.com>
2023-10-02 13:44:43 +0200

    feat: Add fields and constructor to RegistrationResponse DTO

--- src/main/java/com/mycompany/SkySong/dto/RegistrationResponse.java
+++ src/main/java/com/mycompany/SkySong/dto/RegistrationResponse.java
@@ -1,2 +1,11 @@
-package com.mycompany.SkySong.dto;public class RegistrationResponse {
+package com.mycompany.SkySong.dto;
+
+public class RegistrationResponse {
+    private final boolean success;
+    private final String message;
+
+    public RegistrationResponse(boolean success, String message) {
+        this.success = success;
+        this.message = message;
+    }
 }
