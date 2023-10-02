c91fb44cf54169f5200a1b9a73f5c89e5393729c
Champel Mariusz <champelmariusz@gmail.com>
2023-10-02 13:05:30 +0200

    refactor: Convert JWTAuthResponse from record to class with lombok annotations

--- src/main/java/com/mycompany/SkySong/dto/JWTAuthResponse.java
+++ src/main/java/com/mycompany/SkySong/dto/JWTAuthResponse.java
@@ -1,7 +1,15 @@
-package com.mycompany.SkySong.user.entity;
+package com.mycompany.SkySong.dto;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-public record JWTAuthResponse(@JsonProperty("accessToken") String accessToken,
-                              @JsonProperty("tokenType") String tokenType) {
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class JWTAuthResponse {
+    private String accessToken;
+    private String tokenType = "Bearer";
 }
