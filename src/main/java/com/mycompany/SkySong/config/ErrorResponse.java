2ecefe66c6f0577ce7b45bbf130a95822a5c13bd
Champel Mariusz <champelmariusz@gmail.com>
2023-10-02 22:00:24 +0200

    refactor: Delete ErrorResponse and ErrorResponseBuilder classes

--- src/main/java/com/mycompany/SkySong/config/ErrorResponse.java
+++
@@ -1,8 +0,0 @@
-package com.mycompany.SkySong.config;
-
-import com.fasterxml.jackson.annotation.JsonProperty;
-
-public record ErrorResponse(
-        @JsonProperty("Error message") String message
-) {
-}
--- src/main/java/com/mycompany/SkySong/config/ErrorResponseBuilder.java
+++
@@ -1,8 +0,0 @@
-package com.mycompany.SkySong.config;
-
-public class ErrorResponseBuilder {
-    public static ErrorResponse createFromGeneralException(Exception exception) {
-        return new ErrorResponse(exception.getMessage());
-    }
-
-}
