781e2ae72dd3c5901c9e19a65bfe9294ee342371
Champel Mariusz <champelmariusz@gmail.com>
2023-10-02 11:56:03 +0200

    feat: Update securityFilterChain to include exception handling and stateless session management

--- src/main/java/com/mycompany/SkySong/config/SecurityConfig.java
+++ src/main/java/com/mycompany/SkySong/config/SecurityConfig.java
@@ -11,6 +11,7 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -44,10 +45,12 @@ public class SecurityConfig {
                         authorize
                                 .requestMatchers(HttpMethod.GET, "/api/v1/**").permitAll()
                                 .requestMatchers("/api/v1/auth/**").permitAll()
-                                .anyRequest().authenticated()).httpBasic(Customizer.withDefaults());
+                                .anyRequest().authenticated()).httpBasic(Customizer.withDefaults()
+                ).exceptionHandling(exception -> exception
+                        .authenticationEntryPoint(authenticationEntryPoint)
+                ).sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
         return http.build();
     }
-
-
 }
