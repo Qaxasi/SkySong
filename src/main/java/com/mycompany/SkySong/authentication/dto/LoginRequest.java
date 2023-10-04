c64aaa159108ed94d0412306c1f97149af6ebdb4
Champel Mariusz <champelmariusz@gmail.com>
2023-10-04 18:33:03 +0200

    refactor: Add validation constraints to LoginRequest fields

--- src/main/java/com/mycompany/SkySong/authentication/dto/LoginRequest.java
+++ src/main/java/com/mycompany/SkySong/authentication/dto/LoginRequest.java
@@ -1,8 +1,13 @@
 package com.mycompany.SkySong.authentication.dto;
 
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 
 public record LoginRequest(
-        @NotNull String usernameOrEmail,
-        @NotNull String password) {
+         @NotNull(message = "The usernameOrEmail field cannot be null")
+         @NotEmpty(message = "The usernameOrEmail field cannot be empty")
+         String usernameOrEmail,
+         @NotNull(message = "The password field cannot be null")
+         @NotEmpty(message = "The password field cannot be empty")
+         String password) {
 }
