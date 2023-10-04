b6366a37c1fe5f47b61792d002dc743965ed60f8
Champel Mariusz <champelmariusz@gmail.com>
2023-10-04 18:46:07 +0200

    refactor: Refactor RegistrationResponse to record

--- src/main/java/com/mycompany/SkySong/authentication/dto/RegistrationResponse.java
+++ src/main/java/com/mycompany/SkySong/authentication/dto/RegistrationResponse.java
@@ -1,11 +1,4 @@
-package com.mycompany.SkySong.dto;
+package com.mycompany.SkySong.authentication.dto;
 
-public class RegistrationResponse {
-    private final boolean success;
-    private final String message;
-
-    public RegistrationResponse(boolean success, String message) {
-        this.success = success;
-        this.message = message;
-    }
+public record RegistrationResponse(boolean success, String message) {
 }
