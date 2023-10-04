754eb7dde4ba171e18710b12045868078a07f916
Champel Mariusz <champelmariusz@gmail.com>
2023-10-04 19:14:06 +0200

    feat: Add null and empty checks to RegisterRequest fieds

--- src/main/java/com/mycompany/SkySong/authentication/dto/RegisterRequest.java
+++ src/main/java/com/mycompany/SkySong/authentication/dto/RegisterRequest.java
@@ -1,15 +1,23 @@
-package com.mycompany.SkySong.dto;
+package com.mycompany.SkySong.authentication.dto;
 
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 
 public record RegisterRequest(@Pattern(regexp = "^[a-zA-Z0-9]*$",
                               message = "The username can contain only letter and numbers.")
+                              @NotNull(message = "The username field cannot be null.")
+                              @NotEmpty(message = "The username field cannot be empty.")
                               String username,
                               @Email(message = "Invalid email address format.")
+                              @NotNull(message = "The email field cannot be null")
+                              @NotEmpty(message = "The email field cannot be empty")
                               String email,
                               @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}$",
                                       message = "The password must contain an least 8 characters, including " +
                                               "uppercase letters, lowercase letters, numbers, and special characters.")
+                              @NotNull(message = "The password field cannot be null")
+                              @NotEmpty(message = "The password field cannot be empty")
                               String password) {
 }
