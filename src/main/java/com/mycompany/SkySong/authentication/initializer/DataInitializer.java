c6d2b480ef58e7d4a29c906f43ae4b0d31bf8973
Champel Mariusz <champelmariusz@gmail.com>
2023-10-05 18:55:12 +0200

    feat: Added DataInitializer configuration to set up default roles at application start-up

---
+++ src/main/java/com/mycompany/SkySong/authentication/initializer/DataInitializer.java
@@ -0,0 +1,48 @@
+package com.mycompany.SkySong.authentication.initializer;
+
+import com.mycompany.SkySong.authentication.role.entity.Role;
+import com.mycompany.SkySong.authentication.role.entity.UserRole;
+import com.mycompany.SkySong.authentication.role.repository.RoleDAO;
+import com.mycompany.SkySong.authentication.user.entity.User;
+import com.mycompany.SkySong.authentication.user.repository.UserDAO;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.Set;
+
+
+@Configuration
+public class DataInitializer {
+    private final UserDAO userDAO;
+    private final RoleDAO roleDAO;
+    private final PasswordEncoder passwordEncoder;
+
+    public DataInitializer(UserDAO userDAO, RoleDAO roleDAO, PasswordEncoder passwordEncoder) {
+        this.userDAO = userDAO;
+        this.roleDAO = roleDAO;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Bean
+    public CommandLineRunner setupDefaultRoles(RoleDAO roleDAO) {
+        return args -> {
+            Role userRole = roleDAO.findByName(UserRole.ROLE_USER.name())
+                    .orElseGet(() -> {
+                        Role newUserRole = new Role(UserRole.ROLE_USER);
+                        return roleDAO.save(newUserRole);
+                    });
+            Role adminRole = roleDAO.findByName(UserRole.ROLE_ADMIN.name())
+                    .orElseGet(() -> {
+                        Role newAdminRole = new Role(UserRole.ROLE_ADMIN);
+                        return roleDAO.save(newAdminRole);
+                    });
+
+
+        };
+    }
+
+}
+
+
