61b2413454ca045c2df6ba5512af9b0343f10aed
Champel Mariusz <champelmariusz@gmail.com>
2023-10-06 18:25:07 +0200

    test: Add TokenTest class and initialize JWT test setup

--- src/test/java/com/mycompany/SkySong/authentication/security/TokenTest.java
+++ src/test/java/com/mycompany/SkySong/authentication/security/TokenTest.java
@@ -1,2 +1,34 @@
-package com.mycompany.SkySong.authentication.security;public class TokenTest {
+package com.mycompany.SkySong.authentication.security;
+
+import com.mycompany.SkySong.authentication.secutiry.JwtTokenProvider;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.core.Authentication;
+
+
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.util.Base64;
+
+import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+public class TokenTest {
+    private JwtTokenProvider jwtTokenProvider;
+    @Mock
+    private Authentication mockAuth;
+
+    @BeforeEach
+    public void setUp() {
+        Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+        String base64Secret = Base64.getEncoder().encodeToString(key.getEncoded());
+        jwtTokenProvider = new JwtTokenProvider(base64Secret, 100000L);
+    }
 }
