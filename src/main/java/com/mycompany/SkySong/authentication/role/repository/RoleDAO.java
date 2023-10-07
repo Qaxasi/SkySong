ceb384ae5373a98cc709c5fb25d08e2b3f9d81d4
Champel Mariusz <champelmariusz@gmail.com>
2023-10-07 20:11:14 +0200

    refactor: Change findByName method to use UserRole argument

--- src/main/java/com/mycompany/SkySong/authentication/role/repository/RoleDAO.java
+++ src/main/java/com/mycompany/SkySong/authentication/role/repository/RoleDAO.java
@@ -1,10 +1,11 @@
 package com.mycompany.SkySong.authentication.role.repository;
 
 import com.mycompany.SkySong.authentication.role.entity.Role;
+import com.mycompany.SkySong.authentication.role.entity.UserRole;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface RoleDAO extends JpaRepository<Role, Long> {
-    Optional<Role> findByName(String name);
+    Optional<Role> findByName(UserRole roleName);
 }
