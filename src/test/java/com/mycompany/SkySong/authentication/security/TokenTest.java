9c67ba2a1f27e2567b00ee30d77291a0ce1b5d33
Champel Mariusz <champelmariusz@gmail.com>
2023-10-07 15:26:13 +0200

    test: Remove unused imports

--- src/test/java/com/mycompany/SkySong/authentication/security/TokenTest.java
+++ src/test/java/com/mycompany/SkySong/authentication/security/TokenTest.java
@@ -2,7 +2,6 @@ package com.mycompany.SkySong.authentication.security;
 
 import com.mycompany.SkySong.authentication.exception.TokenException;
 import com.mycompany.SkySong.authentication.secutiry.JwtTokenProvider;
-import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
@@ -10,7 +9,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.security.core.Authentication;
 
