245acb807d6d1870071e0c55226ddee4d2762ba8
Champel Mariusz <champelmariusz@gmail.com>
2023-10-07 18:05:07 +0200

    test: Add test to ensure valid token return for valid user credentials

--- src/test/java/com/mycompany/SkySong/authentication/service/AuthServiceImplTest.java
+++ src/test/java/com/mycompany/SkySong/authentication/service/AuthServiceImplTest.java
@@ -1,19 +1,31 @@
 package com.mycompany.SkySong.authentication.service;
 
+import com.mycompany.SkySong.authentication.dto.LoginRequest;
 import com.mycompany.SkySong.authentication.role.repository.RoleDAO;
 import com.mycompany.SkySong.authentication.secutiry.JwtTokenProvider;
 import com.mycompany.SkySong.authentication.service.impl.AuthServiceImpl;
 import com.mycompany.SkySong.authentication.user.entity.User;
 import com.mycompany.SkySong.authentication.user.repository.UserDAO;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class AuthServiceImplTest {
-    @InjectMocks
     private AuthServiceImpl authService;
     @Mock
     private UserDAO userDAO;
@@ -23,9 +35,26 @@ public class AuthServiceImplTest {
     private PasswordEncoder passwordEncoder;
     @Mock
     private JwtTokenProvider jwtTokenProvider;
+    @Mock
+    private AuthenticationManager authenticationManager;
+    @Mock
+    private Authentication authentication;
 
+    @BeforeEach
+    public void setUp() {
+        authService = new AuthServiceImpl(authenticationManager, userDAO, roleDAO, passwordEncoder, jwtTokenProvider);
+    }
+    @Test
+    void shouldReturnValidTokenWhenGivenValidCredentials() {
+        User validUser = new User();
+        validUser.setUsername("testUsername");
+        validUser.setPassword("testPassword");
 
+        when(authenticationManager.authenticate(any(UsernamePasswordAuthenticationToken.class))).thenReturn(authentication);
+        when(jwtTokenProvider.generateToken(authentication)).thenReturn("validToken");
 
+        String token = authService.login(new LoginRequest("testUsername", "testPassword"));
 
-
+        assertEquals("validToken", token);
+    }
 }
