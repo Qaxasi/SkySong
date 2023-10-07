b34c128f673a1d45a59caacc5e6689e891c6193e
Champel Mariusz <champelmariusz@gmail.com>
2023-10-07 16:01:50 +0200

    test: Initialize mocks in AuthServiceImplTest

--- src/test/java/com/mycompany/SkySong/authentication/service/AuthServiceImplTest.java
+++ src/test/java/com/mycompany/SkySong/authentication/service/AuthServiceImplTest.java
@@ -1,10 +1,30 @@
 package com.mycompany.SkySong.authentication.service;
 
+import com.mycompany.SkySong.authentication.role.repository.RoleDAO;
+import com.mycompany.SkySong.authentication.secutiry.JwtTokenProvider;
+import com.mycompany.SkySong.authentication.service.impl.AuthServiceImpl;
+import com.mycompany.SkySong.authentication.user.entity.User;
+import com.mycompany.SkySong.authentication.user.repository.UserDAO;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @ExtendWith(MockitoExtension.class)
 public class AuthServiceImplTest {
+    @InjectMocks
+    private AuthServiceImpl authService;
+    @Mock
+    private UserDAO userDAO;
+    @Mock
+    private RoleDAO roleDAO;
+    @Mock
+    private PasswordEncoder passwordEncoder;
+    @Mock
+    private JwtTokenProvider jwtTokenProvider;
+
+
 
 
 
