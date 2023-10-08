e18a47501f1e8b1790ea326abb47473fc9feff58
Champel Mariusz <champelmariusz@gmail.com>
2023-10-08 17:11:42 +0200

    refactor: Implement custom authentication exception handling in commence method

--- src/main/java/com/mycompany/SkySong/authentication/secutiry/JwtAuthenticationEntryPoint.java
+++ src/main/java/com/mycompany/SkySong/authentication/secutiry/JwtAuthenticationEntryPoint.java
@@ -4,6 +4,7 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
@@ -20,6 +21,10 @@ public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
         log.error("Unauthorized error: {}", authException.getMessage());
         response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
         response.setContentType("application/json");
-        response.getWriter().write(authException.getMessage());
+        if (authException instanceof BadCredentialsException) {
+            response.getWriter().write("Incorrect username/email or password");
+        } else {
+            response.getWriter().write(authException.getMessage());
+        }
     }
 }
