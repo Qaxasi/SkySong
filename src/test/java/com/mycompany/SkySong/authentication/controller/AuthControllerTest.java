a26df534c8307a428068f635cf6c55b226fa0a30
Champel Mariusz <champelmariusz@gmail.com>
2023-10-11 22:52:39 +0200

    test: Modify test data preloading mechanism in AuthControllerTest

--- src/test/java/com/mycompany/SkySong/authentication/controller/AuthControllerTest.java
+++ src/test/java/com/mycompany/SkySong/authentication/controller/AuthControllerTest.java
@@ -1,13 +1,19 @@
 package com.mycompany.SkySong.authentication.controller;
 
+import org.h2.tools.Script;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.http.MediaType;
+import org.springframework.jdbc.datasource.init.ScriptUtils;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
+
+import javax.sql.DataSource;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -19,8 +25,13 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 public class AuthControllerTest {
     @Autowired
     private MockMvc mockMvc;
+    @Autowired
+    private DataSource dataSource;
 
-    @Sql("classpath:data_sql/user-data.sql")
+    @BeforeEach
+    void init() throws Exception {
+        ScriptUtils.executeSqlScript(dataSource.getConnection(), new ClassPathResource("data_sql/user-data.sql"));
+    }
     @Test
     void shouldProvideAccessTokenAndTokenTypeOnSuccessfulLogin() throws Exception {
         final var requestBody = "{\"usernameOrEmail\": \"testEmail@gmail.com\",\"password\": \"testPassword@123\"}";
@@ -32,4 +43,5 @@ public class AuthControllerTest {
                 .andExpect(jsonPath("$.accessToken").exists())
                 .andExpect(jsonPath("$.tokenType").exists());
     }
+
 }
