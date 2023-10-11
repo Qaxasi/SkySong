6d433e0cdf130a9a352cf8ff1a7eab114261139a
Champel Mariusz <champelmariusz@gmail.com>
2023-10-11 22:29:55 +0200

    test: Add integration test for successful login

--- src/test/java/com/mycompany/SkySong/authentication/controller/AuthControllerTest.java
+++ src/test/java/com/mycompany/SkySong/authentication/controller/AuthControllerTest.java
@@ -1,14 +1,37 @@
 package com.mycompany.SkySong.authentication.controller;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
 public class AuthControllerTest {
     @Autowired
     private MockMvc mockMvc;
+
+    @Sql("classpath:data_sql/user-data.sql")
+    @Test
+    void shouldProvideAccessTokenAndTokenTypeOnSuccessfulLogin() throws Exception {
+        final var requestBody = "{\"usernameOrEmail\": \"testEmail@gmail.com\",\"password\": \"testPassword@123\"}";
+
+        mockMvc.perform(post("/api/v1/auth/login")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(requestBody))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.accessToken").exists())
+                .andExpect(jsonPath("$.tokenType").exists());
+    }
+
+
 
 }
