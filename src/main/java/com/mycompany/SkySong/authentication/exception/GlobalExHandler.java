38738340c1ef4324d22d5f645cceb93e62298390
Champel Mariusz <champelmariusz@gmail.com>
2023-10-13 23:17:32 +0200

    feat: Add exception handler for data integrity violations

--- src/main/java/com/mycompany/SkySong/authentication/exception/GlobalExHandler.java
+++ src/main/java/com/mycompany/SkySong/authentication/exception/GlobalExHandler.java
@@ -1,5 +1,7 @@
 package com.mycompany.SkySong.authentication.exception;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import java.util.HashMap;
 import java.util.Map;
 
+@Slf4j
 @ControllerAdvice
 public class GlobalExHandler {
 
@@ -19,6 +22,7 @@ public class GlobalExHandler {
     public ResponseEntity<String> handleRegisterException(final RegisterException ex) {
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
     }
+
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<Map<String, Object>> handleValidationExceptions(MethodArgumentNotValidException ex) {
@@ -34,5 +38,22 @@ public class GlobalExHandler {
         response.put("errors", errors);
 
         return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<?> handleDataIntegrityViolation(DataIntegrityViolationException ex) {
+        String errorMessage = ex.getLocalizedMessage();
+        log.info("errorMessage - " + errorMessage);
+
+        boolean isUsernameError = errorMessage.contains("Duplicate entry") && !errorMessage.contains("@");
+        boolean isEmailError = errorMessage.contains("Duplicate entry") && errorMessage.contains("@");
+
+        if (isUsernameError) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("The provided username is already taken.");
+        } else if (isEmailError) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("The provided email is already taken.");
+        }
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(
+                "The entered data is already in use. Please choose different data.");
     }
 }
