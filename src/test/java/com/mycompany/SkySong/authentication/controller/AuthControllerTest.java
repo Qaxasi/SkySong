d8e9fba22f01dd8cf79baeb0db4db7a11b9426df
Champel Mariusz <champelmariusz@gmail.com>
2023-10-17 22:50:18 +0200

    refactor: Rename class

--- src/test/java/com/mycompany/SkySong/authentication/controller/AuthControllerTest.java
+++ src/test/java/com/mycompany/SkySong/authentication/controller/AuthControllerTest.java
@@ -24,7 +24,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
 @ActiveProfiles("test")
-public class AuthControllerRegisterTest {
+public class AuthControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
