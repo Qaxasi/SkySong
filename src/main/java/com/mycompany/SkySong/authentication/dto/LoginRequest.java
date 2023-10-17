a04086e40d82e45cfa99e373c86f307f18611559
Champel Mariusz <champelmariusz@gmail.com>
2023-10-17 23:03:14 +0200

    refactor: Remove @NotNull annotation from fields in LoginRequest class

--- src/main/java/com/mycompany/SkySong/authentication/dto/LoginRequest.java
+++ src/main/java/com/mycompany/SkySong/authentication/dto/LoginRequest.java
@@ -4,10 +4,8 @@ import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 
 public record LoginRequest(
-         @NotNull(message = "The usernameOrEmail field cannot be null")
-         @NotEmpty(message = "The usernameOrEmail field cannot be empty")
-         String usernameOrEmail,
-         @NotNull(message = "The password field cannot be null")
-         @NotEmpty(message = "The password field cannot be empty")
-         String password) {
+        @NotEmpty(message = "The usernameOrEmail field cannot be empty")
+        String usernameOrEmail,
+        @NotEmpty(message = "The password field cannot be empty")
+        String password) {
 }
