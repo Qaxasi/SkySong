132a8907e37e265c69b9838bc5024897095510ad
Champel Mariusz <champelmariusz@gmail.com>
2023-10-20 18:30:31 +0200

    refactor: Delete @Pattern annotation from fields

--- src/main/java/com/mycompany/SkySong/authentication/dto/RegisterRequest.java
+++ src/main/java/com/mycompany/SkySong/authentication/dto/RegisterRequest.java
@@ -5,18 +5,12 @@ import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 
-public record RegisterRequest(@Pattern(regexp = "^[a-zA-Z0-9]*$",
-                              message = "Invalid username format. The username can contain only letter and numbers.")
-                              @NotEmpty(message = "The username field cannot be empty.")
+public record RegisterRequest(@NotEmpty(message = "The username field cannot be empty.")
                               String username,
                               @Email(message = "Invalid email address format. The email should follow the standard" +
                                       " format (e.g., user@example.com).")
                               @NotEmpty(message = "The email field cannot be empty")
                               String email,
-                              @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}$",
-                                      message = "Invalid password format. The password must contain an least 8 " +
-                                                "characters, including uppercase letters, lowercase letters, numbers," +
-                                                " and special characters.")
                               @NotEmpty(message = "The password field cannot be empty")
                               String password) {
 }
