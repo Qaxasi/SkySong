1ea3c35b3e9241f6fa75a073aa75102100dbeff9
Champel Mariusz <champelmariusz@gmail.com>
2023-10-20 16:33:19 +0200

    feat: Add handler for malformed JSON request

--- src/main/java/com/mycompany/SkySong/authentication/exception/GlobalExHandler.java
+++ src/main/java/com/mycompany/SkySong/authentication/exception/GlobalExHandler.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -37,6 +38,14 @@ public class GlobalExHandler {
         response.put("errors", errors);
 
         return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<String> handleHttpMessageNotReadableException(HttpMessageNotReadableException exception) {
+        String errorMessage = "Invalid JSON format: " + exception.getMessage();
+        return new ResponseEntity<>(errorMessage, HttpStatus.BAD_REQUEST);
+
     }
 
     @ExceptionHandler(DataIntegrityViolationException.class)
