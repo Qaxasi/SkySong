ce07e6f2f27c786fa5f42d808fd6a864726caa4c
Champel Mariusz <champelmariusz@gmail.com>
2023-10-22 22:15:23 +0200

    feat: Add dependencies

--- src/main/java/com/mycompany/SkySong/authentication/service/impl/LoginService.java
+++ src/main/java/com/mycompany/SkySong/authentication/service/impl/LoginService.java
@@ -1,9 +1,18 @@
 package com.mycompany.SkySong.authentication.service.impl;
 
+import com.mycompany.SkySong.authentication.secutiry.JwtTokenProvider;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
 public class LoginService {
+
+    private final AuthenticationManager authenticationManager;
+    private final JwtTokenProvider jwtTokenProvider;
+    public LoginService(AuthenticationManager authenticationManager, JwtTokenProvider jwtTokenProvider) {
+        this.authenticationManager = authenticationManager;
+        this.jwtTokenProvider = jwtTokenProvider;
+    }
 }
