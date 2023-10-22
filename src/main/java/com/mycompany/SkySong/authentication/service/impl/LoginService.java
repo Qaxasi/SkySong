31c27f4638bb613893b4096d6c4b650b91aeb883
Champel Mariusz <champelmariusz@gmail.com>
2023-10-22 22:31:51 +0200

    refactor: Rename service and implement LoginService interface

--- src/main/java/com/mycompany/SkySong/authentication/service/impl/LoginService.java
+++ src/main/java/com/mycompany/SkySong/authentication/service/impl/LoginService.java
@@ -2,15 +2,17 @@ package com.mycompany.SkySong.authentication.service.impl;
 
 import com.mycompany.SkySong.authentication.dto.LoginRequest;
 import com.mycompany.SkySong.authentication.secutiry.JwtTokenProvider;
+import com.mycompany.SkySong.authentication.service.LoginService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
-public class LoginService {
+public class LoginServiceImpl implements LoginService {
 
     private final AuthenticationManager authenticationManager;
     private final JwtTokenProvider jwtTokenProvider;
