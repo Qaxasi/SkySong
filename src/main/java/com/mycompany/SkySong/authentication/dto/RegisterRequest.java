b0f64002b48021aba222d0d856c1a527315fc5f8
Champel Mariusz <champelmariusz@gmail.com>
2023-10-22 18:47:10 +0200

    refactor: Remove unused imports

--- src/main/java/com/mycompany/SkySong/authentication/dto/RegisterRequest.java
+++ src/main/java/com/mycompany/SkySong/authentication/dto/RegisterRequest.java
@@ -1,10 +1,6 @@
 package com.mycompany.SkySong.authentication.dto;
 
-import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Pattern;
-
 public record RegisterRequest(@NotEmpty(message = "The username field cannot be empty.")
                               String username,
                               @NotEmpty(message = "The email field cannot be empty")
