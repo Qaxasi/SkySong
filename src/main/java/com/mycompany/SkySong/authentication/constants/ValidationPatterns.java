b28fd9e18cf18859f6ec789e421da428c01da7a5
Champel Mariusz <champelmariusz@gmail.com>
2023-10-22 18:41:48 +0200

    refactor: Update email validation pattern to accept uppercase letters

--- src/main/java/com/mycompany/SkySong/authentication/constants/ValidationPatterns.java
+++ src/main/java/com/mycompany/SkySong/authentication/constants/ValidationPatterns.java
@@ -2,7 +2,7 @@ package com.mycompany.SkySong.authentication.constants;
 
 public class ValidationPatterns {
     public static final String USERNAME_PATTERN = "^[a-zA-Z0-9]+$";
-    public static final String EMAIL_PATTERN = "^[a-z0-9]+([.]?[a-z0-9]+)*@[a-z0-9]+([.][a-z0-9]+)+$";
+    public static final String EMAIL_PATTERN = "^[a-zA-Z0-9]+([.]?[a-zA-Z0-9]+)*@[a-zA-Z0-9]+([.][a-zA-Z0-9]+)+$";
     public static final String PASSWORD_PATTERN = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}$";
 
 
