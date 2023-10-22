16ec112c909dc06aa9b4c751f4b77e016329b78e
Champel Mariusz <champelmariusz@gmail.com>
2023-10-22 18:46:36 +0200

    refactor: Remove @Email annotation from email field

--- src/main/java/com/mycompany/SkySong/authentication/dto/RegisterRequest.java
+++ src/main/java/com/mycompany/SkySong/authentication/dto/RegisterRequest.java
@@ -7,8 +7,6 @@ import jakarta.validation.constraints.Pattern;
 
 public record RegisterRequest(@NotEmpty(message = "The username field cannot be empty.")
                               String username,
-                              @Email(message = "Invalid email address format. The email should follow the standard" +
-                                      " format (e.g., user@example.com).")
                               @NotEmpty(message = "The email field cannot be empty")
                               String email,
                               @NotEmpty(message = "The password field cannot be empty")
