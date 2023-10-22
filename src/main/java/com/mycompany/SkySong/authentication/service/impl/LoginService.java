84590986df9efea5086ac7feb35a2fa255957f9f
Champel Mariusz <champelmariusz@gmail.com>
2023-10-22 22:17:48 +0200

    feat: Add method to encapsulate user authentication logic in LoginService

--- src/main/java/com/mycompany/SkySong/authentication/service/impl/LoginService.java
+++ src/main/java/com/mycompany/SkySong/authentication/service/impl/LoginService.java
@@ -1,8 +1,11 @@
 package com.mycompany.SkySong.authentication.service.impl;
 
+import com.mycompany.SkySong.authentication.dto.LoginRequest;
 import com.mycompany.SkySong.authentication.secutiry.JwtTokenProvider;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
 @Slf4j
@@ -14,5 +17,11 @@ public class LoginService {
     public LoginService(AuthenticationManager authenticationManager, JwtTokenProvider jwtTokenProvider) {
         this.authenticationManager = authenticationManager;
         this.jwtTokenProvider = jwtTokenProvider;
+    }
+
+    private Authentication authenticationUser(LoginRequest loginRequest) {
+        return authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        loginRequest.usernameOrEmail(), loginRequest.password()));
     }
 }
