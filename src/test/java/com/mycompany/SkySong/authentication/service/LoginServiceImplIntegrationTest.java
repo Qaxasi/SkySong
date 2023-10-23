2dbafa9f94ef35c43209c0f25311d7ae06d7030e
Champel Mariusz <champelmariusz@gmail.com>
2023-10-23 13:56:10 +0200

    feat: Add annotations to LoginServiceImplIntegrationTest class

--- src/test/java/com/mycompany/SkySong/authentication/service/LoginServiceImplIntegrationTest.java
+++ src/test/java/com/mycompany/SkySong/authentication/service/LoginServiceImplIntegrationTest.java
@@ -1,4 +1,11 @@
 package com.mycompany.SkySong.authentication.service;
 
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
 public class LoginServiceImplIntegrationTest {
 }
