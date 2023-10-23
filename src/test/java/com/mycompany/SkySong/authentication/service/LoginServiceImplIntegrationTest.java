b331e5655021fa4072dd57fa47730814b5010cb3
Champel Mariusz <champelmariusz@gmail.com>
2023-10-23 14:09:16 +0200

    test: Add dependencies in LoginServiceImplIntegrationTest

--- src/test/java/com/mycompany/SkySong/authentication/service/LoginServiceImplIntegrationTest.java
+++ src/test/java/com/mycompany/SkySong/authentication/service/LoginServiceImplIntegrationTest.java
@@ -1,11 +1,18 @@
 package com.mycompany.SkySong.authentication.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+
+import javax.sql.DataSource;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
 @ActiveProfiles("test")
 public class LoginServiceImplIntegrationTest {
+    @Autowired
+    private LoginService loginService;
+    @Autowired
+    private DataSource dataSource;
 }
