1fc452fc6290affd3f5982dec72b5a8e485f26e1
Champel Mariusz <champelmariusz@gmail.com>
2023-10-23 14:11:02 +0200

    test: Initialize test data and cleanup post-test in LoginServiceImplIntegrationTest

--- src/test/java/com/mycompany/SkySong/authentication/service/LoginServiceImplIntegrationTest.java
+++ src/test/java/com/mycompany/SkySong/authentication/service/LoginServiceImplIntegrationTest.java
@@ -1,11 +1,17 @@
 package com.mycompany.SkySong.authentication.service;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.init.ScriptUtils;
 import org.springframework.test.context.ActiveProfiles;
 
 import javax.sql.DataSource;
+import java.sql.Connection;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
@@ -15,4 +21,18 @@ public class LoginServiceImplIntegrationTest {
     private LoginService loginService;
     @Autowired
     private DataSource dataSource;
+    @BeforeEach
+    void init() throws Exception {
+        try(Connection connection = dataSource.getConnection()) {
+            ScriptUtils.executeSqlScript(connection, new ClassPathResource("data_sql/role-data.sql"));
+            ScriptUtils.executeSqlScript(connection, new ClassPathResource("data_sql/user-data.sql"));
+        }
+    }
+    @AfterEach
+    void cleanup() throws Exception {
+        JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+        jdbcTemplate.update("DELETE FROM user_roles");
+        jdbcTemplate.update("DELETE FROM users");
+        jdbcTemplate.update("DELETE FROM roles");
+    }
 }
