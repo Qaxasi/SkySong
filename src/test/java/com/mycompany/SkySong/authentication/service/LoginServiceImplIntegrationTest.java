e764e68677dc68e7137298e3a889ab416688b63b
Champel Mariusz <champelmariusz@gmail.com>
2023-10-23 14:40:09 +0200

    refactor: Delete annotation

--- src/test/java/com/mycompany/SkySong/authentication/service/LoginServiceImplIntegrationTest.java
+++ src/test/java/com/mycompany/SkySong/authentication/service/LoginServiceImplIntegrationTest.java
@@ -1,20 +1,26 @@
 package com.mycompany.SkySong.authentication.service;
 
+import com.mycompany.SkySong.authentication.dto.LoginRequest;
+import org.h2.security.auth.AuthenticationException;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.init.ScriptUtils;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.test.context.ActiveProfiles;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@AutoConfigureMockMvc
 @ActiveProfiles("test")
 public class LoginServiceImplIntegrationTest {
     @Autowired
