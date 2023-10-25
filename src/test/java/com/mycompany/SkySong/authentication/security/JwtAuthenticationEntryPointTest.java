10c19bc4d42cdab87f5158b5d00077d498b56855
Champel Mariusz <champelmariusz@gmail.com>
2023-10-25 20:33:47 +0200

    feat: Add annotation to class

--- src/test/java/com/mycompany/SkySong/authentication/security/JwtAuthenticationEntryPointTest.java
+++ src/test/java/com/mycompany/SkySong/authentication/security/JwtAuthenticationEntryPointTest.java
@@ -1,4 +1,8 @@
 package com.mycompany.SkySong.authentication.security;
 
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
 public class JwtAuthenticationEntryPointTest {
 }
