c339b5bc5d6cc9e73d02f83cfef443bc87449289
Champel Mariusz <champelmariusz@gmail.com>
2023-10-25 22:37:45 +0200

    test: Add mocked dependencies to JwtAuthenticationFilterTest

--- src/test/java/com/mycompany/SkySong/authentication/security/JwtAuthenticationFilterTest.java
+++ src/test/java/com/mycompany/SkySong/authentication/security/JwtAuthenticationFilterTest.java
@@ -1,9 +1,26 @@
 package com.mycompany.SkySong.authentication.security;
 
+import com.mycompany.SkySong.authentication.secutiry.service.CustomUserDetailsService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
 @ExtendWith(MockitoExtension.class)
 public class JwtAuthenticationFilterTest {
+    private JwtAuthenticationFilterTest jwtAuthenticationFilterTest;
+    @Mock
+    private JwtTokenProviderTest jwtTokenProviderTest;
+    @Mock
+    private CustomUserDetailsService customUserDetailsService;
+    @Mock
+    private HttpServletResponse response;
+    @Mock
+    private HttpServletRequest request;
+    @Mock
+    private FilterChain filterChain;
 
-    
+
 }
