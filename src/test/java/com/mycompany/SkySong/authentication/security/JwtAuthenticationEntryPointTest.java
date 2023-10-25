8a4320dcbdb9aaf5c920dc1e2d86a955c8b8f583
Champel Mariusz <champelmariusz@gmail.com>
2023-10-25 21:45:41 +0200

    feat: Added initial setup for JwtAuthenticationEntryPointTest with mocked dependencies

--- src/test/java/com/mycompany/SkySong/authentication/security/JwtAuthenticationEntryPointTest.java
+++ src/test/java/com/mycompany/SkySong/authentication/security/JwtAuthenticationEntryPointTest.java
@@ -1,8 +1,28 @@
 package com.mycompany.SkySong.authentication.security;
 
+import com.mycompany.SkySong.authentication.secutiry.JwtAuthenticationEntryPoint;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 
 @ExtendWith(MockitoExtension.class)
 public class JwtAuthenticationEntryPointTest {
+    private JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    @Mock
+    private HttpServletRequest request;
+    @Mock
+    private HttpServletResponse response;
+    @Mock
+    private AuthenticationException authenticationException;
+
+
+
+
+
+
+
 }
