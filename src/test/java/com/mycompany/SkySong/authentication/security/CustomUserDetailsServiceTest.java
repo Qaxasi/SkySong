63f50f0046f080092873afba9566be6fcd0955b2
Champel Mariusz <champelmariusz@gmail.com>
2023-10-25 19:24:07 +0200

    test: Add test to verify fetching user details with correct roles

--- src/test/java/com/mycompany/SkySong/authentication/security/CustomUserDetailsServiceTest.java
+++ src/test/java/com/mycompany/SkySong/authentication/security/CustomUserDetailsServiceTest.java
@@ -15,12 +15,13 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 
 import javax.swing.text.html.Option;
+import java.util.HashSet;
 import java.util.Optional;
 import java.util.Set;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -74,6 +75,20 @@ public class CustomUserDetailsServiceTest {
 
         assertThrows(UsernameNotFoundException.class,
                 () -> customUserDetailsService.loadUserByUsername("testEmail@gmail.com"));
+    }
+    @Test
+    void shouldFetchUserDetailsWithCorrectRolesBasedOnUsername() {
+        Set<Role> roles = new HashSet<>();
+        roles.add(new Role(UserRole.ROLE_USER));
+        roles.add(new Role(UserRole.ROLE_ADMIN));
+
+        User user = new User("testUsername", "testEmail@gmail.com", "testPassword@123", roles);
+        when(userDAO.findByUsername("testUsername")).thenReturn(Optional.of(user));
+
+        UserDetails userDetails = customUserDetailsService.loadUserByUsername("testUsername");
+
+        assertTrue(userDetails.getAuthorities().stream().anyMatch(auth -> "ROLE_USER".equals(auth.getAuthority())));
+        assertTrue(userDetails.getAuthorities().stream().anyMatch(auth -> "ROLE_ADMIN".equals(auth.getAuthority())));
     }
 
 }
