b768687a6dfb8474db367be418f55c4cc43606bc
Champel Mariusz <champelmariusz@gmail.com>
2023-10-25 13:59:39 +0200

    test: Initialize CustomUserDetailsService in test setup

--- src/test/java/com/mycompany/SkySong/authentication/security/CustomUserDetailsServiceTest.java
+++ src/test/java/com/mycompany/SkySong/authentication/security/CustomUserDetailsServiceTest.java
@@ -1,16 +1,32 @@
 package com.mycompany.SkySong.authentication.security;
 
+import com.mycompany.SkySong.authentication.model.entity.Role;
+import com.mycompany.SkySong.authentication.model.entity.User;
+import com.mycompany.SkySong.authentication.model.entity.UserRole;
 import com.mycompany.SkySong.authentication.repository.UserDAO;
 import com.mycompany.SkySong.authentication.secutiry.service.CustomUserDetailsService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.core.userdetails.UserDetails;
+
+
+import java.util.Optional;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class CustomUserDetailsServiceTest {
-
     private CustomUserDetailsService customUserDetailsService;
     @Mock
     private UserDAO userDAO;
-
+    @BeforeEach
+    void setUp() {
+        customUserDetailsService = new CustomUserDetailsService(userDAO);
+    }
 }
