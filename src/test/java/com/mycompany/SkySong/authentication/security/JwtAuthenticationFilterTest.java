cfb9ce52f2b0e67a4a985949f533f0b3beaa2ca5
Champel Mariusz <champelmariusz@gmail.com>
2023-10-25 22:55:25 +0200

    test: Add test for processing valid JWT tokens from Authorization header

--- src/test/java/com/mycompany/SkySong/authentication/security/JwtAuthenticationFilterTest.java
+++ src/test/java/com/mycompany/SkySong/authentication/security/JwtAuthenticationFilterTest.java
@@ -1,18 +1,31 @@
 package com.mycompany.SkySong.authentication.security;
 
+import com.mycompany.SkySong.authentication.secutiry.JwtAuthenticationFilter;
+import com.mycompany.SkySong.authentication.secutiry.JwtTokenProvider;
 import com.mycompany.SkySong.authentication.secutiry.service.CustomUserDetailsService;
+import io.jsonwebtoken.Claims;
 import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class JwtAuthenticationFilterTest {
-    private JwtAuthenticationFilterTest jwtAuthenticationFilterTest;
+    private JwtAuthenticationFilter jwtAuthenticationFilter;
     @Mock
-    private JwtTokenProviderTest jwtTokenProviderTest;
+    private JwtTokenProvider jwtTokenProvider;
     @Mock
     private CustomUserDetailsService customUserDetailsService;
     @Mock
@@ -21,6 +34,26 @@ public class JwtAuthenticationFilterTest {
     private HttpServletRequest request;
     @Mock
     private FilterChain filterChain;
+    @BeforeEach
+    void setUp() {
+        jwtAuthenticationFilter = new JwtAuthenticationFilter(jwtTokenProvider, customUserDetailsService);
+    }
+    @Test
+    void shouldProcessValidTokenFromAuthorizationHeader() throws ServletException, IOException {
+        String token = "validToken";
+        when(request.getHeader("Authorization")).thenReturn("Bearer " + token);
+        when(jwtTokenProvider.validateToken(token)).thenReturn(true);
+
+        Claims mockClaims = mock(Claims.class);
+        when(mockClaims.getSubject()).thenReturn("username");
+        when(jwtTokenProvider.getClaimsFromToken(token)).thenReturn(mockClaims);
+
+        when(customUserDetailsService.loadUserByUsername("username")).thenReturn(mock(UserDetails.class));
+
+        jwtAuthenticationFilter.doFilter(request, response, filterChain);
+
+        verify(filterChain).doFilter(request, response);
+    }
 
 
 }
