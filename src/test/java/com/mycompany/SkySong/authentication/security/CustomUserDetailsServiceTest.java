040185ddc885a0352e32e353bc175deb64bee621
Champel Mariusz <champelmariusz@gmail.com>
2023-10-25 12:51:35 +0200

    test: Initialize CustomUserDetailsServiceTest with Mockito

--- src/test/java/com/mycompany/SkySong/authentication/security/CustomUserDetailsServiceTest.java
+++ src/test/java/com/mycompany/SkySong/authentication/security/CustomUserDetailsServiceTest.java
@@ -1,4 +1,16 @@
 package com.mycompany.SkySong.authentication.security;
 
+import com.mycompany.SkySong.authentication.repository.UserDAO;
+import com.mycompany.SkySong.authentication.secutiry.service.CustomUserDetailsService;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
 public class CustomUserDetailsServiceTest {
+
+    private CustomUserDetailsService customUserDetailsService;
+    @Mock
+    private UserDAO userDAO;
+
 }
