075875898f47df672e922f781df2efb8c4099a41
Champel Mariusz <champelmariusz@gmail.com>
2023-10-25 22:06:52 +0200

    test: Initialized JwtAuthenticationEntryPoint in setUp method

--- src/test/java/com/mycompany/SkySong/authentication/security/JwtAuthenticationEntryPointTest.java
+++ src/test/java/com/mycompany/SkySong/authentication/security/JwtAuthenticationEntryPointTest.java
@@ -1,13 +1,23 @@
 package com.mycompany.SkySong.authentication.security;
 
 import com.mycompany.SkySong.authentication.secutiry.JwtAuthenticationEntryPoint;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class JwtAuthenticationEntryPointTest {
@@ -18,11 +28,9 @@ public class JwtAuthenticationEntryPointTest {
     private HttpServletResponse response;
     @Mock
     private AuthenticationException authenticationException;
-
-
-
-
-
-
+    @BeforeEach
+    void setUp() {
+        jwtAuthenticationEntryPoint = new JwtAuthenticationEntryPoint();
+    }
 
 }
