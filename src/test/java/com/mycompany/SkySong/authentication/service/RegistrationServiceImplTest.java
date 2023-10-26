3acf4747eacea8db619145bb9979f624b4aba86c
Champel Mariusz <champelmariusz@gmail.com>
2023-10-26 19:04:39 +0200

    test: Add RegistrationServiceImpl instance to test class

--- src/test/java/com/mycompany/SkySong/authentication/service/RegistrationServiceImplTest.java
+++ src/test/java/com/mycompany/SkySong/authentication/service/RegistrationServiceImplTest.java
@@ -2,6 +2,7 @@ package com.mycompany.SkySong.authentication.service;
 
 import com.mycompany.SkySong.authentication.repository.RoleDAO;
 import com.mycompany.SkySong.authentication.repository.UserDAO;
+import com.mycompany.SkySong.authentication.service.impl.RegistrationServiceImpl;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -9,6 +10,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 @ExtendWith(MockitoExtension.class)
 public class RegistrationServiceImplTest {
+    private RegistrationServiceImpl registrationService;
     @Mock
     private UserDAO userDAO;
     @Mock
