eafa4841a62c9d9f69ecc86c61dbd77154fb5498
Champel Mariusz <champelmariusz@gmail.com>
2023-10-26 18:51:46 +0200

    test: Add mocked dependencies in RegistrationServiceImplTest

--- src/test/java/com/mycompany/SkySong/authentication/service/RegistrationServiceImplTest.java
+++ src/test/java/com/mycompany/SkySong/authentication/service/RegistrationServiceImplTest.java
@@ -1,8 +1,19 @@
 package com.mycompany.SkySong.authentication.service;
 
+import com.mycompany.SkySong.authentication.repository.RoleDAO;
+import com.mycompany.SkySong.authentication.repository.UserDAO;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @ExtendWith(MockitoExtension.class)
 public class RegistrationServiceImplTest {
+    @Mock
+    private UserDAO userDAO;
+    @Mock
+    private RoleDAO roleDAO;
+    @Mock
+    private PasswordEncoder passwordEncoder;
+
 }
