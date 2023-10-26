46eadea5a9e504153fa5ab309d6bd6548230fec4
Champel Mariusz <champelmariusz@gmail.com>
2023-10-26 22:41:46 +0200

    test: Inject dependencies

--- src/test/java/com/mycompany/SkySong/authentication/service/RegistrationServiceIntegrationTest.java
+++ src/test/java/com/mycompany/SkySong/authentication/service/RegistrationServiceIntegrationTest.java
@@ -1,10 +1,33 @@
 package com.mycompany.SkySong.authentication.service;
 
+import com.mycompany.SkySong.authentication.model.dto.RegisterRequest;
+import com.mycompany.SkySong.authentication.repository.RoleDAO;
+import com.mycompany.SkySong.authentication.repository.UserDAO;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.init.ScriptUtils;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.context.ActiveProfiles;
+
+import javax.management.relation.RoleResult;
+import javax.sql.DataSource;
+import java.sql.Connection;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("test")
 public class RegistrationServiceIntegrationTest {
-    
+    @Autowired
+    private UserDAO userDAO;
+    @Autowired
+    private RoleDAO roleDAO;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+    @Autowired
+    private DataSource dataSource;
+
 }
