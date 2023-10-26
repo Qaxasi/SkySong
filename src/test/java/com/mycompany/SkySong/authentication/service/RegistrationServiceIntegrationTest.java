0e926c5d9f0ac471010ebb81cec3e004b25c9163
Champel Mariusz <champelmariusz@gmail.com>
2023-10-26 22:29:06 +0200

    test: Add annotations for integration testing class

--- src/test/java/com/mycompany/SkySong/authentication/service/RegistrationServiceIntegrationTest.java
+++ src/test/java/com/mycompany/SkySong/authentication/service/RegistrationServiceIntegrationTest.java
@@ -1,4 +1,10 @@
 package com.mycompany.SkySong.authentication.service;
 
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
 public class RegistrationServiceIntegrationTest {
+    
 }
