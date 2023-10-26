beed81c6cad236a731d58e6612af696cba39110c
Champel Mariusz <champelmariusz@gmail.com>
2023-10-26 19:52:38 +0200

    test: Add test for successful user registration with valid credentials

--- src/test/java/com/mycompany/SkySong/authentication/service/RegistrationServiceImplTest.java
+++ src/test/java/com/mycompany/SkySong/authentication/service/RegistrationServiceImplTest.java
@@ -1,13 +1,23 @@
 package com.mycompany.SkySong.authentication.service;
 
+import com.mycompany.SkySong.authentication.model.dto.RegisterRequest;
+import com.mycompany.SkySong.authentication.model.dto.RegistrationResponse;
+import com.mycompany.SkySong.authentication.model.entity.Role;
 import com.mycompany.SkySong.authentication.repository.RoleDAO;
 import com.mycompany.SkySong.authentication.repository.UserDAO;
 import com.mycompany.SkySong.authentication.service.impl.RegistrationServiceImpl;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class RegistrationServiceImplTest {
@@ -23,7 +33,22 @@ public class RegistrationServiceImplTest {
     void setUp() {
         registrationService = new RegistrationServiceImpl(userDAO, roleDAO, passwordEncoder);
     }
+    @Test
+    void shouldSuccessfullyRegisterNewUserWithValidCredentials() {
+        RegisterRequest registerRequest = new RegisterRequest(
+                "testUsername", "testEmail@gmail.com", "testPassword@123");
+
+        when(userDAO.existsByUsername(registerRequest.username())).thenReturn(false);
+        when(userDAO.existsByEmail(registerRequest.email())).thenReturn(false);
+        when(passwordEncoder.encode(registerRequest.password())).thenReturn("encodedPassword");
+        when(roleDAO.findByName(any())).thenReturn(Optional.of(new Role()));
+
+        RegistrationResponse response = registrationService.register(registerRequest);
+
+        String expectedMessage = "User registered successfully";
+        assertEquals(response.message(), expectedMessage);
+    }
 
 
-    
+
 }
