5e76af8f0ed1c93153fc9d022b6c1fbf9265e804
Champel Mariusz <champelmariusz@gmail.com>
2023-10-27 17:17:03 +0200

    refactor: Remove unused imports

--- src/main/java/com/mycompany/SkySong/authentication/exception/GlobalExHandler.java
+++ src/main/java/com/mycompany/SkySong/authentication/exception/GlobalExHandler.java
@@ -1,8 +1,7 @@
 package com.mycompany.SkySong.authentication.exception;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.dao.DataIntegrityViolationException;
-import org.springframework.http.HttpStatus;
+ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.security.authentication.BadCredentialsException;
@@ -10,8 +9,6 @@ import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
 
 import java.util.HashMap;
 import java.util.Map;
