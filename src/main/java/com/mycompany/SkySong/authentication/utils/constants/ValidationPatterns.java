19632417b975284858b8ce041009aaf502555fdf
Champel Mariusz <champelmariusz@gmail.com>
2023-10-29 17:16:40 +0100

    refactor: Update USERNAME_PATTERN to enforce length constraints

--- src/main/java/com/mycompany/SkySong/authentication/utils/constants/ValidationPatterns.java
+++ src/main/java/com/mycompany/SkySong/authentication/utils/constants/ValidationPatterns.java
@@ -1,7 +1,7 @@
 package com.mycompany.SkySong.authentication.utils.constants;
 
 public class ValidationPatterns {
-    public static final String USERNAME_PATTERN = "^[a-zA-Z0-9]+$";
+    public static final String USERNAME_PATTERN = "^[a-zA-Z0-9]{3,20}$";
     public static final String EMAIL_PATTERN = "^[a-zA-Z0-9]+([.]?[a-zA-Z0-9]+)*@[a-zA-Z0-9]+([.][a-zA-Z0-9]+)+$";
     public static final String PASSWORD_PATTERN = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}$";
 
