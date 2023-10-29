0e8bdabf06f481a7dfed4ae6e5f700ef1fa0c0ec
Champel Mariusz <champelmariusz@gmail.com>
2023-10-29 19:22:52 +0100

    test: Add ValidationService to ValidationServiceTest class

--- src/test/java/com/mycompany/SkySong/authentication/service/ValidationServiceTest.java
+++ src/test/java/com/mycompany/SkySong/authentication/service/ValidationServiceTest.java
@@ -1,8 +1,12 @@
 package com.mycompany.SkySong.authentication.service;
 
+import com.mycompany.SkySong.authentication.service.impl.ValidationService;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
 public class ValidationServiceTest {
+    private ValidationService validationService;
+
+
 }
