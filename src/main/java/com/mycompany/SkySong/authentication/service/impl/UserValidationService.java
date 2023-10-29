ebebb75329edab828bf3e90a0483ddf9f639a113
Champel Mariusz <champelmariusz@gmail.com>
2023-10-29 18:57:46 +0100

    feat: Add @Service annotation to class

--- src/main/java/com/mycompany/SkySong/authentication/service/impl/UserValidationService.java
+++ src/main/java/com/mycompany/SkySong/authentication/service/impl/UserValidationService.java
@@ -3,7 +3,9 @@ package com.mycompany.SkySong.authentication.service.impl;
 import com.mycompany.SkySong.authentication.exception.RegisterException;
 import com.mycompany.SkySong.authentication.model.dto.RegisterRequest;
 import com.mycompany.SkySong.authentication.utils.constants.ValidationPatterns;
+import org.springframework.stereotype.Service;
 
+@Service
 public class UserValidationService {
     private void validateCredentials(RegisterRequest registerRequest) {
         validateUsername(registerRequest);
