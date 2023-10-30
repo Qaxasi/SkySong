63dcb7f548913c6d7fb80ce587af9f0a9e765427
Champel Mariusz <champelmariusz@gmail.com>
2023-10-30 16:45:18 +0100

    feat: Inject UserDao

--- src/main/java/com/mycompany/SkySong/authentication/service/impl/DeleteServiceImpl.java
+++ src/main/java/com/mycompany/SkySong/authentication/service/impl/DeleteServiceImpl.java
@@ -1,8 +1,12 @@
 package com.mycompany.SkySong.authentication.service.impl;
 
+import com.mycompany.SkySong.authentication.repository.UserDAO;
 import com.mycompany.SkySong.authentication.service.DeleteService;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class DeleteServiceImpl implements DeleteService {
+    @Autowired
+    private UserDAO userDAO;
     @Override
     public String deleteUser(int userId) {
         return null;
