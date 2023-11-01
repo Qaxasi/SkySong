9df3d17dbb88d2296f791c0c8ef6b9bcd2454e9a
Champel Mariusz <champelmariusz@gmail.com>
2023-11-01 13:50:41 +0100

    feat: Inject DeleteService into UserController

--- src/main/java/com/mycompany/SkySong/authentication/controller/UserController.java
+++ src/main/java/com/mycompany/SkySong/authentication/controller/UserController.java
@@ -1,9 +1,15 @@
 package com.mycompany.SkySong.authentication.controller;
 
+import com.mycompany.SkySong.authentication.service.DeleteService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/users")
 public class UserController {
+    private final DeleteService deleteService;
+
+    public UserController(DeleteService deleteService) {
+        this.deleteService = deleteService;
+    }
 }
