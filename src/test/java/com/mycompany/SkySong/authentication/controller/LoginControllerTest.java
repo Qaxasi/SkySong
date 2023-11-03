950ad2ea4b4d9c4ebab3b17f355172c97bb8ef57
Champel Mariusz <champelmariusz@gmail.com>
2023-11-03 19:31:54 +0100

    test: Inject dependencies in LoginControllerTest

--- src/test/java/com/mycompany/SkySong/authentication/controller/LoginControllerTest.java
+++ src/test/java/com/mycompany/SkySong/authentication/controller/LoginControllerTest.java
@@ -1,11 +1,20 @@
 package com.mycompany.SkySong.authentication.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+
+import javax.sql.DataSource;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
 @ActiveProfiles("test")
 public class LoginControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+    @Autowired
+    private DataSource dataSource;
+
 }
