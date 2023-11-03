181c3300a3fbe8bdef18f1de6ae937678a080e5a
Champel Mariusz <champelmariusz@gmail.com>
2023-11-03 19:01:03 +0100

    feat: Inject LoginService into LoginController

--- src/main/java/com/mycompany/SkySong/authentication/controller/LoginController.java
+++ src/main/java/com/mycompany/SkySong/authentication/controller/LoginController.java
@@ -1,9 +1,14 @@
 package com.mycompany.SkySong.authentication.controller;
 
+import com.mycompany.SkySong.authentication.service.LoginService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/users")
 public class LoginController {
+    private final LoginService loginService;
+    public LoginController(LoginService loginService) {
+        this.loginService = loginService;
+    }
 }
