9e7bd8131998d23adbc17ff5b3921c2ad3285fc8
Champel Mariusz <champelmariusz@gmail.com>
2023-11-03 19:18:04 +0100

    refactor: Remove login endpoint from AuthControlelr

--- src/main/java/com/mycompany/SkySong/authentication/controller/AuthController.java
+++ src/main/java/com/mycompany/SkySong/authentication/controller/AuthController.java
@@ -14,20 +14,10 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/v1/users")
 public class AuthController {
-    private final LoginService loginService;
     private final RegistrationService registrationService;
-    public AuthController(LoginService loginService, RegistrationService registrationService) {
-        this.loginService = loginService;
+    public AuthController(RegistrationService registrationService) {
         this.registrationService = registrationService;
     }
-
-    @PostMapping("/login")
-    public ResponseEntity<JWTAuthResponse> login(@Valid @RequestBody LoginRequest loginRequest) {
-        String token = loginService.login(loginRequest);
-        JWTAuthResponse jwtAuthResponse = new JWTAuthResponse(token);
-        return ResponseEntity.ok(jwtAuthResponse);
-    }
-
     @PostMapping("/register")
     public ResponseEntity<RegistrationResponse> register(@Valid @RequestBody RegisterRequest registerRequest) {
         RegistrationResponse registrationResponse = registrationService.register(registerRequest);
