9391b16d390ada9da09f24d1c535b8e0d3d71876
Champel Mariusz <champelmariusz@gmail.com>
2023-11-03 19:02:02 +0100

    feat: Add login endpoint to LoginController

--- src/main/java/com/mycompany/SkySong/authentication/controller/LoginController.java
+++ src/main/java/com/mycompany/SkySong/authentication/controller/LoginController.java
@@ -1,6 +1,12 @@
 package com.mycompany.SkySong.authentication.controller;
 
+import com.mycompany.SkySong.authentication.model.dto.JWTAuthResponse;
+import com.mycompany.SkySong.authentication.model.dto.LoginRequest;
 import com.mycompany.SkySong.authentication.service.LoginService;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,5 +16,11 @@ public class LoginController {
     private final LoginService loginService;
     public LoginController(LoginService loginService) {
         this.loginService = loginService;
+    }
+    @PostMapping("/login")
+    public ResponseEntity<JWTAuthResponse> login(@Valid @RequestBody LoginRequest loginRequest) {
+        String token = loginService.login(loginRequest);
+        JWTAuthResponse jwtAuthResponse = new JWTAuthResponse(token);
+        return ResponseEntity.ok(jwtAuthResponse);
     }
 }
