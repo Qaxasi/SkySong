f07212d3e6be8e7c393047fc9e46f7453a5937a4
Champel Mariusz <champelmariusz@gmail.com>
2023-11-03 18:24:38 +0100

    feat: Add custom access denied handler for JSON response

---
+++ src/main/java/com/mycompany/SkySong/authentication/secutiry/CustomAccessDeniedHandler.java
@@ -0,0 +1,24 @@
+package com.mycompany.SkySong.authentication.secutiry;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.json.JSONObject;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+@Component
+public class CustomAccessDeniedHandler implements AccessDeniedHandler {
+    @Override
+    public void handle(HttpServletRequest request,
+                       HttpServletResponse response,
+                       AccessDeniedException accessDeniedException) throws IOException, ServletException {
+        response.setContentType("application/json");
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        response.getWriter().write(new JSONObject()
+                .put("error", "You do not have permission to perform this operation")
+                .toString());
+    }
+}
