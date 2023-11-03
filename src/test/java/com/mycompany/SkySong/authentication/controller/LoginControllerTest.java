7913ecad2a05fbef44860df814766cbb37376fee
Champel Mariusz <champelmariusz@gmail.com>
2023-11-03 19:33:41 +0100

    test: Create setup method in LoginControllerTest for initializing database before tests

--- src/test/java/com/mycompany/SkySong/authentication/controller/LoginControllerTest.java
+++ src/test/java/com/mycompany/SkySong/authentication/controller/LoginControllerTest.java
@@ -1,12 +1,16 @@
 package com.mycompany.SkySong.authentication.controller;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.datasource.init.ScriptUtils;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import javax.sql.DataSource;
+import java.sql.Connection;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
@@ -16,5 +20,12 @@ public class LoginControllerTest {
     private MockMvc mockMvc;
     @Autowired
     private DataSource dataSource;
+    @BeforeEach
+    void init() throws Exception {
+        try(Connection connection = dataSource.getConnection()) {
+            ScriptUtils.executeSqlScript(connection, new ClassPathResource("data_sql/user-data.sql"));
+            ScriptUtils.executeSqlScript(connection, new ClassPathResource("data_sql/role-data.sql"));
+        }
+    }
 
 }
