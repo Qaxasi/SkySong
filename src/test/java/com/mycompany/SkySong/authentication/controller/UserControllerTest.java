05086067a1ee3bfd8939f734c56e3ed390a090bf
Champel Mariusz <champelmariusz@gmail.com>
2023-11-04 18:01:43 +0100

    refactor: Rename UserController to UserControllerTest

---
+++ src/test/java/com/mycompany/SkySong/authentication/controller/UserControllerTest.java
@@ -0,0 +1,41 @@
+package com.mycompany.SkySong.authentication.controller;
+
+import com.mycompany.SkySong.testsupport.controller.AssertControllerUtils;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.init.ScriptUtils;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+public class UserControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+    @Autowired
+    private DataSource dataSource;
+    @BeforeEach
+    void init() throws Exception {
+        try(Connection connection = dataSource.getConnection()) {
+            ScriptUtils.executeSqlScript(connection, new ClassPathResource("data_sql/user-data.sql"));
+            ScriptUtils.executeSqlScript(connection, new ClassPathResource("data_sql/role-data.sql"));
+        }
+    }
+    @AfterEach
+    void cleanup() throws Exception {
+        JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+        jdbcTemplate.update("DELETE FROM user_roles");
+        jdbcTemplate.update("DELETE FROM users");
+        jdbcTemplate.update("DELETE FROM roles");
+    }
+}
