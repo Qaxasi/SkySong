0d5224fb4397b09a4709bfe6ec35f158520ae464
Champel Mariusz <champelmariusz@gmail.com>
2023-11-04 17:15:05 +0100

    test: Add annotations to UserController for integration testing setup

--- src/test/java/com/mycompany/SkySong/authentication/controller/UserController.java
+++ src/test/java/com/mycompany/SkySong/authentication/controller/UserController.java
@@ -1,4 +1,11 @@
 package com.mycompany.SkySong.authentication.controller;
 
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
 public class UserController {
 }
