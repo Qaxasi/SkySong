05b2857cf93b69d562d17483a5ec762f64fd492a
Champel Mariusz <champelmariusz@gmail.com>
2023-11-05 00:57:16 +0100

    style: Clean up import spacing for consistency

--- src/main/java/com/mycompany/SkySong/authentication/exception/GlobalExceptionHandler.java
+++ src/main/java/com/mycompany/SkySong/authentication/exception/GlobalExceptionHandler.java
@@ -1,7 +1,7 @@
 package com.mycompany.SkySong.authentication.exception;
 
 import lombok.extern.slf4j.Slf4j;
- import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.security.authentication.BadCredentialsException;
