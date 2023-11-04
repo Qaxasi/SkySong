3484e42e9b3cd713e1bffb4e6752a7e125d0db6d
Champel Mariusz <champelmariusz@gmail.com>
2023-11-04 16:54:14 +0100

    refactor: Remove unused imports

--- src/test/java/com/mycompany/SkySong/authentication/controller/RegistrationControllerTest.java
+++ src/test/java/com/mycompany/SkySong/authentication/controller/RegistrationControllerTest.java
@@ -8,20 +8,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.core.io.ClassPathResource;
-import org.springframework.http.MediaType;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.init.ScriptUtils;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultActions;
-import org.springframework.test.web.servlet.ResultMatcher;
 
 import javax.sql.DataSource;
 
 import java.sql.Connection;
 import java.util.Map;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
