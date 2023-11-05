506170b6958ddc48ca5c81fc3cadc31e1d6378fa
Champel Mariusz <champelmariusz@gmail.com>
2023-11-05 18:21:49 +0100

    feat: Inject DataSource and UserDAO into DeleteServiceImplTest

--- src/test/java/com/mycompany/SkySong/authentication/service/DeleteServiceImplTest.java
+++ src/test/java/com/mycompany/SkySong/authentication/service/DeleteServiceImplTest.java
@@ -1,11 +1,19 @@
 package com.mycompany.SkySong.authentication.service;
 
+import com.mycompany.SkySong.authentication.repository.UserDAO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+
+import javax.sql.DataSource;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
 @ActiveProfiles("test")
 public class DeleteServiceImplTest {
+    @Autowired
+    private DataSource dataSource;
+    @Autowired
+    private UserDAO userDAO;
 }
