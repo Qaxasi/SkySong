6545156cab185bcf58d34be45e253f8823a37afb
Champel Mariusz <champelmariusz@gmail.com>
2023-11-05 22:00:03 +0100

    refactor: Refactor unauthorized response to return JSON object

--- src/main/java/com/mycompany/SkySong/authentication/secutiry/JwtAuthenticationEntryPoint.java
+++ src/main/java/com/mycompany/SkySong/authentication/secutiry/JwtAuthenticationEntryPoint.java
@@ -4,6 +4,7 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.json.JSONObject;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
@@ -19,6 +20,8 @@ public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
                          AuthenticationException authException) throws IOException, ServletException {
         response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
         response.setContentType("application/json");
-        response.getWriter().write("Unauthorized access. Please log in.");
-        }
+        response.getWriter().write(new JSONObject()
+                .put("error", "Unauthorized access. Please log in.")
+                .toString());
+    }
 }
