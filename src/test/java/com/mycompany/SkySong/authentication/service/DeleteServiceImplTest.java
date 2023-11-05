0026172fbb3675ab31037a65a3fa997bed39a1a5
Champel Mariusz <champelmariusz@gmail.com>
2023-11-05 18:23:43 +0100

    test: Implement pre-test DB setup in DeleteServiceImplTest

--- src/test/java/com/mycompany/SkySong/authentication/service/DeleteServiceImplTest.java
+++ src/test/java/com/mycompany/SkySong/authentication/service/DeleteServiceImplTest.java
@@ -1,12 +1,16 @@
 package com.mycompany.SkySong.authentication.service;
 
 import com.mycompany.SkySong.authentication.repository.UserDAO;
+import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.datasource.init.ScriptUtils;
 import org.springframework.test.context.ActiveProfiles;
 
 import javax.sql.DataSource;
+import java.sql.Connection;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
@@ -16,4 +20,11 @@ public class DeleteServiceImplTest {
     private DataSource dataSource;
     @Autowired
     private UserDAO userDAO;
+    @BeforeEach
+    void init() throws Exception {
+        try(Connection connection = dataSource.getConnection()) {
+            ScriptUtils.executeSqlScript(connection, new ClassPathResource("data_sql/user-data.sql"));
+            ScriptUtils.executeSqlScript(connection, new ClassPathResource("data_sql/role-data.sql"));
+        }
+    }
 }
