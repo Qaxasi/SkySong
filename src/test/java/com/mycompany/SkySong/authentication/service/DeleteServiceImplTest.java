364882399f1f9035130b663c110fe9a2d4aa064a
Champel Mariusz <champelmariusz@gmail.com>
2023-11-05 22:58:34 +0100

    test: Inject dependencies into DeleteServiceImplTest

--- src/test/java/com/mycompany/SkySong/authentication/service/DeleteServiceImplTest.java
+++ src/test/java/com/mycompany/SkySong/authentication/service/DeleteServiceImplTest.java
@@ -1,8 +1,15 @@
 package com.mycompany.SkySong.authentication.service;
 
+import com.mycompany.SkySong.authentication.repository.UserDAO;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
 public class DeleteServiceImplTest {
+    private DeleteService deleteService;
+    @Mock
+    private UserDAO userDAO;
+
+
 }
