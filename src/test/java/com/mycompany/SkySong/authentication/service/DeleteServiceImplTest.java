c11b74bfccc3e6cb0e0606cc61bf5e802b6beeb2
Champel Mariusz <champelmariusz@gmail.com>
2023-11-05 23:51:07 +0100

    test: Initialize DeleteService in test setup

--- src/test/java/com/mycompany/SkySong/authentication/service/DeleteServiceImplTest.java
+++ src/test/java/com/mycompany/SkySong/authentication/service/DeleteServiceImplTest.java
@@ -1,15 +1,30 @@
 package com.mycompany.SkySong.authentication.service;
 
+import com.mycompany.SkySong.authentication.model.dto.DeleteResponse;
+import com.mycompany.SkySong.authentication.model.entity.Role;
+import com.mycompany.SkySong.authentication.model.entity.User;
+import com.mycompany.SkySong.authentication.model.entity.UserRole;
 import com.mycompany.SkySong.authentication.repository.UserDAO;
+import com.mycompany.SkySong.authentication.service.impl.DeleteServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class DeleteServiceImplTest {
     private DeleteService deleteService;
     @Mock
     private UserDAO userDAO;
-
-
+    @BeforeEach
+    void init() {
+        deleteService = new DeleteServiceImpl(userDAO);
+    }
 }
