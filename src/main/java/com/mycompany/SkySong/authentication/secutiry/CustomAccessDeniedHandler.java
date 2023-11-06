521da993b2803296bb0dc526d6733a08176dd942
Champel Mariusz <champelmariusz@gmail.com>
2023-11-06 23:12:47 +0100

    refactor: Remove ServletException from handle method in AccessDeniedHandler

--- src/main/java/com/mycompany/SkySong/authentication/secutiry/CustomAccessDeniedHandler.java
+++ src/main/java/com/mycompany/SkySong/authentication/secutiry/CustomAccessDeniedHandler.java
@@ -14,7 +14,7 @@ public class CustomAccessDeniedHandler implements AccessDeniedHandler {
     @Override
     public void handle(HttpServletRequest request,
                        HttpServletResponse response,
-                       AccessDeniedException accessDeniedException) throws IOException, ServletException {
+                       AccessDeniedException accessDeniedException) throws IOException {
         response.setContentType("application/json");
         response.setStatus(HttpServletResponse.SC_FORBIDDEN);
         response.getWriter().write(new JSONObject()
