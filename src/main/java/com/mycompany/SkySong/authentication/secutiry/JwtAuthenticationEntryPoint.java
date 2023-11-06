3393c194b47d09ea05b024615ab8236e4adf649f
Champel Mariusz <champelmariusz@gmail.com>
2023-11-06 20:04:59 +0100

    refactor: Cleanup unnecessary ServletException

--- src/main/java/com/mycompany/SkySong/authentication/secutiry/JwtAuthenticationEntryPoint.java
+++ src/main/java/com/mycompany/SkySong/authentication/secutiry/JwtAuthenticationEntryPoint.java
@@ -17,7 +17,7 @@ public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
     @Override
     public void commence(HttpServletRequest request,
                          HttpServletResponse response,
-                         AuthenticationException authException) throws IOException, ServletException {
+                         AuthenticationException authException) throws IOException {
         response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
         response.setContentType("application/json");
         response.getWriter().write(new JSONObject()
