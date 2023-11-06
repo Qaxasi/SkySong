4a51b2bb8bc7f9fe14de1f558ce1953cfbe2ecb1
Champel Mariusz <champelmariusz@gmail.com>
2023-11-06 18:12:14 +0100

    refactor: Implement a try-catch block to handle potential databse errors

--- src/main/java/com/mycompany/SkySong/authentication/service/impl/DeleteServiceImpl.java
+++ src/main/java/com/mycompany/SkySong/authentication/service/impl/DeleteServiceImpl.java
@@ -1,11 +1,13 @@
 package com.mycompany.SkySong.authentication.service.impl;
 
+import com.mycompany.SkySong.authentication.exception.DatabaseException;
 import com.mycompany.SkySong.authentication.exception.UserNotFoundException;
 import com.mycompany.SkySong.authentication.model.dto.DeleteResponse;
 import com.mycompany.SkySong.authentication.model.entity.User;
 import com.mycompany.SkySong.authentication.repository.UserDAO;
 import com.mycompany.SkySong.authentication.service.DeleteService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -20,15 +22,21 @@ public class DeleteServiceImpl implements DeleteService {
     @Transactional
     @Override
     public DeleteResponse deleteUser(long userId) {
-        return userDAO.findById(userId).map(user -> {
-            userDAO.delete(user);
-            log.error("User with ID {} deleted successfully", userId);
-            return new DeleteResponse("User with ID: " + userId + " deleted successfully.");
-        }).orElseThrow(() -> {
-            log.error("User with ID {} does not exist", userId);
-            return new UserNotFoundException(
-                    "User with ID: " + userId + " does not exist."
-            );
-        });
+        try {
+            return userDAO.findById(userId).map(user -> {
+                userDAO.delete(user);
+                log.error("User with ID {} deleted successfully", userId);
+                return new DeleteResponse("User with ID: " + userId + " deleted successfully.");
+            }).orElseThrow(() -> {
+                log.error("User with ID {} does not exist", userId);
+                return new UserNotFoundException(
+                        "User with ID: " + userId + " does not exist."
+                );
+            });
+        } catch (DataAccessException e) {
+                throw new DatabaseException(
+                        "An unexpected error occurred while deleting user. Please try again later.");
+
+        }
     }
 }
