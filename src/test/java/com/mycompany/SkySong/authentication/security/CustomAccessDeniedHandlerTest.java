8c898fabaf53d248330bd64a6620274fe75dd8ed
Champel Mariusz <champelmariusz@gmail.com>
2023-11-06 22:32:46 +0100

    test: Setup test environment for CustomAccessDeniedHandler

--- src/test/java/com/mycompany/SkySong/authentication/security/CustomAccessDeniedHandlerTest.java
+++ src/test/java/com/mycompany/SkySong/authentication/security/CustomAccessDeniedHandlerTest.java
@@ -1,4 +1,23 @@
 package com.mycompany.SkySong.authentication.security;
 
+import com.mycompany.SkySong.authentication.secutiry.CustomAccessDeniedHandler;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.access.AccessDeniedException;
+
+@ExtendWith(MockitoExtension.class)
 public class CustomAccessDeniedHandlerTest {
+    private CustomAccessDeniedHandler customAccessDeniedHandler;
+    @Mock
+    private HttpServletRequest httpServletRequest;
+    @Mock
+    private HttpServletResponse httpServletResponse;
+    @Mock
+    private AccessDeniedException accessDeniedException;
+
+
+
 }
