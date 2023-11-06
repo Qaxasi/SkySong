e74c711e57ba46c00335f569510d857de5aff2dc
Champel Mariusz <champelmariusz@gmail.com>
2023-11-06 22:49:20 +0100

    test: Add StringWriter and PrintWriter

--- src/test/java/com/mycompany/SkySong/authentication/security/CustomAccessDeniedHandlerTest.java
+++ src/test/java/com/mycompany/SkySong/authentication/security/CustomAccessDeniedHandlerTest.java
@@ -1,14 +1,23 @@
 package com.mycompany.SkySong.authentication.security;
 
 import com.mycompany.SkySong.authentication.secutiry.CustomAccessDeniedHandler;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.security.access.AccessDeniedException;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class CustomAccessDeniedHandlerTest {
@@ -19,6 +28,8 @@ public class CustomAccessDeniedHandlerTest {
     private HttpServletResponse response;
     @Mock
     private AccessDeniedException exception;
+    private StringWriter stringWriter;
+    private PrintWriter printWriter;
     @BeforeEach
     void setUp() {
         customAccessDeniedHandler = new CustomAccessDeniedHandler();
