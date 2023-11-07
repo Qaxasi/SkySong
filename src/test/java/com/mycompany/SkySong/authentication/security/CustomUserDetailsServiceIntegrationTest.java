96941d9ad83c05ecaeb51ef59203335dd80f5e42
Champel Mariusz <champelmariusz@gmail.com>
2023-11-07 12:31:53 +0100

    test: Inject dependencies in CustomUserDetailsServiceIntegrationTest for testing

--- src/test/java/com/mycompany/SkySong/authentication/security/CustomUserDetailsServiceIntegrationTest.java
+++ src/test/java/com/mycompany/SkySong/authentication/security/CustomUserDetailsServiceIntegrationTest.java
@@ -1,9 +1,16 @@
 package com.mycompany.SkySong.authentication.security;
 
+import com.mycompany.SkySong.authentication.repository.UserDAO;
+import com.mycompany.SkySong.authentication.secutiry.service.CustomUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("test")
 public class CustomUserDetailsServiceIntegrationTest {
+    @Autowired
+    private CustomUserDetailsService customUserDetailsService;
+    @Autowired
+    private UserDAO userDAO;
 }
