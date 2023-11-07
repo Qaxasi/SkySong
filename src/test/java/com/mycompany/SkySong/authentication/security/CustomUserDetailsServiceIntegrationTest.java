451690a07242ede0a0207c58d9c9d3e86914b6bf
Champel Mariusz <champelmariusz@gmail.com>
2023-11-07 12:29:18 +0100

    test: Configure CustomUserDetailsServiceIntegrationTest for random port and test profile

--- src/test/java/com/mycompany/SkySong/authentication/security/CustomUserDetailsServiceIntegrationTest.java
+++ src/test/java/com/mycompany/SkySong/authentication/security/CustomUserDetailsServiceIntegrationTest.java
@@ -1,4 +1,9 @@
 package com.mycompany.SkySong.authentication.security;
 
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
 public class CustomUserDetailsServiceIntegrationTest {
 }
