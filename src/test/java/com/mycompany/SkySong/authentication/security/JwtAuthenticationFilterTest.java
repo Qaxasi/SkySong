482fdb9e04ba4b89c748571c7649bfed6c95a224
Champel Mariusz <champelmariusz@gmail.com>
2023-11-07 18:16:45 +0100

    test: Update test to expect blockage of requests without token

--- src/test/java/com/mycompany/SkySong/authentication/security/JwtAuthenticationFilterTest.java
+++ src/test/java/com/mycompany/SkySong/authentication/security/JwtAuthenticationFilterTest.java
@@ -75,8 +75,8 @@ public class JwtAuthenticationFilterTest {
 
         jwtAuthenticationFilter.doFilterInternal(request, response, filterChain);
 
-        verify(filterChain).doFilter(request, response);
-        verify(response, never()).setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        verify(filterChain, never()).doFilter(request, response);
+        verify(response).setStatus(HttpServletResponse.SC_UNAUTHORIZED);
     }
     @Test
     void shouldNotProcessRequestWithMalformedToken() throws ServletException, IOException {
