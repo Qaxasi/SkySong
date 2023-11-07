f94a26f67b4f05945af5df8f2383a130c28fbd83
Champel Mariusz <champelmariusz@gmail.com>
2023-11-07 14:10:44 +0100

    feat: Assign user and admin roles to testAdmin in seeding SQL

--- src/main/resources/data_sql/role-data.sql
+++ src/main/resources/data_sql/role-data.sql
@@ -5,3 +5,6 @@ INSERT INTO roles (name)
           ('ROLE_ADMIN');
 
 INSERT INTO user_roles (user_id, role_id) VALUES(1, 1);
+
+INSERT INTO user_roles (user_id, role_id) VALUES(2, 1);
+INSERT INTO user_roles (user_id, role_id) VALUES(2, 2);
