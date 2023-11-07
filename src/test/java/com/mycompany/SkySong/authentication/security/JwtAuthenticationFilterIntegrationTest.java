929d8fde942ff5a9bce7f7cdec23a814a6134107
Champel Mariusz <champelmariusz@gmail.com>
2023-11-07 19:09:58 +0100

    test: Inject dependencies in JwtAuthenticationFilterIntegrationTest for enhanced testing

--- src/test/java/com/mycompany/SkySong/authentication/security/JwtAuthenticationFilterIntegrationTest.java
+++ src/test/java/com/mycompany/SkySong/authentication/security/JwtAuthenticationFilterIntegrationTest.java
@@ -1,9 +1,24 @@
 package com.mycompany.SkySong.authentication.security;
 
+import com.mycompany.SkySong.authentication.repository.UserDAO;
+import com.mycompany.SkySong.authentication.secutiry.JwtAuthenticationFilter;
+import com.mycompany.SkySong.authentication.secutiry.JwtTokenProvider;
+import com.mycompany.SkySong.authentication.secutiry.service.CustomUserDetailsService;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("test")
 public class JwtAuthenticationFilterIntegrationTest {
+    @Autowired
+    private JwtAuthenticationFilter jwtAuthenticationFilter;
+    @Autowired
+    private JwtTokenProvider jwtTokenProvider;
+    @Autowired
+    private CustomUserDetailsService customUserDetailsService;
+    @Autowired
+    private UserDAO userDAO;
+
 }
