2f8bcbf15543e24abf0d48f9222b4027ce5afe3e
Champel Mariusz <champelmariusz@gmail.com>
2023-11-07 12:52:26 +0100

    test: Add @AfterEach cleanup to reset DB in integration tests

--- src/test/java/com/mycompany/SkySong/authentication/security/CustomUserDetailsServiceIntegrationTest.java
+++ src/test/java/com/mycompany/SkySong/authentication/security/CustomUserDetailsServiceIntegrationTest.java
@@ -5,11 +5,13 @@ import com.mycompany.SkySong.authentication.model.entity.User;
 import com.mycompany.SkySong.authentication.model.entity.UserRole;
 import com.mycompany.SkySong.authentication.repository.UserDAO;
 import com.mycompany.SkySong.authentication.secutiry.service.CustomUserDetailsService;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.init.ScriptUtils;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.test.context.ActiveProfiles;
@@ -36,5 +38,11 @@ public class CustomUserDetailsServiceIntegrationTest {
             ScriptUtils.executeSqlScript(connection, new ClassPathResource("data_sql/role-data.sql"));
 
         }
+    }@AfterEach
+    void cleanup() throws Exception {
+        JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+        jdbcTemplate.update("DELETE FROM user_roles");
+        jdbcTemplate.update("DELETE FROM users");
+        jdbcTemplate.update("DELETE FROM roles");
     }
 }
