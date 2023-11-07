d1296c0f1fd131b1f65c13b0d1eed440e0486c73
Champel Mariusz <champelmariusz@gmail.com>
2023-11-07 22:06:42 +0100

    test: Inject dependencies into JwtAuthenticationFilterIntegrationTest

--- src/test/java/com/mycompany/SkySong/authentication/security/JwtAuthenticationFilterIntegrationTest.java
+++ src/test/java/com/mycompany/SkySong/authentication/security/JwtAuthenticationFilterIntegrationTest.java
@@ -4,10 +4,40 @@ import com.mycompany.SkySong.authentication.repository.UserDAO;
 import com.mycompany.SkySong.authentication.secutiry.JwtAuthenticationFilter;
 import com.mycompany.SkySong.authentication.secutiry.JwtTokenProvider;
 import com.mycompany.SkySong.authentication.secutiry.service.CustomUserDetailsService;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Encoders;
+import io.jsonwebtoken.security.Keys;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.init.ScriptUtils;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
+
+import javax.crypto.SecretKey;
+import javax.sql.DataSource;
+import javax.xml.crypto.Data;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("test")
@@ -18,14 +48,12 @@ public class JwtAuthenticationFilterIntegrationTest {
     private JwtTokenProvider jwtTokenProvider;
     @Autowired
     private CustomUserDetailsService customUserDetailsService;
+    @MockBean
+    private FilterChain filterChain;
     @Autowired
-    private UserDAO userDAO;
-    @BeforeEach
-    void setUp() {
-        String jwtSecret = "jwtToken";
-        long tokenExpirationTime = 1000L;
-        jwtTokenProvider = new JwtTokenProvider(jwtSecret, tokenExpirationTime);
-        customUserDetailsService = new CustomUserDetailsService(userDAO);
-        jwtAuthenticationFilter = new JwtAuthenticationFilter(jwtTokenProvider, customUserDetailsService);
-    }
+    private DataSource dataSource;
+
+    private MockHttpServletRequest request;
+
+    private MockHttpServletResponse response;
 }
