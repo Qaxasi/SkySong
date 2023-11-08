aa2a155d5ffe4a5876d6335aad4d2f1a5e1e2ef0
Champel Mariusz <champelmariusz@gmail.com>
2023-11-08 12:14:35 +0100

    test: Add secret key and expiration constants to JwtAuthenticationFilterUtils

--- src/test/java/com/mycompany/SkySong/testsupport/security/JwtAuthenticationFilterUtils.java
+++ src/test/java/com/mycompany/SkySong/testsupport/security/JwtAuthenticationFilterUtils.java
@@ -1,4 +1,18 @@
 package com.mycompany.SkySong.testsupport.security;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
 public class JwtAuthenticationFilterUtils {
+    private static final Key SECRET_KEY = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+    private static final long EXPIRATION_MS = 1000L;
+
+
 }
