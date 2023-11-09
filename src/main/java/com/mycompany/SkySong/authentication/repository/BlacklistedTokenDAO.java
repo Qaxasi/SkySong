7edf54038f08823d96431d075b4f762794d0f9c7
Champel Mariusz <champelmariusz@gmail.com>
2023-11-09 22:16:33 +0100

    feat: Add existsByToken method to BlacklistedTokenDAO

--- src/main/java/com/mycompany/SkySong/authentication/repository/BlacklistedTokenDAO.java
+++ src/main/java/com/mycompany/SkySong/authentication/repository/BlacklistedTokenDAO.java
@@ -4,4 +4,5 @@ import com.mycompany.SkySong.authentication.model.entity.BlacklistedToken;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BlacklistedTokenDAO extends JpaRepository<BlacklistedToken, Long> {
+    boolean existsByToken(String token);
 }
