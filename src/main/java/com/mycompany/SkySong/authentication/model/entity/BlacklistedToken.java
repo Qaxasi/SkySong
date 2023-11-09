19924857c9d8188c3a327b003d5d961bc0c865e5
Champel Mariusz <champelmariusz@gmail.com>
2023-11-09 22:01:18 +0100

    feat: Define ID and token columns in BlacklistedToken JPA entity

--- src/main/java/com/mycompany/SkySong/authentication/model/entity/BlacklistedToken.java
+++ src/main/java/com/mycompany/SkySong/authentication/model/entity/BlacklistedToken.java
@@ -1,11 +1,13 @@
 package com.mycompany.SkySong.authentication.model.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name = "blacklisted_token")
 public class BlacklistedToken {
-
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false, unique = true)
+    private String token;
 }
