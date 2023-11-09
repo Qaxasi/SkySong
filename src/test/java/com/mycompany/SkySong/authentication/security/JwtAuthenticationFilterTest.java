ecd8dcc5d01711a7749cfa9da91b227988efb34b
Champel Mariusz <champelmariusz@gmail.com>
2023-11-09 19:28:13 +0100

    refactor: Remove unused imports

--- src/test/java/com/mycompany/SkySong/authentication/security/JwtAuthenticationFilterTest.java
+++ src/test/java/com/mycompany/SkySong/authentication/security/JwtAuthenticationFilterTest.java
@@ -15,7 +15,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.context.SecurityContextImpl;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
@@ -23,7 +22,6 @@ import java.io.IOException;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
-import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
