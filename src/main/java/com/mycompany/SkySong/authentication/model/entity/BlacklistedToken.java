2ce445cf32048b2dfa91f561e8e241b47f690d81
Champel Mariusz <champelmariusz@gmail.com>
2023-11-09 21:45:51 +0100

    feat: Annotate BlacklistedToken as a JPA entity with table mapping

--- src/main/java/com/mycompany/SkySong/authentication/model/entity/BlacklistedToken.java
+++ src/main/java/com/mycompany/SkySong/authentication/model/entity/BlacklistedToken.java
@@ -1,4 +1,11 @@
 package com.mycompany.SkySong.authentication.model.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "blacklisted_token")
 public class BlacklistedToken {
+
+
 }
