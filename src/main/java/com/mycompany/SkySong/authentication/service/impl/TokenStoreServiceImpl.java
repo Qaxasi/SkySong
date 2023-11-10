b17cb8f42ee11cdc271e1cabbc11d32a11d4cbef
Champel Mariusz <champelmariusz@gmail.com>
2023-11-10 17:15:39 +0100

    refactor: Refactor blacklistToken to utilize Redis for temporary token storage

--- src/main/java/com/mycompany/SkySong/authentication/service/impl/TokenStoreServiceImpl.java
+++ src/main/java/com/mycompany/SkySong/authentication/service/impl/TokenStoreServiceImpl.java
@@ -4,6 +4,8 @@ import com.mycompany.SkySong.authentication.model.entity.BlacklistedToken;
 import com.mycompany.SkySong.authentication.service.TokenStoreService;
 import org.springframework.data.redis.core.RedisTemplate;
 
+import java.util.concurrent.TimeUnit;
+
 public class TokenStoreServiceImpl implements TokenStoreService {
     private final RedisTemplate<String, String> redisTemplate;
     public TokenStoreServiceImpl(RedisTemplate<String, String> redisTemplate) {
@@ -11,10 +13,6 @@ public class TokenStoreServiceImpl implements TokenStoreService {
     }
     @Override
     public void blacklistToken(String token) {
-        if (!blacklistedTokenDAO.existsByToken(token)) {
-            BlacklistedToken blacklistedToken = new BlacklistedToken();
-            blacklistedToken.setToken(token);
-            blacklistedTokenDAO.save(blacklistedToken);
-        }
+        redisTemplate.opsForValue().set(token, "blacklisted", 15, TimeUnit.MINUTES);
     }
 }
