9cc730f27aae5a0dc483109eea2a205e08abfcaa
Champel Mariusz <champelmariusz@gmail.com>
2023-11-10 15:59:00 +0100

    feat: Add token blacklisting logic in TokenStoreServiceImpl

--- src/main/java/com/mycompany/SkySong/authentication/service/impl/TokenStoreServiceImpl.java
+++ src/main/java/com/mycompany/SkySong/authentication/service/impl/TokenStoreServiceImpl.java
@@ -1,5 +1,6 @@
 package com.mycompany.SkySong.authentication.service.impl;
 
+import com.mycompany.SkySong.authentication.model.entity.BlacklistedToken;
 import com.mycompany.SkySong.authentication.repository.BlacklistedTokenDAO;
 import com.mycompany.SkySong.authentication.service.TokenStoreService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,5 +10,10 @@ public class TokenStoreServiceImpl implements TokenStoreService {
     private BlacklistedTokenDAO blacklistedTokenDAO;
     @Override
     public void blacklistToken(String token) {
+        if (!blacklistedTokenDAO.existsByToken(token)) {
+            BlacklistedToken blacklistedToken = new BlacklistedToken();
+            blacklistedToken.setToken(token);
+            blacklistedTokenDAO.save(blacklistedToken);
+        }
     }
 }
