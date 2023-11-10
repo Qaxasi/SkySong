9bac425fe37ebc7bb25d7851b954c277fa77ab72
Champel Mariusz <champelmariusz@gmail.com>
2023-11-10 21:40:09 +0100

    feat: Add getCookie method to CookieUtils for retrieving specific cookies

--- src/main/java/com/mycompany/SkySong/authentication/utils/constants/CookieUtils.java
+++ src/main/java/com/mycompany/SkySong/authentication/utils/constants/CookieUtils.java
@@ -1,4 +1,22 @@
 package com.mycompany.SkySong.authentication.utils.constants;
 
+
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.util.Arrays;
+import java.util.Optional;
+
 public class CookieUtils {
+
+    public static Optional<Cookie> getCookie(HttpServletRequest request, String name) {
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null && cookies.length > 0) {
+            return Arrays.stream(cookies)
+                    .filter(cookie -> name.equals(cookie.getName()))
+                    .findFirst();
+        }
+        return Optional.empty();
+    }
 }
