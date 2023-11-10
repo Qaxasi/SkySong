c7ec47b04f7f560ba6a0547ccf47455d4e1dffc2
Champel Mariusz <champelmariusz@gmail.com>
2023-11-10 22:15:49 +0100

    refactor: Remove TokenStoreService dependency from UserController

--- src/main/java/com/mycompany/SkySong/authentication/controller/UserController.java
+++ src/main/java/com/mycompany/SkySong/authentication/controller/UserController.java
@@ -6,20 +6,14 @@ import com.mycompany.SkySong.authentication.service.DeleteService;
 import com.mycompany.SkySong.authentication.service.TokenStoreService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/users")
 public class UserController {
     private final DeleteService deleteService;
-    private final TokenStoreService tokenStoreService;
-
-    public UserController(DeleteService deleteService, TokenStoreService tokenStoreService) {
+    public UserController(DeleteService deleteService) {
         this.deleteService = deleteService;
-        this.tokenStoreService = tokenStoreService;
     }
     @DeleteMapping({"/","/{userId}"})
     public ResponseEntity<DeleteResponse> delete(@PathVariable(required = false) Long userId) {
