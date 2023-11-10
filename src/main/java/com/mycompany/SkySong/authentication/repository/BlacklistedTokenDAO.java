0545f6a7f608d9e92f9b68c766640d30b3d86040
Champel Mariusz <champelmariusz@gmail.com>
2023-11-10 19:49:11 +0100

    refactor: Delete BlacklistedTokenDAO interface

--- src/main/java/com/mycompany/SkySong/authentication/repository/BlacklistedTokenDAO.java
+++
@@ -1,8 +0,0 @@
-package com.mycompany.SkySong.authentication.repository;
-
-import com.mycompany.SkySong.authentication.model.entity.BlacklistedToken;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BlacklistedTokenDAO extends JpaRepository<BlacklistedToken, Long> {
-    boolean existsByToken(String token);
-}
