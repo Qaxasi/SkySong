83f7dc6cf0c0ad339b9936134a5677d9fd9aee14
Champel Mariusz <champelmariusz@gmail.com>
2023-11-10 21:43:32 +0100

    feat: Add addCookie method to CookieUtils for create and add cookies to response

--- src/main/java/com/mycompany/SkySong/authentication/utils/constants/CookieUtils.java
+++ src/main/java/com/mycompany/SkySong/authentication/utils/constants/CookieUtils.java
@@ -19,4 +19,12 @@ public class CookieUtils {
         }
         return Optional.empty();
     }
+    public static void addCookie(HttpServletResponse response, String name, String value, int maxAge) {
+        Cookie cookie = new Cookie(name, value);
+        cookie.setPath("/");
+        cookie.setHttpOnly(true);
+        cookie.setSecure(true);
+        cookie.setMaxAge(maxAge);
+        response.addCookie(cookie);
+    }
 }
