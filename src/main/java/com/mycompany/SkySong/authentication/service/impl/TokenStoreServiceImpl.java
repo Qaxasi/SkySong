c03335331f99c5a681f816871d2e2eb93650ade5
Champel Mariusz <champelmariusz@gmail.com>
2023-11-10 17:10:10 +0100

    feat: Inject RedisTemplate

--- src/main/java/com/mycompany/SkySong/authentication/service/impl/TokenStoreServiceImpl.java
+++ src/main/java/com/mycompany/SkySong/authentication/service/impl/TokenStoreServiceImpl.java
@@ -1,11 +1,14 @@
 package com.mycompany.SkySong.authentication.service.impl;
 
 import com.mycompany.SkySong.authentication.model.entity.BlacklistedToken;
-import com.mycompany.SkySong.authentication.repository.BlacklistedTokenDAO;
 import com.mycompany.SkySong.authentication.service.TokenStoreService;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 
 public class TokenStoreServiceImpl implements TokenStoreService {
+    private final RedisTemplate<String, String> redisTemplate;
+    public TokenStoreServiceImpl(RedisTemplate<String, String> redisTemplate) {
+        this.redisTemplate = redisTemplate;
+    }
     @Override
     public void blacklistToken(String token) {
         if (!blacklistedTokenDAO.existsByToken(token)) {
