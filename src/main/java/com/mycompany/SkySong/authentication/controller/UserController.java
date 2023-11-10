32b5ec7770331f43c7d62d824d7c0a0c30565dda
Champel Mariusz <champelmariusz@gmail.com>
2023-11-10 16:01:42 +0100

    feat: Inject TokenStoreService in UserController

--- src/main/java/com/mycompany/SkySong/authentication/controller/UserController.java
+++ src/main/java/com/mycompany/SkySong/authentication/controller/UserController.java
@@ -3,6 +3,7 @@ package com.mycompany.SkySong.authentication.controller;
 import com.mycompany.SkySong.authentication.exception.NullOrEmptyInputException;
 import com.mycompany.SkySong.authentication.model.dto.DeleteResponse;
 import com.mycompany.SkySong.authentication.service.DeleteService;
+import com.mycompany.SkySong.authentication.service.TokenStoreService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -14,9 +15,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/users")
 public class UserController {
     private final DeleteService deleteService;
+    private final TokenStoreService tokenStoreService;
 
-    public UserController(DeleteService deleteService) {
+    public UserController(DeleteService deleteService, TokenStoreService tokenStoreService) {
         this.deleteService = deleteService;
+        this.tokenStoreService = tokenStoreService;
     }
     @DeleteMapping({"/","/{userId}"})
     public ResponseEntity<DeleteResponse> delete(@PathVariable(required = false) Long userId) {
