26e53cf98d4bef4767c4ab98543780d673bc343c
Champel Mariusz <champelmariusz@gmail.com>
2023-11-10 15:55:21 +0100

    feat: Inject BlacklistedTokenDAO to TokenStoreServiceImpl

--- src/main/java/com/mycompany/SkySong/authentication/service/impl/TokenStoreServiceImpl.java
+++ src/main/java/com/mycompany/SkySong/authentication/service/impl/TokenStoreServiceImpl.java
@@ -1,8 +1,12 @@
 package com.mycompany.SkySong.authentication.service.impl;
 
+import com.mycompany.SkySong.authentication.repository.BlacklistedTokenDAO;
 import com.mycompany.SkySong.authentication.service.TokenStoreService;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class TokenStoreServiceImpl implements TokenStoreService {
+    @Autowired
+    private BlacklistedTokenDAO blacklistedTokenDAO;
     @Override
     public void blacklistToken(String token) {
     }
