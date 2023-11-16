33afd3070f214048192d20815c1a64276e845954
Champel Mariusz <champelmariusz@gmail.com>
2023-11-16 20:17:16 +0100

    test: Configure UserDAO mock in setUp method

--- src/test/java/com/mycompany/SkySong/authentication/controller/DeleteUserControllerSecurityTest.java
+++ src/test/java/com/mycompany/SkySong/authentication/controller/DeleteUserControllerSecurityTest.java
@@ -1,12 +1,21 @@
 package com.mycompany.SkySong.authentication.controller;
 
+import com.mycompany.SkySong.authentication.model.entity.Role;
+import com.mycompany.SkySong.authentication.model.entity.User;
+import com.mycompany.SkySong.authentication.model.entity.UserRole;
 import com.mycompany.SkySong.authentication.repository.UserDAO;
+import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.Optional;
+import java.util.Set;
+
+import static org.mockito.Mockito.when;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
@@ -16,4 +25,13 @@ public class DeleteUserControllerSecurityTest {
     private MockMvc mockMvc;
     @MockBean
     private UserDAO userDAO;
+    @BeforeEach
+    void setUp() {
+        Role role = new Role(UserRole.ROLE_USER);
+        Set<Role> roles = Set.of(role);
+
+        when(userDAO.findById(1L))
+                .thenReturn(Optional.of(new User(1, "testUsername", "testEmail@gmail.com",
+                        "$2a$10$VEbWwz6NcL4y6MgKEE/sJuWiFe2EoVbru6gJ.6Miu6G16NWfqlxci", roles)));
+    }
 }
