0b5eaaf27d1ac3c7731b031b07c3c7265bcc6eda
Champel Mariusz <champelmariusz@gmail.com>
2023-11-16 15:13:59 +0100

    Remove WebMvcTest annotation

--- src/test/java/com/mycompany/SkySong/authentication/controller/LoginControllerTest.java
+++ src/test/java/com/mycompany/SkySong/authentication/controller/LoginControllerTest.java
@@ -22,8 +22,6 @@ import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 
-
-@WebMvcTest(controllers = LoginController.class)
 public class LoginControllerTest {
     @Autowired
     private MockMvc mockMvc;
