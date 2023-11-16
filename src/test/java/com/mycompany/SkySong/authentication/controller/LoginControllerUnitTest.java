f6b8f10a42c6b078877b521770165e10337b2d35
Champel Mariusz <champelmariusz@gmail.com>
2023-11-16 17:34:29 +0100

    test: Configure LoginControllerUnitTest for mockito testing

--- src/test/java/com/mycompany/SkySong/authentication/controller/LoginControllerUnitTest.java
+++ src/test/java/com/mycompany/SkySong/authentication/controller/LoginControllerUnitTest.java
@@ -1,4 +1,21 @@
 package com.mycompany.SkySong.authentication.controller;
 
+
+import com.mycompany.SkySong.authentication.service.LoginService;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.web.servlet.MockMvc;
+
+@ExtendWith(MockitoExtension.class)
 public class LoginControllerUnitTest {
+    @Autowired
+    private MockMvc mockMvc;
+    @Mock
+    private LoginService loginService;
+    @InjectMocks
+    private LoginController loginController;
+
 }
