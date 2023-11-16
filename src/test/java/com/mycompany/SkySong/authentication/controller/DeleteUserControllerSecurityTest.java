b0bc9bd68d6f19ad16235fd2904b0f940e1e78ea
Champel Mariusz <champelmariusz@gmail.com>
2023-11-16 20:31:49 +0100

    test: Add test for admin user deletion success scenario

--- src/test/java/com/mycompany/SkySong/authentication/controller/DeleteUserControllerSecurityTest.java
+++ src/test/java/com/mycompany/SkySong/authentication/controller/DeleteUserControllerSecurityTest.java
@@ -4,11 +4,14 @@ import com.mycompany.SkySong.authentication.model.entity.Role;
 import com.mycompany.SkySong.authentication.model.entity.User;
 import com.mycompany.SkySong.authentication.model.entity.UserRole;
 import com.mycompany.SkySong.authentication.repository.UserDAO;
+import com.mycompany.SkySong.testsupport.controller.DeleteRequestAssertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -33,5 +36,13 @@ public class DeleteUserControllerSecurityTest {
         when(userDAO.findById(1L))
                 .thenReturn(Optional.of(new User(1, "testUsername", "testEmail@gmail.com",
                         "$2a$10$VEbWwz6NcL4y6MgKEE/sJuWiFe2EoVbru6gJ.6Miu6G16NWfqlxci", roles)));
+    }
+    @Test
+    @WithMockUser(roles="ADMIN")
+    void shouldReceiveOkStatusWhenDeletingUserWithValidId() throws Exception{
+        long userId = 1L;
+
+        DeleteRequestAssertions.assertDeleteStatusReturns(
+                mockMvc, "/api/v1/users/" + userId, 200);
     }
 }
