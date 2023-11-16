e6537fa1667ca774801a5f7453ec5baed4564974
Champel Mariusz <champelmariusz@gmail.com>
2023-11-16 20:14:07 +0100

    test: Configure integration testing for DeleteUserControllerSecurityTest

--- src/test/java/com/mycompany/SkySong/authentication/controller/DeleteUserControllerSecurityTest.java
+++ src/test/java/com/mycompany/SkySong/authentication/controller/DeleteUserControllerSecurityTest.java
@@ -1,3 +1,12 @@
 package com.mycompany.SkySong.authentication.controller;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
 public class DeleteUserControllerSecurityTest {
+
 }
