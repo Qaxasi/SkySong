6405949e1318dcd59125919367acf925b7cc320c
Champel Mariusz <champelmariusz@gmail.com>
2023-11-17 19:24:09 +0100

    test: Inject MockMvc into LogoutControllerTest

--- src/test/java/com/mycompany/SkySong/authentication/controller/LogoutControllerTest.java
+++ src/test/java/com/mycompany/SkySong/authentication/controller/LogoutControllerTest.java
@@ -1,7 +1,11 @@
 package com.mycompany.SkySong.authentication.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
 
 @WebMvcTest(LogoutController.class)
 public class LogoutControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
 }
