22bd2ff85cfbd0232b628409f49d06890c047d75
Champel Mariusz <champelmariusz@gmail.com>
2023-11-17 21:15:45 +0100

    test: Inject mocked dependencies in LogoutControllerTest

--- src/test/java/com/mycompany/SkySong/authentication/controller/LogoutControllerTest.java
+++ src/test/java/com/mycompany/SkySong/authentication/controller/LogoutControllerTest.java
@@ -1,9 +1,16 @@
 package com.mycompany.SkySong.authentication.controller;
 
 import com.mycompany.SkySong.authentication.config.SecurityConfig;
+import com.mycompany.SkySong.authentication.secutiry.CustomAccessDeniedHandler;
+import com.mycompany.SkySong.authentication.secutiry.JwtAuthenticationEntryPoint;
+import com.mycompany.SkySong.authentication.secutiry.JwtTokenProvider;
+import com.mycompany.SkySong.testsupport.controller.PostRequestAssertions;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.test.web.servlet.MockMvc;
 
 @WebMvcTest(LogoutController.class)
@@ -11,4 +18,12 @@ import org.springframework.test.web.servlet.MockMvc;
 public class LogoutControllerTest {
     @Autowired
     private MockMvc mockMvc;
+    @MockBean
+    private JwtTokenProvider jwtTokenProvider;
+    @MockBean
+    private UserDetailsService userDetailsService;
+    @MockBean
+    private JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    @MockBean
+    private CustomAccessDeniedHandler customAccessDeniedHandler;
 }
