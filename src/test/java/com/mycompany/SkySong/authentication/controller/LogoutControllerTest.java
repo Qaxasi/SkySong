f3f34334a4d995594078b8fbffea104d736148f7
Champel Mariusz <champelmariusz@gmail.com>
2023-11-17 19:38:29 +0100

    test: Integrate SecurityConfig in LogoutController test

--- src/test/java/com/mycompany/SkySong/authentication/controller/LogoutControllerTest.java
+++ src/test/java/com/mycompany/SkySong/authentication/controller/LogoutControllerTest.java
@@ -1,10 +1,13 @@
 package com.mycompany.SkySong.authentication.controller;
 
+import com.mycompany.SkySong.authentication.config.SecurityConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.web.servlet.MockMvc;
 
 @WebMvcTest(LogoutController.class)
+@Import(SecurityConfig.class)
 public class LogoutControllerTest {
     @Autowired
     private MockMvc mockMvc;
