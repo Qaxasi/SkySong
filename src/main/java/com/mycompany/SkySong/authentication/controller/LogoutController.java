882e727d8f956d2fb37e6a2cb7c58f88c56b3930
Champel Mariusz <champelmariusz@gmail.com>
2023-11-17 22:07:58 +0100

    refactor: Add error handling in LogoutController

--- src/main/java/com/mycompany/SkySong/authentication/controller/LogoutController.java
+++ src/main/java/com/mycompany/SkySong/authentication/controller/LogoutController.java
@@ -1,8 +1,10 @@
 package com.mycompany.SkySong.authentication.controller;
 
+import com.mycompany.SkySong.authentication.exception.ServiceFailureException;
 import com.mycompany.SkySong.authentication.utils.CookieUtils;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,7 +15,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class LogoutController {
     @PostMapping("/logout")
     public ResponseEntity<?> logout(HttpServletRequest request, HttpServletResponse response) {
-        CookieUtils.deleteCookie(request, response, "auth_token");
-        return ResponseEntity.ok("User logged out successfully");
+        try {
+            CookieUtils.deleteCookie(request, response, "auth_token");
+            return ResponseEntity.ok("User logged out successfully");
+        } catch (RuntimeException e) {
+            throw new ServiceFailureException("Logout failed due to an internal error");
+        }
     }
 }
