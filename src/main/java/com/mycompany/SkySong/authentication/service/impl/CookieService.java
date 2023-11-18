eca3efb6483107b1f8e1ba5640f4b297a7391169
Champel Mariusz <champelmariusz@gmail.com>
2023-11-18 23:31:30 +0100

    feat: Add new functions to CookieService interface

--- src/main/java/com/mycompany/SkySong/authentication/service/impl/CookieService.java
+++ src/main/java/com/mycompany/SkySong/authentication/service/impl/CookieService.java
@@ -1,4 +1,13 @@
 package com.mycompany.SkySong.authentication.service.impl;
 
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.util.Optional;
+
 public interface CookieService {
+    Optional<Cookie> getCookie(HttpServletRequest request, String name);
+    void addCookie(HttpServletResponse response, String name, String value, int maxAge);
+    void deleteCookie(HttpServletRequest request, HttpServletResponse response, String name);
 }
