dcb0553bf874e0b0ab96e062a94d115997562aa3
Champel Mariusz <champelmariusz@gmail.com>
2023-11-18 22:26:12 +0100

    test: Add test for logout success without cookie

--- src/test/java/com/mycompany/SkySong/authentication/controller/LogoutControllerTest.java
+++ src/test/java/com/mycompany/SkySong/authentication/controller/LogoutControllerTest.java
@@ -16,6 +16,9 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 @WebMvcTest(LogoutController.class)
 @Import(SecurityConfig.class)
 public class LogoutControllerTest {
@@ -52,5 +55,14 @@ public class LogoutControllerTest {
     void test() throws Exception {
         Cookie mockCookie = new Cookie("auth_token", "token-value");
         CookieAssertions.assertCookieIsDeleted(mockMvc, "/api/v1/users/logout", mockCookie);
+    }
+    @Test
+    @WithMockUser
+    void shouldReturnStatusOkWhenLogoutWithoutCookie() throws Exception {
+        PostRequestAssertions.assertPostStatusReturnsWithoutBodyAndCookie(mockMvc,
+                "/api/v1/users/logout",
+                200);
+        mockMvc.perform(post("/api/v1/users/logout"))
+                .andExpect(status().isOk());
     }
 }
