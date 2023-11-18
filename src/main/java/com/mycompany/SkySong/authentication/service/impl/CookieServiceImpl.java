c48203034d436de715e67d299782d739bcb217eb
Champel Mariusz <champelmariusz@gmail.com>
2023-11-18 23:38:42 +0100

    refactor: Rename class

--- src/main/java/com/mycompany/SkySong/authentication/service/impl/CookieServiceImpl.java
+++ src/main/java/com/mycompany/SkySong/authentication/service/impl/CookieServiceImpl.java
@@ -8,7 +8,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.util.Arrays;
 import java.util.Optional;
 
-public class CookieUtils {
+public class CookieServiceImpl {
 
     public static Optional<Cookie> getCookie(HttpServletRequest request, String name) {
         Cookie[] cookies = request.getCookies();
