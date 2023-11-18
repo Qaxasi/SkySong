380057b7428f1cec94a47fe4e85204a196228df2
Champel Mariusz <champelmariusz@gmail.com>
2023-11-18 15:23:26 +0100

    test: Add annotation to LogoutControllerIntegrationTest class

--- src/test/java/com/mycompany/SkySong/authentication/controller/LogoutControllerIntegrationTest.java
+++ src/test/java/com/mycompany/SkySong/authentication/controller/LogoutControllerIntegrationTest.java
@@ -1,4 +1,7 @@
 package com.mycompany.SkySong.authentication.controller;
 
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class LogoutControllerIntegrationTest {
 }
