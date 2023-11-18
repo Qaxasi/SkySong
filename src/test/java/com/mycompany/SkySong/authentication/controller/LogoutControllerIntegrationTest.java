397775bde66dfa371702f954dbcf20fd2990825a
Champel Mariusz <champelmariusz@gmail.com>
2023-11-18 22:18:38 +0100

    refactor: Delete test class

--- src/test/java/com/mycompany/SkySong/authentication/controller/LogoutControllerIntegrationTest.java
+++
@@ -1,7 +0,0 @@
-package com.mycompany.SkySong.authentication.controller;
-
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-public class LogoutControllerIntegrationTest {
-}
