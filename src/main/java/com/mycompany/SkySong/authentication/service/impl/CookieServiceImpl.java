88f26c27c70592bd325133f6fe0a7c20e1f8dcd0
Champel Mariusz <champelmariusz@gmail.com>
2023-11-18 23:59:18 +0100

    feat: Annotate methods with Override in CookieServiceImpl

--- src/main/java/com/mycompany/SkySong/authentication/service/impl/CookieServiceImpl.java
+++ src/main/java/com/mycompany/SkySong/authentication/service/impl/CookieServiceImpl.java
@@ -10,7 +10,7 @@ import java.util.Arrays;
 import java.util.Optional;
 
 public class CookieServiceImpl implements CookieService {
-
+    @Override
     public Optional<Cookie> getCookie(HttpServletRequest request, String name) {
         Cookie[] cookies = request.getCookies();
         if (cookies != null && cookies.length > 0) {
@@ -20,6 +20,7 @@ public class CookieServiceImpl implements CookieService {
         }
         return Optional.empty();
     }
+    @Override
     public void addCookie(HttpServletResponse response, String name, String value, int maxAge) {
         Cookie cookie = new Cookie(name, value);
         cookie.setPath("/");
@@ -28,6 +29,7 @@ public class CookieServiceImpl implements CookieService {
         cookie.setMaxAge(maxAge);
         response.addCookie(cookie);
     }
+    @Override
     public void deleteCookie(HttpServletRequest request, HttpServletResponse response, String name) {
         Cookie[] cookies = request.getCookies();
         if (cookies != null) {
