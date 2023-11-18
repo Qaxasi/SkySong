04869dac625b563be96ed42630e389457a8de8df
Champel Mariusz <champelmariusz@gmail.com>
2023-11-18 22:37:46 +0100

    test: Add cookie deletion assertion method

--- src/test/java/com/mycompany/SkySong/testsupport/controller/CookieAssertions.java
+++ src/test/java/com/mycompany/SkySong/testsupport/controller/CookieAssertions.java
@@ -1,4 +1,21 @@
 package com.mycompany.SkySong.testsupport.controller;
 
+import jakarta.servlet.http.Cookie;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+
 public class CookieAssertions {
+
+    public static void assertCookieIsDeleted(MockMvc mockMvc, String endpoint, Cookie cookie) throws Exception {
+        mockMvc.perform(post(endpoint).cookie(cookie))
+                .andExpect(result -> {
+                    String setCookieHeader = result.getResponse().getHeader("Set-Cookie");
+                    assertNotNull(setCookieHeader);
+                    assertTrue(setCookieHeader.contains("auth_token=") &&
+                            (setCookieHeader.contains("Max-Age=0") || setCookieHeader.contains("Expires")));
+                });
+    }
 }
