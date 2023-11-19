a124202e1289bd4a1282f6fede589c3645543758
Champel Mariusz <champelmariusz@gmail.com>
2023-11-19 23:24:17 +0100

    refactor: Update delete method to return ApiResponse in controller

--- src/main/java/com/mycompany/SkySong/authentication/controller/DeleteUserController.java
+++ src/main/java/com/mycompany/SkySong/authentication/controller/DeleteUserController.java
@@ -1,6 +1,7 @@
 package com.mycompany.SkySong.authentication.controller;
 
 import com.mycompany.SkySong.authentication.exception.NullOrEmptyInputException;
+import com.mycompany.SkySong.authentication.model.dto.ApiResponse;
 import com.mycompany.SkySong.authentication.model.dto.DeleteResponse;
 import com.mycompany.SkySong.authentication.service.DeleteUserService;
 import org.springframework.http.HttpStatus;
@@ -15,11 +16,11 @@ public class DeleteUserController {
         this.deleteUserService = deleteUserService;
     }
     @DeleteMapping({"/","/{userId}"})
-    public ResponseEntity<DeleteResponse> delete(@PathVariable(required = false) Long userId) {
+    public ResponseEntity<ApiResponse> delete(@PathVariable(required = false) Long userId) {
         if (userId == null) {
             throw new NullOrEmptyInputException("User ID is required and cannot be empty.");
         }
-        DeleteResponse deleteResponse = deleteUserService.deleteUser(userId);
+        ApiResponse deleteResponse = deleteUserService.deleteUser(userId);
         return new ResponseEntity<>(deleteResponse, HttpStatus.OK);
     }
 }
