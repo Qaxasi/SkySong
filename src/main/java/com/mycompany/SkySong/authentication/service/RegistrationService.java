eca55dc1fd6a3b6b143a73748d4e75067bd28ade
Champel Mariusz <champelmariusz@gmail.com>
2023-11-19 23:39:42 +0100

    refactor: Delete unused import

--- src/main/java/com/mycompany/SkySong/authentication/service/RegistrationService.java
+++ src/main/java/com/mycompany/SkySong/authentication/service/RegistrationService.java
@@ -2,7 +2,6 @@ package com.mycompany.SkySong.authentication.service;
 
 import com.mycompany.SkySong.authentication.model.dto.ApiResponse;
 import com.mycompany.SkySong.authentication.model.dto.RegisterRequest;
-import com.mycompany.SkySong.authentication.model.dto.RegistrationResponse;
 
 public interface RegistrationService {
     ApiResponse register(RegisterRequest registerRequest);
