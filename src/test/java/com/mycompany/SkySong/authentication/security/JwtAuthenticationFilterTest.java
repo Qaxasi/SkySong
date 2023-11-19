e09366ec290bbe7b81a94fad811489c7c5aabcfb
Champel Mariusz <champelmariusz@gmail.com>
2023-11-19 15:38:27 +0100

    test: Set up mock dependency injection for JwtAuthenticationFilter

--- src/test/java/com/mycompany/SkySong/authentication/security/JwtAuthenticationFilterTest.java
+++ src/test/java/com/mycompany/SkySong/authentication/security/JwtAuthenticationFilterTest.java
@@ -13,6 +13,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -27,6 +28,7 @@ import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class JwtAuthenticationFilterTest {
+    @InjectMocks
     private JwtAuthenticationFilter jwtAuthenticationFilter;
     @Mock
     private CookieService cookieService;
