58c9586c673f279a9e4876b21d3b9a1b1157f18a
Champel Mariusz <champelmariusz@gmail.com>
2023-11-19 20:45:10 +0100

    test: Add SpringBootTest and MockMvc to LogoutController test

--- src/test/java/com/mycompany/SkySong/authentication/controller/LogoutControllerIntegrationTest.java
+++ src/test/java/com/mycompany/SkySong/authentication/controller/LogoutControllerIntegrationTest.java
@@ -1,4 +1,10 @@
 package com.mycompany.SkySong.authentication.controller;
 
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
 public class LogoutControllerIntegrationTest {
+
 }
