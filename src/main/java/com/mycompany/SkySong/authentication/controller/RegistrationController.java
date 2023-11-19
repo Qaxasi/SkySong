39c161c6b36cd9786ad1b4b74723787897c2aac0
Champel Mariusz <champelmariusz@gmail.com>
2023-11-19 23:43:45 +0100

    refactor: Delete unused import

--- src/main/java/com/mycompany/SkySong/authentication/controller/RegistrationController.java
+++ src/main/java/com/mycompany/SkySong/authentication/controller/RegistrationController.java
@@ -2,7 +2,6 @@ package com.mycompany.SkySong.authentication.controller;
 
 import com.mycompany.SkySong.authentication.model.dto.ApiResponse;
 import com.mycompany.SkySong.authentication.model.dto.RegisterRequest;
-import com.mycompany.SkySong.authentication.model.dto.RegistrationResponse;
 import com.mycompany.SkySong.authentication.service.RegistrationService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
