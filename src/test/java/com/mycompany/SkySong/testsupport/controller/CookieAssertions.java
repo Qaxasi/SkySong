36913d75aa320cb2457ee0389413e8deeb2ba291
Champel Mariusz <champelmariusz@gmail.com>
2023-11-19 20:10:11 +0100

    test: Transfer of assertions related to cookies to a separate class

--- src/test/java/com/mycompany/SkySong/testsupport/controller/CookieAssertions.java
+++ src/test/java/com/mycompany/SkySong/testsupport/controller/CookieAssertions.java
@@ -1,13 +1,49 @@
 package com.mycompany.SkySong.testsupport.controller;
 
 import jakarta.servlet.http.Cookie;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.cookie;
 
 public class CookieAssertions {
+    public static void assertCookieExist(MockMvc mockMvc, String endpoint, String requestBody, String cookieName) throws Exception {
+        mockMvc.perform(post(endpoint)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestBody))
+                .andExpect(cookie().exists(cookieName));
+    }
+    public static void assertCookieDoesNotExist(MockMvc mockMvc, String endpoint, String requestBody,
+                                                String cookieName) throws Exception {
+        mockMvc.perform(post(endpoint)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestBody))
+                .andExpect(cookie().doesNotExist(cookieName));
+    }
+    public static void assertCookieIsHttpOnly(MockMvc mockMvc, String endpoint, String requestBody,
+                                              String cookieName) throws Exception {
+        mockMvc.perform(post(endpoint)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestBody))
+                .andExpect(cookie().httpOnly(cookieName, true));
+    }
+    public static void assertCookieMaxAge(MockMvc mockMvc, String endpoint, String requestBody,
+                                          String cookieName, int expectedMaxAge) throws Exception {
+        mockMvc.perform(post(endpoint)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestBody))
+                .andExpect(cookie().maxAge(cookieName, expectedMaxAge));
+    }
+    public static void assertCookieIsSecure(MockMvc mockMvc, String endpoint, String requestBody,
+                                            String cookieName) throws Exception {
+        mockMvc.perform(post(endpoint)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestBody))
+                .andExpect(cookie().secure(cookieName, true));
+    }
 
     public static void assertCookieIsDeleted(MockMvc mockMvc, String endpoint, Cookie cookie) throws Exception {
         mockMvc.perform(post(endpoint).cookie(cookie))
--- src/test/java/com/mycompany/SkySong/testsupport/controller/PostRequestAssertions.java
+++ src/test/java/com/mycompany/SkySong/testsupport/controller/PostRequestAssertions.java
@@ -12,42 +12,6 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 public class PostRequestAssertions {
-
-    public static void assertCookieExist(MockMvc mockMvc, String endpoint, String requestBody, String cookieName) throws Exception {
-        mockMvc.perform(post(endpoint)
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(requestBody))
-                .andExpect(cookie().exists(cookieName));
-    }
-    public static void assertCookieDoesNotExist(MockMvc mockMvc, String endpoint, String requestBody,
-                                                String cookieName) throws Exception {
-        mockMvc.perform(post(endpoint)
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(requestBody))
-                .andExpect(cookie().doesNotExist(cookieName));
-    }
-    public static void assertCookieIsHttpOnly(MockMvc mockMvc, String endpoint, String requestBody,
-                                              String cookieName) throws Exception {
-        mockMvc.perform(post(endpoint)
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(requestBody))
-                .andExpect(cookie().httpOnly(cookieName, true));
-    }
-    public static void assertCookieMaxAge(MockMvc mockMvc, String endpoint, String requestBody,
-                                          String cookieName, int expectedMaxAge) throws Exception {
-        mockMvc.perform(post(endpoint)
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(requestBody))
-                .andExpect(cookie().maxAge(cookieName, expectedMaxAge));
-    }
-    public static void assertCookieIsSecure(MockMvc mockMvc, String endpoint, String requestBody,
-                                            String cookieName) throws Exception {
-        mockMvc.perform(post(endpoint)
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(requestBody))
-                .andExpect(cookie().secure(cookieName, true));
-    }
-
     public static void assertPostStatusReturns(MockMvc mockMvc, String endpoint, String requestBody, int expectedStatusCode) throws Exception {
         mockMvc.perform(post(endpoint)
                         .contentType(MediaType.APPLICATION_JSON)
