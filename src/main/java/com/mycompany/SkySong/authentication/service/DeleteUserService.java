71da190e90c3995c89e4711e0a7063e629a1a7ce
Champel Mariusz <champelmariusz@gmail.com>
2023-11-19 23:22:51 +0100

    refactor: Remove unused import

--- src/main/java/com/mycompany/SkySong/authentication/service/DeleteUserService.java
+++ src/main/java/com/mycompany/SkySong/authentication/service/DeleteUserService.java
@@ -1,7 +1,6 @@
 package com.mycompany.SkySong.authentication.service;
 
 import com.mycompany.SkySong.authentication.model.dto.ApiResponse;
-import com.mycompany.SkySong.authentication.model.dto.DeleteResponse;
 
 public interface DeleteUserService {
     ApiResponse deleteUser(long userId);
