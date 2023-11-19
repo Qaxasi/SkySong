4939ec9a0872eeaf9474388886e973fb11fd8273
Champel Mariusz <champelmariusz@gmail.com>
2023-11-19 19:24:50 +0100

    refactor: Remove unused import

--- src/test/java/com/mycompany/SkySong/authentication/controller/LogoutControllerTest.java
+++ src/test/java/com/mycompany/SkySong/authentication/controller/LogoutControllerTest.java
@@ -12,7 +12,6 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
