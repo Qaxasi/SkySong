675665ed6ddebfaee7f787a9520bc139a5b875c1
Champel Mariusz <champelmariusz@gmail.com>
2023-11-20 13:07:24 +0100

    feat: Implement messages.properties loading in ApplicationMessageService

--- src/main/java/com/mycompany/SkySong/authentication/service/impl/ApplicationMessageService.java
+++ src/main/java/com/mycompany/SkySong/authentication/service/impl/ApplicationMessageService.java
@@ -2,10 +2,20 @@ package com.mycompany.SkySong.authentication.service.impl;
 
 import com.mycompany.SkySong.authentication.service.MessageService;
 
+import java.io.IOException;
 import java.util.Properties;
 
 public class ApplicationMessageService implements MessageService {
     private final Properties messages = new Properties();
+
+    public ApplicationMessageService() {
+        try {
+            messages.load(getClass().getClassLoader().getResourceAsStream("messages.properties"));
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to load messages.properties");
+        }
+    }
+
     @Override
     public String getMessage(String key) {
         return null;
