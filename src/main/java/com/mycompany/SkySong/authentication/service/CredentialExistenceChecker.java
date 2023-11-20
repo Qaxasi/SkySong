47856b27a947a162c756cebc495057a3329d73a7
Champel Mariusz <champelmariusz@gmail.com>
2023-11-20 20:50:29 +0100

    feat: Add checkForExistingCredentials method into CredentialExistenceChecker interface

--- src/main/java/com/mycompany/SkySong/authentication/service/CredentialExistenceChecker.java
+++ src/main/java/com/mycompany/SkySong/authentication/service/CredentialExistenceChecker.java
@@ -1,4 +1,7 @@
 package com.mycompany.SkySong.authentication.service;
 
+import com.mycompany.SkySong.authentication.model.dto.RegisterRequest;
+
 public interface CredentialExistenceChecker {
+    void checkForExistingCredentials(RegisterRequest registerRequest);
 }
