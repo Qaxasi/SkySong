e30ef0c3c421f08620c22973294495c7b23bd44a
Champel Mariusz <champelmariusz@gmail.com>
2023-11-20 21:41:34 +0100

    feat: Implement CredentialExistenceChecker in CredentialExistenceCheckerImpl

--- src/main/java/com/mycompany/SkySong/authentication/service/impl/CredentialExistenceCheckerImpl.java
+++ src/main/java/com/mycompany/SkySong/authentication/service/impl/CredentialExistenceCheckerImpl.java
@@ -1,7 +1,13 @@
 package com.mycompany.SkySong.authentication.service.impl;
 
+import com.mycompany.SkySong.authentication.model.dto.RegisterRequest;
+import com.mycompany.SkySong.authentication.service.CredentialExistenceChecker;
 import org.springframework.stereotype.Service;
 
 @Service
-public class CredentialExistenceCheckerImpl {
+public class CredentialExistenceCheckerImpl implements CredentialExistenceChecker {
+    @Override
+    public void checkForExistingCredentials(RegisterRequest registerRequest) {
+
+    }
 }
