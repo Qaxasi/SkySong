8243cf4ea194b09c368eb8b287c2edc0506b5b59
Champel Mariusz <champelmariusz@gmail.com>
2023-11-20 13:04:34 +0100

    feat: Add properties field for message storage

--- src/main/java/com/mycompany/SkySong/authentication/service/impl/ApplicationMessageService.java
+++ src/main/java/com/mycompany/SkySong/authentication/service/impl/ApplicationMessageService.java
@@ -2,7 +2,10 @@ package com.mycompany.SkySong.authentication.service.impl;
 
 import com.mycompany.SkySong.authentication.service.MessageService;
 
+import java.util.Properties;
+
 public class ApplicationMessageService implements MessageService {
+    private final Properties messages = new Properties();
     @Override
     public String getMessage(String key) {
         return null;
