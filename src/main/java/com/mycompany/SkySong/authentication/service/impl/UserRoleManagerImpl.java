dde6c002774d773362c07ea3ae54698bdf2c7271
Champel Mariusz <champelmariusz@gmail.com>
2023-11-20 23:10:47 +0100

    feat: Implement UserRoleManager in UserRoleManagerImpl

--- src/main/java/com/mycompany/SkySong/authentication/service/impl/UserRoleManagerImpl.java
+++ src/main/java/com/mycompany/SkySong/authentication/service/impl/UserRoleManagerImpl.java
@@ -1,7 +1,13 @@
 package com.mycompany.SkySong.authentication.service.impl;
 
+import com.mycompany.SkySong.authentication.model.entity.Role;
+import com.mycompany.SkySong.authentication.service.UserRoleManager;
 import org.springframework.stereotype.Service;
 
 @Service
-public class UserRoleManagerImpl {
+public class UserRoleManagerImpl implements UserRoleManager {
+    @Override
+    public Role getUserRole() {
+        return null;
+    }
 }
