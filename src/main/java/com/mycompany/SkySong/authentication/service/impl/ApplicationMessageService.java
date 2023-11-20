5c503afc53080ad284559388743e7dcd5ecc4bf9
Champel Mariusz <champelmariusz@gmail.com>
2023-11-20 12:32:55 +0100

    feat: Implement MessageService in ApplicationMessageService

--- src/main/java/com/mycompany/SkySong/authentication/service/impl/ApplicationMessageService.java
+++ src/main/java/com/mycompany/SkySong/authentication/service/impl/ApplicationMessageService.java
@@ -1,4 +1,15 @@
 package com.mycompany.SkySong.authentication.service.impl;
 
-public class ApplicationMessageService {
+import com.mycompany.SkySong.authentication.service.MessageService;
+
+public class ApplicationMessageService implements MessageService {
+    @Override
+    public String getMessage(String key) {
+        return null;
+    }
+
+    @Override
+    public String getMessage(String key, Object... params) {
+        return null;
+    }
 }
