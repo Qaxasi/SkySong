34288c53f3f6bbe6425661d11a6ad3cf6899c4aa
Champel Mariusz <champelmariusz@gmail.com>
2023-11-21 00:22:24 +0100

    feat: Add @Slf4j annotation to UserRoleManagerImpl

--- src/main/java/com/mycompany/SkySong/authentication/service/impl/UserRoleManagerImpl.java
+++ src/main/java/com/mycompany/SkySong/authentication/service/impl/UserRoleManagerImpl.java
@@ -5,9 +5,11 @@ import com.mycompany.SkySong.authentication.model.entity.Role;
 import com.mycompany.SkySong.authentication.model.entity.UserRole;
 import com.mycompany.SkySong.authentication.repository.RoleDAO;
 import com.mycompany.SkySong.authentication.service.UserRoleManager;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 public class UserRoleManagerImpl implements UserRoleManager {
     private final RoleDAO roleDAO;
     private final ApplicationMessageService messageService;
