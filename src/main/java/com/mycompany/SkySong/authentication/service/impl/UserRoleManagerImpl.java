125da0d952e558d161a0634cd6acc59e35f6a943
Champel Mariusz <champelmariusz@gmail.com>
2023-11-20 23:12:10 +0100

    feat: Inject dependencies into UserRoleManagerImpl

--- src/main/java/com/mycompany/SkySong/authentication/service/impl/UserRoleManagerImpl.java
+++ src/main/java/com/mycompany/SkySong/authentication/service/impl/UserRoleManagerImpl.java
@@ -1,11 +1,20 @@
 package com.mycompany.SkySong.authentication.service.impl;
 
 import com.mycompany.SkySong.authentication.model.entity.Role;
+import com.mycompany.SkySong.authentication.repository.UserDAO;
 import com.mycompany.SkySong.authentication.service.UserRoleManager;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserRoleManagerImpl implements UserRoleManager {
+    private final UserDAO userDAO;
+    private final ApplicationMessageService messageService;
+
+    public UserRoleManagerImpl(UserDAO userDAO, ApplicationMessageService messageService) {
+        this.userDAO = userDAO;
+        this.messageService = messageService;
+    }
+
     @Override
     public Role getUserRole() {
         return null;
