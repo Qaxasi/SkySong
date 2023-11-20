fea55fa89c1b3241b0b41c1e637698dbad43df39
Champel Mariusz <champelmariusz@gmail.com>
2023-11-20 20:30:17 +0100

    feat: Add validateCredentials method to ValidationService interface

--- src/main/java/com/mycompany/SkySong/authentication/service/ValidationService.java
+++ src/main/java/com/mycompany/SkySong/authentication/service/ValidationService.java
@@ -1,4 +1,7 @@
 package com.mycompany.SkySong.authentication.service;
 
+import com.mycompany.SkySong.authentication.model.dto.RegisterRequest;
+
 public interface ValidationService {
+    void validateCredentials(RegisterRequest registerRequest);
 }
