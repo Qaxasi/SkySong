0b9b9e0f0d83e764e0aa9922f665af58cd1c0f75
Champel Mariusz <champelmariusz@gmail.com>
2023-11-20 21:44:34 +0100

    feat: Inject dependencies into CredentialExistenceCheckerImpl

--- src/main/java/com/mycompany/SkySong/authentication/service/impl/CredentialExistenceCheckerImpl.java
+++ src/main/java/com/mycompany/SkySong/authentication/service/impl/CredentialExistenceCheckerImpl.java
@@ -1,13 +1,23 @@
 package com.mycompany.SkySong.authentication.service.impl;
 
 import com.mycompany.SkySong.authentication.model.dto.RegisterRequest;
+import com.mycompany.SkySong.authentication.repository.UserDAO;
 import com.mycompany.SkySong.authentication.service.CredentialExistenceChecker;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CredentialExistenceCheckerImpl implements CredentialExistenceChecker {
+    private final UserDAO userDAO;
+    private final ApplicationMessageService messageService;
+    public CredentialExistenceCheckerImpl(UserDAO userDAO, ApplicationMessageService messageService) {
+        this.userDAO = userDAO;
+        this.messageService = messageService;
+    }
+
     @Override
     public void checkForExistingCredentials(RegisterRequest registerRequest) {
 
     }
+
+
 }
