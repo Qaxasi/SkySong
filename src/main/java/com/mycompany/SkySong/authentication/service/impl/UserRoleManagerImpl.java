d49598445965510655ffc044679eddc8231cd6a9
Champel Mariusz <champelmariusz@gmail.com>
2023-11-21 00:21:20 +0100

    refactor: Update getRoleByName in UserRoleManagerImpl with DB query and error handling

--- src/main/java/com/mycompany/SkySong/authentication/service/impl/UserRoleManagerImpl.java
+++ src/main/java/com/mycompany/SkySong/authentication/service/impl/UserRoleManagerImpl.java
@@ -1,6 +1,8 @@
 package com.mycompany.SkySong.authentication.service.impl;
 
+import com.mycompany.SkySong.authentication.exception.ServiceFailureException;
 import com.mycompany.SkySong.authentication.model.entity.Role;
+import com.mycompany.SkySong.authentication.model.entity.UserRole;
 import com.mycompany.SkySong.authentication.repository.RoleDAO;
 import com.mycompany.SkySong.authentication.service.UserRoleManager;
 import org.springframework.stereotype.Service;
@@ -15,7 +17,11 @@ public class UserRoleManagerImpl implements UserRoleManager {
         this.messageService = messageService;
     }
     @Override
-    public Role getRoleByName(String roleName) {
-        return null;
+    public Role getRoleByName(UserRole userRole) {
+        return roleDAO.findByName(userRole)
+                .orElseThrow(() -> {
+                    log.error("User role not set in the system!");
+                    return new ServiceFailureException(messageService.getMessage("user.role.not-set"));
+                });
     }
 }
