dc7fa98744e84125d4af19fbdcb8d53bfd453584
Champel Mariusz <champelmariusz@gmail.com>
2023-11-20 14:00:39 +0100

    feat: Inject ApplicationMessageService dependency into DeleteUserController

--- src/main/java/com/mycompany/SkySong/authentication/controller/DeleteUserController.java
+++ src/main/java/com/mycompany/SkySong/authentication/controller/DeleteUserController.java
@@ -3,6 +3,7 @@ package com.mycompany.SkySong.authentication.controller;
 import com.mycompany.SkySong.authentication.exception.NullOrEmptyInputException;
 import com.mycompany.SkySong.authentication.model.dto.ApiResponse;
 import com.mycompany.SkySong.authentication.service.DeleteUserService;
+import com.mycompany.SkySong.authentication.service.impl.ApplicationMessageService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,8 +12,10 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/v1/users")
 public class DeleteUserController {
     private final DeleteUserService deleteUserService;
-    public DeleteUserController(DeleteUserService deleteUserService) {
+    private final ApplicationMessageService messageService;
+    public DeleteUserController(DeleteUserService deleteUserService, ApplicationMessageService messageService) {
         this.deleteUserService = deleteUserService;
+        this.messageService = messageService;
     }
     @DeleteMapping({"/","/{userId}"})
     public ResponseEntity<ApiResponse> delete(@PathVariable(required = false) Long userId) {
