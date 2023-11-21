b1b02cfb1f00a3d2ab90368007b44a572b7c3cb1
Champel Mariusz <champelmariusz@gmail.com>
2023-11-21 18:27:35 +0100

    refactor: Rename ApplicationMessageService field in JwtExceptionHandler

--- src/main/java/com/mycompany/SkySong/authentication/secutiry/JwtExceptionHandler.java
+++ src/main/java/com/mycompany/SkySong/authentication/secutiry/JwtExceptionHandler.java
@@ -1,11 +1,13 @@
 package com.mycompany.SkySong.authentication.secutiry;
 
+import com.mycompany.SkySong.authentication.exception.TokenException;
 import com.mycompany.SkySong.authentication.service.impl.ApplicationMessageService;
+import io.jsonwebtoken.ExpiredJwtException;
 
 public class JwtExceptionHandler {
-    private final ApplicationMessageService applicationMessageService;
+    private final ApplicationMessageService messageService;
 
-    public JwtExceptionHandler(ApplicationMessageService applicationMessageService) {
-        this.applicationMessageService = applicationMessageService;
+    public JwtExceptionHandler(ApplicationMessageService messageService) {
+        this.messageService = messageService;
     }
 }
