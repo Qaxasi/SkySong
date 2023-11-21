2e326a2f05282adaf04e7284a34de2b7d1575f33
Champel Mariusz <champelmariusz@gmail.com>
2023-11-21 23:31:20 +0100

    refactor: Refactor RegisterException to extend ApplicationRuntimeException

--- src/main/java/com/mycompany/SkySong/authentication/exception/RegisterException.java
+++ src/main/java/com/mycompany/SkySong/authentication/exception/RegisterException.java
@@ -1,6 +1,6 @@
 package com.mycompany.SkySong.authentication.exception;
 
-public class RegisterException extends RuntimeException {
+public class RegisterException extends ApplicationRuntimeException {
     public RegisterException(String message) {
         super(message);
     }
