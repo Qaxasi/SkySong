f3540c415512d1f11a5f4f1a112aa64bcf717373
Champel Mariusz <champelmariusz@gmail.com>
2023-11-21 16:01:03 +0100

    feat: Inject dependencies into UserFactoryImpl

--- src/main/java/com/mycompany/SkySong/authentication/service/impl/UserFactoryImpl.java
+++ src/main/java/com/mycompany/SkySong/authentication/service/impl/UserFactoryImpl.java
@@ -5,10 +5,17 @@ import com.mycompany.SkySong.authentication.model.dto.RegisterRequest;
 import com.mycompany.SkySong.authentication.model.entity.Role;
 import com.mycompany.SkySong.authentication.model.entity.User;
 import com.mycompany.SkySong.authentication.service.UserFactory;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserFactoryImpl implements UserFactory {
+    private final PasswordEncoder passwordEncoder;
+    private final ApplicationMessageService messageService;
+    public UserFactoryImpl(PasswordEncoder passwordEncoder, ApplicationMessageService messageService) {
+        this.passwordEncoder = passwordEncoder;
+        this.messageService = messageService;
+    }
     @Override
     public User createUser(RegisterRequest registerRequest, Role role) {
         return null;
