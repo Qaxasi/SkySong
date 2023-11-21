c2ec718730cdff10addad17f2e4aad8bdc1c3ff6
Champel Mariusz <champelmariusz@gmail.com>
2023-11-21 15:53:35 +0100

    feat: Implement UserFactory in UserFactoryImpl

--- src/main/java/com/mycompany/SkySong/authentication/service/impl/UserFactoryImpl.java
+++ src/main/java/com/mycompany/SkySong/authentication/service/impl/UserFactoryImpl.java
@@ -1,4 +1,13 @@
 package com.mycompany.SkySong.authentication.service.impl;
 
-public class UserFactoryImpl {
+import com.mycompany.SkySong.authentication.model.dto.RegisterRequest;
+import com.mycompany.SkySong.authentication.model.entity.Role;
+import com.mycompany.SkySong.authentication.model.entity.User;
+import com.mycompany.SkySong.authentication.service.UserFactory;
+
+public class UserFactoryImpl implements UserFactory {
+    @Override
+    public User createUser(RegisterRequest registerRequest, Role role) {
+        return null;
+    }
 }
