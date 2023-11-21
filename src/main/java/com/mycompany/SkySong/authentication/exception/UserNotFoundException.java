f3da193939f6055f16408b30b7c3042f7a3b2d18
Champel Mariusz <champelmariusz@gmail.com>
2023-11-21 23:32:59 +0100

    refactor: Refactor UserNotFoundException to extend ApplicationRuntimeException

--- src/main/java/com/mycompany/SkySong/authentication/exception/UserNotFoundException.java
+++ src/main/java/com/mycompany/SkySong/authentication/exception/UserNotFoundException.java
@@ -1,6 +1,6 @@
 package com.mycompany.SkySong.authentication.exception;
 
-public class UserNotFoundException extends RuntimeException {
+public class UserNotFoundException extends ApplicationRuntimeException {
     public UserNotFoundException(String message) {
         super(message);
     }
