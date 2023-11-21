427ac5a850d0e2f38a1360a85a180e1eeac099ee
Champel Mariusz <champelmariusz@gmail.com>
2023-11-21 22:31:11 +0100

    feat: Declare DatabaseException in deleteUser method of DeleteUserService interface

--- src/main/java/com/mycompany/SkySong/authentication/service/DeleteUserService.java
+++ src/main/java/com/mycompany/SkySong/authentication/service/DeleteUserService.java
@@ -1,7 +1,8 @@
 package com.mycompany.SkySong.authentication.service;
 
+import com.mycompany.SkySong.authentication.exception.DatabaseException;
 import com.mycompany.SkySong.authentication.model.dto.ApiResponse;
 
 public interface DeleteUserService {
-    ApiResponse deleteUser(long userId);
+    ApiResponse deleteUser(long userId) throws DatabaseException;
 }
