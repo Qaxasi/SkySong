707fbbf9b62c47489071748855947301533c6f5e
Champel Mariusz <champelmariusz@gmail.com>
2023-11-21 18:24:30 +0100

    feat: Inject dependencies into JwtExceptionHandler

--- src/main/java/com/mycompany/SkySong/authentication/secutiry/JwtExceptionHandler.java
+++ src/main/java/com/mycompany/SkySong/authentication/secutiry/JwtExceptionHandler.java
@@ -1,4 +1,11 @@
 package com.mycompany.SkySong.authentication.secutiry;
 
+import com.mycompany.SkySong.authentication.service.impl.ApplicationMessageService;
+
 public class JwtExceptionHandler {
+    private final ApplicationMessageService applicationMessageService;
+
+    public JwtExceptionHandler(ApplicationMessageService applicationMessageService) {
+        this.applicationMessageService = applicationMessageService;
+    }
 }
