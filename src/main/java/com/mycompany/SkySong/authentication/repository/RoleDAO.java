bc56156075c30c462a82ec1457f4815e51320f1f
Champel Mariusz <champelmariusz@gmail.com>
2023-11-22 19:50:39 +0100

    feat: Annotate RoleDAO interface with @Repository

--- src/main/java/com/mycompany/SkySong/authentication/repository/RoleDAO.java
+++ src/main/java/com/mycompany/SkySong/authentication/repository/RoleDAO.java
@@ -3,9 +3,10 @@ package com.mycompany.SkySong.authentication.repository;
 import com.mycompany.SkySong.authentication.model.entity.Role;
 import com.mycompany.SkySong.authentication.model.entity.UserRole;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
-
+@Repository
 public interface RoleDAO extends JpaRepository<Role, Long> {
     Optional<Role> findByName(UserRole roleName);
 }
