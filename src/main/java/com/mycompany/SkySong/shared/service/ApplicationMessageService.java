cb9fa4e8324f4276cc8b34b338f13d89337c0d55
Champel Mariusz <champelmariusz@gmail.com>
2023-11-22 23:34:19 +0100

    refactor: Rename interface

--- src/main/java/com/mycompany/SkySong/shared/service/ApplicationMessageService.java
+++ src/main/java/com/mycompany/SkySong/shared/service/ApplicationMessageService.java
@@ -1,6 +1,6 @@
 package com.mycompany.SkySong.shared.service;
 
-public interface MessageService {
+public interface ApplicationMessageService {
     String getMessage(String key);
     String getMessage(String key, Object...params);
 }
