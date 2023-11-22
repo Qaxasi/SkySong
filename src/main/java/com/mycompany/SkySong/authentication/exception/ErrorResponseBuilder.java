8e1596e6d42de26306afcb4e0850e5d6f712f44a
Champel Mariusz <champelmariusz@gmail.com>
2023-11-22 13:05:28 +0100

    feat: Add createValidationErrorResponse method to ErrorResponseBuilder

--- src/main/java/com/mycompany/SkySong/authentication/exception/ErrorResponseBuilder.java
+++ src/main/java/com/mycompany/SkySong/authentication/exception/ErrorResponseBuilder.java
@@ -12,4 +12,9 @@ public class ErrorResponseBuilder {
         response.put("error", errorMessage);
         return new ResponseEntity<>(response, status);
     }
+    public static ResponseEntity<Object> createValidationErrorResponse(Map<String, String> errors, HttpStatus status) {
+        Map<String, Object> response = new HashMap<>();
+        response.put("errors", errors);
+        return new ResponseEntity<>(response, status);
+    }
 }
