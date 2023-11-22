c3fe23a4a5cb978f26d657646e273254ec9b7255
Champel Mariusz <champelmariusz@gmail.com>
2023-11-23 00:31:07 +0100

    refactor: Refactor UserRoleManagerImpl to use ApplicationMessageService interface

--- src/main/java/com/mycompany/SkySong/auth/service/UserRoleManagerImpl.java
+++ src/main/java/com/mycompany/SkySong/auth/service/UserRoleManagerImpl.java
@@ -4,6 +4,7 @@ import com.mycompany.SkySong.shared.exception.InternalErrorException;
 import com.mycompany.SkySong.auth.model.entity.Role;
 import com.mycompany.SkySong.auth.model.entity.UserRole;
 import com.mycompany.SkySong.auth.repository.RoleDAO;
+import com.mycompany.SkySong.shared.service.ApplicationMessageService;
 import com.mycompany.SkySong.shared.service.ApplicationMessageServiceImpl;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -12,9 +13,9 @@ import org.springframework.stereotype.Service;
 @Slf4j
 public class UserRoleManagerImpl implements UserRoleManager {
     private final RoleDAO roleDAO;
-    private final ApplicationMessageServiceImpl messageService;
+    private final ApplicationMessageService messageService;
 
-    public UserRoleManagerImpl(RoleDAO roleDAO, ApplicationMessageServiceImpl messageService) {
+    public UserRoleManagerImpl(RoleDAO roleDAO, ApplicationMessageService messageService) {
         this.roleDAO = roleDAO;
         this.messageService = messageService;
     }
