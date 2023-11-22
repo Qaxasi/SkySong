54264ae3699b90fbb22b6d2d634f0467b3d0a4a6
Champel Mariusz <champelmariusz@gmail.com>
2023-11-22 19:51:09 +0100

    feat: Annotate UserDAO interface with @Repository

--- src/main/java/com/mycompany/SkySong/authentication/repository/UserDAO.java
+++ src/main/java/com/mycompany/SkySong/authentication/repository/UserDAO.java
@@ -2,9 +2,10 @@ package com.mycompany.SkySong.authentication.repository;
 
 import com.mycompany.SkySong.authentication.model.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
-
+@Repository
 public interface UserDAO extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
     Optional<User> findByUsername(String username);
