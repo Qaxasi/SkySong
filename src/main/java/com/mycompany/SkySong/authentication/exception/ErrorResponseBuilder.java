f68a831a4618db8a84532a0bf14f96bed396fb27
Champel Mariusz <champelmariusz@gmail.com>
2023-11-22 13:02:07 +0100

    feat: Add createErrorResponse method to ErrorResponseBuilder

--- src/main/java/com/mycompany/SkySong/authentication/exception/ErrorResponseBuilder.java
+++ src/main/java/com/mycompany/SkySong/authentication/exception/ErrorResponseBuilder.java
@@ -1,4 +1,15 @@
 package com.mycompany.SkySong.authentication.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class ErrorResponseBuilder {
+    public static ResponseEntity<Object> createErrorResponse(String errorMessage, HttpStatus status) {
+        Map<String, Object> response = new HashMap<>();
+        response.put("error", errorMessage);
+        return new ResponseEntity<>(response, status);
+    }
 }
