10cbe04d47b42a9bd7331783992bf860ee5f4f22
Champel Mariusz <champelmariusz@gmail.com>
2023-11-23 18:05:17 +0100

    refactor: Rename interface

--- src/main/java/com/mycompany/SkySong/auth/security/CookieAdder.java
+++ src/main/java/com/mycompany/SkySong/auth/security/CookieAdder.java
@@ -2,6 +2,6 @@ package com.mycompany.SkySong.auth.security;
 
 import jakarta.servlet.http.HttpServletResponse;
 
-public interface AddCookie {
+public interface CookieAdder {
     void addCookie(HttpServletResponse response, String name, String value, int maxAge);
 }
