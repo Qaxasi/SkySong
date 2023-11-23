d2d25591f18d2bba344e1c10d7ee5419ca82b306
Champel Mariusz <champelmariusz@gmail.com>
2023-11-23 19:11:44 +0100

    test: Add mocks to CookieAdder test

--- src/test/java/com/mycompany/SkySong/auth/security/CookieAdderTest.java
+++ src/test/java/com/mycompany/SkySong/auth/security/CookieAdderTest.java
@@ -1,8 +1,15 @@
 package com.mycompany.SkySong.auth.security;
 
+import jakarta.servlet.http.HttpServletResponse;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
 public class CookieAdderTest {
+    @InjectMocks
+    private CookieAdder cookieAdder;
+    @Mock
+    private HttpServletResponse response;
 }
