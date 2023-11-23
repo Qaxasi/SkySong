62b9a45cf8dec72f7c8557953dcffab4f4b1b543
Champel Mariusz <champelmariusz@gmail.com>
2023-11-23 14:17:37 +0100

    test: Add mocked dependencies into CookieServiceImplTest

--- src/test/java/com/mycompany/SkySong/auth/security/CookieServiceImplTest.java
+++ src/test/java/com/mycompany/SkySong/auth/security/CookieServiceImplTest.java
@@ -1,8 +1,15 @@
 package com.mycompany.SkySong.auth.security;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
 public class CookieServiceImplTest {
+    @InjectMocks
+    private CookieServiceImpl cookieService;
+    @Mock
+    private HttpServletRequest request;
 }
