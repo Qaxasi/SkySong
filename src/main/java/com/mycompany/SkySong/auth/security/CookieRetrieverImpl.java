77f02edbc0eceaea9f54c805d80c5d3b4b47e553
Champel Mariusz <champelmariusz@gmail.com>
2023-11-23 18:19:10 +0100

    feat: Implement CookieRetriever in CookieRetrieverImpl class

--- src/main/java/com/mycompany/SkySong/auth/security/CookieRetrieverImpl.java
+++ src/main/java/com/mycompany/SkySong/auth/security/CookieRetrieverImpl.java
@@ -1,4 +1,13 @@
 package com.mycompany.SkySong.auth.security;
 
-class CookieRetrieverImpl {
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+
+import java.util.Optional;
+
+class CookieRetrieverImpl implements CookieRetriever {
+    @Override
+    public Optional<Cookie> getCookie(HttpServletRequest request, String name) {
+        return Optional.empty();
+    }
 }
