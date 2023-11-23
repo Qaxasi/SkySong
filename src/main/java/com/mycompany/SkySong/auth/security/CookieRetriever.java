eacf89ed64e43fe7355d42eae75dde8108019903
Champel Mariusz <champelmariusz@gmail.com>
2023-11-23 18:17:00 +0100

    feat: Add getCookie method to CookieRetriever interface

--- src/main/java/com/mycompany/SkySong/auth/security/CookieRetriever.java
+++ src/main/java/com/mycompany/SkySong/auth/security/CookieRetriever.java
@@ -1,4 +1,10 @@
 package com.mycompany.SkySong.auth.security;
 
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+
+import java.util.Optional;
+
 public interface CookieRetriever {
+    Optional<Cookie> getCookie(HttpServletRequest request, String name);
 }
