ea65029fbdd97a47ce539987c7fdb3a7160b95b3
Champel Mariusz <champelmariusz@gmail.com>
2023-11-23 18:25:44 +0100

    feat: Annotate CookieDeleterImpl class as a spring service

--- src/main/java/com/mycompany/SkySong/auth/security/CookieDeleterImpl.java
+++ src/main/java/com/mycompany/SkySong/auth/security/CookieDeleterImpl.java
@@ -2,7 +2,9 @@ package com.mycompany.SkySong.auth.security;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Service;
 
+@Service
 public class CookieDeleterImpl implements CookieDeleter {
     @Override
     public void deleteCookie(HttpServletRequest request, HttpServletResponse response, String name) {
