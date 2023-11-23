8eb93a701931c82f32c718a1c91b116a8622f39a
Champel Mariusz <champelmariusz@gmail.com>
2023-11-23 18:33:24 +0100

    refactor: Refactor LogoutController to use CookieDeleter

--- src/main/java/com/mycompany/SkySong/auth/controller/LogoutController.java
+++ src/main/java/com/mycompany/SkySong/auth/controller/LogoutController.java
@@ -1,5 +1,6 @@
 package com.mycompany.SkySong.auth.controller;
 
+import com.mycompany.SkySong.auth.security.CookieDeleter;
 import com.mycompany.SkySong.shared.exception.InternalErrorException;
 import com.mycompany.SkySong.auth.security.CookieService;
 import com.mycompany.SkySong.shared.service.ApplicationMessageService;
@@ -14,16 +15,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/users")
 public class LogoutController {
-    private final CookieService cookieService;
+    private final CookieDeleter cookieDeleter;
     private final ApplicationMessageService messageService;
-    public LogoutController(CookieService cookieService, ApplicationMessageService messageService) {
-        this.cookieService = cookieService;
+    public LogoutController(CookieDeleter cookieDeleter, ApplicationMessageService messageService) {
+        this.cookieDeleter = cookieDeleter;
         this.messageService = messageService;
     }
     @PostMapping("/logout")
     public ResponseEntity<?> logout(HttpServletRequest request, HttpServletResponse response) {
         try {
-            cookieService.deleteCookie(request, response, "auth_token");
+            cookieDeleter.deleteCookie(request, response, "auth_token");
             return ResponseEntity.ok(messageService.getMessage("logout.success"));
         } catch (RuntimeException e) {
             log.error("Error while logging out: " + e.getMessage(), e);
