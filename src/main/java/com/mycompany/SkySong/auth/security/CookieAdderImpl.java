90e7a0274ffefe4c974dc8f11dc2a5b5ac9cd7f4
Champel Mariusz <champelmariusz@gmail.com>
2023-11-23 18:10:01 +0100

    feat: Implement addCookie method in CookieAdderImpl

--- src/main/java/com/mycompany/SkySong/auth/security/CookieAdderImpl.java
+++ src/main/java/com/mycompany/SkySong/auth/security/CookieAdderImpl.java
@@ -1,5 +1,6 @@
 package com.mycompany.SkySong.auth.security;
 
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Service;
 
@@ -7,6 +8,11 @@ import org.springframework.stereotype.Service;
 public class CookieAdderImpl implements CookieAdder {
     @Override
     public void addCookie(HttpServletResponse response, String name, String value, int maxAge) {
-
+        Cookie cookie = new Cookie(name, value);
+        cookie.setPath("/");
+        cookie.setHttpOnly(true);
+        cookie.setSecure(true);
+        cookie.setMaxAge(maxAge);
+        response.addCookie(cookie);
     }
 }
