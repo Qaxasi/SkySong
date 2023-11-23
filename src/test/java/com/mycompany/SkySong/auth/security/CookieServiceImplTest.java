e73d243bd44a7b389681e67f273e083f9a376bdd
Champel Mariusz <champelmariusz@gmail.com>
2023-11-23 14:28:58 +0100

    test: Add test for no cookie scenario

--- src/test/java/com/mycompany/SkySong/auth/security/CookieServiceImplTest.java
+++ src/test/java/com/mycompany/SkySong/auth/security/CookieServiceImplTest.java
@@ -1,10 +1,17 @@
 package com.mycompany.SkySong.auth.security;
 
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class CookieServiceImplTest {
@@ -12,4 +19,13 @@ public class CookieServiceImplTest {
     private CookieServiceImpl cookieService;
     @Mock
     private HttpServletRequest request;
+
+    @Test
+    void shouldReturnEmptyOptionalWhenNoCookieRequest() {
+        when(request.getCookies()).thenReturn(null);
+
+        Optional<Cookie> result = cookieService.getCookie(request, "testCookie");
+
+        assertEquals(result, Optional.empty());
+    }
 }
