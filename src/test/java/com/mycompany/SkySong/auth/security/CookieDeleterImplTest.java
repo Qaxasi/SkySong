5b4b5e4495d744ae86c7a9d5a183ce9010c9f16c
Champel Mariusz <champelmariusz@gmail.com>
2023-11-23 22:19:30 +0100

    test: Add mocks to CookieDeleterImplTest

--- src/test/java/com/mycompany/SkySong/auth/security/CookieDeleterImplTest.java
+++ src/test/java/com/mycompany/SkySong/auth/security/CookieDeleterImplTest.java
@@ -1,8 +1,20 @@
 package com.mycompany.SkySong.auth.security;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
 public class CookieDeleterImplTest {
+    @InjectMocks
+    private CookieDeleterImpl cookieDeleter;
+    @Mock
+    private HttpServletRequest request;
+    @Mock
+    private HttpServletResponse response;
+
+
 }
