11ca8a965b7df893e51125f97191eed890d9cb14
Champel Mariusz <champelmariusz@gmail.com>
2023-11-23 14:11:56 +0100

    test: Add MockitoExtension to CookieServiceImplTest

--- src/test/java/com/mycompany/SkySong/auth/security/CookieServiceImplTest.java
+++ src/test/java/com/mycompany/SkySong/auth/security/CookieServiceImplTest.java
@@ -1,4 +1,8 @@
 package com.mycompany.SkySong.auth.security;
 
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
 public class CookieServiceImplTest {
 }
