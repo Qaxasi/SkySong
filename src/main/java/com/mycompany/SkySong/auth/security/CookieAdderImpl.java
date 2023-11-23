a79413d77947041fcb340ea24f659b10048717cc
Champel Mariusz <champelmariusz@gmail.com>
2023-11-23 18:07:22 +0100

    feat: Implement CookieAdder in CookieAdderImpl class

--- src/main/java/com/mycompany/SkySong/auth/security/CookieAdderImpl.java
+++ src/main/java/com/mycompany/SkySong/auth/security/CookieAdderImpl.java
@@ -1,4 +1,10 @@
 package com.mycompany.SkySong.auth.security;
 
-public class CookieAdderImpl {
+import jakarta.servlet.http.HttpServletResponse;
+
+public class CookieAdderImpl implements CookieAdder {
+    @Override
+    public void addCookie(HttpServletResponse response, String name, String value, int maxAge) {
+
+    }
 }
