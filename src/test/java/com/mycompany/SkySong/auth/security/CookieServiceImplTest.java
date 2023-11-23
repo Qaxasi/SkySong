192b36a6f9cd3cdff83f2b2718b02c40d5ce84e5
Champel Mariusz <champelmariusz@gmail.com>
2023-11-23 14:58:47 +0100

    test: Add test for retrieving specific cookie

--- src/test/java/com/mycompany/SkySong/auth/security/CookieServiceImplTest.java
+++ src/test/java/com/mycompany/SkySong/auth/security/CookieServiceImplTest.java
@@ -10,7 +10,10 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Optional;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -30,11 +33,23 @@ public class CookieServiceImplTest {
     }
     @Test
     void shouldReturnEmptyOptionalWhenRequestedCookieNotInRequest() {
-        Cookie[] cookies = {new Cookie("differentName", "testValue")};
+        Cookie[] cookies = { new Cookie("differentName", "testValue") };
         when(request.getCookies()).thenReturn(cookies);
 
         Optional<Cookie> result = cookieService.getCookie(request, "testCookie");
 
         assertEquals(result, Optional.empty());
+    }
+    @Test
+    void shouldReturnCookieWhenRequestedCookieInRequest() {
+        Cookie expectedCookie = new Cookie("testCookie", "testValue");
+        Cookie[] cookies = { expectedCookie, new Cookie("differentName", "differentValue") };
+
+        when(request.getCookies()).thenReturn(cookies);
+
+        Optional<Cookie> result = cookieService.getCookie(request, "testCookie");
+
+        assertTrue(result.isPresent());
+        assertThat(result.get(), is(expectedCookie));
     }
 }
