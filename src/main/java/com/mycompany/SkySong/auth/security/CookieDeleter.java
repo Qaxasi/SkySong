c2be2c9790edcbbe18bb7be29143f3206ef0b625
Champel Mariusz <champelmariusz@gmail.com>
2023-11-23 18:23:11 +0100

    feat: Add deleteCookie method to CookieDeleter interface

--- src/main/java/com/mycompany/SkySong/auth/security/CookieDeleter.java
+++ src/main/java/com/mycompany/SkySong/auth/security/CookieDeleter.java
@@ -1,4 +1,8 @@
 package com.mycompany.SkySong.auth.security;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
 public interface CookieDeleter {
+    void deleteCookie(HttpServletRequest request, HttpServletResponse response, String name);
 }
