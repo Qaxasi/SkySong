0db90f15f9e446a66d9945154412d35f91563e21
Champel Mariusz <champelmariusz@gmail.com>
2023-11-23 18:02:48 +0100

    feat: Add addCookie method to AddCookie interface

--- src/main/java/com/mycompany/SkySong/auth/security/AddCookie.java
+++ src/main/java/com/mycompany/SkySong/auth/security/AddCookie.java
@@ -1,4 +1,7 @@
 package com.mycompany.SkySong.auth.security;
 
+import jakarta.servlet.http.HttpServletResponse;
+
 public interface AddCookie {
+    void addCookie(HttpServletResponse response, String name, String value, int maxAge);
 }
