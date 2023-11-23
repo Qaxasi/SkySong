2f28f19b3ab804e58d0e719309dc21b951ea7373
Champel Mariusz <champelmariusz@gmail.com>
2023-11-23 18:19:41 +0100

    feat: Annotate CookieRetrieverImpl class as a spring service

--- src/main/java/com/mycompany/SkySong/auth/security/CookieRetrieverImpl.java
+++ src/main/java/com/mycompany/SkySong/auth/security/CookieRetrieverImpl.java
@@ -2,9 +2,10 @@ package com.mycompany.SkySong.auth.security;
 
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.stereotype.Service;
 
 import java.util.Optional;
-
+@Service
 class CookieRetrieverImpl implements CookieRetriever {
     @Override
     public Optional<Cookie> getCookie(HttpServletRequest request, String name) {
