74f59cd3c649babbc1fd5a817d71675a911d2fc1
Champel Mariusz <champelmariusz@gmail.com>
2023-11-23 18:08:10 +0100

    feat: Annotate CookieAdderImpl class as a spring service

--- src/main/java/com/mycompany/SkySong/auth/security/CookieAdderImpl.java
+++ src/main/java/com/mycompany/SkySong/auth/security/CookieAdderImpl.java
@@ -1,7 +1,9 @@
 package com.mycompany.SkySong.auth.security;
 
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Service;
 
+@Service
 public class CookieAdderImpl implements CookieAdder {
     @Override
     public void addCookie(HttpServletResponse response, String name, String value, int maxAge) {
