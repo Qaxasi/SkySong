4bb45258da9572c0f71a1bcedd77c78e5ff27a46
Champel Mariusz <champelmariusz@gmail.com>
2023-11-23 19:38:26 +0100

    test: Replace CookieAdder with CookieAdderImpl

--- src/test/java/com/mycompany/SkySong/auth/security/CookieAdderTest.java
+++ src/test/java/com/mycompany/SkySong/auth/security/CookieAdderTest.java
@@ -1,15 +1,21 @@
 package com.mycompany.SkySong.auth.security;
 
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletResponse;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.verify;
+
 @ExtendWith(MockitoExtension.class)
 public class CookieAdderTest {
     @InjectMocks
-    private CookieAdder cookieAdder;
+    private CookieAdderImpl cookieAdder;
     @Mock
     private HttpServletResponse response;
 }
