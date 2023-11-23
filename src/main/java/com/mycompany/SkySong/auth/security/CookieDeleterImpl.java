539bb97d2a23e480945f84d88bb7076e14f84515
Champel Mariusz <champelmariusz@gmail.com>
2023-11-23 18:25:12 +0100

    feat: Implement CookieDeleter in CookieDeleterImpl class

--- src/main/java/com/mycompany/SkySong/auth/security/CookieDeleterImpl.java
+++ src/main/java/com/mycompany/SkySong/auth/security/CookieDeleterImpl.java
@@ -1,4 +1,11 @@
 package com.mycompany.SkySong.auth.security;
 
-public class CookieDeleterImpl {
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class CookieDeleterImpl implements CookieDeleter {
+    @Override
+    public void deleteCookie(HttpServletRequest request, HttpServletResponse response, String name) {
+
+    }
 }
