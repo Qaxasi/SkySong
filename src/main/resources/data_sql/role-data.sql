d85f8169ba0fbcd5218a68e4595c94fb71c86312
Champel Mariusz <champelmariusz@gmail.com>
2023-11-24 17:04:59 +0100

    refactor: Refactor update user_roles insertion to dynamically link users and roles

--- src/main/resources/data_sql/role-data.sql
+++ src/main/resources/data_sql/role-data.sql
@@ -4,7 +4,17 @@ INSERT INTO roles (name)
     VALUES('ROLE_USER'),
           ('ROLE_ADMIN');
 
-INSERT INTO user_roles (user_id, role_id) VALUES(1, 1);
+INSERT INTO user_roles (user_id, role_id)
+SELECT u.id, r.id
+FROM users u, roles r
+WHERE u.username = 'testUsername' AND r.name = 'ROLE_USER';
 
-INSERT INTO user_roles (user_id, role_id) VALUES(2, 1);
-INSERT INTO user_roles (user_id, role_id) VALUES(2, 2);
+INSERT INTO user_roles (user_id, role_id)
+SELECT u.id, r.id
+FROM users u, roles r
+WHERE u.username = 'testAdmin' AND r.name = 'ROLE_USER';
+
+INSERT INTO user_roles (user_id, role_id)
+SELECT u.id, r.id
+FROM users u, roles r
+WHERE u.username = 'testAdmin' AND r.name = 'ROLE_ADMIN';
