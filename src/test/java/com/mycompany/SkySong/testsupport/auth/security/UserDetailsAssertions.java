aa64a1169f03629086d9a8fb955db8b30ded6d93
Champel Mariusz <champelmariusz@gmail.com>
2023-11-24 18:51:39 +0100

    test: Add hasAuthority method to UserDetailsAssertions

--- src/test/java/com/mycompany/SkySong/testsupport/auth/security/UserDetailsAssertions.java
+++ src/test/java/com/mycompany/SkySong/testsupport/auth/security/UserDetailsAssertions.java
@@ -1,4 +1,11 @@
 package com.mycompany.SkySong.testsupport.auth.security;
 
+import org.springframework.security.core.userdetails.UserDetails;
+
 public class UserDetailsAssertions {
+    public static boolean hasAuthority(UserDetails userDetails, String authority) {
+        return userDetails.getAuthorities().stream()
+                .anyMatch(auth -> auth.getAuthority().equals(authority));
+    }
+
 }
