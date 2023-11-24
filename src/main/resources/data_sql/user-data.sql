0b87e44fb017f5636a884e11c5b6fec1d3eb2f70
Champel Mariusz <champelmariusz@gmail.com>
2023-11-24 17:06:51 +0100

    refactor: Delete user_data.sql script

--- src/main/resources/data_sql/user-data.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO users (username, email, password)
-    VALUES('testUsername', 'testEmail@gmail.com',
-     '$2a$10$VEbWwz6NcL4y6MgKEE/sJuWiFe2EoVbru6gJ.6Miu6G16NWfqlxci');
-
-INSERT INTO users (username, email, password)
-    VALUES('testAdmin', 'testAdmin@gmail.com',
-     '$2a$10$VEbWwz6NcL4y6MgKEE/sJuWiFe2EoVbru6gJ.6Miu6G16NWfqlxci');
