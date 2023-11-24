3583782d2ae05f6f2742974c8516cc00bf5590aa
Champel Mariusz <champelmariusz@gmail.com>
2023-11-24 20:26:52 +0100

    test: Add assertPasswordMatches method for password verification

--- src/test/java/com/mycompany/SkySong/testsupport/auth/security/UserDetailsAssertions.java
+++ src/test/java/com/mycompany/SkySong/testsupport/auth/security/UserDetailsAssertions.java
@@ -1,11 +1,18 @@
 package com.mycompany.SkySong.testsupport.auth.security;
 
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class UserDetailsAssertions {
     public static boolean hasAuthority(UserDetails userDetails, String authority) {
         return userDetails.getAuthorities().stream()
                 .anyMatch(auth -> auth.getAuthority().equals(authority));
     }
-
+    public static void assertPasswordMatches(UserDetails userDetails,
+                                             PasswordEncoder passwordEncoder,
+                                             String password) {
+        assertTrue(passwordEncoder.matches(password, userDetails.getPassword()));
+    }
 }
