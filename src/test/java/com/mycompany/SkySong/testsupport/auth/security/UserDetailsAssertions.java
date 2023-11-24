ac7ec896251e4d2540b69783fc400d40312d70b2
Champel Mariusz <champelmariusz@gmail.com>
2023-11-24 21:18:26 +0100

    test: Refactor hasAuthority method to support multiple authorities

--- src/test/java/com/mycompany/SkySong/testsupport/auth/security/UserDetailsAssertions.java
+++ src/test/java/com/mycompany/SkySong/testsupport/auth/security/UserDetailsAssertions.java
@@ -1,15 +1,24 @@
 package com.mycompany.SkySong.testsupport.auth.security;
 
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class UserDetailsAssertions {
-    public static boolean hasAuthority(UserDetails userDetails, String authority) {
+    public static boolean hasAuthority(UserDetails userDetails, String... authority) {
+        Set<String> requiredAuthorities = new HashSet<>(Arrays.asList(authority));
         return userDetails.getAuthorities().stream()
-                .anyMatch(auth -> auth.getAuthority().equals(authority));
+                .map(GrantedAuthority::getAuthority)
+                .collect(Collectors.toSet())
+                .containsAll(requiredAuthorities);
     }
     public static void assertUserDetails(UserDetails userDetails,
                                              PasswordEncoder passwordEncoder,
