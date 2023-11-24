d96fd0bb7135f60d6f136f3938576654b2c77cc0
Champel Mariusz <champelmariusz@gmail.com>
2023-11-24 22:03:57 +0100

    test: Annotate CustomUserDetailsService field with @InjectMocks

--- src/test/java/com/mycompany/SkySong/auth/security/CustomUserDetailsServiceTest.java
+++ src/test/java/com/mycompany/SkySong/auth/security/CustomUserDetailsServiceTest.java
@@ -5,6 +5,7 @@ import com.mycompany.SkySong.auth.security.CustomUserDetailsService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -16,6 +17,7 @@ import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class CustomUserDetailsServiceTest {
+    @InjectMocks
     private CustomUserDetailsService customUserDetailsService;
     @Mock
     private UserDAO userDAO;
