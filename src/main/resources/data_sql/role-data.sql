8be66ada6b6185189c9446dca910a94a06423d64
Champel Mariusz <champelmariusz@gmail.com>
2023-11-24 17:06:19 +0100

    feat: Add users insertion to initial database setup script

--- src/main/resources/data_sql/role-data.sql
+++ src/main/resources/data_sql/role-data.sql
@@ -1,4 +1,10 @@
+INSERT INTO users (username, email, password)
+    VALUES('testUsername', 'testEmail@gmail.com',
+     '$2a$10$VEbWwz6NcL4y6MgKEE/sJuWiFe2EoVbru6gJ.6Miu6G16NWfqlxci');
 
+INSERT INTO users (username, email, password)
+    VALUES('testAdmin', 'testAdmin@gmail.com',
+     '$2a$10$VEbWwz6NcL4y6MgKEE/sJuWiFe2EoVbru6gJ.6Miu6G16NWfqlxci');
 
 INSERT INTO roles (name)
     VALUES('ROLE_USER'),
