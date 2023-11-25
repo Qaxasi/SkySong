067ffe1bfa78479911a5b7c417f0625d9b7386f1
Champel Mariusz <champelmariusz@gmail.com>
2023-11-25 23:03:08 +0100

    refactor: Remove unused imports

--- src/test/java/com/mycompany/SkySong/auth/security/JwtAuthenticationFilterIntegrationTest.java
+++ src/test/java/com/mycompany/SkySong/auth/security/JwtAuthenticationFilterIntegrationTest.java
@@ -1,9 +1,5 @@
 package com.mycompany.SkySong.auth.security;
 
-import com.mycompany.SkySong.auth.security.CustomUserDetailsService;
-import com.mycompany.SkySong.auth.security.JwtAuthenticationFilter;
-import com.mycompany.SkySong.auth.security.JwtTokenProviderImpl;
-import com.mycompany.SkySong.testsupport.security.JwtAuthenticationFilterUtils;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.Cookie;
