1a8f272e239c6ec396dc4797a65fd9ae309d7080
Champel Mariusz <champelmariusz@gmail.com>
2023-11-26 20:32:21 +0100

    refactor: Handle invalid token case in JwtFilter

--- src/main/java/com/mycompany/SkySong/auth/security/JwtAuthenticationFilter.java
+++ src/main/java/com/mycompany/SkySong/auth/security/JwtAuthenticationFilter.java
@@ -67,8 +67,12 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
             authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
 
             SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+            filterChain.doFilter(request, response);
+        } else {
+            jwtAuthenticationEntryPoint.commence(request, response, new InsufficientAuthenticationException(
+                    messageService.getMessage("unauthorized.token.invalid")
+            ));
         }
-        filterChain.doFilter(request, response);
     }
     @Override
     protected boolean shouldNotFilter(HttpServletRequest request) {
