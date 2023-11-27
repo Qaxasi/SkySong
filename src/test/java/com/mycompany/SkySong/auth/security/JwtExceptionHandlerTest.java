b9bcff0c11a7bf54686f253c712dc8905302cd23
Champel Mariusz <champelmariusz@gmail.com>
2023-11-27 23:42:10 +0100

    test: Add test handling of ExpiredJwtException

--- src/test/java/com/mycompany/SkySong/auth/security/JwtExceptionHandlerTest.java
+++ src/test/java/com/mycompany/SkySong/auth/security/JwtExceptionHandlerTest.java
@@ -1,10 +1,15 @@
 package com.mycompany.SkySong.auth.security;
 
+import com.mycompany.SkySong.shared.exception.TokenException;
 import com.mycompany.SkySong.shared.service.ApplicationMessageService;
+import io.jsonwebtoken.ExpiredJwtException;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @ExtendWith(MockitoExtension.class)
 public class JwtExceptionHandlerTest {
@@ -12,5 +17,12 @@ public class JwtExceptionHandlerTest {
     private JwtExceptionHandler jwtExceptionHandler;
     @Mock
     private ApplicationMessageService messageService;
+
+    @Test
+    void shouldThrowTokenExceptionForExpiredJwtException() {
+        ExpiredJwtException exception = new ExpiredJwtException(null, null, null);
+
+        assertThrows(TokenException.class, () -> jwtExceptionHandler.handleException(exception));
+    }
 
 }
