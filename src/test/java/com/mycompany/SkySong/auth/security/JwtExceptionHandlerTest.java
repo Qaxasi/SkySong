dd4e5d61c630ae41709cc905d722f139eeb4c7ce
Champel Mariusz <champelmariusz@gmail.com>
2023-11-27 23:07:44 +0100

    test: Inject mocked dependencies into JwtExceptionHandlerTest

--- src/test/java/com/mycompany/SkySong/auth/security/JwtExceptionHandlerTest.java
+++ src/test/java/com/mycompany/SkySong/auth/security/JwtExceptionHandlerTest.java
@@ -1,10 +1,16 @@
 package com.mycompany.SkySong.auth.security;
 
+import com.mycompany.SkySong.shared.service.ApplicationMessageService;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
 public class JwtExceptionHandlerTest {
-
+    @InjectMocks
+    private JwtExceptionHandler jwtExceptionHandler;
+    @Mock
+    private ApplicationMessageService messageService;
 
 }
