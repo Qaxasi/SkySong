923082dbc718fa56656bb598cd6cc07e12d07048
Champel Mariusz <champelmariusz@gmail.com>
2023-11-29 22:40:32 +0100

    refactor: Delete few tests

--- src/test/java/com/mycompany/SkySong/auth/security/JwtTokenProviderImplTest.java
+++ src/test/java/com/mycompany/SkySong/auth/security/JwtTokenProviderImplTest.java
@@ -74,34 +74,4 @@ public class JwtTokenProviderImplTest {
 
         assertNotNull(claims.getExpiration());
     }
-    @Test
-    void shouldThrowExceptionForExpiredToken() throws InterruptedException {
-        when(mockAuth.getName()).thenReturn("testUser");
-
-        String token = jwtTokenProvider.generateToken(mockAuth);
-        Thread.sleep(1001);
-        assertThrows(TokenException.class, () -> jwtTokenProvider.validateToken(token));
-    }
-
-    @Test
-    void shouldThrowTokenExceptionForMalformedToken() {
-        String malformedToken = "not-valid-token";
-
-        assertThrows(TokenException.class, () -> jwtTokenProvider.validateToken(malformedToken));
-    }
-    @Test
-    void shouldThrowTokenExceptionForEmptyClaims() {
-        String emptyClaimsToken = "";
-
-        assertThrows(TokenException.class, () -> jwtTokenProvider.validateToken(emptyClaimsToken));
-    }
-    @Test
-    void shouldThrowTokenExceptionForInvalidAlgorithm() {
-        String unsupportedToken = Jwts.builder()
-                .setSubject("testUser")
-                .setExpiration(new Date(System.currentTimeMillis() + 100L))
-                .compact();
-
-        assertThrows(TokenException.class, () -> jwtTokenProvider.validateToken(unsupportedToken));
-    }
 }
