61425b93001ab4edf1e5700ed54ce5837dc951b8
Champel Mariusz <champelmariusz@gmail.com>
2023-12-01 00:56:02 +0100

    test: Inject mocked dependencies into CredentialExistenceCheckerImplTest

--- src/test/java/com/mycompany/SkySong/auth/service/CredentialExistenceCheckerImplTest.java
+++ src/test/java/com/mycompany/SkySong/auth/service/CredentialExistenceCheckerImplTest.java
@@ -1,9 +1,18 @@
 package com.mycompany.SkySong.auth.service;
 
+import com.mycompany.SkySong.shared.repository.UserDAO;
+import com.mycompany.SkySong.shared.service.ApplicationMessageService;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-
 
 @ExtendWith(MockitoExtension.class)
 public class CredentialExistenceCheckerImplTest {
+    @InjectMocks
+    private CredentialExistenceCheckerImpl credentialExistenceChecker;
+    @Mock
+    private UserDAO userDAO;
+    @Mock
+    private ApplicationMessageService messageService;
 }
