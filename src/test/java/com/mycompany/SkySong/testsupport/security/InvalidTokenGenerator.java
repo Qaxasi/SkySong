887367acfd0b3dd06cbc9e7897d0acda264f6452
Champel Mariusz <champelmariusz@gmail.com>
2023-11-30 16:50:06 +0100

    test: Add createBaseBuilder for initializing JWT with custom parameters

--- src/test/java/com/mycompany/SkySong/testsupport/security/InvalidTokenGenerator.java
+++ src/test/java/com/mycompany/SkySong/testsupport/security/InvalidTokenGenerator.java
@@ -1,5 +1,6 @@
 package com.mycompany.SkySong.testsupport.security;
 
+import io.jsonwebtoken.JwtBuilder;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
@@ -17,6 +18,12 @@ public class InvalidTokenGenerator {
     private static Date getExpirationDate(boolean expired) {
         Date now = getCurrentDate();
         return new Date(now.getTime() + (expired ? -EXPIRATION_MS : EXPIRATION_MS));
+    }
+    private static JwtBuilder createBaseBuilder(Date now, Date expirationDate, Key key) {
+        return Jwts.builder()
+                .setIssuedAt(now)
+                .setExpiration(expirationDate)
+                .signWith(key);
     }
 
     public static String generateTokenWithoutSubject(Key key) {
