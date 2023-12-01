bb8da40b731b3f9eb9ba528cc6b2c264ecc87426
Champel Mariusz <champelmariusz@gmail.com>
2023-12-01 18:40:37 +0100

    test: Annotate LoginServiceImpl with @InjectMocks

--- src/test/java/com/mycompany/SkySong/auth/service/LoginServiceImplTest.java
+++ src/test/java/com/mycompany/SkySong/auth/service/LoginServiceImplTest.java
@@ -6,6 +6,7 @@ import com.mycompany.SkySong.shared.service.ApplicationMessageService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -19,6 +20,7 @@ import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class LoginServiceImplTest {
+    @InjectMocks
     private LoginServiceImpl loginService;
     @Mock
     private JwtTokenProvider jwtTokenProvider;
