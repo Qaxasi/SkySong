05493052420c57f5bb5fed70f96d9cf0f0e5b71c
Champel Mariusz <champelmariusz@gmail.com>
2023-12-01 17:26:29 +0100

    test: Remove unnecessary throws clause from cleanup method

--- src/test/java/com/mycompany/SkySong/auth/service/LoginServiceImplIntegrationTest.java
+++ src/test/java/com/mycompany/SkySong/auth/service/LoginServiceImplIntegrationTest.java
@@ -44,7 +44,7 @@ public class LoginServiceImplIntegrationTest {
         }
     }
     @AfterEach
-    void cleanup() throws Exception {
+    void cleanup() {
         JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
         jdbcTemplate.update("DELETE FROM user_roles");
         jdbcTemplate.update("DELETE FROM users");
