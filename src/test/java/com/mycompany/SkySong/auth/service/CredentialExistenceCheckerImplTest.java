0fbd5627e8497dd82cc192615b6aa4d80a7882ef
Champel Mariusz <champelmariusz@gmail.com>
2023-12-01 01:02:38 +0100

    test: Add test for existing username

--- src/test/java/com/mycompany/SkySong/auth/service/CredentialExistenceCheckerImplTest.java
+++ src/test/java/com/mycompany/SkySong/auth/service/CredentialExistenceCheckerImplTest.java
@@ -1,11 +1,17 @@
 package com.mycompany.SkySong.auth.service;
 
+import com.mycompany.SkySong.auth.model.dto.RegisterRequest;
+import com.mycompany.SkySong.shared.exception.RegisterException;
 import com.mycompany.SkySong.shared.repository.UserDAO;
 import com.mycompany.SkySong.shared.service.ApplicationMessageService;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class CredentialExistenceCheckerImplTest {
@@ -15,4 +21,14 @@ public class CredentialExistenceCheckerImplTest {
     private UserDAO userDAO;
     @Mock
     private ApplicationMessageService messageService;
+    @Test
+    void shouldThrowRegisterExceptionIfUsernameExists() {
+        RegisterRequest registerRequest = new RegisterRequest(
+                "testUsername", "testUniqueEmail@gmail.com", "testPassword@123");
+
+        when(userDAO.existsByUsername("testUsername")).thenThrow(new RegisterException("Username exist!"));
+
+        assertThrows(RegisterException.class,
+                () -> credentialExistenceChecker.checkForExistingCredentials(registerRequest));
+    }
 }
