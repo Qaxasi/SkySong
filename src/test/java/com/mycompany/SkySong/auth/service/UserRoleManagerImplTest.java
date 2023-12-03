6d58328173e60c5b60961a0efaa1e98e6a74f4e1
Champel Mariusz <champelmariusz@gmail.com>
2023-12-03 21:37:49 +0100

    test: Inject mocked dependencies into UserRoleManagerImplTest

--- src/test/java/com/mycompany/SkySong/auth/service/UserRoleManagerImplTest.java
+++ src/test/java/com/mycompany/SkySong/auth/service/UserRoleManagerImplTest.java
@@ -1,8 +1,19 @@
 package com.mycompany.SkySong.auth.service;
 
+import com.mycompany.SkySong.auth.repository.RoleDAO;
+import com.mycompany.SkySong.shared.service.ApplicationMessageService;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
 public class UserRoleManagerImplTest {
+    @InjectMocks
+    private UserRoleManagerImplTest userRoleManagerImplTest;
+    @Mock
+    private RoleDAO roleDAO;
+    @Mock
+    private ApplicationMessageService messageService;
+
 }
