d64a248aef52302d034a9da6ab840e3b8d4a318b
Champel Mariusz <champelmariusz@gmail.com>
2023-12-03 15:14:06 +0100

    test: Rename test class

--- src/test/java/com/mycompany/SkySong/auth/service/ValidationServiceImplTest.java
+++ src/test/java/com/mycompany/SkySong/auth/service/ValidationServiceImplTest.java
@@ -14,7 +14,7 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
-public class ValidationServiceTest {
+public class ValidationServiceImplTest {
     @InjectMocks
     private ValidationServiceImpl validationService;
     @Mock
