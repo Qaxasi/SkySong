b7ea536ddf3387c8ddace0499c0874a076a43595
Champel Mariusz <champelmariusz@gmail.com>
2023-12-03 15:13:22 +0100

    refactor: Rename test class

--- src/test/java/com/mycompany/SkySong/auth/service/LoginServiceIntegrationTest.java
+++ src/test/java/com/mycompany/SkySong/auth/service/LoginServiceIntegrationTest.java
@@ -23,7 +23,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("test")
-public class LoginServiceImplIntegrationTest {
+public class LoginServiceIntegrationTest {
     @Autowired
     private LoginService loginService;
     @Autowired
