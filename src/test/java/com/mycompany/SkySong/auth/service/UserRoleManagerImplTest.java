5a291f94d94b1d6fa973fff6399a0d660a6d8b43
Champel Mariusz <champelmariusz@gmail.com>
2023-12-03 22:47:22 +0100

    test: Add test to verify exception when role not found

--- src/test/java/com/mycompany/SkySong/auth/service/UserRoleManagerImplTest.java
+++ src/test/java/com/mycompany/SkySong/auth/service/UserRoleManagerImplTest.java
@@ -1,11 +1,22 @@
 package com.mycompany.SkySong.auth.service;
 
+import com.mycompany.SkySong.auth.model.entity.Role;
+import com.mycompany.SkySong.auth.model.entity.UserRole;
 import com.mycompany.SkySong.auth.repository.RoleDAO;
+import com.mycompany.SkySong.shared.exception.InternalErrorException;
 import com.mycompany.SkySong.shared.service.ApplicationMessageService;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class UserRoleManagerImplTest {
@@ -15,5 +26,10 @@ public class UserRoleManagerImplTest {
     private RoleDAO roleDAO;
     @Mock
     private ApplicationMessageService messageService;
+    @Test
+    void shouldThrowExceptionWhenRoleNotExist() {
+        when(roleDAO.findByName(any())).thenReturn(Optional.empty());
 
+        assertThrows(InternalErrorException.class, () -> userRoleManagerImpl.getRoleByName(any()));
+    }
 }
