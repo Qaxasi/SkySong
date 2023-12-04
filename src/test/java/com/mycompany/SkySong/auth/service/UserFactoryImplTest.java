c2b8fff781ecd95793deadeb72380b2bb1000063
Champel Mariusz <champelmariusz@gmail.com>
2023-12-04 13:13:36 +0100

    test: Add test for user creation with details

--- src/test/java/com/mycompany/SkySong/auth/service/UserFactoryImplTest.java
+++ src/test/java/com/mycompany/SkySong/auth/service/UserFactoryImplTest.java
@@ -1,11 +1,20 @@
 package com.mycompany.SkySong.auth.service;
 
+import com.mycompany.SkySong.auth.model.dto.RegisterRequest;
+import com.mycompany.SkySong.auth.model.entity.Role;
+import com.mycompany.SkySong.auth.model.entity.UserRole;
+import com.mycompany.SkySong.shared.entity.User;
 import com.mycompany.SkySong.shared.service.ApplicationMessageService;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class UserFactoryImplTest {
@@ -15,4 +24,24 @@ public class UserFactoryImplTest {
     private PasswordEncoder passwordEncoder;
     @Mock
     private ApplicationMessageService messageService;
+
+    @Test
+    void shouldCreateUserWithGivenDetails() {
+        String username = "testUsername";
+        String email = "testEmail@gmail.com";
+        String password = "testPassword@123";
+        String encodedPassword = "encodedPassword";
+        Role role = new Role(UserRole.ROLE_USER);
+
+        RegisterRequest registerRequest = new RegisterRequest(username, email, password);
+
+        when(passwordEncoder.encode(password)).thenReturn(encodedPassword);
+
+        User user = userFactory.createUser(registerRequest, role);
+
+        assertEquals(user.getUsername(), registerRequest.username());
+        assertEquals(user.getEmail(), registerRequest.email());
+        assertEquals(user.getPassword(), encodedPassword);
+        assertTrue(user.getRoles().contains(role));
+    }
 }
