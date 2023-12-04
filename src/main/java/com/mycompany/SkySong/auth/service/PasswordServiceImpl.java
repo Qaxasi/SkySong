2355edd53ac844729ceff020a2aef3982697dcfe
Champel Mariusz <champelmariusz@gmail.com>
2023-12-05 00:47:45 +0100

    test: Add logging

--- src/main/java/com/mycompany/SkySong/auth/service/PasswordServiceImpl.java
+++ src/main/java/com/mycompany/SkySong/auth/service/PasswordServiceImpl.java
@@ -1,9 +1,11 @@
 package com.mycompany.SkySong.auth.service;
 
 import com.mycompany.SkySong.shared.service.ApplicationMessageService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 @Service
+@Slf4j
 public class PasswordServiceImpl implements PasswordService {
     private final PasswordEncoder passwordEncoder;
     private final ApplicationMessageService messageService;
