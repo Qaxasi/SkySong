c3cf13bf11343dd96ba95a785b039ed33c805d7a
Champel Mariusz <champelmariusz@gmail.com>
2023-12-04 12:42:07 +0100

    test: Inject mocked dependencies into UserFactoryImplTest

--- src/test/java/com/mycompany/SkySong/auth/service/UserFactoryImplTest.java
+++ src/test/java/com/mycompany/SkySong/auth/service/UserFactoryImplTest.java
@@ -1,8 +1,18 @@
 package com.mycompany.SkySong.auth.service;
 
+import com.mycompany.SkySong.shared.service.ApplicationMessageService;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @ExtendWith(MockitoExtension.class)
 public class UserFactoryImplTest {
+    @InjectMocks
+    private UserFactoryImpl userFactory;
+    @Mock
+    private PasswordEncoder passwordEncoder;
+    @Mock
+    private ApplicationMessageService messageService;
 }
