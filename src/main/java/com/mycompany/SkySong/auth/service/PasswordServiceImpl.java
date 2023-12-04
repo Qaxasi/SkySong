4270fcbef1e7718fab272db119079977f9b97bcf
Champel Mariusz <champelmariusz@gmail.com>
2023-12-05 00:46:39 +0100

    test: Inject ApplicationMessageService into PasswordServiceImpl

--- src/main/java/com/mycompany/SkySong/auth/service/PasswordServiceImpl.java
+++ src/main/java/com/mycompany/SkySong/auth/service/PasswordServiceImpl.java
@@ -1,15 +1,16 @@
 package com.mycompany.SkySong.auth.service;
 
+import com.mycompany.SkySong.shared.service.ApplicationMessageService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 @Service
 public class PasswordServiceImpl implements PasswordService {
     private final PasswordEncoder passwordEncoder;
-
-    public PasswordServiceImpl(PasswordEncoder passwordEncoder) {
+    private final ApplicationMessageService messageService;
+    public PasswordServiceImpl(PasswordEncoder passwordEncoder, ApplicationMessageService messageService) {
         this.passwordEncoder = passwordEncoder;
+        this.messageService = messageService;
     }
-
     @Override
     public String encodePassword(String password) {
         return null;
