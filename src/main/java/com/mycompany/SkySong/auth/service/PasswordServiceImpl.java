6d7bb8f5b7ae0c876ca5f685ace90ff434ebcc4e
Champel Mariusz <champelmariusz@gmail.com>
2023-12-05 00:55:50 +0100

    feat: Implement password encoding method

--- src/main/java/com/mycompany/SkySong/auth/service/PasswordServiceImpl.java
+++ src/main/java/com/mycompany/SkySong/auth/service/PasswordServiceImpl.java
@@ -1,5 +1,6 @@
 package com.mycompany.SkySong.auth.service;
 
+import com.mycompany.SkySong.shared.exception.InternalErrorException;
 import com.mycompany.SkySong.shared.service.ApplicationMessageService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -9,12 +10,18 @@ import org.springframework.stereotype.Service;
 public class PasswordServiceImpl implements PasswordService {
     private final PasswordEncoder passwordEncoder;
     private final ApplicationMessageService messageService;
+
     public PasswordServiceImpl(PasswordEncoder passwordEncoder, ApplicationMessageService messageService) {
         this.passwordEncoder = passwordEncoder;
         this.messageService = messageService;
     }
     @Override
     public String encodePassword(String password) {
-        return null;
+        try {
+            return passwordEncoder.encode(password);
+        } catch (IllegalArgumentException ex) {
+            log.error("Error encoding the password: " + ex.getMessage(), ex);
+            throw new InternalErrorException(messageService.getMessage("password.encoding.error"));
+        }
     }
 }
