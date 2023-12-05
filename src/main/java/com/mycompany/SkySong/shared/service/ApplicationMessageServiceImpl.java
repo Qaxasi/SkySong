ebe6faf0f4692247e31e3aef3f9d8b003aae82a2
Champel Mariusz <champelmariusz@gmail.com>
2023-12-05 23:33:49 +0100

    refactor: Refactor ApplicationMessageServiceImpl to use constructor-based properties loading

--- src/main/java/com/mycompany/SkySong/shared/service/ApplicationMessageServiceImpl.java
+++ src/main/java/com/mycompany/SkySong/shared/service/ApplicationMessageServiceImpl.java
@@ -3,6 +3,7 @@ package com.mycompany.SkySong.shared.service;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.text.MessageFormat;
 import java.util.Properties;
 @Service
@@ -10,8 +11,15 @@ class ApplicationMessageServiceImpl implements ApplicationMessageService {
     private final Properties messages = new Properties();
 
     public ApplicationMessageServiceImpl() {
+        this(ApplicationMessageServiceImpl.class.getClassLoader().getResourceAsStream("messages.properties"));
+    }
+    ApplicationMessageServiceImpl(InputStream propertiesStream) {
         try {
-            messages.load(getClass().getClassLoader().getResourceAsStream("messages.properties"));
+            if (propertiesStream != null) {
+                messages.load(propertiesStream);
+            } else {
+                throw new IOException("Properties file is null  ");
+            }
         } catch (IOException e) {
             throw new RuntimeException("Failed to load messages.properties");
         }
