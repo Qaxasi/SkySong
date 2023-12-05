de34c3d583e098d75ef7bfadbb20fb9a88e07f75
Champel Mariusz <champelmariusz@gmail.com>
2023-12-05 17:37:27 +0100

    test: Add test for successful password encoding

--- src/test/java/com/mycompany/SkySong/auth/service/PasswordServiceImplIntegrationTest.java
+++ src/test/java/com/mycompany/SkySong/auth/service/PasswordServiceImplIntegrationTest.java
@@ -1,9 +1,12 @@
 package com.mycompany.SkySong.auth.service;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 @Profile("test")
@@ -11,4 +14,12 @@ public class PasswordServiceImplIntegrationTest {
     @Autowired
     private PasswordService passwordService;
     private PasswordEncoder passwordEncoder;
+    @Test
+    void shouldEncodePasswordSuccessfully() {
+        String password = "testPassword";
+
+        String encodedPassword = passwordService.encodePassword(password);
+
+        assertTrue(passwordEncoder.matches(password, encodedPassword));
+    }
 }
