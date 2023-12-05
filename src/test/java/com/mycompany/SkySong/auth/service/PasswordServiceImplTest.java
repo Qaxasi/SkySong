5a7b0a38739245bfd55b531170ff98e37ccc2fbb
Champel Mariusz <champelmariusz@gmail.com>
2023-12-05 16:51:40 +0100

    test: Add test for exception handling with null password

--- src/test/java/com/mycompany/SkySong/auth/service/PasswordServiceImplTest.java
+++ src/test/java/com/mycompany/SkySong/auth/service/PasswordServiceImplTest.java
@@ -1,11 +1,16 @@
 package com.mycompany.SkySong.auth.service;
 
+import com.mycompany.SkySong.shared.exception.InternalErrorException;
 import com.mycompany.SkySong.shared.service.ApplicationMessageService;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class PasswordServiceImplTest {
@@ -15,4 +20,9 @@ public class PasswordServiceImplTest {
     private PasswordEncoder passwordEncoder;
     @Mock
     private ApplicationMessageService messageService;
+    @Test
+    void shouldThrowInternalErrorExceptionWhenPasswordIsNull() {
+        when(passwordEncoder.encode(null)).thenThrow(new IllegalArgumentException("test error"));
+        assertThrows(InternalErrorException.class, () -> passwordService.encodePassword(null));
+    }
 }
