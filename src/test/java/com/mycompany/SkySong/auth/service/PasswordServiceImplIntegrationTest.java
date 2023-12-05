2b95c7ee20a7b6ad5e736083d849aa53998267d5
Champel Mariusz <champelmariusz@gmail.com>
2023-12-05 17:33:51 +0100

    test: Inject dependencies into PasswordServiceImplIntegrationTest

--- src/test/java/com/mycompany/SkySong/auth/service/PasswordServiceImplIntegrationTest.java
+++ src/test/java/com/mycompany/SkySong/auth/service/PasswordServiceImplIntegrationTest.java
@@ -1,9 +1,14 @@
 package com.mycompany.SkySong.auth.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Profile;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootTest
 @Profile("test")
 public class PasswordServiceImplIntegrationTest {
+    @Autowired
+    private PasswordService passwordService;
+    private PasswordEncoder passwordEncoder;
 }
