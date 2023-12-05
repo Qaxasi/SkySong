6d9c972754abe1c38756e6781b24d6340b6d0fda
Champel Mariusz <champelmariusz@gmail.com>
2023-12-05 16:40:19 +0100

    test: Add MockitoExtension to PasswordServiceImplTest for unit test support

--- src/test/java/com/mycompany/SkySong/auth/service/PasswordServiceImplTest.java
+++ src/test/java/com/mycompany/SkySong/auth/service/PasswordServiceImplTest.java
@@ -1,4 +1,8 @@
 package com.mycompany.SkySong.auth.service;
 
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
 public class PasswordServiceImplTest {
 }
