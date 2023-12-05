a9001ea79f1b216d8aaf20c3a20a52358991e638
Champel Mariusz <champelmariusz@gmail.com>
2023-12-05 17:32:43 +0100

    test: Initialize Spring Boot testing environment for PasswordServiceImplIntegrationTest

--- src/test/java/com/mycompany/SkySong/auth/service/PasswordServiceImplIntegrationTest.java
+++ src/test/java/com/mycompany/SkySong/auth/service/PasswordServiceImplIntegrationTest.java
@@ -1,4 +1,9 @@
 package com.mycompany.SkySong.auth.service;
 
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Profile;
+
+@SpringBootTest
+@Profile("test")
 public class PasswordServiceImplIntegrationTest {
 }
