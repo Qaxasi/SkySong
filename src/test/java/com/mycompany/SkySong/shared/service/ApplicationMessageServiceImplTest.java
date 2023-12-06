b74208b22930969af49b12f7cfd43aee79a7a64c
Champel Mariusz <champelmariusz@gmail.com>
2023-12-06 16:33:48 +0100

    test: Add test for exception on missing properties file

--- src/test/java/com/mycompany/SkySong/shared/service/ApplicationMessageServiceImplTest.java
+++ src/test/java/com/mycompany/SkySong/shared/service/ApplicationMessageServiceImplTest.java
@@ -1,9 +1,23 @@
 package com.mycompany.SkySong.shared.service;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.io.InputStream;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @ExtendWith(MockitoExtension.class)
 public class ApplicationMessageServiceImplTest {
 
+    @Test
+    void shouldThrowExceptionWhenPropertiesFileNotFound() {
+        InputStream invalidPropertiesStream = this.getClass().getClassLoader()
+                .getResourceAsStream("nonexistent.properties");
+
+        assertThrows(RuntimeException.class,
+                () -> new ApplicationMessageServiceImpl(invalidPropertiesStream));
+    }
 }
