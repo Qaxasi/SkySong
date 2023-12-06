fa9fc5aa1cc51a19715a58ec3c3ebeed60e8819a
Champel Mariusz <champelmariusz@gmail.com>
2023-12-06 15:55:33 +0100

    test: Inject ApplicationMessageService dependency

--- src/test/java/com/mycompany/SkySong/shared/service/ApplicationMessageServiceIntegrationTest.java
+++ src/test/java/com/mycompany/SkySong/shared/service/ApplicationMessageServiceIntegrationTest.java
@@ -1,8 +1,11 @@
 package com.mycompany.SkySong.shared.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("test")
 public class ApplicationMessageServiceIntegrationTest {
+    @Autowired
+    private ApplicationMessageService messageService;
 }
