0862d2eccc572d2ca8e354e6c3744e4a1e75be0e
Champel Mariusz <champelmariusz@gmail.com>
2023-12-06 15:54:38 +0100

    test: Configure ApplicationMessageServiceIntegrationTest with SpringBootTest and test profile

--- src/test/java/com/mycompany/SkySong/shared/service/ApplicationMessageServiceIntegrationTest.java
+++ src/test/java/com/mycompany/SkySong/shared/service/ApplicationMessageServiceIntegrationTest.java
@@ -1,3 +1,8 @@
 package com.mycompany.SkySong.shared.service;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
 public class ApplicationMessageServiceIntegrationTest {
 }
