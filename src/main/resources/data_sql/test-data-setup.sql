31916e730c42502aeed7e4f14935e190f5317f6a
Champel Mariusz <champelmariusz@gmail.com>
2023-12-06 21:31:05 +0100

    test: Add SQL insert to link new test user with role in database script

--- src/main/resources/data_sql/test-data-setup.sql
+++ src/main/resources/data_sql/test-data-setup.sql
@@ -22,6 +22,11 @@ WHERE u.username = 'testUsername' AND r.name = 'ROLE_USER';
 INSERT INTO user_roles (user_id, role_id)
 SELECT u.id, r.id
 FROM users u, roles r
+WHERE u.username = 'testSecondUsername' AND r.name = 'ROLE_USER';
+
+INSERT INTO user_roles (user_id, role_id)
+SELECT u.id, r.id
+FROM users u, roles r
 WHERE u.username = 'testAdmin' AND r.name = 'ROLE_USER';
 
 INSERT INTO user_roles (user_id, role_id)
