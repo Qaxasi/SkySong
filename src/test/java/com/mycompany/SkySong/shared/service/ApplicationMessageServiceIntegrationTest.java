a78ab9f183fd72924df9d215ac655ea615103fdf
Champel Mariusz <champelmariusz@gmail.com>
2023-12-06 15:56:33 +0100

    test: Add test to verify messageService injection

--- src/test/java/com/mycompany/SkySong/shared/service/ApplicationMessageServiceIntegrationTest.java
+++ src/test/java/com/mycompany/SkySong/shared/service/ApplicationMessageServiceIntegrationTest.java
@@ -1,11 +1,19 @@
 package com.mycompany.SkySong.shared.service;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("test")
 public class ApplicationMessageServiceIntegrationTest {
     @Autowired
     private ApplicationMessageService messageService;
+    @Test
+    void shouldBeInjectedCorrectly() {
+        assertNotNull(messageService);
+    }
 }
