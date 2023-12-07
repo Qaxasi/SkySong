683a478ad5aee1ee247dd0919a69881dfea1e97e
Champel Mariusz <champelmariusz@gmail.com>
2023-12-07 22:03:15 +0100

    test: Add method to fetch user ID by username from database

--- src/test/java/com/mycompany/SkySong/testsupport/UserTestHelper.java
+++ src/test/java/com/mycompany/SkySong/testsupport/UserTestHelper.java
@@ -1,4 +1,28 @@
 package com.mycompany.SkySong.testsupport;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class UserTestHelper {
+    public Long getUserIdByUsername(String username) throws SQLException {
+        String query = "SELECT id FROM users WHERE username = ?";
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement statement = connection.prepareStatement(query)) {
+
+            statement.setString(1, username);
+
+            ResultSet resultSet = statement.executeQuery();
+
+            if (resultSet.next()) {
+                return resultSet.getLong("id");
+            } else {
+                return null;
+            }
+        }
+    }
 }
