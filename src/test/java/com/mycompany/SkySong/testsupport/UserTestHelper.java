f96712f6783065e7c906a04577a5accd0d2241f8
Champel Mariusz <champelmariusz@gmail.com>
2023-12-07 22:47:36 +0100

    test: Add method to preform login and extract token in tests

--- src/test/java/com/mycompany/SkySong/testsupport/UserTestHelper.java
+++ src/test/java/com/mycompany/SkySong/testsupport/UserTestHelper.java
@@ -1,10 +1,18 @@
 package com.mycompany.SkySong.testsupport;
 
+import com.jayway.jsonpath.JsonPath;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
 import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 public class UserTestHelper {
     private final DataSource dataSource;
@@ -26,5 +34,15 @@ public class UserTestHelper {
                 return null;
             }
         }
+    }
+    public String loginAndGetToken(MockMvc mockMvc, String requestBody) throws Exception {
+        MvcResult mvcResult = mockMvc.perform(post("/api/v1/users/login")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestBody))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        String responseString = mvcResult.getResponse().getContentAsString();
+        return JsonPath.parse(responseString).read("$.accessToken");
     }
 }
