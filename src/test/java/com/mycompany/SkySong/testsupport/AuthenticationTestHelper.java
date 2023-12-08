8441d45590be9a3eb1233cdf4b1fe5cf1af9c1dc
Champel Mariusz <champelmariusz@gmail.com>
2023-12-08 22:20:21 +0100

    test: Refactor loginAndGetCookie to return Cookie object with JWT token

--- src/test/java/com/mycompany/SkySong/testsupport/AuthenticationTestHelper.java
+++ src/test/java/com/mycompany/SkySong/testsupport/AuthenticationTestHelper.java
@@ -1,6 +1,7 @@
 package com.mycompany.SkySong.testsupport;
 
 import com.jayway.jsonpath.JsonPath;
+import jakarta.servlet.http.Cookie;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
@@ -9,7 +10,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 public class AuthenticationTestHelper {
 
-    public static String loginAndGetCookie(MockMvc mockMvc, String requestBody) throws Exception {
+    public static Cookie loginAndGetCookie(MockMvc mockMvc, String requestBody) throws Exception {
         MvcResult mvcResult = mockMvc.perform(post("/api/v1/users/login")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(requestBody))
@@ -17,6 +18,7 @@ public class AuthenticationTestHelper {
                 .andReturn();
 
         String responseString = mvcResult.getResponse().getContentAsString();
-        return JsonPath.parse(responseString).read("$.accessToken");
+        String jwtToken = JsonPath.parse(responseString).read("$.accessToken");
+        return new Cookie("auth_token", jwtToken);
     }
 }
