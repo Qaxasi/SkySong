3b1bf61760adfb2e377cb075d584b919b367fa3a
Champel Mariusz <champelmariusz@gmail.com>
2023-12-11 00:52:22 +0100

    test: Add constant for existing username error message

--- src/test/java/com/mycompany/SkySong/testsupport/TestMessages.java
+++ src/test/java/com/mycompany/SkySong/testsupport/TestMessages.java
@@ -2,4 +2,5 @@ package com.mycompany.SkySong.testsupport;
 
 public class TestMessages {
     public static final String EMAIL_EXIST = "Email is already exist!.";
+    public static final String USERNAME_EXIST = "Username is already exist!.";
 }
