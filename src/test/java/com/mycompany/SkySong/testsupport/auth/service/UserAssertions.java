59cb3dee51da2738a36b5d0e2e9921e01f468952
Champel Mariusz <champelmariusz@gmail.com>
2023-12-10 17:07:21 +0100

    test: Add assertUserExist method for database existence

--- src/test/java/com/mycompany/SkySong/testsupport/auth/service/UserAssertions.java
+++ src/test/java/com/mycompany/SkySong/testsupport/auth/service/UserAssertions.java
@@ -1,4 +1,14 @@
 package com.mycompany.SkySong.testsupport.auth.service;
 
+import javax.sql.DataSource;
+import java.sql.SQLException;
+
+import static com.mycompany.SkySong.testsupport.auth.service.RegistrationHelper.doesUserExist;
+
 public class UserAssertions {
+    public static void assertUserExist(String username, DataSource dataSource) throws SQLException {
+        if (!doesUserExist(username, dataSource)) {
+            throw new AssertionError("User " + username + " should exist in database.");
+        }
+    }
 }
