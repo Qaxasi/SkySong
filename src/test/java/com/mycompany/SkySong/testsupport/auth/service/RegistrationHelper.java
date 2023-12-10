12230b6846792b3dfe51d272c3af30c930671f9e
Champel Mariusz <champelmariusz@gmail.com>
2023-12-10 14:46:59 +0100

    test: Add utility method to new user registration

--- src/test/java/com/mycompany/SkySong/testsupport/auth/service/RegistrationHelper.java
+++ src/test/java/com/mycompany/SkySong/testsupport/auth/service/RegistrationHelper.java
@@ -1,4 +1,17 @@
 package com.mycompany.SkySong.testsupport.auth.service;
 
+import com.mycompany.SkySong.auth.model.dto.RegisterRequest;
+import com.mycompany.SkySong.auth.service.RegistrationService;
+import com.mycompany.SkySong.shared.exception.DatabaseException;
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class RegistrationHelper {
+    public static void newUserRegisterRequest(RegistrationService registrationService) throws DatabaseException {
+        String username = "testUniqueUsername";
+        String email = "testUniqueEmail@gmail.com";
+        String password = "testPassword@123";
+        RegisterRequest registerRequest = new RegisterRequest(username, email, password);
+
+        registrationService.register(registerRequest);
+    }
 }
