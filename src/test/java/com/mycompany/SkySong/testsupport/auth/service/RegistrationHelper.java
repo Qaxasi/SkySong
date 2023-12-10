36814db6b46b0fce9defa7fee5bccfbd4586b5db
Champel Mariusz <champelmariusz@gmail.com>
2023-12-10 15:00:26 +0100

    test: Add method to check user existence by username in database

--- src/test/java/com/mycompany/SkySong/testsupport/auth/service/RegistrationHelper.java
+++ src/test/java/com/mycompany/SkySong/testsupport/auth/service/RegistrationHelper.java
@@ -4,11 +4,32 @@ import com.mycompany.SkySong.auth.model.dto.RegisterRequest;
 import com.mycompany.SkySong.auth.service.RegistrationService;
 import com.mycompany.SkySong.shared.exception.DatabaseException;
 
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class RegistrationHelper {
     public static RegisterRequest newUserRegisterRequest(RegistrationService registrationService) throws DatabaseException {
         String username = "testUniqueUsername";
         String email = "testUniqueEmail@gmail.com";
         String password = "testPassword@123";
         return new RegisterRequest(username, email, password);
+    }
+    private boolean doesUserExist(String username, DataSource dataSource) throws SQLException {
+        String query = "SELECT COUNT(*) " +
+                       "FROM users" +
+                      " WHERE username = ?";
+
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement statement = connection.prepareStatement(query)) {
+
+            statement.setString(1, username);
+
+            ResultSet resultSet = statement.executeQuery();
+
+            return resultSet.next() && resultSet.getInt(1) > 0;
+        }
     }
 }
