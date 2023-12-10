fb6ed3f829f8abf4f0076358fc9511db8e65d97c
Champel Mariusz <champelmariusz@gmail.com>
2023-12-10 22:12:30 +0100

    test: Add custom assertion to verify role assignment

--- src/test/java/com/mycompany/SkySong/testsupport/auth/service/UserAssertions.java
+++ src/test/java/com/mycompany/SkySong/testsupport/auth/service/UserAssertions.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Component;
 import javax.sql.DataSource;
 import java.sql.SQLException;
 
+import static com.mycompany.SkySong.testsupport.auth.service.RegistrationHelper.doesRoleAddedToNewUser;
 import static com.mycompany.SkySong.testsupport.auth.service.RegistrationHelper.doesUserExist;
 @Component
 public class UserAssertions {
@@ -22,6 +23,11 @@ public class UserAssertions {
     public static void assertUserDoesNotExist(String username) throws SQLException {
         if (doesUserExist(username, dataSource)) {
             throw new AssertionError("User " + username + " should not exist in database.");
+        }
+    }
+    public static void assertUserRole(String username, String expectedRole) throws SQLException {
+        if (!doesRoleAddedToNewUser(username,expectedRole, dataSource)) {
+            throw new AssertionError("Expected role " + expectedRole + " for user " + username);
         }
     }
 }
