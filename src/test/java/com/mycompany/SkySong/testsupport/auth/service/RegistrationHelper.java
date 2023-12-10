24cbcb88a0b42616d201ec4b541f68498d359e3b
Champel Mariusz <champelmariusz@gmail.com>
2023-12-10 14:49:44 +0100

    test: Refactor newUserRegisterRequest to return RegisterRequest object

--- src/test/java/com/mycompany/SkySong/testsupport/auth/service/RegistrationHelper.java
+++ src/test/java/com/mycompany/SkySong/testsupport/auth/service/RegistrationHelper.java
@@ -3,15 +3,12 @@ package com.mycompany.SkySong.testsupport.auth.service;
 import com.mycompany.SkySong.auth.model.dto.RegisterRequest;
 import com.mycompany.SkySong.auth.service.RegistrationService;
 import com.mycompany.SkySong.shared.exception.DatabaseException;
-import org.springframework.beans.factory.annotation.Autowired;
 
 public class RegistrationHelper {
-    public static void newUserRegisterRequest(RegistrationService registrationService) throws DatabaseException {
+    public static RegisterRequest newUserRegisterRequest(RegistrationService registrationService) throws DatabaseException {
         String username = "testUniqueUsername";
         String email = "testUniqueEmail@gmail.com";
         String password = "testPassword@123";
-        RegisterRequest registerRequest = new RegisterRequest(username, email, password);
-
-        registrationService.register(registerRequest);
+        return new RegisterRequest(username, email, password);
     }
 }
