6db67b6776427c26c490f48cf824a6d5c3800660
Champel Mariusz <champelmariusz@gmail.com>
2023-12-10 18:21:39 +0100

    test: Annotate UserAssertions as a Spring component

--- src/test/java/com/mycompany/SkySong/testsupport/auth/service/UserAssertions.java
+++ src/test/java/com/mycompany/SkySong/testsupport/auth/service/UserAssertions.java
@@ -1,17 +1,20 @@
 package com.mycompany.SkySong.testsupport.auth.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
 import javax.sql.DataSource;
 import java.sql.SQLException;
 
 import static com.mycompany.SkySong.testsupport.auth.service.RegistrationHelper.doesUserExist;
-
+@Component
 public class UserAssertions {
     public static void assertUserExist(String username, DataSource dataSource) throws SQLException {
         if (!doesUserExist(username, dataSource)) {
             throw new AssertionError("User " + username + " should exist in database.");
         }
     }
-    public static void assertUserDoesNotExist(String username, DataSource dataSource) throws SQLException {
+    public static void assertUserDoesNotExist(String username) throws SQLException {
         if (doesUserExist(username, dataSource)) {
             throw new AssertionError("User " + username + " should not exist in database.");
         }
