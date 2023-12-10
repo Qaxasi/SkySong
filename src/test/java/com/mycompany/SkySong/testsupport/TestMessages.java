89effd5e93a8b4af50cf4fda7059365940d8efc7
Champel Mariusz <champelmariusz@gmail.com>
2023-12-11 00:57:47 +0100

    test: Add constant for invalid email format error message

--- src/test/java/com/mycompany/SkySong/testsupport/TestMessages.java
+++ src/test/java/com/mycompany/SkySong/testsupport/TestMessages.java
@@ -5,4 +5,6 @@ public class TestMessages {
     public static final String USERNAME_EXIST = "Username is already exist!.";
     public static final String INVALID_PASSWORD_FORMAT = "Invalid password format. The password must contain an " +
             "least 8 characters, including uppercase letters, lowercase letters, numbers, and special characters.";
+    public static final String INVALID_EMAIL_FORMAT = "Invalid email address format. The email should follow the " +
+            "standard format (e.g., user@example.com) and be between 6 and 30 characters long.";
 }
