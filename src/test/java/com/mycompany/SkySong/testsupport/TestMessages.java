c717ce63eaab8b0ad25007c11ae2f7949f334f7c
Champel Mariusz <champelmariusz@gmail.com>
2023-12-11 00:53:51 +0100

    test: Add constant for invalid password format error message

--- src/test/java/com/mycompany/SkySong/testsupport/TestMessages.java
+++ src/test/java/com/mycompany/SkySong/testsupport/TestMessages.java
@@ -3,4 +3,6 @@ package com.mycompany.SkySong.testsupport;
 public class TestMessages {
     public static final String EMAIL_EXIST = "Email is already exist!.";
     public static final String USERNAME_EXIST = "Username is already exist!.";
+    public static final String INVALID_PASSWORD_FORMAT = "Invalid password format. The password must contain an " +
+            "least 8 characters, including uppercase letters, lowercase letters, numbers, and special characters.";
 }
