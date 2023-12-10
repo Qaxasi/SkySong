530e4914024faaa0cc177f65d1cfb25b3cda079e
Champel Mariusz <champelmariusz@gmail.com>
2023-12-10 21:13:13 +0100

    test: Inject RegistrationService into RegistrationHelper

--- src/test/java/com/mycompany/SkySong/testsupport/auth/service/RegistrationHelper.java
+++ src/test/java/com/mycompany/SkySong/testsupport/auth/service/RegistrationHelper.java
@@ -3,14 +3,21 @@ package com.mycompany.SkySong.testsupport.auth.service;
 import com.mycompany.SkySong.auth.model.dto.RegisterRequest;
 import com.mycompany.SkySong.auth.service.RegistrationService;
 import com.mycompany.SkySong.shared.exception.DatabaseException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-
+@Component
 public class RegistrationHelper {
+    private static RegistrationService registrationService;
+    @Autowired
+    public RegistrationHelper(RegistrationService registrationService) {
+        RegistrationHelper.registrationService = registrationService;
+    }
     public static RegisterRequest createValidRegisterRequest() {
         return new RegisterRequest("testUsername", "testEmail@gmail.com", "testPassword@123");
     }
