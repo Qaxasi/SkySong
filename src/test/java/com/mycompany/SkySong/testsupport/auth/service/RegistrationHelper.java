6da0f8c587860a052ef2647229f43523e9c4d947
Champel Mariusz <champelmariusz@gmail.com>
2023-12-11 20:23:19 +0100

    test: Comvert method to static

--- src/test/java/com/mycompany/SkySong/testsupport/auth/service/RegistrationHelper.java
+++ src/test/java/com/mycompany/SkySong/testsupport/auth/service/RegistrationHelper.java
@@ -4,32 +4,32 @@ import com.mycompany.SkySong.auth.model.dto.LoginRequest;
 import com.mycompany.SkySong.auth.model.dto.RegisterRequest;
 
 public class RegistrationHelper {
-    public RegisterRequest createValidRegisterRequest() {
+    public static RegisterRequest createValidRegisterRequest() {
         return new RegisterRequest("testUsername", "testEmail@gmail.com", "testPassword@123");
     }
-    public LoginRequest createUserLoginRequest() {
+    public static LoginRequest createUserLoginRequest() {
         return new LoginRequest("testUsername", "testPassword@123");
     }
-    public RegisterRequest createValidRegisterRequestWithUsername(String username) {
+    public static RegisterRequest createValidRegisterRequestWithUsername(String username) {
         return new RegisterRequest(username, username + "@gmail.com", "testPassword@123");
     }
-    public RegisterRequest createInvalidUsernameRequest() {
+    public static RegisterRequest createInvalidUsernameRequest() {
         return new RegisterRequest(
                 "invalidUsername$Format", "testEmail@gmail.com", "testPassword@123");
     }
-    public RegisterRequest createInvalidEmailRequest() {
+    public static RegisterRequest createInvalidEmailRequest() {
         return new RegisterRequest(
                 "testUsername", "invalidEmailFormat", "testPassword@123");
     }
-    public RegisterRequest createInvalidPasswordRequest() {
+    public static RegisterRequest createInvalidPasswordRequest() {
         return new RegisterRequest(
                 "testUsername", "testEmail@gmail.com", "invalidPassword");
     }
-    public RegisterRequest createExistUsernameRequest() {
+    public static RegisterRequest createExistUsernameRequest() {
         return new RegisterRequest(
                 "testExistUsername", "testEmail@gmail.com", "testPassword@123");
     }
-    public RegisterRequest createExistEmailRequest() {
+    public static RegisterRequest createExistEmailRequest() {
         return new RegisterRequest(
                 "testUsername", "testExistEmail@gmail.com", "testPassword@123");
     }
