a44cacda23c54dce85863033364b911ca3c29449
Champel Mariusz <champelmariusz@gmail.com>
2023-12-11 20:11:08 +0100

    test: Remove unused assertions

--- src/test/java/com/mycompany/SkySong/testsupport/auth/service/UserAssertions.java
+++ src/test/java/com/mycompany/SkySong/testsupport/auth/service/UserAssertions.java
@@ -8,21 +8,6 @@ import java.sql.SQLException;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 public class UserAssertions {
-    public static void assertUserExist(String username) throws SQLException {
-        if (!doesUserExist(username, dataSource)) {
-            throw new AssertionError("User " + username + " should exist in database.");
-        }
-    }
-    public static void assertUserDoesNotExist(String username) throws SQLException {
-        if (doesUserExist(username, dataSource)) {
-            throw new AssertionError("User " + username + " should not exist in database.");
-        }
-    }
-    public static void assertUserRole(String username, String expectedRole) throws SQLException {
-        if (!doesRoleAddedToNewUser(username,expectedRole, dataSource)) {
-            throw new AssertionError("Expected role " + expectedRole + " for user " + username);
-        }
-    }
     public static void assertValidationException(Executable executable) {
         assertThrows(CredentialValidationException.class, executable);
     }
