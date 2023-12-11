e70088d458d2f3325a721c3c0d8796118b319b4e
Champel Mariusz <champelmariusz@gmail.com>
2023-12-11 20:57:51 +0100

    test: Annotate class as a spring component

--- src/test/java/com/mycompany/SkySong/testsupport/auth/service/DatabaseHelper.java
+++ src/test/java/com/mycompany/SkySong/testsupport/auth/service/DatabaseHelper.java
@@ -1,11 +1,13 @@
 package com.mycompany.SkySong.testsupport.auth.service;
 
+import org.springframework.stereotype.Component;
+
 import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-
+@Component
 public class DatabaseHelper {
     private final DataSource dataSource;
     public DatabaseHelper(DataSource dataSource) {
