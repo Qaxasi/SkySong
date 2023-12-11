a22b991251953c3ce6fe88cb7959e207898bb07b
Champel Mariusz <champelmariusz@gmail.com>
2023-12-11 19:18:16 +0100

    test: Update SQL script

--- src/main/resources/data_sql/test-data-setup.sql
+++ src/main/resources/data_sql/test-data-setup.sql
@@ -3,11 +3,6 @@ INSERT INTO users (username, email, password)
       '$2a$10$VEbWwz6NcL4y6MgKEE/sJuWiFe2EoVbru6gJ.6Miu6G16NWfqlxci');
 
 INSERT INTO users (username, email, password)
-    VALUES('testThirdUsername', 'testThirdEmail@gmail.com',
-     '$2a$10$VEbWwz6NcL4y6MgKEE/sJuWiFe2EoVbru6gJ.6Miu6G16NWfqlxci');
-
-
-INSERT INTO users (username, email, password)
     VALUES('testAdmin', 'testAdmin@gmail.com',
      '$2a$10$VEbWwz6NcL4y6MgKEE/sJuWiFe2EoVbru6gJ.6Miu6G16NWfqlxci');
 
@@ -18,12 +13,7 @@ INSERT INTO roles (name)
 INSERT INTO user_roles (user_id, role_id)
 SELECT u.id, r.id
 FROM users u, roles r
-WHERE u.username = 'testUsername' AND r.name = 'ROLE_USER';
-
-INSERT INTO user_roles (user_id, role_id)
-SELECT u.id, r.id
-FROM users u, roles r
-WHERE u.username = 'testSecondUsername' AND r.name = 'ROLE_USER';
+WHERE u.username = 'testExistUsername' AND r.name = 'ROLE_USER';
 
 INSERT INTO user_roles (user_id, role_id)
 SELECT u.id, r.id
