d485e5d74165266e48a6d17229980e9f8db89e40
Champel Mariusz <champelmariusz@gmail.com>
2023-12-12 18:43:15 +0100

    refactor: Remove unused imports

--- src/test/java/com/mycompany/SkySong/testsupport/auth/service/UserAssertions.java
+++ src/test/java/com/mycompany/SkySong/testsupport/auth/service/UserAssertions.java
@@ -1,6 +1,5 @@
 package com.mycompany.SkySong.testsupport.auth.service;
 
-import com.mycompany.SkySong.shared.exception.CredentialValidationException;
 import org.junit.jupiter.api.function.Executable;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
