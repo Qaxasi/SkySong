c4bd86b6bb0eb7d20973f61fd4e5e240146d4aba
Champel Mariusz <champelmariusz@gmail.com>
2023-12-12 22:16:33 +0100

    refactor: Change DatabaseException to extend ApplicationRuntimeException

--- src/main/java/com/mycompany/SkySong/shared/exception/DatabaseException.java
+++ src/main/java/com/mycompany/SkySong/shared/exception/DatabaseException.java
@@ -1,6 +1,6 @@
 package com.mycompany.SkySong.shared.exception;
 
-public class DatabaseException extends ApplicationException {
+public class DatabaseException extends ApplicationRuntimeException {
     public DatabaseException(String message) {
         super(message);
     }
