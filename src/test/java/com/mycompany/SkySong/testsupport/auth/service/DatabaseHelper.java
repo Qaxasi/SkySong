8575459234411595036bd0fec7daf424764faf01
Champel Mariusz <champelmariusz@gmail.com>
2023-12-12 17:07:45 +0100

    test: Add try-catch block to handle SQLException in hasUserRole method

--- src/test/java/com/mycompany/SkySong/testsupport/auth/service/DatabaseHelper.java
+++ src/test/java/com/mycompany/SkySong/testsupport/auth/service/DatabaseHelper.java
@@ -33,24 +33,28 @@ public class DatabaseHelper {
                 throw new RuntimeException("Error during checking if user exists " + e.getMessage(), e);
         }
     }
-    public boolean hasUserRole(String username, String roleName) throws SQLException {
-        String query = "SELECT COUNT(*) FROM user_roles ur " +
-                "JOIN users u ON ur.user_id = u.id " +
-                "JOIN roles r ON ur.role_id = r.id " +
-                "WHERE u.username = ? AND r.name = ?";
+    public boolean hasUserRole(String username, String roleName) {
+        try {
+            String query = "SELECT COUNT(*) FROM user_roles ur " +
+                    "JOIN users u ON ur.user_id = u.id " +
+                    "JOIN roles r ON ur.role_id = r.id " +
+                    "WHERE u.username = ? AND r.name = ?";
 
-        try (Connection connection = dataSource.getConnection();
-             PreparedStatement statement = connection.prepareStatement(query)) {
+            try (Connection connection = dataSource.getConnection();
+                 PreparedStatement statement = connection.prepareStatement(query)) {
 
-            statement.setString(1, username);
-            statement.setString(2, roleName);
-            ResultSet resultSet = statement.executeQuery();
+                statement.setString(1, username);
+                statement.setString(2, roleName);
+                ResultSet resultSet = statement.executeQuery();
 
-            if (resultSet.next()) {
-                return resultSet.getInt(1) > 0;
-            } else {
-                return false;
+                if (resultSet.next()) {
+                    return resultSet.getInt(1) > 0;
+                } else {
+                    return false;
+                }
             }
+        } catch (SQLException e) {
+            throw new RuntimeException("Error during checking user role: " + e.getMessage(), e);
         }
     }
 }
