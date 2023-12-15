3933cb209002aa54f194c630dcbd76fcb3449fb5
Champel Mariusz <champelmariusz@gmail.com>
2023-12-15 20:02:55 +0100

    test: Delete unused test

--- src/test/java/com/mycompany/SkySong/auth/controller/LogoutControllerTest.java
+++ src/test/java/com/mycompany/SkySong/auth/controller/LogoutControllerTest.java
@@ -49,59 +49,62 @@ public class LogoutControllerTest {
                 mockCookie,
                 200);
     }
-//    @Test
-//    @WithMockUser
-//    void shouldReturnMessageAfterSuccessfulLogout() throws Exception {
-//        Cookie mockCookie = new Cookie("auth_token", "token-value");
-//        String expectedMessage = "{\"message\":\"User logged out successfully\"}";
-//        when(messageService.getMessage("logout.success")).thenReturn("User logged out successfully");
-//
-//        PostRequestAssertions.assertMessageReturns(mockMvc,
-//                "/api/v1/users/logout",
-//                mockCookie,
-//                expectedMessage);
-//    }
-//    @Test
-//    @WithMockUser
-//    void shouldDeleteAuthTokenCookieOnLogout() throws Exception {
-//        Cookie mockCookie = new Cookie("auth_token", "token-value");
-//        CookieAssertions.assertCookieIsDeleted(mockMvc, "/api/v1/users/logout", mockCookie);
-//    }
-//    @Test
-//    @WithMockUser
-//    void shouldReturnStatusOkWhenLogoutWithoutCookie() throws Exception {
-//        PostRequestAssertions.assertPostStatusReturnsWithoutBodyAndCookie(mockMvc,
-//                "/api/v1/users/logout",
-//                200);
-//    }
-//    @Test
-//    @WithMockUser
-//    void shouldHandleServiceFailureException() throws Exception {
-//        doThrow(new RuntimeException("Internal Error")).when(cookieDeleter).deleteCookie(any(), any(), any());
-//
-//        PostRequestAssertions.assertPostStatusReturnsWithoutBodyAndCookie(
-//                mockMvc,
-//                "/api/v1/users/logout",
-//                500);
-//    }
-//    @Test
-//    @WithMockUser
-//    void shouldReturnErrorMessageWhenHandleServiceFailureException() throws Exception {
-//        doThrow(new RuntimeException("Internal Error")).when(cookieDeleter).deleteCookie(any(), any(), any());
-//        String expectedMessage = "{\"error\":\"Logout failed due to an internal error\"}";
-//        when(messageService.getMessage("logout.failure")).thenReturn("Logout failed due to an internal error");
-//
-//        PostRequestAssertions.assertMessageReturnsWithoutCookie(
-//                mockMvc,
-//                "/api/v1/users/logout",
-//                expectedMessage);
-//    }
-//    @Test
-//    @WithMockUser
-//    void shouldProcessLogoutForAnonymousUser() throws Exception {
-//        PostRequestAssertions.assertPostStatusReturnsWithoutBodyAndCookie(
-//                mockMvc,
-//                "/api/v1/users/logout",
-//                200);
-//    }
+    @Test
+    @WithMockUser
+    void shouldReturnMessageAfterSuccessfulLogout() throws Exception {
+        Cookie mockCookie = new Cookie("auth_token", "token-value");
+        String expectedMessage = "{\"message\":\"User logged out successfully\"}";
+        when(messageService.getMessage("logout.success")).thenReturn("User logged out successfully");
+
+        PostRequestAssertions.assertResponseMessage(mockMvc,
+                "/api/v1/users/logout",
+                mockCookie,
+                expectedMessage);
+    }
+    @Test
+    @WithMockUser
+    void shouldDeleteAuthTokenCookieOnLogout() throws Exception {
+        doAnswer(invocation -> {
+            HttpServletResponse response = invocation.getArgument(1);
+            Cookie modifiedCookie = new Cookie("auth_token", null);
+            modifiedCookie.setMaxAge(0);
+            modifiedCookie.setPath("/");
+            response.addCookie(modifiedCookie);
+            return null;
+        }).when(cookieDeleter).deleteCookie(any(HttpServletRequest.class),
+                any(HttpServletResponse.class), eq("auth_token"));
+
+        Cookie mockCookie = new Cookie("auth_token", "token-value");
+        CookieAssertions.assertCookieIsDeleted(mockMvc, "/api/v1/users/logout", mockCookie);
+    }
+    @Test
+    @WithMockUser
+    void shouldHandleServiceFailureException() throws Exception {
+        doThrow(new RuntimeException("Internal Error")).when(cookieDeleter).deleteCookie(any(), any(), any());
+
+        PostRequestAssertions.assertPostStatusNoBodyWithCookie(
+                mockMvc,
+                "/api/v1/users/logout",
+                500);
+    }
+    @Test
+    @WithMockUser
+    void shouldReturnErrorMessageWhenHandleServiceFailureException() throws Exception {
+        doThrow(new RuntimeException("Internal Error")).when(cookieDeleter).deleteCookie(any(), any(), any());
+        String expectedMessage = "{\"error\":\"Logout failed due to an internal error\"}";
+        when(messageService.getMessage("logout.failure")).thenReturn("Logout failed due to an internal error");
+
+        PostRequestAssertions.assertResponseMessageWithoutCookie(
+                mockMvc,
+                "/api/v1/users/logout",
+                expectedMessage);
+    }
+    @Test
+    @WithMockUser
+    void shouldReturnStatusOkWhenLogoutWithoutCookie() throws Exception {
+        PostRequestAssertions.assertPostStatusNoBodyWithCookie(
+                mockMvc,
+                "/api/v1/users/logout",
+                200);
+    }
 }
