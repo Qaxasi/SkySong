4828dc8351f7a4778aa666633a1f599cc5397bb9
Champel Mariusz <champelmariusz@gmail.com>
2023-12-16 13:52:54 +0100

    test: Add utility method for mocking cookie deletion behavior

--- src/test/java/com/mycompany/SkySong/testsupport/auth/controller/LogoutControllerHelper.java
+++ src/test/java/com/mycompany/SkySong/testsupport/auth/controller/LogoutControllerHelper.java
@@ -1,12 +1,31 @@
 package com.mycompany.SkySong.testsupport.auth.controller;
 
+import com.mycompany.SkySong.auth.security.CookieDeleter;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.doAnswer;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 public class LogoutControllerHelper {
+    private void setupCookieDeletion(CookieDeleter cookieDeleter, String cookieName) {
+        doAnswer(invocation -> {
+            HttpServletResponse response = invocation.getArgument(1);
+            Cookie modifiedCookie = new Cookie(cookieName, null);
+            modifiedCookie.setMaxAge(0);
+            modifiedCookie.setPath("/");
+            response.addCookie(modifiedCookie);
+            return null;
+        }).when(cookieDeleter).deleteCookie(any(HttpServletRequest.class),
+                any(HttpServletResponse.class), eq(cookieName));
+
+    }
     public static void assertPostStatusNoBodyWithCookie(MockMvc mockMvc, String endpoint,
                                                         int expectedStatusCode) throws Exception {
         mockMvc.perform(post(endpoint)
