d3d1da41c95e2b96762fc9aecbe480a764ff64b5
Champel Mariusz <champelmariusz@gmail.com>
2023-12-16 13:16:32 +0100

    test: Add utility method for status check

--- src/test/java/com/mycompany/SkySong/testsupport/auth/controller/LogoutControllerHelper.java
+++ src/test/java/com/mycompany/SkySong/testsupport/auth/controller/LogoutControllerHelper.java
@@ -1,4 +1,16 @@
 package com.mycompany.SkySong.testsupport.auth.controller;
 
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 public class LogoutControllerHelper {
+    public static void assertPostStatusNoBodyWithCookie(MockMvc mockMvc, String endpoint,
+                                                        int expectedStatusCode) throws Exception {
+        mockMvc.perform(post(endpoint)
+                        .with(SecurityMockMvcRequestPostProcessors.csrf()))
+                .andExpect(status().is(expectedStatusCode));
+    }
 }
