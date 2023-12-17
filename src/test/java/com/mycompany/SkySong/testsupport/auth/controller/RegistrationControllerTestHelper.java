5c2980af3faac3fc55ade03e96bc555186313bfd
Champel Mariusz <champelmariusz@gmail.com>
2023-12-17 21:11:18 +0100

    test: Add constant with invalid email format

--- src/test/java/com/mycompany/SkySong/testsupport/auth/controller/RegistrationControllerTestHelper.java
+++ src/test/java/com/mycompany/SkySong/testsupport/auth/controller/RegistrationControllerTestHelper.java
@@ -17,6 +17,8 @@ public class RegistrationControllerTestHelper {
             "{\"username\": \"User\", \"email\": \"existEmail\", \"password\": \"Password#3\"}";
     public static final String INVALID_USERNAME_FORMAT =
             "{\"username\": \"invalidFormat\", \"email\": \"mail@mail.com\", \"password\": \"Password#3\"}";
+    public static final String INVALID_EMAIL_FORMAT =
+            "{\"username\": \"User\", \"email\": \"invalidEmail\", \"password\": \"Password#3\"}";
 
     public static void mockSuccessRegistration(RegistrationService registration) {
         when(registration.register(any(RegisterRequest.class))).thenReturn(
