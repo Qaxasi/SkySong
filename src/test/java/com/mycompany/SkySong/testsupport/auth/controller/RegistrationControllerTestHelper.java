7862e96933f7796601ef4005f5a2418da6e917d8
Champel Mariusz <champelmariusz@gmail.com>
2023-12-17 19:03:32 +0100

    test: Add valid reigster request constant

--- src/test/java/com/mycompany/SkySong/testsupport/auth/controller/RegistrationControllerTestHelper.java
+++ src/test/java/com/mycompany/SkySong/testsupport/auth/controller/RegistrationControllerTestHelper.java
@@ -1,4 +1,7 @@
 package com.mycompany.SkySong.testsupport.auth.controller;
 
 public class RegistrationControllerTestHelper {
+    private static final String VALID_REQUEST =
+            "{\"username\": \"testUniqueUsername\", \"email\": \"testUniqeEmail@gmail.com\", " +
+            "\"password\": \"testPassword@123\"}";
 }
