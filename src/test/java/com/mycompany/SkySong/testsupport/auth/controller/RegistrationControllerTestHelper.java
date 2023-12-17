724e76c6c27a4ea9707f70b629db2d082a34c634
Champel Mariusz <champelmariusz@gmail.com>
2023-12-17 19:21:35 +0100

    test: Add mock method for successful user registration

--- src/test/java/com/mycompany/SkySong/testsupport/auth/controller/RegistrationControllerTestHelper.java
+++ src/test/java/com/mycompany/SkySong/testsupport/auth/controller/RegistrationControllerTestHelper.java
@@ -1,7 +1,19 @@
 package com.mycompany.SkySong.testsupport.auth.controller;
 
+import com.mycompany.SkySong.auth.model.dto.RegisterRequest;
+import com.mycompany.SkySong.auth.service.RegistrationService;
+import com.mycompany.SkySong.shared.dto.ApiResponse;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
 public class RegistrationControllerTestHelper {
     private static final String VALID_REQUEST =
             "{\"username\": \"testUniqueUsername\", \"email\": \"testUniqeEmail@gmail.com\", " +
             "\"password\": \"testPassword@123\"}";
+
+    public static void mockSuccessRegistration(RegistrationService registrationService) {
+        when(registrationService.register(any(RegisterRequest.class))).thenReturn(
+                new ApiResponse("User registered successfully"));
+    }
 }
