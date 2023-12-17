8117bbc30b4455330b3ebab2fb728a0422d1fbb0
Champel Mariusz <champelmariusz@gmail.com>
2023-12-17 21:24:23 +0100

    test: Add malformed constant

--- src/test/java/com/mycompany/SkySong/testsupport/auth/controller/RegistrationControllerTestHelper.java
+++ src/test/java/com/mycompany/SkySong/testsupport/auth/controller/RegistrationControllerTestHelper.java
@@ -21,6 +21,8 @@ public class RegistrationControllerTestHelper {
             "{\"username\": \"User\", \"email\": \"invalidEmail\", \"password\": \"Password#3\"}";
     public static final String INVALID_PASSWORD_FORMAT =
             "{\"username\": \"User\", \"email\": \"mail@mail.com\", \"password\": \"invalidFormat\"}";
+    public static final String MALFORMED_REQUEST =
+            "{\"username\": \"User\", \"email\": \"mail@mail.com\", \"password\": \"Password#3\"";
 
     public static void mockSuccessRegistration(RegistrationService registration) {
         when(registration.register(any(RegisterRequest.class))).thenReturn(
