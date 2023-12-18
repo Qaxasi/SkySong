3874e71261f1d44b5ec4e2ca19506fb42d6634dc
Champel Mariusz <champelmariusz@gmail.com>
2023-12-18 22:21:11 +0100

    test: Implement addCookie method for cookie handling in tests

--- src/test/java/com/mycompany/SkySong/testsupport/auth/security/CookieAdderImplTestHelper.java
+++ src/test/java/com/mycompany/SkySong/testsupport/auth/security/CookieAdderImplTestHelper.java
@@ -1,4 +1,19 @@
 package com.mycompany.SkySong.testsupport.auth.security;
 
+import com.mycompany.SkySong.auth.security.CookieAdder;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
+import org.mockito.ArgumentCaptor;
+
+import static org.mockito.Mockito.verify;
+
 public class CookieAdderImplTestHelper {
+    public static Cookie addCookie(CookieAdder cookieAdder, HttpServletResponse response, String name,
+                                   String value, int maxAge) {
+        cookieAdder.addCookie(response, name, value, maxAge);
+
+        ArgumentCaptor<Cookie> cookieCaptor = ArgumentCaptor.forClass(Cookie.class);
+        verify(response).addCookie(cookieCaptor.capture());
+        return cookieCaptor.getValue();
+    }
 }
