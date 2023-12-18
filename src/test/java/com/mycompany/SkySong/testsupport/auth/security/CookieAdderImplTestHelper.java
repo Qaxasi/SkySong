e4c77b38491723b055c0fff5a3210be7c7a111a3
Champel Mariusz <champelmariusz@gmail.com>
2023-12-18 22:27:14 +0100

    test: Add utility method for cookie verification

--- src/test/java/com/mycompany/SkySong/testsupport/auth/security/CookieAdderImplTestHelper.java
+++ src/test/java/com/mycompany/SkySong/testsupport/auth/security/CookieAdderImplTestHelper.java
@@ -7,6 +7,8 @@ import org.mockito.ArgumentCaptor;
 
 import static org.mockito.Mockito.verify;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class CookieAdderImplTestHelper {
     public static Cookie addCookie(CookieAdder cookieAdder, HttpServletResponse response, String name,
                                    String value, int maxAge) {
@@ -15,5 +17,16 @@ public class CookieAdderImplTestHelper {
         ArgumentCaptor<Cookie> cookieCaptor = ArgumentCaptor.forClass(Cookie.class);
         verify(response).addCookie(cookieCaptor.capture());
         return cookieCaptor.getValue();
+    }
+    public static void assertCookieProperties(Cookie cookie, String expectedName, String expectedValue,
+                                              int expectedMaxAge, String expectedPath, boolean isHttpOnly,
+                                              boolean isSecure) {
+        assertThat(cookie).isNotNull();
+        assertThat(cookie.getName()).isEqualTo(expectedName);
+        assertThat(cookie.getValue()).isEqualTo(expectedValue);
+        assertThat(cookie.getMaxAge()).isEqualTo(expectedMaxAge);
+        assertThat(cookie.getPath()).isEqualTo(expectedPath);
+        assertThat(cookie.isHttpOnly()).isEqualTo(isHttpOnly);
+        assertThat(cookie.getSecure()).isEqualTo(isSecure);
     }
 }
