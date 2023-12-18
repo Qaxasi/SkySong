3537dd0e9acaf374742f5cec4d56569a81e0782a
Champel Mariusz <champelmariusz@gmail.com>
2023-12-18 20:02:40 +0100

    test: Rename and update constant

--- src/test/java/com/mycompany/SkySong/testsupport/auth/controller/RegistrationControllerTestHelper.java
+++ src/test/java/com/mycompany/SkySong/testsupport/auth/controller/RegistrationControllerTestHelper.java
@@ -11,8 +11,8 @@ import static org.mockito.Mockito.when;
 public class RegistrationControllerTestHelper {
     public static final String VALID_REQUEST =
             "{\"username\": \"User\", \"email\": \"mail@mail.com\", \"password\": \"Password#3\"}";
-    public static final String EXIST_USERNAME =
-            "{\"username\": \"existUser\", \"email\": \"mail@mail.com\", \"password\": \"Password#3\"}";
+    public static final String INVALID_CREDENTIALS =
+            "{\"username\": \"invalid\", \"email\": \"invalid\", \"password\": \"invalid\"}";
     public static final String MALFORMED_REQUEST =
             "{\"username\": \"User\", \"email\": \"mail@mail.com\", \"password\": \"Password#3\"";
     public static final String EMPTY_CREDENTIALS =
