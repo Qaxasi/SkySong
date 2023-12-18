d33843f2cd21aca8e0fe49610dbe9646ff2c5b18
Champel Mariusz <champelmariusz@gmail.com>
2023-12-18 22:51:09 +0100

    refactor: Delete unused imports

--- src/test/java/com/mycompany/SkySong/auth/security/CookieAdderImplTest.java
+++ src/test/java/com/mycompany/SkySong/auth/security/CookieAdderImplTest.java
@@ -5,13 +5,11 @@ import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletResponse;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
 public class CookieAdderImplTest {
