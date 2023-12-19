ae489917e539a84aa3d159f3a39de6b8d0131bcf
Champel Mariusz <champelmariusz@gmail.com>
2023-12-19 17:48:41 +0100

    test: Refactor test class to use directly method calls

--- src/test/java/com/mycompany/SkySong/auth/controller/RegistrationControllerTest.java
+++ src/test/java/com/mycompany/SkySong/auth/controller/RegistrationControllerTest.java
@@ -4,8 +4,6 @@ import com.mycompany.SkySong.auth.security.*;
 import com.mycompany.SkySong.shared.config.SecurityConfig;
 import com.mycompany.SkySong.auth.service.RegistrationService;
 import com.mycompany.SkySong.shared.service.ApplicationMessageService;
-import com.mycompany.SkySong.testsupport.auth.controller.RegistrationControllerTestHelper;
-import com.mycompany.SkySong.testsupport.controller.PostRequestAssertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -16,6 +14,8 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.Map;
 
+import static com.mycompany.SkySong.testsupport.auth.controller.RegistrationControllerTestHelper.*;
+import static com.mycompany.SkySong.testsupport.controller.PostRequestAssertions.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(RegistrationController.class)
@@ -40,45 +40,26 @@ public class RegistrationControllerTest {
     private final String endpoint = "/api/v1/users/register";
     @Test
     void whenRegistrationSuccess_Return201() throws Exception {
-        RegistrationControllerTestHelper.mockSuccessRegistration(registration);
-        PostRequestAssertions.assertPostStatus(
-                mockMvc,
-                endpoint,
-                RegistrationControllerTestHelper.VALID_REQUEST,
-                201);
+        mockSuccessRegistration(registration);
+        assertPostStatus(mockMvc, endpoint, VALID_REQUEST, 201);
     }
     @Test
     void whenRegistrationSuccess_ReturnCorrectFieldName() throws Exception {
-        RegistrationControllerTestHelper.mockSuccessRegistration(registration);
-        PostRequestAssertions.assertPostRequestFields(
-                mockMvc,
-                endpoint,
-                RegistrationControllerTestHelper.VALID_REQUEST,
-                jsonPath("$.message").isNotEmpty());
+        mockSuccessRegistration(registration);
+        assertPostRequestFields(mockMvc, endpoint, VALID_REQUEST, jsonPath("$.message").isNotEmpty());
     }
     @Test
     void whenInvalidCredentials_ReturnBadRequest() throws Exception {
-        RegistrationControllerTestHelper.mockInvalidCredentials(registration);
-        PostRequestAssertions.assertPostStatus(
-                mockMvc,
-                endpoint,
-                RegistrationControllerTestHelper.INVALID_CREDENTIALS,
-                400);
+       mockInvalidCredentials(registration);
+       assertPostStatus(mockMvc, endpoint, INVALID_CREDENTIALS, 400);
     }
     @Test
     void whenMalformedRequest_ReturnBadRequest() throws Exception {
-        PostRequestAssertions.assertPostStatus(
-                mockMvc,
-                endpoint,
-                RegistrationControllerTestHelper.MALFORMED_REQUEST,
-                400);
+        assertPostStatus(mockMvc, endpoint, MALFORMED_REQUEST, 400);
     }
     @Test
     void whenEmptyCredentials_ReturnErrorMessages() throws Exception {
-        PostRequestAssertions.assertPostJsonResponse(
-                mockMvc,
-                endpoint,
-                RegistrationControllerTestHelper.EMPTY_CREDENTIALS,
+        assertPostJsonResponse(mockMvc, endpoint, EMPTY_CREDENTIALS,
                 Map.of("$.errors.username", "The username field cannot be empty.",
                         "$.errors.email","The email field cannot be empty",
                         "$.errors.password", "The password field cannot be empty"));
