1dad1677d3eba5ed88cad2365e554a831af24cac
Champel Mariusz <champelmariusz@gmail.com>
2023-12-19 17:41:01 +0100

    test: Refactor tests to use direct method calls for test helper functions

--- src/test/java/com/mycompany/SkySong/auth/controller/LogoutControllerTest.java
+++ src/test/java/com/mycompany/SkySong/auth/controller/LogoutControllerTest.java
@@ -3,8 +3,6 @@ package com.mycompany.SkySong.auth.controller;
 import com.mycompany.SkySong.auth.security.*;
 import com.mycompany.SkySong.auth.security.JwtAuthenticationEntryPoint;
 import com.mycompany.SkySong.shared.service.ApplicationMessageService;
-import com.mycompany.SkySong.testsupport.auth.controller.CookieAssertions;
-import com.mycompany.SkySong.testsupport.auth.controller.LogoutControllerTestHelper;
 import jakarta.servlet.http.Cookie;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -12,6 +10,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
+
+import static com.mycompany.SkySong.testsupport.auth.controller.CookieAssertions.assertCookieIsDeleted;
+import static com.mycompany.SkySong.testsupport.auth.controller.LogoutControllerTestHelper.*;
 
 @WebMvcTest(LogoutController.class)
 public class LogoutControllerTest {
@@ -30,25 +31,25 @@ public class LogoutControllerTest {
     private final String endpoint = "/api/v1/users/logout";
     @BeforeEach
     void setup() {
-        LogoutControllerTestHelper.setupCookieDeletion(cookieDeleter, "auth_token");
+        setupCookieDeletion(cookieDeleter, "auth_token");
     }
     @Test
     void whenSuccessfulLogout_ReturnStatusOk() throws Exception {
-        Cookie mockCookie = LogoutControllerTestHelper.mockedTokenCookie();
-        LogoutControllerTestHelper.assertStatus(mockMvc, endpoint, mockCookie, 200);
+        Cookie mockCookie = mockedTokenCookie();
+        assertStatus(mockMvc, endpoint, mockCookie, 200);
     }
     @Test
     void whenSuccessfulLogout_DeleteAuthTokenCookie() throws Exception {
-        Cookie mockCookie = LogoutControllerTestHelper.mockedTokenCookie();
-        CookieAssertions.assertCookieIsDeleted(mockMvc, endpoint, mockCookie);
+        Cookie mockCookie = mockedTokenCookie();
+        assertCookieIsDeleted(mockMvc, endpoint, mockCookie);
     }
     @Test
     void whenLogoutFails_HandleException() throws Exception {
-        LogoutControllerTestHelper.configureCookieDeleterToThrowException(cookieDeleter);
-        LogoutControllerTestHelper.assertStatusWithoutCookie(mockMvc, endpoint, 500);
+        configureCookieDeleterToThrowException(cookieDeleter);
+        assertStatusWithoutCookie(mockMvc, endpoint, 500);
     }
     @Test
     void whenLogoutWithoutCookie_ReturnStatusOk() throws Exception {
-        LogoutControllerTestHelper.assertStatusWithoutCookie(mockMvc, endpoint,200);
+        assertStatusWithoutCookie(mockMvc, endpoint,200);
     }
 }
