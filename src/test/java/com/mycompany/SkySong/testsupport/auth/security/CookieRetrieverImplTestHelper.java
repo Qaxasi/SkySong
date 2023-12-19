c72170439952140ba48a0fdad37f66095f105521
Champel Mariusz <champelmariusz@gmail.com>
2023-12-19 19:27:25 +0100

    test: Add utility method to test absence of cookie returns empty optional

--- src/test/java/com/mycompany/SkySong/testsupport/auth/security/CookieRetrieverImplTestHelper.java
+++ src/test/java/com/mycompany/SkySong/testsupport/auth/security/CookieRetrieverImplTestHelper.java
@@ -3,6 +3,7 @@ package com.mycompany.SkySong.testsupport.auth.security;
 import com.mycompany.SkySong.auth.security.CookieRetriever;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 import java.util.Optional;
 
@@ -13,9 +14,15 @@ public class CookieRetrieverImplTestHelper {
     public static void assertNoCookiesReturnEmptyOptional(CookieRetriever retriever,
                                                           HttpServletRequest request, String cookieName) {
         when(request.getCookies()).thenReturn(null);
-
         Optional<Cookie> result = retriever.getCookie(request,  cookieName);
-
         assertEquals(result, Optional.empty());
+    }
+    public static void assertCookieNotInRequestReturnsEmptyOptional(CookieRetriever cookieRetriever,
+                                                                    HttpServletRequest request,
+                                                                    Cookie[] cookies,
+                                                                    String requestedCookieName) {
+        when(request.getCookies()).thenReturn(cookies);
+        Optional<Cookie> result = cookieRetriever.getCookie(request, requestedCookieName);
+        assertEquals(Optional.empty(), result);
     }
 }
