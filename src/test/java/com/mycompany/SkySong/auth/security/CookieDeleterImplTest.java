20e634aa69119049196873d7e89459abb3aaa5a0
Champel Mariusz <champelmariusz@gmail.com>
2023-12-19 16:30:57 +0100

    refactor: Delete unsued imports

--- src/test/java/com/mycompany/SkySong/auth/security/CookieDeleterImplTest.java
+++ src/test/java/com/mycompany/SkySong/auth/security/CookieDeleterImplTest.java
@@ -1,19 +1,14 @@
 package com.mycompany.SkySong.auth.security;
 
-import com.mycompany.SkySong.testsupport.auth.security.CookieAdderImplTestHelper;
 import com.mycompany.SkySong.testsupport.auth.security.CookieDeleterImplTestHelper;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class CookieDeleterImplTest {
