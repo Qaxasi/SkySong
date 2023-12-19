fc4d1455e240b147076002ef99501e0e96a0f534
Champel Mariusz <champelmariusz@gmail.com>
2023-12-19 17:11:15 +0100

    test: Add utility method to assert empty optional when no cookies are present

--- src/test/java/com/mycompany/SkySong/testsupport/auth/security/CookieRetrieverImplTestHelper.java
+++ src/test/java/com/mycompany/SkySong/testsupport/auth/security/CookieRetrieverImplTestHelper.java
@@ -1,4 +1,21 @@
 package com.mycompany.SkySong.testsupport.auth.security;
 
+import com.mycompany.SkySong.auth.security.CookieRetriever;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
 public class CookieRetrieverImplTestHelper {
+    public static void assertNoCookiesReturnEmptyOptional(CookieRetriever retriever,
+                                                          HttpServletRequest request, String cookieName) {
+        when(request.getCookies()).thenReturn(null);
+
+        Optional<Cookie> result = retriever.getCookie(request,  cookieName);
+
+        assertEquals(result, Optional.empty());
+    }
 }
