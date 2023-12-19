a0cdb912b6264e0306abbb9561c7e9eda0709f19
Champel Mariusz <champelmariusz@gmail.com>
2023-12-19 13:03:26 +0100

    test: Add utility method to handle and verify cookie deletion in tests

--- src/test/java/com/mycompany/SkySong/testsupport/auth/security/CookieDeleterImplTestHelper.java
+++ src/test/java/com/mycompany/SkySong/testsupport/auth/security/CookieDeleterImplTestHelper.java
@@ -1,4 +1,27 @@
 package com.mycompany.SkySong.testsupport.auth.security;
 
+import com.mycompany.SkySong.auth.security.CookieDeleter;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.mockito.ArgumentCaptor;
+
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
 public class CookieDeleterImplTestHelper {
+    public static Cookie performCookieDeletionAndGetModifiedCookie(CookieDeleter deleter,
+                                                                   HttpServletRequest request,
+                                                                   HttpServletResponse response,
+                                                                   String cookieName,
+                                                                   Cookie... cookies) {
+
+        when(request.getCookies()).thenReturn(cookies);
+
+        deleter.deleteCookie(request, response, cookieName);
+
+        ArgumentCaptor<Cookie> cookieCaptor = ArgumentCaptor.forClass(Cookie.class);
+        verify(response).addCookie(cookieCaptor.capture());
+        return cookieCaptor.getValue();
+    }
 }
