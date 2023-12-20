922d91949b0244f87f681132c11d773afdc61664
Champel Mariusz <champelmariusz@gmail.com>
2023-12-20 22:20:06 +0100

    test: Delete utility method

--- src/test/java/com/mycompany/SkySong/testsupport/auth/security/UserDetailsAssertions.java
+++ src/test/java/com/mycompany/SkySong/testsupport/auth/security/UserDetailsAssertions.java
@@ -8,8 +8,6 @@ import java.util.HashSet;
 import java.util.Set;
 import java.util.stream.Collectors;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 public class UserDetailsAssertions {
     public static boolean hasAuthority(UserDetails userDetails, String... authority) {
         Set<String> requiredAuthorities = new HashSet<>(Arrays.asList(authority));
@@ -17,11 +15,5 @@ public class UserDetailsAssertions {
                 .map(GrantedAuthority::getAuthority)
                 .collect(Collectors.toSet())
                 .containsAll(requiredAuthorities);
-    }
-    public static void assertUserDetails(UserDetails userDetails,
-                                         String expectedUsername,
-                                         String expectedPassword) {
-        assertEquals(expectedUsername, userDetails.getUsername());
-        assertEquals(expectedPassword, userDetails.getPassword());
     }
 }
