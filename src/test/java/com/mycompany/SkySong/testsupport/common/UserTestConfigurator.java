c019bb92dbe3b9388b1f1289b1d920fdd15fc678
Champel Mariusz <champelmariusz@gmail.com>
2023-12-20 13:22:49 +0100

    test: Add utility method to initialize UserDAO mock with user roles

--- src/test/java/com/mycompany/SkySong/testsupport/common/UserTestConfigurator.java
+++ src/test/java/com/mycompany/SkySong/testsupport/common/UserTestConfigurator.java
@@ -1,4 +1,33 @@
 package com.mycompany.SkySong.testsupport.common;
 
+import com.mycompany.SkySong.auth.model.entity.Role;
+import com.mycompany.SkySong.auth.model.entity.UserRole;
+import com.mycompany.SkySong.shared.entity.User;
+import com.mycompany.SkySong.shared.repository.UserDAO;
+
+import java.util.Optional;
+import java.util.Set;
+
+import static org.mockito.Mockito.when;
+
 public class UserTestConfigurator {
+    public static void setupUsers(UserDAO userDAO) {
+        Role userRole = new Role(UserRole.ROLE_USER);
+        Set<Role> userRoles = Set.of(userRole);
+
+        User user = new User(1, "User", "mail@mail.com",
+                "$2a$10$px7fuh00336krhkSkZ9cQ.FRCVpTo5MLy90cx3rSYN5tISQxsRebq", userRoles);
+
+        Role adminRole = new Role(UserRole.ROLE_ADMIN);
+        Set<Role> adminRoles = Set.of(userRole, adminRole);
+
+        User admin = new User("Admin", "adminMail@mail.com",
+                "$2a$10$px7fuh00336krhkSkZ9cQ.FRCVpTo5MLy90cx3rSYN5tISQxsRebq", adminRoles);
+
+        when(userDAO.findByUsername("User")).thenReturn(Optional.of(user));
+        when(userDAO.findByEmail("mail@mail.com")).thenReturn(Optional.of(user));
+
+        when(userDAO.findByUsername("Admin")).thenReturn(Optional.of(admin));
+        when(userDAO.findByUsername("adminMail@mail.com")).thenReturn(Optional.of(admin));
+    }
 }
