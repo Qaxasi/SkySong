5d530608890ff2bf105598e58c7163516a414adb
Champel Mariusz <champelmariusz@gmail.com>
2023-12-20 22:36:53 +0100

    test: Add utility method to verify user authorities

--- src/test/java/com/mycompany/SkySong/testsupport/auth/security/CustomUserDetailsServiceTestHelper.java
+++ src/test/java/com/mycompany/SkySong/testsupport/auth/security/CustomUserDetailsServiceTestHelper.java
@@ -1,4 +1,19 @@
 package com.mycompany.SkySong.testsupport.auth.security;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 public class CustomUserDetailsServiceTestHelper {
+    public static boolean hasAuthority(UserDetails userDetails, String... authority) {
+        Set<String> requiredAuthorities = new HashSet<>(Arrays.asList(authority));
+        return userDetails.getAuthorities().stream()
+                .map(GrantedAuthority::getAuthority)
+                .collect(Collectors.toSet())
+                .containsAll(requiredAuthorities);
+    }
 }
