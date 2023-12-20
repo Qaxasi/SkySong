1b7f4a59b39bd3a90cb198329befdd482d76b9ac
Champel Mariusz <champelmariusz@gmail.com>
2023-12-20 23:37:36 +0100

    test: Optimize user initialization in utility method using createUser

--- src/test/java/com/mycompany/SkySong/testsupport/common/UserTestConfigurator.java
+++ src/test/java/com/mycompany/SkySong/testsupport/common/UserTestConfigurator.java
@@ -19,21 +19,14 @@ public class UserTestConfigurator {
     }
     public static void setupUsers(UserDAO userDAO) {
         Role userRole = new Role(UserRole.ROLE_USER);
-        Set<Role> userRoles = Set.of(userRole);
-
-        User user = new User(1, "User", "mail@mail.com",
-                "$2a$10$px7fuh00336krhkSkZ9cQ.FRCVpTo5MLy90cx3rSYN5tISQxsRebq", userRoles);
-
         Role adminRole = new Role(UserRole.ROLE_ADMIN);
-        Set<Role> adminRoles = Set.of(userRole, adminRole);
 
-        User admin = new User("Admin", "adminMail@mail.com",
-                "$2a$10$px7fuh00336krhkSkZ9cQ.FRCVpTo5MLy90cx3rSYN5tISQxsRebq", adminRoles);
+        User regularUser = createUser("User", "mail@mail.com",
+                "$2a$10$px7fuh00336krhkSkZ9cQ.FRCVpTo5MLy90cx3rSYN5tISQxsRebq", userRole);
+        setupExistingUser(userDAO, regularUser);
 
-        when(userDAO.findByUsername("User")).thenReturn(Optional.of(user));
-        when(userDAO.findByEmail("mail@mail.com")).thenReturn(Optional.of(user));
-
-        when(userDAO.findByUsername("Admin")).thenReturn(Optional.of(admin));
-        when(userDAO.findByEmail("adminMail@mail.com")).thenReturn(Optional.of(admin));
+        User adminUser = createUser("Admin", "adminMail@mail.com",
+                "$2a$10$px7fuh00336krhkSkZ9cQ.FRCVpTo5MLy90cx3rSYN5tISQxsRebq", userRole, adminRole);
+        setupExistingUser(userDAO, adminUser);
     }
 }
