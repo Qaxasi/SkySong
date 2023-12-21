77c08863406b03dac5587a682eb7df6562f6f701
Champel Mariusz <champelmariusz@gmail.com>
2023-12-21 18:33:04 +0100

    test: Add custom assertion for standardized auth failure testing

--- src/test/java/com/mycompany/SkySong/testsupport/auth/security/JwtAuthenticationEntryPointTestHelper.java
+++ src/test/java/com/mycompany/SkySong/testsupport/auth/security/JwtAuthenticationEntryPointTestHelper.java
@@ -1,4 +1,36 @@
 package com.mycompany.SkySong.testsupport.auth.security;
 
+import com.mycompany.SkySong.auth.security.JwtAuthenticationEntryPoint;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.json.JSONObject;
+import org.springframework.security.core.AuthenticationException;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
 public class JwtAuthenticationEntryPointTestHelper {
+    public static void assertAuthenticationMessage(JwtAuthenticationEntryPoint authenticationEntryPoint,
+                                                   HttpServletRequest request,
+                                                   HttpServletResponse response,
+                                                   AuthenticationException exception,
+                                                   String expectedMessage) throws IOException {
+        StringWriter stringWriter = new StringWriter();
+        PrintWriter printWriter = new PrintWriter(stringWriter);
+
+        when(response.getWriter()).thenReturn(printWriter);
+
+        authenticationEntryPoint.commence(request, response, exception);
+
+        printWriter.flush();
+        String jsonResponse = stringWriter.toString();
+
+        JSONObject jsonObject = new JSONObject(jsonResponse);
+
+        assertEquals(expectedMessage, jsonObject.getString("error"));
+    }
 }
