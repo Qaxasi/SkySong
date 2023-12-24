c4a72f238012584d6a07a886093b536be291214d
Champel Mariusz <champelmariusz@gmail.com>
2023-12-24 23:38:41 +0100

    test: Add utility method for mock request path setup

--- src/test/java/com/mycompany/SkySong/testsupport/auth/security/JwtAuthenticationFilterTestHelper.java
+++ src/test/java/com/mycompany/SkySong/testsupport/auth/security/JwtAuthenticationFilterTestHelper.java
@@ -1,4 +1,12 @@
 package com.mycompany.SkySong.testsupport.auth.security;
 
+import jakarta.servlet.http.HttpServletRequest;
+
+import static org.mockito.Mockito.when;
+
 public class JwtAuthenticationFilterTestHelper {
+    private static void setupRequestPath(HttpServletRequest request,
+                                         String path) {
+        when(request.getRequestURI()).thenReturn(path);
+    }
 }
