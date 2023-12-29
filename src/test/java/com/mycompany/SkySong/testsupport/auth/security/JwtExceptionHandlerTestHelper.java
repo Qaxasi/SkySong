248793c98754be1cef9210f684f8aded5976aa5e
Champel Mariusz <champelmariusz@gmail.com>
2023-12-29 22:40:31 +0100

    test: Add method to assert TokenException throwing in handler

--- src/test/java/com/mycompany/SkySong/testsupport/auth/security/JwtExceptionHandlerTestHelper.java
+++ src/test/java/com/mycompany/SkySong/testsupport/auth/security/JwtExceptionHandlerTestHelper.java
@@ -1,4 +1,12 @@
 package com.mycompany.SkySong.testsupport.auth.security;
 
+import com.mycompany.SkySong.auth.security.JwtExceptionHandler;
+import com.mycompany.SkySong.shared.exception.TokenException;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class JwtExceptionHandlerTestHelper {
+    public static void assertThrowsTokenException(JwtExceptionHandler handler, Exception e) {
+        assertThrows(TokenException.class, () -> handler.handleException(e));
+    }
 }
