a52892fd3c0a4f078b7ed3851d7552604645bbbe
Champel Mariusz <champelmariusz@gmail.com>
2023-12-30 17:38:13 +0100

    test: Add method to test user auth by username

--- src/test/java/com/mycompany/SkySong/testsupport/auth/service/LoginServiceIntegrationTestHelper.java
+++ src/test/java/com/mycompany/SkySong/testsupport/auth/service/LoginServiceIntegrationTestHelper.java
@@ -1,10 +1,22 @@
 package com.mycompany.SkySong.testsupport.auth.service;
 
 import com.mycompany.SkySong.auth.model.dto.LoginRequest;
+import com.mycompany.SkySong.auth.service.LoginService;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class LoginServiceIntegrationTestHelper {
     public static LoginRequest validRequest = new LoginRequest("User", "Password#3");
     public static LoginRequest invalidPassword = new LoginRequest("User", "invalid");
     public static LoginRequest invalidUsername = new LoginRequest("invalid", "Password#3");
     public static LoginRequest invalidEmail = new LoginRequest("invalid@mail.com", "Password#3");
+    public static void assertUserAuthWithUsername(String username, LoginService login) {
+        login.login(validRequest);
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        assertEquals(username, authentication.getName());
+    }
 }
