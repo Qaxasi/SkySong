f8a21f42c73a65034323d44ba6344090ae90364f
Champel Mariusz <champelmariusz@gmail.com>
2023-12-30 16:15:57 +0100

    test: Add SQL inserts for user and associated roles setup

--- src/main/resources/data_sql/test-data-setup.sql
+++ src/main/resources/data_sql/test-data-setup.sql
@@ -1,6 +1,10 @@
+ INSERT INTO users (username, email, password)
+     VALUES('User', 'mail@mail.com',
+       '$2a$10$VEbWwz6NcL4y6MgKEE/sJuWiFe2EoVbru6gJ.6Miu6G16NWfqlxci');
+
 INSERT INTO users (username, email, password)
-    VALUES('existUsername', 'existMail@mail.com',
-      '$2a$10$VEbWwz6NcL4y6MgKEE/sJuWiFe2EoVbru6gJ.6Miu6G16NWfqlxci');
+   VALUES('existUsername', 'existMail@mail.com',
+     '$2a$10$VEbWwz6NcL4y6MgKEE/sJuWiFe2EoVbru6gJ.6Miu6G16NWfqlxci');
 
 INSERT INTO users (username, email, password)
     VALUES('testAdmin', 'testAdmin@gmail.com',
@@ -13,7 +17,12 @@ INSERT INTO roles (name)
 INSERT INTO user_roles (user_id, role_id)
 SELECT u.id, r.id
 FROM users u, roles r
-WHERE u.username = 'testExistUsername' AND r.name = 'ROLE_USER';
+WHERE u.username = 'User' AND r.name = 'ROLE_USER';
+
+INSERT INTO user_roles (user_id, role_id)
+SELECT u.id, r.id
+FROM users u, roles r
+WHERE u.username = 'existUsername' AND r.name = 'ROLE_USER';
 
 INSERT INTO user_roles (user_id, role_id)
 SELECT u.id, r.id
