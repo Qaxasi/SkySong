8a567374969a19b2cb1cc744925b17aec4e54bf9
Champel Mariusz <champelmariusz@gmail.com>
2023-12-30 17:08:14 +0100

    test: Update password hash in user insert SQL statement

--- src/main/resources/data_sql/test-data-setup.sql
+++ src/main/resources/data_sql/test-data-setup.sql
@@ -1,14 +1,14 @@
  INSERT INTO users (username, email, password)
      VALUES('User', 'mail@mail.com',
-       '$2a$10$VEbWwz6NcL4y6MgKEE/sJuWiFe2EoVbru6gJ.6Miu6G16NWfqlxci');
+       '$2a$10$WeBMjqOaJviwm/DV1BRvdOl19JrCMsJyrPUCFDaP.FnxJ.sqwSFiW');
 
 INSERT INTO users (username, email, password)
    VALUES('existUsername', 'existMail@mail.com',
-     '$2a$10$VEbWwz6NcL4y6MgKEE/sJuWiFe2EoVbru6gJ.6Miu6G16NWfqlxci');
+     '$2a$10$WeBMjqOaJviwm/DV1BRvdOl19JrCMsJyrPUCFDaP.FnxJ.sqwSFiW');
 
 INSERT INTO users (username, email, password)
     VALUES('testAdmin', 'testAdmin@gmail.com',
-     '$2a$10$VEbWwz6NcL4y6MgKEE/sJuWiFe2EoVbru6gJ.6Miu6G16NWfqlxci');
+     '$2a$10$WeBMjqOaJviwm/DV1BRvdOl19JrCMsJyrPUCFDaP.FnxJ.sqwSFiW');
 
 INSERT INTO roles (name)
     VALUES('ROLE_USER'),
