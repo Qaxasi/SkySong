3ea8e4ba4b7c3a94175bfd11dd4aa809571001f5
Champel Mariusz <champelmariusz@gmail.com>
2023-12-30 14:02:23 +0100

    test: Add test for exception handling in username existence check

--- src/test/java/com/mycompany/SkySong/testsupport/auth/service/CredentialExistenceCheckerImplTestHelper.java
+++ src/test/java/com/mycompany/SkySong/testsupport/auth/service/CredentialExistenceCheckerImplTestHelper.java
@@ -1,4 +1,25 @@
 package com.mycompany.SkySong.testsupport.auth.service;
 
+
+import com.mycompany.SkySong.auth.model.dto.RegisterRequest;
+import com.mycompany.SkySong.auth.service.CredentialExistenceChecker;
+import com.mycompany.SkySong.shared.repository.UserDAO;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.when;
+
 public class CredentialExistenceCheckerImplTestHelper {
+    public static void assertUsernameException(UserDAO userDAO,
+                                               CredentialExistenceChecker checker,
+                                               String username,
+                                               boolean existUsername,
+                                               Class<? extends Exception> exception) {
+
+        RegisterRequest request = new RegisterRequest(username, "mail@mail.com", "Password#3");
+
+        when(userDAO.existsByUsername(username)).thenReturn(existUsername);
+
+        assertThrows(exception,
+                () -> checker.checkForExistingCredentials(request));
+    }
 }
