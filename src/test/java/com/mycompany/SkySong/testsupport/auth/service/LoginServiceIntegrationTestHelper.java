1edff9b5761fd80706f763f579ea9c5f408bf638
Champel Mariusz <champelmariusz@gmail.com>
2023-12-30 17:31:20 +0100

    test: Add test cases for login request validation

--- src/test/java/com/mycompany/SkySong/testsupport/auth/service/LoginServiceIntegrationTestHelper.java
+++ src/test/java/com/mycompany/SkySong/testsupport/auth/service/LoginServiceIntegrationTestHelper.java
@@ -1,4 +1,10 @@
 package com.mycompany.SkySong.testsupport.auth.service;
 
+import com.mycompany.SkySong.auth.model.dto.LoginRequest;
+
 public class LoginServiceIntegrationTestHelper {
+    public static LoginRequest validRequest = new LoginRequest("User", "Password#3");
+    public static LoginRequest invalidPassword = new LoginRequest("User", "invalid");
+    public static LoginRequest invalidUsername = new LoginRequest("invalid", "Password#3");
+    public static LoginRequest invalidEmail = new LoginRequest("invalid@mail.com", "Password#3");
 }
