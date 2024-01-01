ba2af199f39fefe97458d729278b8c7c5bc4d777
Champel Mariusz <champelmariusz@gmail.com>
2024-01-01 23:23:20 +0100

    test: Introduce authentication method in interface

--- src/main/java/com/mycompany/SkySong/auth/service/UserAuthenticationService.java
+++ src/main/java/com/mycompany/SkySong/auth/service/UserAuthenticationService.java
@@ -1,4 +1,8 @@
 package com.mycompany.SkySong.auth.service;
 
+import com.mycompany.SkySong.auth.model.dto.LoginRequest;
+import org.springframework.security.core.Authentication;
+
 public interface UserAuthenticationService {
+    Authentication authenticateUser(LoginRequest loginRequest);
 }
