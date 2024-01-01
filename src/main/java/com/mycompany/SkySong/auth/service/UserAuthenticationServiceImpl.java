07bcbedd4272ec6bba6333610de229ee6c18e607
Champel Mariusz <champelmariusz@gmail.com>
2024-01-01 23:27:47 +0100

    test: Inject AuthenticationManager dependency

--- src/main/java/com/mycompany/SkySong/auth/service/UserAuthenticationServiceImpl.java
+++ src/main/java/com/mycompany/SkySong/auth/service/UserAuthenticationServiceImpl.java
@@ -1,7 +1,13 @@
 package com.mycompany.SkySong.auth.service;
 
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserAuthenticationServiceImpl {
+    private final AuthenticationManager authenticationManager;
+
+    public UserAuthenticationServiceImpl(AuthenticationManager authenticationManager) {
+        this.authenticationManager = authenticationManager;
+    }
 }
