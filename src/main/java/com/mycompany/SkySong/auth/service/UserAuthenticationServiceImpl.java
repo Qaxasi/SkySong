ec6029e69885918b0f93275b9a73d767589eddf2
Champel Mariusz <champelmariusz@gmail.com>
2024-01-01 23:51:46 +0100

    feat: Implement interface with authentication method in service

--- src/main/java/com/mycompany/SkySong/auth/service/UserAuthenticationServiceImpl.java
+++ src/main/java/com/mycompany/SkySong/auth/service/UserAuthenticationServiceImpl.java
@@ -1,13 +1,22 @@
 package com.mycompany.SkySong.auth.service;
 
+import com.mycompany.SkySong.auth.model.dto.LoginRequest;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
 @Service
-public class UserAuthenticationServiceImpl {
+public class UserAuthenticationServiceImpl implements UserAuthenticationService {
     private final AuthenticationManager authenticationManager;
 
     public UserAuthenticationServiceImpl(AuthenticationManager authenticationManager) {
         this.authenticationManager = authenticationManager;
+    }
+    @Override
+    public Authentication authenticateUser(LoginRequest loginRequest) {
+        return authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        loginRequest.usernameOrEmail(), loginRequest.password()));
     }
 }
