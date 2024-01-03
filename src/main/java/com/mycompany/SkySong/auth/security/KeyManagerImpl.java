781500fcceb084d492af8c855a10e5c952884498
Champel Mariusz <champelmariusz@gmail.com>
2024-01-03 17:09:31 +0100

    feat: Add constructor with JWT secret in key manager class

--- src/main/java/com/mycompany/SkySong/auth/security/KeyManagerImpl.java
+++ src/main/java/com/mycompany/SkySong/auth/security/KeyManagerImpl.java
@@ -1,8 +1,15 @@
 package com.mycompany.SkySong.auth.security;
 
+import org.springframework.beans.factory.annotation.Value;
+
 import java.security.Key;
 
 public class KeyManagerImpl implements KeyManager {
+    private final String jwtSecret;
+
+    public KeyManagerImpl(@Value("${JWT_SECRET}") String jwtSecret) {
+        this.jwtSecret = jwtSecret;
+    }
     @Override
     public Key getKey() {
         return null;
