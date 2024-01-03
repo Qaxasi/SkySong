39d3079918f8553529565d1b861b4d235b30eef2
Champel Mariusz <champelmariusz@gmail.com>
2024-01-03 17:25:17 +0100

    feat: Inject dependencies

--- src/main/java/com/mycompany/SkySong/auth/security/TokenValidatorImpl.java
+++ src/main/java/com/mycompany/SkySong/auth/security/TokenValidatorImpl.java
@@ -4,6 +4,13 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class TokenValidatorImpl implements TokenValidator {
+    private final KeyManager keyManager;
+    private final JwtExceptionHandler jwtExceptionHandler;
+
+    public TokenValidatorImpl(KeyManager keyManager, JwtExceptionHandler jwtExceptionHandler) {
+        this.keyManager = keyManager;
+        this.jwtExceptionHandler = jwtExceptionHandler;
+    }
     @Override
     public boolean validateToken(String token) {
         return false;
