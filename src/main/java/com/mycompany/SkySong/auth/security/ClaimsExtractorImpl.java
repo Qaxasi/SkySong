c686d6d440db6839d0459b658114f6c9b77e9ecb
Champel Mariusz <champelmariusz@gmail.com>
2024-01-03 17:31:07 +0100

    feat: Annotate class as a spring component

--- src/main/java/com/mycompany/SkySong/auth/security/ClaimsExtractorImpl.java
+++ src/main/java/com/mycompany/SkySong/auth/security/ClaimsExtractorImpl.java
@@ -1,7 +1,9 @@
 package com.mycompany.SkySong.auth.security;
 
 import io.jsonwebtoken.Claims;
+import org.springframework.stereotype.Component;
 
+@Component
 public class ClaimsExtractorImpl implements ClaimsExtractor {
     @Override
     public Claims getClaimsFromToken(String token) {
