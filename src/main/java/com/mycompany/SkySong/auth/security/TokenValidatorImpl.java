80df42ad02aa951b09bfbfa1c765f2e3489fe957
Champel Mariusz <champelmariusz@gmail.com>
2024-01-03 17:24:18 +0100

    feat: Annotate class as a spring component

--- src/main/java/com/mycompany/SkySong/auth/security/TokenValidatorImpl.java
+++ src/main/java/com/mycompany/SkySong/auth/security/TokenValidatorImpl.java
@@ -1,5 +1,8 @@
 package com.mycompany.SkySong.auth.security;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class TokenValidatorImpl implements TokenValidator {
     @Override
     public boolean validateToken(String token) {
