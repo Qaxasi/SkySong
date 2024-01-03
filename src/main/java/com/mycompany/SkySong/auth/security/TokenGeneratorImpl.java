a0ec146976fa91dc74c4e3d2a56e87993e626674
Champel Mariusz <champelmariusz@gmail.com>
2024-01-03 17:15:45 +0100

    feat: Inject dependencies

--- src/main/java/com/mycompany/SkySong/auth/security/TokenGeneratorImpl.java
+++ src/main/java/com/mycompany/SkySong/auth/security/TokenGeneratorImpl.java
@@ -1,11 +1,22 @@
 package com.mycompany.SkySong.auth.security;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 
 @Component
 public class TokenGeneratorImpl implements TokenGenerator {
+    private final long jwtExpirationDate;
+    private final DateProvider dateProvider;
+    private final KeyManager key;
 
+    public TokenGeneratorImpl(@Value("${app-jwt-expiration-milliseconds}") long jwtExpirationDate,
+                              DateProvider dateProvider,
+                              KeyManager key) {
+        this.jwtExpirationDate = jwtExpirationDate;
+        this.dateProvider = dateProvider;
+        this.key = key;
+    }
     @Override
     public String generateToken(Authentication authentication) {
         return null;
