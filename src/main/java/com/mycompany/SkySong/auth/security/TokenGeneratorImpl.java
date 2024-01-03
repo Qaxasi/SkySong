50be7c2e428e3b4222c59384003943179331cebb
Champel Mariusz <champelmariusz@gmail.com>
2024-01-03 17:04:41 +0100

    feat: Annotate class as a spring component

--- src/main/java/com/mycompany/SkySong/auth/security/TokenGeneratorImpl.java
+++ src/main/java/com/mycompany/SkySong/auth/security/TokenGeneratorImpl.java
@@ -1,8 +1,11 @@
 package com.mycompany.SkySong.auth.security;
 
 import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
 
+@Component
 public class TokenGeneratorImpl implements TokenGenerator {
+
     @Override
     public String generateToken(Authentication authentication) {
         return null;
