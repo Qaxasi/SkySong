5377df4ded8e2b74b05a0e0fb8c0cf20b3414b9c
Champel Mariusz <champelmariusz@gmail.com>
2024-01-09 18:15:26 +0100

    test: Configure class with spring boot and testcontainers annotations

--- src/test/java/com/mycompany/SkySong/testsupport/BaseIT.java
+++ src/test/java/com/mycompany/SkySong/testsupport/BaseIT.java
@@ -1,4 +1,11 @@
 package com.mycompany.SkySong.testsupport;
 
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
+@Testcontainers
 public class BaseIT {
 }
