2ed3a296dac5dbb529f51b605348e8d1de940363
Champel Mariusz <champelmariusz@gmail.com>
2024-01-10 23:53:53 +0100

    test: Remove user and role setup method

--- src/test/java/com/mycompany/SkySong/testsupport/auth/controller/LoginControllerHelper.java
+++ src/test/java/com/mycompany/SkySong/testsupport/auth/controller/LoginControllerHelper.java
@@ -1,15 +1,5 @@
 package com.mycompany.SkySong.testsupport.auth.controller;
 
-import com.mycompany.SkySong.auth.model.entity.Role;
-import com.mycompany.SkySong.auth.model.entity.UserRole;
-import com.mycompany.SkySong.shared.entity.User;
-import com.mycompany.SkySong.shared.repository.UserDAO;
-
-import java.util.Optional;
-import java.util.Set;
-
-import static org.mockito.Mockito.when;
-
 public class LoginControllerHelper {
     public static final String validCredentials =
             "{\"usernameOrEmail\": \"mail@mail.com\",\"password\": \"Password#3\"}";
@@ -19,12 +9,5 @@ public class LoginControllerHelper {
             "{\"usernameOrEmail\": \"mail@mail.com\",\"password\": \"Password#3\"";
     public static final String emptyCredentials = "{\"usernameOrEmail\": \"\",\"password\": \"\"}";
     public static final String loginUri = "/api/v1/users/login";
-    public static void setup(UserDAO userDAO) {
-        Role role = new Role(UserRole.ROLE_USER);
-        Set<Role> roles = Set.of(role);
 
-        when(userDAO.findByEmail("mail@mail.com"))
-                .thenReturn(Optional.of(new User(1, "User", "mail@mail.com",
-                        "$2a$10$px7fuh00336krhkSkZ9cQ.FRCVpTo5MLy90cx3rSYN5tISQxsRebq", roles)));
-    }
 }
