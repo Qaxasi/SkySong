99282db9b755de368a16f6b1cb63a19862d63138
Champel Mariusz <champelmariusz@gmail.com>
2024-01-10 23:54:35 +0100

    style: Add space

--- src/test/java/com/mycompany/SkySong/testsupport/auth/controller/LoginControllerHelper.java
+++ src/test/java/com/mycompany/SkySong/testsupport/auth/controller/LoginControllerHelper.java
@@ -7,7 +7,8 @@ public class LoginControllerHelper {
             "{\"usernameOrEmail\": \"invalid\",\"password\": \"invalid\"}";
     public static final String malformedJson =
             "{\"usernameOrEmail\": \"mail@mail.com\",\"password\": \"Password#3\"";
-    public static final String emptyCredentials = "{\"usernameOrEmail\": \"\",\"password\": \"\"}";
+    public static final String emptyCredentials =
+            "{\"usernameOrEmail\": \"\",\"password\": \"\"}";
     public static final String loginUri = "/api/v1/users/login";
 
 }
