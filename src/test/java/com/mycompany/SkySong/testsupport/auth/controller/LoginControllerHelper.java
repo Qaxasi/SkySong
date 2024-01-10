0b0c1f9f900e3755a34d65e70955706c61af1036
Champel Mariusz <champelmariusz@gmail.com>
2024-01-10 23:52:13 +0100

    test: Add constant for login URI endpoint

--- src/test/java/com/mycompany/SkySong/testsupport/auth/controller/LoginControllerHelper.java
+++ src/test/java/com/mycompany/SkySong/testsupport/auth/controller/LoginControllerHelper.java
@@ -18,6 +18,7 @@ public class LoginControllerHelper {
     public static final String malformedJson =
             "{\"usernameOrEmail\": \"mail@mail.com\",\"password\": \"Password#3\"";
     public static final String emptyCredentials = "{\"usernameOrEmail\": \"\",\"password\": \"\"}";
+    public static final String loginUri = "/api/v1/users/login";
     public static void setup(UserDAO userDAO) {
         Role role = new Role(UserRole.ROLE_USER);
         Set<Role> roles = Set.of(role);
