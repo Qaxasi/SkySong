09adbc614ecf429b35aa3a715f0ff60e4ecbf099
Champel Mariusz <champelmariusz@gmail.com>
2024-01-10 17:53:19 +0100

    test: Add MySQL Testcontainer setup

--- src/test/java/com/mycompany/SkySong/testsupport/BaseIT.java
+++ src/test/java/com/mycompany/SkySong/testsupport/BaseIT.java
@@ -1,11 +1,23 @@
 package com.mycompany.SkySong.testsupport;
 
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("test")
 @Testcontainers
 public class BaseIT {
+    @Container
+    private static MySQLContainer<?> container = new MySQLContainer<>("mysql:8.2.0");
+
+    @DynamicPropertySource
+    public static void containerConfig(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", container::getJdbcUrl);
+        registry.add("spring.datasource.username", container::getUsername);
+        registry.add("spring.datasource.password", container::getPassword);
+    }
 }
