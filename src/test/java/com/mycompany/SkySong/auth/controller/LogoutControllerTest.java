afb295ae78680f7147cfbf693d221caf8517f76b
Champel Mariusz <champelmariusz@gmail.com>
2024-01-12 21:19:20 +0100

    test: Extend BaseIT in LogoutControllerTest

--- src/test/java/com/mycompany/SkySong/auth/controller/LogoutControllerTest.java
+++ src/test/java/com/mycompany/SkySong/auth/controller/LogoutControllerTest.java
@@ -3,10 +3,13 @@ package com.mycompany.SkySong.auth.controller;
 import com.mycompany.SkySong.auth.security.*;
 import com.mycompany.SkySong.auth.security.JwtAuthenticationEntryPoint;
 import com.mycompany.SkySong.shared.service.ApplicationMessageService;
+import com.mycompany.SkySong.testsupport.BaseIT;
+import com.mycompany.SkySong.testsupport.auth.controller.LoginHelper;
 import jakarta.servlet.http.Cookie;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
@@ -14,25 +17,11 @@ import org.springframework.test.web.servlet.MockMvc;
 import static com.mycompany.SkySong.testsupport.auth.controller.CookieAssertions.assertCookieIsDeleted;
 import static com.mycompany.SkySong.testsupport.auth.controller.LogoutControllerTestHelper.*;
 
-@WebMvcTest(LogoutController.class)
-public class LogoutControllerTest {
+@AutoConfigureMockMvc
+public class LogoutControllerTest extends BaseIT {
     @Autowired
     private MockMvc mockMvc;
-    @MockBean
-    private CookieDeleter cookieDeleter;
-    @MockBean
-    private ApplicationMessageService messageService;
-    @MockBean
-    private JwtTokenProvider tokenProvider;
-    @MockBean
-    private CookieRetriever cookieRetriever;
-    @MockBean
-    private JwtAuthenticationEntryPoint authEntryPoint;
     private final String endpoint = "/api/v1/users/logout";
-    @BeforeEach
-    void setup() {
-        setupCookieDeletion(cookieDeleter, "auth_token");
-    }
     @Test
     void whenSuccessfulLogout_ReturnStatusOk() throws Exception {
         Cookie mockCookie = mockedTokenCookie();
