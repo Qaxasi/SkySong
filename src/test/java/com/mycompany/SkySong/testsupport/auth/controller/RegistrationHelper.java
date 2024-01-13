9c61ab193111e61756da1a6a18942aa2ef610cf7
Champel Mariusz <champelmariusz@gmail.com>
2024-01-13 02:04:06 +0100

    test: Rename constants

--- src/test/java/com/mycompany/SkySong/testsupport/auth/controller/RegistrationHelper.java
+++ src/test/java/com/mycompany/SkySong/testsupport/auth/controller/RegistrationHelper.java
@@ -1,12 +1,12 @@
 package com.mycompany.SkySong.testsupport.auth.controller;
 
 public class RegistrationHelper {
-    public static final String VALID_REQUEST =
+    public static final String validRequest =
             "{\"username\": \"New\", \"email\": \"new@mail.com\", \"password\": \"Password#3\"}";
-    public static final String INVALID_CREDENTIALS =
+    public static final String invalidRequest =
             "{\"username\": \"invalid\", \"email\": \"invalid\", \"password\": \"invalid\"}";
-    public static final String MALFORMED_REQUEST =
+    public static final String malformedRequest =
             "{\"username\": \"New\", \"email\": \"new@mail.com\", \"password\": \"Password#3\"";
-    public static final String EMPTY_CREDENTIALS =
+    public static final String emptyCredentials =
             "{\"username\": \"\", \"email\": \"\", \"password\": \"\"}";
 }
