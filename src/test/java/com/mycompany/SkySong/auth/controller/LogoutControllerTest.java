6958d71b351754478678872609c39549c52ef364
Champel Mariusz <champelmariusz@gmail.com>
2024-01-13 01:12:29 +0100

    refactor: Delete unused imports

--- src/test/java/com/mycompany/SkySong/auth/controller/LogoutControllerTest.java
+++ src/test/java/com/mycompany/SkySong/auth/controller/LogoutControllerTest.java
@@ -1,26 +1,16 @@
 package com.mycompany.SkySong.auth.controller;
 
-import com.mycompany.SkySong.auth.security.*;
-import com.mycompany.SkySong.auth.security.JwtAuthenticationEntryPoint;
-import com.mycompany.SkySong.shared.service.ApplicationMessageService;
 import com.mycompany.SkySong.testsupport.BaseIT;
 import com.mycompany.SkySong.testsupport.auth.controller.LoginHelper;
 import jakarta.servlet.http.Cookie;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
-import static com.mycompany.SkySong.testsupport.auth.controller.CookieAssertions.assertCookieIsDeleted;
-import static com.mycompany.SkySong.testsupport.auth.controller.LogoutControllerTestHelper.*;
-import static org.junit.Assert.assertNull;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
