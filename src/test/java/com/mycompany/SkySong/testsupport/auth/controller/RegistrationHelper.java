1190cac35dbbb76d920bfdcf23e8e4feb2faa62d
Champel Mariusz <champelmariusz@gmail.com>
2024-01-13 02:02:28 +0100

    test: Update constants with new user details

--- src/test/java/com/mycompany/SkySong/testsupport/auth/controller/RegistrationHelper.java
+++ src/test/java/com/mycompany/SkySong/testsupport/auth/controller/RegistrationHelper.java
@@ -2,11 +2,11 @@ package com.mycompany.SkySong.testsupport.auth.controller;
 
 public class RegistrationHelper {
     public static final String VALID_REQUEST =
-            "{\"username\": \"User\", \"email\": \"mail@mail.com\", \"password\": \"Password#3\"}";
+            "{\"username\": \"New\", \"email\": \"new@mail.com\", \"password\": \"Password#3\"}";
     public static final String INVALID_CREDENTIALS =
             "{\"username\": \"invalid\", \"email\": \"invalid\", \"password\": \"invalid\"}";
     public static final String MALFORMED_REQUEST =
-            "{\"username\": \"User\", \"email\": \"mail@mail.com\", \"password\": \"Password#3\"";
+            "{\"username\": \"New\", \"email\": \"new@mail.com\", \"password\": \"Password#3\"";
     public static final String EMPTY_CREDENTIALS =
             "{\"username\": \"\", \"email\": \"\", \"password\": \"\"}";
 }
