8a915792830584d430769dd6bb86c52f0972a9dd
Champel Mariusz <champelmariusz@gmail.com>
2024-01-14 22:01:05 +0100

    test: Inject dependencies into test class

--- src/test/java/com/mycompany/SkySong/auth/security/ClaimsExtractorImplTest.java
+++ src/test/java/com/mycompany/SkySong/auth/security/ClaimsExtractorImplTest.java
@@ -1,8 +1,14 @@
 package com.mycompany.SkySong.auth.security;
 
+import com.mycompany.SkySong.auth.service.LoginService;
 import com.mycompany.SkySong.testsupport.BaseIT;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+
 public class ClaimsExtractorImplTest extends BaseIT {
-
-
+    @Autowired
+    private ClaimsExtractor claimsExtractor;
+    @Autowired
+    private LoginService login;
 }
