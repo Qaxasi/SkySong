bb94753e91859fd05330b21972631e9627e1f2a6
Champel Mariusz <champelmariusz@gmail.com>
2024-01-14 22:07:06 +0100

    test: Add login utility method

--- src/test/java/com/mycompany/SkySong/auth/security/ClaimsExtractorImplTest.java
+++ src/test/java/com/mycompany/SkySong/auth/security/ClaimsExtractorImplTest.java
@@ -1,14 +1,18 @@
 package com.mycompany.SkySong.auth.security;
 
+import com.mycompany.SkySong.auth.model.dto.LoginRequest;
 import com.mycompany.SkySong.auth.service.LoginService;
 import com.mycompany.SkySong.testsupport.BaseIT;
 
 import org.springframework.beans.factory.annotation.Autowired;
-
 
 public class ClaimsExtractorImplTest extends BaseIT {
     @Autowired
     private ClaimsExtractor claimsExtractor;
     @Autowired
     private LoginService login;
+
+    private String loginAndGetToken() {
+        return login.login(new LoginRequest("User", "Password#3"));
+    }
 }
