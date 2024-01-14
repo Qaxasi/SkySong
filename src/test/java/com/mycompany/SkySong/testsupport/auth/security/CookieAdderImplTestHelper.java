095d482492b9e1e4717ca9086a4b612543e6bd48
Champel Mariusz <champelmariusz@gmail.com>
2024-01-14 23:39:05 +0100

    test: Delete unused utility method

--- src/test/java/com/mycompany/SkySong/testsupport/auth/security/CookieAdderImplTestHelper.java
+++ src/test/java/com/mycompany/SkySong/testsupport/auth/security/CookieAdderImplTestHelper.java
@@ -1,23 +1,10 @@
 package com.mycompany.SkySong.testsupport.auth.security;
 
-import com.mycompany.SkySong.auth.security.CookieAdder;
 import jakarta.servlet.http.Cookie;
-import jakarta.servlet.http.HttpServletResponse;
-import org.mockito.ArgumentCaptor;
-
-import static org.mockito.Mockito.verify;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class CookieAdderImplTestHelper {
-    public static Cookie addCookie(CookieAdder cookieAdder, HttpServletResponse response, String name,
-                                   String value, int maxAge) {
-        cookieAdder.addCookie(response, name, value, maxAge);
-
-        ArgumentCaptor<Cookie> cookieCaptor = ArgumentCaptor.forClass(Cookie.class);
-        verify(response).addCookie(cookieCaptor.capture());
-        return cookieCaptor.getValue();
-    }
     public static void assertCookieProperties(Cookie cookie, String expectedName, String expectedValue,
                                               int expectedMaxAge, String expectedPath, boolean isHttpOnly,
                                               boolean isSecure) {
