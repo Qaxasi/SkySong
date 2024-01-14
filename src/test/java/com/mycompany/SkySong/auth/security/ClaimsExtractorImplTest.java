c6da8ff449250898b37155e2ee5e15b6467f962a
Champel Mariusz <champelmariusz@gmail.com>
2024-01-14 22:10:36 +0100

    test: Add test to verify username extraction from token

--- src/test/java/com/mycompany/SkySong/auth/security/ClaimsExtractorImplTest.java
+++ src/test/java/com/mycompany/SkySong/auth/security/ClaimsExtractorImplTest.java
@@ -4,13 +4,27 @@ import com.mycompany.SkySong.auth.model.dto.LoginRequest;
 import com.mycompany.SkySong.auth.service.LoginService;
 import com.mycompany.SkySong.testsupport.BaseIT;
 
+import io.jsonwebtoken.Claims;
+import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ClaimsExtractorImplTest extends BaseIT {
     @Autowired
     private ClaimsExtractor claimsExtractor;
     @Autowired
     private LoginService login;
+
+    @Test
+    void whenTokenExtracted_UsernameMatches() throws Exception {
+        String token = loginAndGetToken();
+
+        Claims claims = claimsExtractor.getClaimsFromToken(token);
+        String username = claims.getSubject();
+
+        assertEquals(username, "User");
+    }
 
     private String loginAndGetToken() {
         return login.login(new LoginRequest("User", "Password#3"));
