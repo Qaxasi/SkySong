74c768c895b75e6f3b6542acda8db3507523c015
Champel Mariusz <champelmariusz@gmail.com>
2024-01-19 00:08:46 +0100

    test: Add setUp method

--- src/test/java/com/mycompany/SkySong/auth/security/CookieRetrieverImplTest.java
+++ src/test/java/com/mycompany/SkySong/auth/security/CookieRetrieverImplTest.java
@@ -2,20 +2,27 @@ package com.mycompany.SkySong.auth.security;
 
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.mock.web.MockHttpServletRequest;
 
 import static com.mycompany.SkySong.testsupport.auth.security.CookieRetrieverImplTestHelper.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class CookieRetrieverImplTest {
 
-    private CookieRetrieverImpl cookieRetriever;
+    private CookieRetrieverImpl retriever;
 
     private HttpServletRequest request;
+    @BeforeEach
+    void setUp() {
+        retriever = new CookieRetrieverImpl();
+        request = new MockHttpServletRequest();
+    }
 
     @Test
     void whenNoCookieInRequest_ReturnOptionalEmpty() {
