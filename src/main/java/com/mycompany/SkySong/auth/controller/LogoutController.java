e89bfe1d981150f1682af50b825f7f636f8ecebb
Champel Mariusz <champelmariusz@gmail.com>
2024-01-18 21:47:46 +0100

    refactor: Remove logging and exception handling from controller

--- src/main/java/com/mycompany/SkySong/auth/controller/LogoutController.java
+++ src/main/java/com/mycompany/SkySong/auth/controller/LogoutController.java
@@ -2,16 +2,14 @@ package com.mycompany.SkySong.auth.controller;
 
 import com.mycompany.SkySong.auth.security.CookieDeleter;
 import com.mycompany.SkySong.shared.dto.ApiResponse;
-import com.mycompany.SkySong.shared.exception.InternalErrorException;
 import com.mycompany.SkySong.shared.service.ApplicationMessageService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-@Slf4j
+
 @RestController
 @RequestMapping("/api/v1/users")
 public class LogoutController {
@@ -23,12 +21,7 @@ public class LogoutController {
     }
     @PostMapping("/logout")
     public ResponseEntity<?> logout(HttpServletRequest request, HttpServletResponse response) {
-        try {
-            cookieDeleter.deleteCookie(request, response, "auth_token");
-            return ResponseEntity.ok(new ApiResponse(messageService.getMessage("logout.success")));
-        } catch (RuntimeException e) {
-            log.error("Error while logging out: " + e.getMessage(), e);
-            throw new InternalErrorException(messageService.getMessage("logout.failure"));
-        }
+        cookieDeleter.deleteCookie(request, response, "auth_token");
+        return ResponseEntity.ok(new ApiResponse(messageService.getMessage("logout.success")));
     }
 }
