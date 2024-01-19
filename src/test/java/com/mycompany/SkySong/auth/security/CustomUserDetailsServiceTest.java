9e657d0bdf9872e49c47ac65939db14fad755c56
Champel Mariusz <champelmariusz@gmail.com>
2024-01-19 02:06:27 +0100

    test: Update tests

--- src/test/java/com/mycompany/SkySong/auth/security/CustomUserDetailsServiceTest.java
+++ src/test/java/com/mycompany/SkySong/auth/security/CustomUserDetailsServiceTest.java
@@ -24,46 +24,51 @@ public class CustomUserDetailsServiceTest extends BaseIT {
 
     @Test
     void whenMissingUserByUsername_ThrowException() {
-        setupNonExistentUserByUsername(userDAO, "mark");
-
         assertThrows(UsernameNotFoundException.class,
-                () -> customUserDetailsService.loadUserByUsername("mark"));
+                () -> detailsService.loadUserByUsername("mark"));
     }
     @Test
     void whenMissingUserByEmail_ThrowException() {
-        setupNonExistentUserByEmail(userDAO, "mark@mail.com");
 
         //In our implementation, the email can serve as the username
         assertThrows(UsernameNotFoundException.class,
-                () -> customUserDetailsService.loadUserByUsername("mark@mail.com"));
+                () -> detailsService.loadUserByUsername("mark@mail.com"));
     }
     @Test
     void whenUserRegular_AdminRoleIsNotAssigned() {
-        UserDetails userDetails = setupAndLoadRegularUserByUsername(userDAO, customUserDetailsService);
+        //when
+        UserDetails userDetails = detailsService.loadUserByUsername("User");
 
+        //then
         assertUserDoesNotHaveAuthorities(userDetails, "ROLE_ADMIN");
     }
     @Test
     void whenLoadedRegularUserByUsername_AssignsUserRole() {
-        UserDetails userDetails = setupAndLoadRegularUserByUsername(userDAO, customUserDetailsService);
+        //when
+        UserDetails userDetails = detailsService.loadUserByUsername("User");
 
+        //then
         assertUserHasAuthorities(userDetails, "ROLE_USER");
     }
     @Test
     void whenLoadedRegularUserByEmail_AssignsUserRole() {
-        UserDetails userDetails = setupAndLoadRegularUserByEmail(userDAO, customUserDetailsService);
+        //when
+        UserDetails userDetails = detailsService.loadUserByUsername("mail@mail.com");
 
+        //then
         assertUserHasAuthorities(userDetails, "ROLE_USER");
     }
     @Test
     void whenLoadedAdminUserByUsername_AssignsUserAndAdminRole() {
-        UserDetails userDetails = setupAndLoadAdminUserByUsername(userDAO, customUserDetailsService);
+        UserDetails userDetails = detailsService.loadUserByUsername("testAdmin");
 
         assertUserHasAuthorities(userDetails, "ROLE_USER", "ROLE_ADMIN");
     }
     @Test
     void whenLoadedAdminUserByEmail_AssignsUserAndAdminRole() {
-        UserDetails userDetails = setupAndLoadAdminUserByEmail(userDAO, customUserDetailsService);
+
+        //In our implementation, the email can serve as the username
+        UserDetails userDetails = detailsService.loadUserByUsername("testAdmin@mail.com");
 
         assertUserHasAuthorities(userDetails, "ROLE_USER", "ROLE_ADMIN");
     }
