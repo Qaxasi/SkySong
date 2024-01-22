8b6aa78159679a71372a4de006ff4bbf19ad4a3e
Champel Mariusz <champelmariusz@gmail.com>
2024-01-23 00:47:14 +0100

    test: Remove registrationUri constant

--- src/test/java/com/mycompany/SkySong/testsupport/auth/controller/RegistrationControllerHelper.java
+++ src/test/java/com/mycompany/SkySong/testsupport/auth/controller/RegistrationControllerHelper.java
@@ -1,7 +1,6 @@
 package com.mycompany.SkySong.testsupport.auth.controller;
 
 public class RegistrationControllerHelper {
-    public static final String registrationUri = "/api/v1/users/register";
     public static final String validCredentials =
             "{\"username\": \"New\", \"email\": \"new@mail.com\", \"password\": \"Password#3\"}";
     public static final String invalidCredentials =
