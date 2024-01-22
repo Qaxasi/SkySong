ef537a3f7fd52ff70da38d99a5647d1aee6c9645
Champel Mariusz <champelmariusz@gmail.com>
2024-01-22 16:49:14 +0100

    test: Add TokenValidator dependency

--- src/test/java/com/mycompany/SkySong/auth/security/TokenValidatorTest.java
+++ src/test/java/com/mycompany/SkySong/auth/security/TokenValidatorTest.java
@@ -1,6 +1,19 @@
 package com.mycompany.SkySong.auth.security;
 
+import com.mycompany.SkySong.shared.exception.TokenException;
 import com.mycompany.SkySong.testsupport.BaseIT;
+import com.mycompany.SkySong.testsupport.auth.security.TokenValidatorTestHelper;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.security.Key;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class TokenValidatorTest extends BaseIT {
+    @Autowired
+    private TokenValidator validator;
+
 }
