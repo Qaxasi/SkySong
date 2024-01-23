84c53cc0583c6c4d3c59ffad696a90f60badb75b
Champel Mariusz <champelmariusz@gmail.com>
2024-01-23 20:22:45 +0100

    test: Reorder annotations in record

--- src/main/java/com/mycompany/SkySong/auth/model/dto/RegisterRequest.java
+++ src/main/java/com/mycompany/SkySong/auth/model/dto/RegisterRequest.java
@@ -2,10 +2,10 @@ package com.mycompany.SkySong.auth.model.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotEmpty;
-public record RegisterRequest(@JsonProperty @NotEmpty(message = "The username field cannot be empty")
-                              String username,
-                              @JsonProperty @NotEmpty(message = "The email field cannot be empty")
-                              String email,
-                              @JsonProperty @NotEmpty(message = "The password field cannot be empty")
-                              String password) {
+public record RegisterRequest(@NotEmpty(message = "The username field cannot be empty")
+                              @JsonProperty String username,
+                              @NotEmpty(message = "The email field cannot be empty")
+                              @JsonProperty String email,
+                              @NotEmpty(message = "The password field cannot be empty")
+                              @JsonProperty String password) {
 }
