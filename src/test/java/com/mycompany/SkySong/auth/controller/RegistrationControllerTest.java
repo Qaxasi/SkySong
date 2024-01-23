2f506b2288de366e80d9f7f189963211f40190f4
Champel Mariusz <champelmariusz@gmail.com>
2024-01-23 01:29:41 +0100

    test: Refactor test for readability

--- src/test/java/com/mycompany/SkySong/auth/controller/RegistrationControllerTest.java
+++ src/test/java/com/mycompany/SkySong/auth/controller/RegistrationControllerTest.java
@@ -1,7 +1,7 @@
 package com.mycompany.SkySong.auth.controller;
 
 import com.mycompany.SkySong.testsupport.BaseIT;
-import com.mycompany.SkySong.testsupport.auth.controller.RegistrationControllerHelper;
+import com.mycompany.SkySong.testsupport.auth.controller.RegistrationRequests;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -10,7 +10,6 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.transaction.annotation.Transactional;
 
-import static com.mycompany.SkySong.testsupport.auth.controller.RegistrationControllerHelper.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -23,39 +22,60 @@ public class RegistrationControllerTest extends BaseIT {
     @Test
     @Transactional
     void whenRegistrationSuccess_Return201() throws Exception {
+        // given
+        String registrationJson = RegistrationRequests.validCredentials;
+
+        // when & then
         mockMvc.perform(post(registrationUri)
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content(validCredentials))
+                        .content(registrationJson))
                 .andExpect(status().is(201));
     }
     @Test
     @Transactional
     void whenRegistrationSuccess_ReturnCorrectFieldName() throws Exception {
+        // given
+        String registrationJson = RegistrationRequests.validCredentials;
+
+        // when & then
         mockMvc.perform(post(registrationUri)
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content(validCredentials))
+                        .content(registrationJson))
                 .andExpect(jsonPath("$.message").isNotEmpty());
     }
     @Test
     void whenInvalidCredentials_ReturnBadRequest() throws Exception {
+        // given
+        String registrationJson = RegistrationRequests.invalidCredentials;
+
+        // when & then
         mockMvc.perform(post(registrationUri)
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content(invalidCredentials))
+                        .content(registrationJson))
                 .andExpect(status().is(400));
     }
     @Test
     void whenMalformedRequest_ReturnBadRequest() throws Exception {
+        // given
+        String registrationJson = RegistrationRequests.malformedRequest;
+
+        // when & then
         mockMvc.perform(post(registrationUri)
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content(malformedRequest))
+                        .content(registrationJson))
                 .andExpect(status().is(400));
     }
     @Test
     void whenEmptyCredentials_ReturnErrorMessages() throws Exception {
+        // given
+        String registrationRequest = RegistrationRequests.emptyCredentials;
+
+        // when
         ResultActions actions = mockMvc.perform(post(registrationUri)
                 .contentType(MediaType.APPLICATION_JSON)
-                .content(emptyCredentials));
+                .content(registrationRequest));
 
+        // then
         actions.andExpect(jsonPath("$.errors.username")
                         .value("The username field cannot be empty"))
                 .andExpect(jsonPath("$.errors.email")
