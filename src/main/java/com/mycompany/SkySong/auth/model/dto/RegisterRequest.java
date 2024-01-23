5b6a481f803bf21a106172e71de2db9822ead0f0
Champel Mariusz <champelmariusz@gmail.com>
2024-01-23 20:19:49 +0100

    refactor: Annotate RegisterRequest fields with @JsonProperty

--- src/main/java/com/mycompany/SkySong/auth/model/dto/RegisterRequest.java
+++ src/main/java/com/mycompany/SkySong/auth/model/dto/RegisterRequest.java
@@ -1,10 +1,11 @@
 package com.mycompany.SkySong.auth.model.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotEmpty;
-public record RegisterRequest(@NotEmpty(message = "The username field cannot be empty")
+public record RegisterRequest(@JsonProperty @NotEmpty(message = "The username field cannot be empty")
                               String username,
-                              @NotEmpty(message = "The email field cannot be empty")
+                              @JsonProperty @NotEmpty(message = "The email field cannot be empty")
                               String email,
-                              @NotEmpty(message = "The password field cannot be empty")
+                              @JsonProperty @NotEmpty(message = "The password field cannot be empty")
                               String password) {
 }
