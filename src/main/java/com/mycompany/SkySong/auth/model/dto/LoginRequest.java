f83905753405699f9b8206747bfc02681bad23e7
Champel Mariusz <champelmariusz@gmail.com>
2024-01-23 20:21:00 +0100

    refactor: Annotate LoginRequest fields with @JsonProperty

--- src/main/java/com/mycompany/SkySong/auth/model/dto/LoginRequest.java
+++ src/main/java/com/mycompany/SkySong/auth/model/dto/LoginRequest.java
@@ -1,10 +1,11 @@
 package com.mycompany.SkySong.auth.model.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotEmpty;
 
 public record LoginRequest(
         @NotEmpty(message = "The usernameOrEmail field cannot be empty")
-        String usernameOrEmail,
+        @JsonProperty String usernameOrEmail,
         @NotEmpty(message = "The password field cannot be empty")
-        String password) {
+        @JsonProperty String password) {
 }
