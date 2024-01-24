f6533381aa031675a6ee8584e184d74944874211
Champel Mariusz <champelmariusz@gmail.com>
2024-01-25 00:27:25 +0100

    test: Inject dependency

--- src/test/java/com/mycompany/SkySong/auth/security/ClaimsExtractorImplTest.java
+++ src/test/java/com/mycompany/SkySong/auth/security/ClaimsExtractorImplTest.java
@@ -1,7 +1,7 @@
 package com.mycompany.SkySong.auth.security;
 
-import com.mycompany.SkySong.auth.model.dto.LoginRequest;
 import com.mycompany.SkySong.testsupport.BaseIT;
+import com.mycompany.SkySong.testsupport.TokenGeneratorHelper;
 import io.jsonwebtoken.Claims;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.junit.jupiter.api.Test;
@@ -11,6 +11,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class ClaimsExtractorImplTest extends BaseIT {
     @Autowired
     private ClaimsExtractor claimsExtractor;
+    @Autowired
+    private TokenGeneratorHelper tokenHelper;
     @Test
     void whenTokenExtracted_UsernameMatches() {
         //given
