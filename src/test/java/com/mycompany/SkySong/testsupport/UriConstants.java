b17428960c5b1cd2c742600263f516c2c91bbe7f
Champel Mariusz <champelmariusz@gmail.com>
2024-01-24 22:11:55 +0100

    test: Add constant for logout URI

--- src/test/java/com/mycompany/SkySong/testsupport/UriConstants.java
+++ src/test/java/com/mycompany/SkySong/testsupport/UriConstants.java
@@ -2,4 +2,6 @@ package com.mycompany.SkySong.testsupport;
 
 public class UriConstants {
     public static final String LOGIN_URI = "/api/v1/users/login";
+    public static final String LOGOUT_URI = "/api/v1/users/logout";
+
 }
