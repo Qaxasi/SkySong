213402e5837f116de47263b7c3f444ffc4b6d173
Champel Mariusz <champelmariusz@gmail.com>
2024-01-24 16:45:34 +0100

    test: Refactor LoginRequests to use LoginRequest object

--- src/test/java/com/mycompany/SkySong/testsupport/auth/controller/LoginRequests.java
+++ src/test/java/com/mycompany/SkySong/testsupport/auth/controller/LoginRequests.java
@@ -1,12 +1,16 @@
 package com.mycompany.SkySong.testsupport.auth.controller;
 
+import com.mycompany.SkySong.auth.model.dto.LoginRequest;
+
 public class LoginRequests {
-    public static final String validCredentials =
-            "{\"usernameOrEmail\": \"User\",\"password\": \"Password#3\"}";
-    public static final String invalidCredentials =
-            "{\"usernameOrEmail\": \"invalid\",\"password\": \"invalid\"}";
-    public static final String malformedJson =
+    public static final LoginRequest VALID_CREDENTIALS =
+            new LoginRequest("User", "Password#3");
+    public static final LoginRequest INVALID_CREDENTIALS =
+            new LoginRequest("invalid", "invalid");
+    public static final LoginRequest EMPTY_CREDENTIALS =
+            new LoginRequest("", "");
+
+
+    public static final String MALFORMED_JSON =
             "{\"usernameOrEmail\": \"mail@mail.com\",\"password\": \"Password#3\"";
-    public static final String emptyCredentials =
-            "{\"usernameOrEmail\": \"\",\"password\": \"\"}";
 }
