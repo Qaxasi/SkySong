3c567bd5364dddf25dccf2b88837241a820046dc
Champel Mariusz <champelmariusz@gmail.com>
2024-01-24 15:45:47 +0100

    test: Refactor RegistrationRequests to use RegisterRequest object

--- src/test/java/com/mycompany/SkySong/testsupport/auth/controller/RegistrationRequests.java
+++ src/test/java/com/mycompany/SkySong/testsupport/auth/controller/RegistrationRequests.java
@@ -1,12 +1,16 @@
 package com.mycompany.SkySong.testsupport.auth.controller;
 
+import com.mycompany.SkySong.auth.model.dto.RegisterRequest;
+
 public class RegistrationRequests {
-    public static final String validCredentials =
-            "{\"username\": \"New\", \"email\": \"new@mail.com\", \"password\": \"Password#3\"}";
-    public static final String invalidCredentials =
-            "{\"username\": \"invalid\", \"email\": \"invalid\", \"password\": \"invalid\"}";
-    public static final String malformedRequest =
+    public static final RegisterRequest VALID_CREDENTIALS =
+            new RegisterRequest("New", "new@mail.com", "Password#3");
+    public static final RegisterRequest INVALID_CREDENTIALS =
+            new RegisterRequest("invalid", "invalid", "invalid");
+    public static final RegisterRequest EMPTY_CREDENTIALS =
+            new RegisterRequest("", "", "");
+
+
+    public static final String MALFORMED_REQUEST =
             "{\"username\": \"New\", \"email\": \"new@mail.com\", \"password\": \"Password#3\"";
-    public static final String emptyCredentials =
-            "{\"username\": \"\", \"email\": \"\", \"password\": \"\"}";
 }
