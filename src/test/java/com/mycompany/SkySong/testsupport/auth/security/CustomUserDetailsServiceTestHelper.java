5c1927a0f734497de82e4c95022e40b3530db16a
Champel Mariusz <champelmariusz@gmail.com>
2024-01-25 00:37:23 +0100

    test: Remove unnecessary utility methods

--- src/test/java/com/mycompany/SkySong/testsupport/auth/security/CustomUserDetailsServiceTestHelper.java
+++ src/test/java/com/mycompany/SkySong/testsupport/auth/security/CustomUserDetailsServiceTestHelper.java
@@ -1,8 +1,5 @@
 package com.mycompany.SkySong.testsupport.auth.security;
 
-import com.mycompany.SkySong.auth.security.CustomUserDetailsService;
-import com.mycompany.SkySong.shared.entity.User;
-import com.mycompany.SkySong.shared.repository.UserDAO;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
@@ -10,8 +7,6 @@ import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.stream.Collectors;
-
-import static com.mycompany.SkySong.testsupport.common.UserTestConfigurator.*;
 
 public class CustomUserDetailsServiceTestHelper {
     public static void assertUserHasAuthorities(UserDetails userDetails, String... authorities) {
@@ -36,29 +31,5 @@ public class CustomUserDetailsServiceTestHelper {
                 throw new AssertionError("User have forbidden authority: " + authority);
             }
         }
-    }
-    public static UserDetails setupAndLoadRegularUserByUsername(UserDAO userDAO,
-                                                                CustomUserDetailsService detailsService) {
-        User regularUser = createRegularUser();
-        setupExistingUserByUsername(userDAO, regularUser);
-        return detailsService.loadUserByUsername(regularUser.getUsername());
-    }
-    public static UserDetails setupAndLoadRegularUserByEmail(UserDAO userDAO,
-                                                             CustomUserDetailsService detailsService) {
-        User regularUser = createRegularUser();
-        setupExistingUserByEmail(userDAO, regularUser);
-        return detailsService.loadUserByUsername(regularUser.getEmail());
-    }
-    public static UserDetails setupAndLoadAdminUserByUsername(UserDAO userDAO,
-                                                              CustomUserDetailsService detailsService) {
-        User adminUser = createAdminUser();
-        setupExistingUserByUsername(userDAO, adminUser);
-        return detailsService.loadUserByUsername(adminUser.getUsername());
-    }
-    public static UserDetails setupAndLoadAdminUserByEmail(UserDAO userDAO,
-                                                              CustomUserDetailsService detailsService) {
-        User adminUser = createAdminUser();
-        setupExistingUserByEmail(userDAO, adminUser);
-        return detailsService.loadUserByUsername(adminUser.getEmail());
     }
 }
