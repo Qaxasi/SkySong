59a23302de5614a337e55f988f5217221b146ed1
Champel Mariusz <champelmariusz@gmail.com>
2024-01-24 23:25:04 +0100

    test: Add utility method to simulate login and obtain auth cookie

--- src/test/java/com/mycompany/SkySong/testsupport/auth/controller/LogoutHelper.java
+++ src/test/java/com/mycompany/SkySong/testsupport/auth/controller/LogoutHelper.java
@@ -1,11 +1,30 @@
 package com.mycompany.SkySong.testsupport.auth.controller;
 
+import com.mycompany.SkySong.testsupport.UriConstants;
+import jakarta.servlet.http.Cookie;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.stereotype.Component;
 import org.springframework.test.web.servlet.MockMvc;
+
+import static com.mycompany.SkySong.testsupport.JsonUtils.asJsonString;
+import static com.mycompany.SkySong.testsupport.UriConstants.LOGIN_URI;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 @Component
 public class LogoutHelper {
     @Autowired
     private MockMvc mockMvc;
+    public Cookie loginAndGetCookie() throws Exception {
+        MockHttpServletResponse response =
+                mockMvc.perform(post(LOGIN_URI)
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(asJsonString(LoginRequests.VALID_CREDENTIALS)))
+                        .andExpect(status().isOk())
+                        .andReturn().getResponse();
 
+        return response.getCookie("auth_token");
+    }
 }
