be5b0ed4bb3a38c9e4074b1535b201ddd2229a7f
Champel Mariusz <champelmariusz@gmail.com>
2024-01-24 17:29:38 +0100

    test: Inject MockMvc dependencies

--- src/test/java/com/mycompany/SkySong/testsupport/auth/controller/LogoutHelper.java
+++ src/test/java/com/mycompany/SkySong/testsupport/auth/controller/LogoutHelper.java
@@ -1,4 +1,10 @@
 package com.mycompany.SkySong.testsupport.auth.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.web.servlet.MockMvc;
+
 public class LogoutHelper {
+    @Autowired
+    private MockMvc mockMvc;
+    
 }
