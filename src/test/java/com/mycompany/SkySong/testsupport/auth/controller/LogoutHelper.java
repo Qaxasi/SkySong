1c2474d4c6e9d5f2940ce73bc0dd3a9ab71d86ba
Champel Mariusz <champelmariusz@gmail.com>
2024-01-24 21:49:54 +0100

    test: Annotate class as a spring component

--- src/test/java/com/mycompany/SkySong/testsupport/auth/controller/LogoutHelper.java
+++ src/test/java/com/mycompany/SkySong/testsupport/auth/controller/LogoutHelper.java
@@ -1,10 +1,11 @@
 package com.mycompany.SkySong.testsupport.auth.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import org.springframework.test.web.servlet.MockMvc;
-
+@Component
 public class LogoutHelper {
     @Autowired
     private MockMvc mockMvc;
-    
+
 }
