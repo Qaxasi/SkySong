365f180eb3b63724234b518f8c57e853a2e5aa28
Champel Mariusz <champelmariusz@gmail.com>
2024-01-24 23:21:08 +0100

    refactor: Update SQL script

--- src/main/resources/data_sql/test-setup.sql
+++ src/main/resources/data_sql/test-setup.sql
@@ -19,17 +19,17 @@
      FOREIGN KEY (role_id) REFERENCES roles (id)
  );
 
- INSERT INTO users (username, email, password)
-     VALUES('User', 'mail@mail.com',
-       '$2a$10$WeBMjqOaJviwm/DV1BRvdOl19JrCMsJyrPUCFDaP.FnxJ.sqwSFiW');
+INSERT INTO users (username, email, password)
+    VALUES('User', 'mail@mail.com',
+        '$2a$10$WeBMjqOaJviwm/DV1BRvdOl19JrCMsJyrPUCFDaP.FnxJ.sqwSFiW');
 
 INSERT INTO users (username, email, password)
-   VALUES('existUser', 'existMail@mail.com',
-     '$2a$10$WeBMjqOaJviwm/DV1BRvdOl19JrCMsJyrPUCFDaP.FnxJ.sqwSFiW');
+   VALUES('Mark', 'mark@mail.com',
+      '$2a$10$WeBMjqOaJviwm/DV1BRvdOl19JrCMsJyrPUCFDaP.FnxJ.sqwSFiW');
 
 INSERT INTO users (username, email, password)
     VALUES('testAdmin', 'testAdmin@mail.com',
-     '$2a$10$WeBMjqOaJviwm/DV1BRvdOl19JrCMsJyrPUCFDaP.FnxJ.sqwSFiW');
+       '$2a$10$WeBMjqOaJviwm/DV1BRvdOl19JrCMsJyrPUCFDaP.FnxJ.sqwSFiW');
 
 INSERT INTO roles (name)
     VALUES('ROLE_USER'),
