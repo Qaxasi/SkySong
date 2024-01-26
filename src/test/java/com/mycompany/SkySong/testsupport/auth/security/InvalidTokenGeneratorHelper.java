23290270987a44e51543ae96f4927638a940b9c2
Champel Mariusz <champelmariusz@gmail.com>
2024-01-26 18:54:45 +0100

    test: Change InvalidTokenGeneratorHelper to use instance fields and methods

--- src/test/java/com/mycompany/SkySong/testsupport/auth/security/InvalidTokenGeneratorHelper.java
+++ src/test/java/com/mycompany/SkySong/testsupport/auth/security/InvalidTokenGeneratorHelper.java
@@ -10,28 +10,28 @@ import java.util.Date;
 import java.util.HashMap;
 
 public class InvalidTokenGeneratorHelper {
-    private static final long EXPIRATION_MS = 1000L;
-    private static final Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+    private final long EXPIRATION_MS = 1000L;
+    private final Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
     private static Date getCurrentDate() {
         return new Date();
     }
-    private static Date getExpirationDate(boolean expired) {
+    private Date getExpirationDate(boolean expired) {
         Date now = getCurrentDate();
         return new Date(now.getTime() + (expired ? -EXPIRATION_MS : EXPIRATION_MS));
     }
-    private static JwtBuilder createBaseBuilder(Date now, Date expirationDate) {
+    private JwtBuilder createBaseBuilder(Date now, Date expirationDate) {
         return Jwts.builder()
                 .setIssuedAt(now)
                 .setExpiration(expirationDate)
                 .signWith(key);
     }
-    public static String generateMalformedToken() {
+    public String generateMalformedToken() {
         String token = createBaseBuilder(getCurrentDate(), getExpirationDate(false))
                 .setSubject("User")
                 .compact();
         return token.substring(0, token.length() / 2);
     }
-    public static String generateTokenWithUnsupportedSignature() {
+    public String generateTokenWithUnsupportedSignature() {
         Key strongerKey = Keys.secretKeyFor(SignatureAlgorithm.HS512);
 
         return createBaseBuilder(getCurrentDate(), getExpirationDate(false))
@@ -39,17 +39,17 @@ public class InvalidTokenGeneratorHelper {
                 .signWith(strongerKey, SignatureAlgorithm.HS512)
                 .compact();
     }
-    public static String generateTokenWithEmptyClaims() {
+    public String generateTokenWithEmptyClaims() {
         return createBaseBuilder(getCurrentDate(), getExpirationDate(false))
                 .setClaims(new HashMap<>())
                 .compact();
     }
-    public static String generateExpiredToken() {
+    public String generateExpiredToken() {
         return createBaseBuilder(getCurrentDate(), getExpirationDate(true))
                 .setSubject("User")
                 .compact();
     }
-    public static String generateTokenWithInvalidSignature() {
+    public String generateTokenWithInvalidSignature() {
         String token = createBaseBuilder(getCurrentDate(), getExpirationDate(false))
                 .setSubject("User")
                 .compact();
