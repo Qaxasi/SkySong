6d18dcee14c9a33334db09ba2ecbdd4a96db2532
Champel Mariusz <champelmariusz@gmail.com>
2024-01-27 18:09:25 +0100

    test: Remove unused imports

--- src/test/java/com/mycompany/SkySong/auth/security/JwtAuthenticationFilterTest.java
+++ src/test/java/com/mycompany/SkySong/auth/security/JwtAuthenticationFilterTest.java
@@ -1,23 +1,6 @@
 package com.mycompany.SkySong.auth.security;
 
-import com.mycompany.SkySong.shared.service.ApplicationMessageService;
 import com.mycompany.SkySong.testsupport.BaseIT;
-import jakarta.servlet.ServletException;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.security.authentication.InsufficientAuthenticationException;
-import org.springframework.security.core.context.SecurityContextHolder;
-
-import java.io.IOException;
-
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertNull;
-import static org.mockito.ArgumentMatchers.*;
-import static org.mockito.Mockito.never;
-import static org.mockito.Mockito.verify;
-
 
 public class JwtAuthenticationFilterTest extends BaseIT {
 }
