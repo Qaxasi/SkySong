62a34fdba7f5858a2f753063516e4389b84c3e32
Champel Mariusz <champelmariusz@gmail.com>
2024-01-28 23:23:45 +0100

    refactor: Delete unused imports

--- src/test/java/com/mycompany/SkySong/auth/service/CredentialExistenceCheckerTest.java
+++ src/test/java/com/mycompany/SkySong/auth/service/CredentialExistenceCheckerTest.java
@@ -2,18 +2,10 @@ package com.mycompany.SkySong.auth.service;
 
 import com.mycompany.SkySong.auth.model.dto.RegisterRequest;
 import com.mycompany.SkySong.shared.exception.CredentialValidationException;
-import com.mycompany.SkySong.shared.repository.UserDAO;
-import com.mycompany.SkySong.shared.service.ApplicationMessageService;
 import com.mycompany.SkySong.testsupport.BaseIT;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import static com.mycompany.SkySong.testsupport.auth.service.CredentialExistenceCheckerImplTestHelper.assertEmailException;
-import static com.mycompany.SkySong.testsupport.auth.service.CredentialExistenceCheckerImplTestHelper.assertUsernameException;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class CredentialExistenceCheckerTest extends BaseIT {
