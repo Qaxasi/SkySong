982b57747365cdc2bfdefd895ac4124f84e981ac
Champel Mariusz <champelmariusz@gmail.com>
2024-01-28 17:32:00 +0100

    test: Remove @Autowired from tokenGenerator field

--- src/test/java/com/mycompany/SkySong/auth/security/TokenGeneratorTest.java
+++ src/test/java/com/mycompany/SkySong/auth/security/TokenGeneratorTest.java
@@ -1,8 +1,6 @@
 package com.mycompany.SkySong.auth.security;
 
-import org.springframework.beans.factory.annotation.Autowired;
-
 public class TokenGeneratorTest {
-    @Autowired
     private TokenGenerator tokenGenerator;
 }
+
