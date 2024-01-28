7ef6552f07fe6d7d412b6f65aa4f4a1517620f23
Champel Mariusz <champelmariusz@gmail.com>
2024-01-28 17:39:01 +0100

    test: Initialize dependencies in setUp method

--- src/test/java/com/mycompany/SkySong/auth/security/TokenGeneratorTest.java
+++ src/test/java/com/mycompany/SkySong/auth/security/TokenGeneratorTest.java
@@ -1,8 +1,16 @@
 package com.mycompany.SkySong.auth.security;
 
+import org.junit.jupiter.api.BeforeEach;
+
 public class TokenGeneratorTest {
     private TokenGenerator tokenGenerator;
     private DateProvider dateProvider;
     private KeyManager keyManager;
+    @BeforeEach
+    void setUp() {
+        keyManager = new KeyManagerImpl("1237tfdh47326hdhjee6e2redgd2dgf12wfsfd2e2iefdgger61gydb");
+        dateProvider = new DateProvider();
+        tokenGenerator = new TokenGeneratorImpl(1000L, dateProvider, keyManager);
+    }
 }
 
