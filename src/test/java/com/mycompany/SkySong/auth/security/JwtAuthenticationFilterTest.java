93b61894d99b9ab5762fa68c116e203edc74ebcd
Champel Mariusz <champelmariusz@gmail.com>
2024-01-28 20:46:39 +0100

    test: Inject dependencies

--- src/test/java/com/mycompany/SkySong/auth/security/JwtAuthenticationFilterTest.java
+++ src/test/java/com/mycompany/SkySong/auth/security/JwtAuthenticationFilterTest.java
@@ -2,8 +2,19 @@ package com.mycompany.SkySong.auth.security;
 
 import com.mycompany.SkySong.auth.model.dto.LoginRequest;
 import com.mycompany.SkySong.testsupport.BaseIT;
+import com.mycompany.SkySong.testsupport.TokenGeneratorHelper;
+import com.mycompany.SkySong.testsupport.auth.security.InvalidTokenGeneratorHelper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.test.web.servlet.MockMvc;
 
 @AutoConfigureMockMvc
 public class JwtAuthenticationFilterTest extends BaseIT {
+    @Autowired
+    private MockMvc mockMvc;
+    @Autowired
+    private TokenGeneratorHelper tokenGenerator;
+    @Autowired
+    private InvalidTokenGeneratorHelper invalidTokenGenerator;
+
 }
