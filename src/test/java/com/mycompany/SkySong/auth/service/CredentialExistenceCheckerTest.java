901c6e067e1b4267e24cdc49d8787de388c84419
Champel Mariusz <champelmariusz@gmail.com>
2024-01-28 23:14:02 +0100

    test: Extend class with BaseIT

--- src/test/java/com/mycompany/SkySong/auth/service/CredentialExistenceCheckerTest.java
+++ src/test/java/com/mycompany/SkySong/auth/service/CredentialExistenceCheckerTest.java
@@ -3,6 +3,7 @@ package com.mycompany.SkySong.auth.service;
 import com.mycompany.SkySong.shared.exception.CredentialValidationException;
 import com.mycompany.SkySong.shared.repository.UserDAO;
 import com.mycompany.SkySong.shared.service.ApplicationMessageService;
+import com.mycompany.SkySong.testsupport.BaseIT;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -12,7 +13,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import static com.mycompany.SkySong.testsupport.auth.service.CredentialExistenceCheckerImplTestHelper.assertEmailException;
 import static com.mycompany.SkySong.testsupport.auth.service.CredentialExistenceCheckerImplTestHelper.assertUsernameException;
 
-public class CredentialExistenceCheckerTest {
+public class CredentialExistenceCheckerTest extends BaseIT {
     @Test
     void whenUsernameExist_ThrowException() {
         assertUsernameException(
