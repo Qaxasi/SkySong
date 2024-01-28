53c4a8fbe7005a9e3c7b6f29c242a8b3861618ea
Champel Mariusz <champelmariusz@gmail.com>
2024-01-28 21:17:33 +0100

    test: Add test to verify registration path bypasses security filter

--- src/test/java/com/mycompany/SkySong/auth/security/JwtAuthenticationFilterTest.java
+++ src/test/java/com/mycompany/SkySong/auth/security/JwtAuthenticationFilterTest.java
@@ -1,17 +1,21 @@
 package com.mycompany.SkySong.auth.security;
 
 import com.mycompany.SkySong.auth.model.dto.LoginRequest;
+import com.mycompany.SkySong.auth.model.dto.RegisterRequest;
 import com.mycompany.SkySong.testsupport.BaseIT;
 import com.mycompany.SkySong.testsupport.TokenGeneratorHelper;
 import com.mycompany.SkySong.testsupport.auth.controller.LoginRequests;
+import com.mycompany.SkySong.testsupport.auth.controller.RegistrationRequests;
 import com.mycompany.SkySong.testsupport.auth.security.InvalidTokenGeneratorHelper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
 
 import static com.mycompany.SkySong.testsupport.JsonUtils.asJsonString;
+import static com.mycompany.SkySong.testsupport.UriConstants.REGISTRATION_URI;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -35,4 +39,17 @@ public class JwtAuthenticationFilterTest extends BaseIT {
                         .content(asJsonString(request)))
                 .andExpect(status().is(200));
     }
+    @Test
+    @Transactional
+    void whenRegistrationPath_ThenBypassSecurityFilter() throws Exception {
+        // given
+        RegisterRequest request = RegistrationRequests.VALID_CREDENTIALS;
+
+        // when & then
+        mockMvc.perform(post(REGISTRATION_URI)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(asJsonString(request)))
+                .andExpect(status().is(201));
+    }
 }
+
