f403e0dd3be29381267a2e64ba83125ec844f199
Champel Mariusz <champelmariusz@gmail.com>
2024-01-28 18:06:38 +0100

    test: Add test to verify generated token contains expected subject

--- src/test/java/com/mycompany/SkySong/auth/security/TokenGeneratorTest.java
+++ src/test/java/com/mycompany/SkySong/auth/security/TokenGeneratorTest.java
@@ -1,6 +1,14 @@
 package com.mycompany.SkySong.auth.security;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.Jwts;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TokenGeneratorTest {
     private TokenGenerator tokenGenerator;
@@ -11,6 +19,21 @@ public class TokenGeneratorTest {
         keyManager = new KeyManagerImpl("1237tfdh47326hdhjee6e2redgd2dgf12wfsfd2e2iefdgger61gydb");
         dateProvider = new DateProvider();
         tokenGenerator = new TokenGeneratorImpl(1000L, dateProvider, keyManager);
+    }
+    @Test
+    void whenTokenGenerated_ContainsExpectedSubject() {
+        // given
+        Authentication authentication = new UsernamePasswordAuthenticationToken("User", null);
+
+        // when
+        String token = tokenGenerator.generateToken(authentication);
+        Jws<Claims> parsedToken = Jwts.parserBuilder()
+                .setSigningKey(keyManager.getKey())
+                .build()
+                .parseClaimsJws(token);
+
+        // then
+        assertEquals("User", parsedToken.getBody().getSubject());
     }
 }
 
