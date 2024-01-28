0e7890e8da24270a1264c4c38161245ee6151943
Champel Mariusz <champelmariusz@gmail.com>
2024-01-28 21:14:54 +0100

    test: Add test to verify login path bypasses security filter

--- src/test/java/com/mycompany/SkySong/auth/security/JwtAuthenticationFilterTest.java
+++ src/test/java/com/mycompany/SkySong/auth/security/JwtAuthenticationFilterTest.java
@@ -3,10 +3,17 @@ package com.mycompany.SkySong.auth.security;
 import com.mycompany.SkySong.auth.model.dto.LoginRequest;
 import com.mycompany.SkySong.testsupport.BaseIT;
 import com.mycompany.SkySong.testsupport.TokenGeneratorHelper;
+import com.mycompany.SkySong.testsupport.auth.controller.LoginRequests;
 import com.mycompany.SkySong.testsupport.auth.security.InvalidTokenGeneratorHelper;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+
+import static com.mycompany.SkySong.testsupport.JsonUtils.asJsonString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @AutoConfigureMockMvc
 public class JwtAuthenticationFilterTest extends BaseIT {
@@ -17,4 +24,15 @@ public class JwtAuthenticationFilterTest extends BaseIT {
     @Autowired
     private InvalidTokenGeneratorHelper invalidTokenGenerator;
 
+    @Test
+    void whenLoginPath_ThenBypassSecurityFilter() throws Exception {
+        // given
+        LoginRequest request = LoginRequests.VALID_CREDENTIALS;
+
+        // when & then
+        mockMvc.perform(post("/api/v1/users/login")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(asJsonString(request)))
+                .andExpect(status().is(200));
+    }
 }
