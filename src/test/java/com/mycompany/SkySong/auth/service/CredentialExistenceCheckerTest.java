59327b56e85d3c885b54099212ded102755d4a61
Champel Mariusz <champelmariusz@gmail.com>
2024-01-28 23:22:13 +0100

    test: Inject dependencies

--- src/test/java/com/mycompany/SkySong/auth/service/CredentialExistenceCheckerTest.java
+++ src/test/java/com/mycompany/SkySong/auth/service/CredentialExistenceCheckerTest.java
@@ -1,5 +1,6 @@
 package com.mycompany.SkySong.auth.service;
 
+import com.mycompany.SkySong.auth.model.dto.RegisterRequest;
 import com.mycompany.SkySong.shared.exception.CredentialValidationException;
 import com.mycompany.SkySong.shared.repository.UserDAO;
 import com.mycompany.SkySong.shared.service.ApplicationMessageService;
@@ -9,11 +10,15 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import static com.mycompany.SkySong.testsupport.auth.service.CredentialExistenceCheckerImplTestHelper.assertEmailException;
 import static com.mycompany.SkySong.testsupport.auth.service.CredentialExistenceCheckerImplTestHelper.assertUsernameException;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class CredentialExistenceCheckerTest extends BaseIT {
+    @Autowired
+    private CredentialExistenceChecker existenceChecker;
     @Test
     void whenUsernameExist_ThrowException() {
         assertUsernameException(
