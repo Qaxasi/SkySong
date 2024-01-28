1599d6a986ba1cb89226fa109ad56682ff56e788
Champel Mariusz <champelmariusz@gmail.com>
2024-01-28 20:39:27 +0100

    test: Add @AutoConfigureMockMvc annotation

--- src/test/java/com/mycompany/SkySong/auth/security/JwtAuthenticationFilterTest.java
+++ src/test/java/com/mycompany/SkySong/auth/security/JwtAuthenticationFilterTest.java
@@ -1,6 +1,9 @@
 package com.mycompany.SkySong.auth.security;
 
+import com.mycompany.SkySong.auth.model.dto.LoginRequest;
 import com.mycompany.SkySong.testsupport.BaseIT;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 
+@AutoConfigureMockMvc
 public class JwtAuthenticationFilterTest extends BaseIT {
 }
