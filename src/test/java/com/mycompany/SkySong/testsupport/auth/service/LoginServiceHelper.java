c190e2f2a16e265a78a150b4176baf71161f094d
Champel Mariusz <champelmariusz@gmail.com>
2024-01-29 16:19:14 +0100

    test: Refactor instances to use final and uppercase

--- src/test/java/com/mycompany/SkySong/testsupport/auth/service/LoginServiceHelper.java
+++ src/test/java/com/mycompany/SkySong/testsupport/auth/service/LoginServiceHelper.java
@@ -9,20 +9,26 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class LoginServiceHelper {
-    public static LoginRequest validRequest = new LoginRequest("User", "Password#3");
-    public static LoginRequest invalidPassword = new LoginRequest("User", "invalid");
-    public static LoginRequest invalidUsername = new LoginRequest("invalid", "Password#3");
-    public static LoginRequest invalidEmail = new LoginRequest("invalid@mail.com", "Password#3");
-    public static void assertUserAuthWithUsername(String username, LoginService login) {
-        login.login(validRequest);
+    public static final LoginRequest VALID_CREDENTIALS =
+            new LoginRequest("User", "Password#3");
+    public static final LoginRequest INVALID_PASSWORD =
+            new LoginRequest("User", "invalid");
+    public static final LoginRequest INVALID_USERNAME =
+            new LoginRequest("invalid", "Password#3");
+    public static final LoginRequest INVALID_EMAIL =
+            new LoginRequest("invalid@mail.com", "Password#3");
 
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 
-        assertEquals(username, authentication.getName());
-    }
-    public static void assertLoginFailureWithMessage(LoginService login, LoginRequest request,
-                                                     Class<? extends Exception> expectedException, String message) {
-        Exception exception = assertThrows(expectedException, () -> login.login(request));
-        assertEquals(message, exception.getMessage());
-    }
+//    public static void assertUserAuthWithUsername(String username, LoginService login) {
+//        login.login(validRequest);
+//
+//        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+//
+//        assertEquals(username, authentication.getName());
+//    }
+//    public static void assertLoginFailureWithMessage(LoginService login, LoginRequest request,
+//                                                     Class<? extends Exception> expectedException, String message) {
+//        Exception exception = assertThrows(expectedException, () -> login.login(request));
+//        assertEquals(message, exception.getMessage());
+//    }
 }
