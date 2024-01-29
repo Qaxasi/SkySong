5ebf8269d5a55d9a667654ff30e03415bfbe2844
Champel Mariusz <champelmariusz@gmail.com>
2024-01-29 20:25:37 +0100

    refactor: Packages refactor

--- src/test/java/com/mycompany/SkySong/testsupport/auth/controller/LogoutControllerHelper.java
+++ src/test/java/com/mycompany/SkySong/testsupport/auth/controller/LogoutControllerHelper.java
@@ -1,6 +1,6 @@
 package com.mycompany.SkySong.testsupport.auth.controller;
 
-import com.mycompany.SkySong.testsupport.LoginRequests;
+import com.mycompany.SkySong.testsupport.auth.LoginRequests;
 import jakarta.servlet.http.Cookie;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockHttpServletResponse;
