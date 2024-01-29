af7da0cb4ebd254ac49f9fb348005b92e8d9c141
Champel Mariusz <champelmariusz@gmail.com>
2024-01-29 20:28:28 +0100

    test: Remove constant

--- src/test/java/com/mycompany/SkySong/testsupport/auth/RegistrationRequests.java
+++ src/test/java/com/mycompany/SkySong/testsupport/auth/RegistrationRequests.java
@@ -5,8 +5,6 @@ import com.mycompany.SkySong.auth.model.dto.RegisterRequest;
 public class RegistrationRequests {
     public static final RegisterRequest VALID_CREDENTIALS =
             new RegisterRequest("New", "new@mail.com", "Password#3");
-    public static final RegisterRequest INVALID_CREDENTIALS =
-            new RegisterRequest("invalid", "invalid", "invalid");
     public static final RegisterRequest EMPTY_CREDENTIALS =
             new RegisterRequest("", "", "");
 
