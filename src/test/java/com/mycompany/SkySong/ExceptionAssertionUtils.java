59fb7e2c558c11f89e3486161ea1d355bec1a272
Champel Mariusz <champelmariusz@gmail.com>
2024-01-29 17:48:00 +0100

    test: Add assertException method

--- src/test/java/com/mycompany/SkySong/ExceptionAssertionUtils.java
+++ src/test/java/com/mycompany/SkySong/ExceptionAssertionUtils.java
@@ -1,4 +1,15 @@
 package com.mycompany.SkySong;
 
+import org.junit.jupiter.api.function.Executable;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class ExceptionAssertionUtils {
+    public static void assertException(Executable executable, Class<? extends Throwable> expectedException,
+                                       String expectedMessage) {
+        Throwable exception = assertThrows(expectedException, executable);
+        assertEquals(expectedMessage, exception.getMessage());
+
+    }
 }
