dae7c54f666c631ae722e89a68a23485345733c4
Champel Mariusz <champelmariusz@gmail.com>
2024-01-29 19:05:52 +0100

    test: Simplify PasswordServiceImpl by removing error handling and dependency

--- src/main/java/com/mycompany/SkySong/auth/service/PasswordServiceImpl.java
+++ src/main/java/com/mycompany/SkySong/auth/service/PasswordServiceImpl.java
@@ -1,27 +1,18 @@
 package com.mycompany.SkySong.auth.service;
 
-import com.mycompany.SkySong.shared.exception.InternalErrorException;
-import com.mycompany.SkySong.shared.service.ApplicationMessageService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 @Service
-@Slf4j
 public class PasswordServiceImpl implements PasswordService {
     private final PasswordEncoder passwordEncoder;
-    private final ApplicationMessageService messageService;
 
-    public PasswordServiceImpl(PasswordEncoder passwordEncoder, ApplicationMessageService messageService) {
+    public PasswordServiceImpl(PasswordEncoder passwordEncoder) {
         this.passwordEncoder = passwordEncoder;
-        this.messageService = messageService;
     }
+
     @Override
     public String encodePassword(String password) {
-        try {
-            return passwordEncoder.encode(password);
-        } catch (IllegalArgumentException ex) {
-            log.error("Error encoding the password: " + ex.getMessage(), ex);
-            throw new InternalErrorException(messageService.getMessage("password.encoding.error"));
-        }
+        return passwordEncoder.encode(password);
     }
 }
