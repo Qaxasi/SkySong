636ee6ff6cfa9d789fae39a8f9f2994fc38e601a
Champel Mariusz <champelmariusz@gmail.com>
2024-01-29 17:07:40 +0100

    test: Add constants for LoginRequest

--- src/test/java/com/mycompany/SkySong/testsupport/auth/controller/LoginRequests.java
+++ src/test/java/com/mycompany/SkySong/testsupport/auth/controller/LoginRequests.java
@@ -5,6 +5,12 @@ import com.mycompany.SkySong.auth.model.dto.LoginRequest;
 public class LoginRequests {
     public static final LoginRequest VALID_CREDENTIALS =
             new LoginRequest("User", "Password#3");
+    public static final LoginRequest INVALID_PASSWORD =
+            new LoginRequest("User", "invalid");
+    public static final LoginRequest INVALID_USERNAME =
+            new LoginRequest("invalid", "Password#3");
+    public static final LoginRequest INVALID_EMAIL =
+            new LoginRequest("invalid@mail.com", "Password#3");
     public static final LoginRequest EMPTY_CREDENTIALS =
             new LoginRequest("", "");
 
