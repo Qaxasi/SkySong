0e58212c842c6d9acdcf9d7b589840acb48bfb1b
Champel Mariusz <champelmariusz@gmail.com>
2024-01-30 20:40:32 +0100

    test: Use constants directly in tests

--- src/test/java/com/mycompany/SkySong/auth/service/CredentialExistenceCheckerTest.java
+++ src/test/java/com/mycompany/SkySong/auth/service/CredentialExistenceCheckerTest.java
@@ -3,6 +3,7 @@ package com.mycompany.SkySong.auth.service;
 import com.mycompany.SkySong.auth.model.dto.RegisterRequest;
 import com.mycompany.SkySong.shared.exception.CredentialValidationException;
 import com.mycompany.SkySong.testsupport.BaseIT;
+import com.mycompany.SkySong.testsupport.auth.RegistrationHelper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -15,7 +16,7 @@ public class CredentialExistenceCheckerTest extends BaseIT {
     @Test
     void whenUsernameExist_ThrowException() {
         // given
-        RegisterRequest request = registrationHelper.existUsername;
+        RegisterRequest request = RegistrationHelper.EXIST_USERNAME;
 
         // when & then
         assertThrows(CredentialValidationException.class,
@@ -25,7 +26,7 @@ public class CredentialExistenceCheckerTest extends BaseIT {
     @Test
     void whenEmailExist_ThrowException() {
         // given
-        RegisterRequest request = registrationHelper.existEmail;
+        RegisterRequest request = RegistrationHelper.EXIST_EMAIL;
 
         // when & then
         assertThrows(CredentialValidationException.class,
