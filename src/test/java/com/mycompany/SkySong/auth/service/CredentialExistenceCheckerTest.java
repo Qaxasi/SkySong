0ba49bb24dc7175065f6eab04f35d1c23b68c3c3
Champel Mariusz <champelmariusz@gmail.com>
2024-01-30 19:51:21 +0100

    test: Inject RegistrationHelper and refactor tests to use it

--- src/test/java/com/mycompany/SkySong/auth/service/CredentialExistenceCheckerTest.java
+++ src/test/java/com/mycompany/SkySong/auth/service/CredentialExistenceCheckerTest.java
@@ -3,6 +3,7 @@ package com.mycompany.SkySong.auth.service;
 import com.mycompany.SkySong.auth.model.dto.RegisterRequest;
 import com.mycompany.SkySong.shared.exception.CredentialValidationException;
 import com.mycompany.SkySong.testsupport.BaseIT;
+import com.mycompany.SkySong.testsupport.auth.RegistrationHelper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -11,18 +12,24 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 public class CredentialExistenceCheckerTest extends BaseIT {
     @Autowired
     private CredentialExistenceChecker existenceChecker;
+    @Autowired
+    private RegistrationHelper registrationHelper;
     @Test
     void whenUsernameExist_ThrowException() {
-        RegisterRequest request = new RegisterRequest("Max", "mail@mail.com", "Password#3");
+        // given
+        RegisterRequest request = registrationHelper.existUsername;
 
+        // when & then
         assertThrows(CredentialValidationException.class,
                 () -> existenceChecker.checkForExistingCredentials(request));
 
     }
     @Test
     void whenEmailExist_ThrowException() {
-        RegisterRequest request = new RegisterRequest("User", "max@mail.com", "Password#3");
+        // given
+        RegisterRequest request = registrationHelper.existEmail;
 
+        // when & then
         assertThrows(CredentialValidationException.class,
                 () -> existenceChecker.checkForExistingCredentials(request));
     }
