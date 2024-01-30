c45f22ab2f8c00233b953997fe565448ac4dd152
Champel Mariusz <champelmariusz@gmail.com>
2024-01-30 20:37:31 +0100

    test: Update fields to static final, keep 'register' method static and update names to uppercase'

--- src/test/java/com/mycompany/SkySong/testsupport/auth/RegistrationHelper.java
+++ src/test/java/com/mycompany/SkySong/testsupport/auth/RegistrationHelper.java
@@ -4,29 +4,29 @@ import com.mycompany.SkySong.auth.model.dto.LoginRequest;
 import com.mycompany.SkySong.auth.model.dto.RegisterRequest;
 
 public class RegistrationHelper {
-    public LoginRequest login =
+    public static final LoginRequest LOGIN =
             new LoginRequest("User", "Password#3");
 
-    public RegisterRequest validCredentials =
+    public static final RegisterRequest VALID_CREDENTIALS =
             new RegisterRequest("New", "new@mail.com", "Password#3");
-    public RegisterRequest emptyCredentials =
+    public static final RegisterRequest EMPTY_CREDENTIALS =
             new RegisterRequest("", "", "");
 
-    public RegisterRequest existEmail =
+    public static final RegisterRequest EXIST_EMAIL =
             new RegisterRequest("User", "mark@mail.com", "Password#3");
-    public RegisterRequest existUsername =
+    public static final RegisterRequest EXIST_USERNAME =
             new RegisterRequest("Mark", "mail@mail.com", "Password#3");
 
-    public RegisterRequest invalidUsername =
+    public static final RegisterRequest INVALID_USERNAME =
             new RegisterRequest("invalid#", "mail@mail.com", "Password#3");
-    public RegisterRequest invalidEmail =
+    public static final RegisterRequest INVALID_EMAIL =
             new RegisterRequest("User", "invalid", "Password#3");
-    public RegisterRequest invalidPassword =
+    public static final RegisterRequest INVALID_PASSWORD =
             new RegisterRequest("User", "mail@mail.com", "invalid");
 
-    public RegisterRequest register(String username) {
+    public static RegisterRequest REGISTER(String username) {
         return new RegisterRequest(username, "new@mail.com", "Password#3");
     }
-    public String malformedRequest =
+    public static final String MALFORMED_REQUEST =
             "{\"username\": \"New\", \"email\": \"new@mail.com\", \"password\": \"Password#3\"";
 }
