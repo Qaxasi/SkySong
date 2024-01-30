663ec5c0518cd4b1b635ad9c01fc0fbdb3b10141
Champel Mariusz <champelmariusz@gmail.com>
2024-01-30 20:38:53 +0100

    test: Remove autowired dependency

--- src/test/java/com/mycompany/SkySong/auth/service/CredentialExistenceCheckerTest.java
+++ src/test/java/com/mycompany/SkySong/auth/service/CredentialExistenceCheckerTest.java
@@ -3,7 +3,6 @@ package com.mycompany.SkySong.auth.service;
 import com.mycompany.SkySong.auth.model.dto.RegisterRequest;
 import com.mycompany.SkySong.shared.exception.CredentialValidationException;
 import com.mycompany.SkySong.testsupport.BaseIT;
-import com.mycompany.SkySong.testsupport.auth.RegistrationHelper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -12,8 +11,7 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 public class CredentialExistenceCheckerTest extends BaseIT {
     @Autowired
     private CredentialExistenceChecker existenceChecker;
-    @Autowired
-    private RegistrationHelper registrationHelper;
+
     @Test
     void whenUsernameExist_ThrowException() {
         // given
