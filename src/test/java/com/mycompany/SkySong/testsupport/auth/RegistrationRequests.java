8870f953a296771c573c7d66bc4776854b3e01ab
Champel Mariusz <champelmariusz@gmail.com>
2024-01-30 18:02:29 +0100

    test: Add new requests and dynamic method

--- src/test/java/com/mycompany/SkySong/testsupport/auth/RegistrationRequests.java
+++ src/test/java/com/mycompany/SkySong/testsupport/auth/RegistrationRequests.java
@@ -1,14 +1,32 @@
 package com.mycompany.SkySong.testsupport.auth;
 
+import com.mycompany.SkySong.auth.model.dto.LoginRequest;
 import com.mycompany.SkySong.auth.model.dto.RegisterRequest;
 
 public class RegistrationRequests {
+    public static LoginRequest login =
+            new LoginRequest("User", "Password#3");
+
     public static final RegisterRequest VALID_CREDENTIALS =
             new RegisterRequest("New", "new@mail.com", "Password#3");
     public static final RegisterRequest EMPTY_CREDENTIALS =
             new RegisterRequest("", "", "");
 
+    public static RegisterRequest EXIST_EMAIL =
+            new RegisterRequest("User", "mark@mail.com", "Password#3");
+    public static RegisterRequest EXIST_USERNAME =
+            new RegisterRequest("Mark", "mail@mail.com", "Password#3");
 
-    public static final String MALFORMED_REQUEST =
+    public static RegisterRequest INVALID_USERNAME =
+            new RegisterRequest("invalid#", "mail@mail.com", "Password#3");
+    public static RegisterRequest INVALID_EMAIL =
+            new RegisterRequest("User", "invalid", "Password#3");
+    public static RegisterRequest INVALID_PASSWORD =
+            new RegisterRequest("User", "mail@mail.com", "invalid");
+
+    public static RegisterRequest register(String username) {
+        return new RegisterRequest(username, "mail@mail.com", "Password#3");
+    }
+    public String MALFORMED_REQUEST =
             "{\"username\": \"New\", \"email\": \"new@mail.com\", \"password\": \"Password#3\"";
 }
