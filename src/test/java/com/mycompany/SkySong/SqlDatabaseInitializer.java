feb744c3136146fce613376bd5fa970ecacb36c3
Champel Mariusz <champelmariusz@gmail.com>
2024-01-31 23:51:15 +0100

    test: Annotate class as a spring component and inject DataSource

--- src/test/java/com/mycompany/SkySong/SqlDatabaseInitializer.java
+++ src/test/java/com/mycompany/SkySong/SqlDatabaseInitializer.java
@@ -1,4 +1,17 @@
 package com.mycompany.SkySong;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.datasource.init.ScriptUtils;
+import org.springframework.stereotype.Component;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+@Component
 public class SqlDatabaseInitializer {
+
+    @Autowired
+    private DataSource dataSource;
+
+    
 }
