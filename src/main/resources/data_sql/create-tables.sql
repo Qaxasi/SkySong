b37f21bfb8a074167cf9943f4573b9169ed51995
Champel Mariusz <champelmariusz@gmail.com>
2024-01-31 22:37:03 +0100

    feat: Create new SQL script who creates tables

---
+++ src/main/resources/data_sql/create-tables.sql
@@ -0,0 +1,20 @@
+ CREATE TABLE users (
+     id INT AUTO_INCREMENT PRIMARY KEY,
+     username VARCHAR(50) NOT NULL UNIQUE,
+     email VARCHAR(100) NOT NULL UNIQUE,
+     password VARCHAR(255) NOT NULL,
+     enabled BOOLEAN NOT NULL DEFAULT TRUE
+     );
+
+ CREATE TABLE roles (
+     id INT AUTO_INCREMENT PRIMARY KEY,
+     name VARCHAR(50) NOT NULL UNIQUE
+ );
+
+ CREATE TABLE user_roles (
+     user_id INT NOT NULL,
+     role_id INT NOT NULL,
+     PRIMARY KEY (user_id, role_id),
+     FOREIGN KEY (user_id) REFERENCES users (id),
+     FOREIGN KEY (role_id) REFERENCES roles (id)
+ );
