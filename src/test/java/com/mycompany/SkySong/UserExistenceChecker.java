c454e60c06a54d1c194bb904a3374f03504e9520
Champel Mariusz <champelmariusz@gmail.com>
2024-02-01 17:29:05 +0100

    test: Annotate class as a spring component and DataSource

--- src/test/java/com/mycompany/SkySong/UserExistenceChecker.java
+++ src/test/java/com/mycompany/SkySong/UserExistenceChecker.java
@@ -1,4 +1,14 @@
 package com.mycompany.SkySong;
 
+import org.springframework.stereotype.Component;
+
+import javax.sql.DataSource;
+
+@Component
 public class UserExistenceChecker {
+    private final DataSource dataSource;
+    public UserExistenceChecker(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+    
 }
