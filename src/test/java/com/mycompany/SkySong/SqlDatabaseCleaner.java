7e15466573ca98acb32e9757fd9cd2873fce90da
Champel Mariusz <champelmariusz@gmail.com>
2024-02-01 16:46:58 +0100

    test: Add list of tables to clean

--- src/test/java/com/mycompany/SkySong/SqlDatabaseCleaner.java
+++ src/test/java/com/mycompany/SkySong/SqlDatabaseCleaner.java
@@ -4,12 +4,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 @Component
 public class SqlDatabaseCleaner {
 
     private final JdbcTemplate jdbcTemplate;
+    private final List<String> tablesToClean = Arrays.asList("user_roles", "users", "roles");
 
     public SqlDatabaseCleaner(JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
     }
+
 }
