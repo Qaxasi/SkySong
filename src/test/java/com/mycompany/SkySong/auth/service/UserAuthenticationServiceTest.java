cec26a8332353c33fd757af814055181df306307
Champel Mariusz <champelmariusz@gmail.com>
2024-02-01 23:13:06 +0100

    test: Add setup and cleanup method for DB test initialization and cleanup

--- src/test/java/com/mycompany/SkySong/auth/service/UserAuthenticationServiceTest.java
+++ src/test/java/com/mycompany/SkySong/auth/service/UserAuthenticationServiceTest.java
@@ -22,4 +22,13 @@ public class UserAuthenticationServiceTest extends BaseIT {
     @Autowired
     private SqlDatabaseCleaner cleaner;
 
+    @BeforeEach
+    void setUp() throws Exception {
+        initializer.setup("data_sql/test-setup.sql");
+    }
+
+    @AfterEach
+    void cleanUp() {
+        cleaner.clean();
+    }
 }
