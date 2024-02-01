c72bf1173807ae16edb199513b907b982606f7cf
Champel Mariusz <champelmariusz@gmail.com>
2024-02-01 21:19:39 +0100

    test: Annotate class as a spring component and add DataSource

--- src/test/java/com/mycompany/SkySong/UserRoleChecker.java
+++ src/test/java/com/mycompany/SkySong/UserRoleChecker.java
@@ -1,4 +1,16 @@
 package com.mycompany.SkySong;
 
+import org.springframework.stereotype.Component;
+
+import javax.sql.DataSource;
+
+@Component
 public class UserRoleChecker {
+
+    private final DataSource dataSource;
+
+    public UserRoleChecker(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
 }
