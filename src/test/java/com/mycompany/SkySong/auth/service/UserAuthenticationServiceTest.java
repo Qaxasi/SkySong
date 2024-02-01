7cb825086559fe9323fe872b170c851dfff652fb
Champel Mariusz <champelmariusz@gmail.com>
2024-02-01 23:00:29 +0100

    test: Inject dependencies

--- src/test/java/com/mycompany/SkySong/auth/service/UserAuthenticationServiceTest.java
+++ src/test/java/com/mycompany/SkySong/auth/service/UserAuthenticationServiceTest.java
@@ -1,7 +1,25 @@
 package com.mycompany.SkySong.auth.service;
 
+import com.mycompany.SkySong.SqlDatabaseCleaner;
+import com.mycompany.SkySong.SqlDatabaseInitializer;
 import com.mycompany.SkySong.testsupport.BaseIT;
+import com.mycompany.SkySong.testsupport.auth.LoginRequests;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.Authentication;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 
 public class UserAuthenticationServiceTest extends BaseIT {
+    @Autowired
+    private UserAuthenticationService userAuth;
+    @Autowired
+    private SqlDatabaseInitializer initializer;
+    @Autowired
+    private SqlDatabaseCleaner cleaner;
 
 }
