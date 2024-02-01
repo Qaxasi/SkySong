3b47a2e088e62ce50b76ebbba16a2a89966a9949
Champel Mariusz <champelmariusz@gmail.com>
2024-02-01 16:50:11 +0100

    refactor: Delete unused import

--- src/test/java/com/mycompany/SkySong/SqlDatabaseCleaner.java
+++ src/test/java/com/mycompany/SkySong/SqlDatabaseCleaner.java
@@ -1,6 +1,5 @@
 package com.mycompany.SkySong;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
