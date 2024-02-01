94d3e9cfa47ff589a697d3a21eb84f3ca0b97067
Champel Mariusz <champelmariusz@gmail.com>
2024-02-01 16:43:20 +0100

    test: Add JdbcTemplate and constructor to SqlDatabaseCleaner

--- src/test/java/com/mycompany/SkySong/SqlDatabaseCleaner.java
+++ src/test/java/com/mycompany/SkySong/SqlDatabaseCleaner.java
@@ -1,4 +1,15 @@
 package com.mycompany.SkySong;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
 public class SqlDatabaseCleaner {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public SqlDatabaseCleaner(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
 }
