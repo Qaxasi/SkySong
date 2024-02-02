623e4895877bc3c51ddc2f094e62a9bcea750ba7
Champel Mariusz <champelmariusz@gmail.com>
2024-02-02 19:12:36 +0100

    test: Add methods for DB initialization and cleanup

--- src/test/java/com/mycompany/SkySong/auth/service/RoleManagerTest.java
+++ src/test/java/com/mycompany/SkySong/auth/service/RoleManagerTest.java
@@ -3,6 +3,8 @@ package com.mycompany.SkySong.auth.service;
 import com.mycompany.SkySong.SqlDatabaseCleaner;
 import com.mycompany.SkySong.SqlDatabaseInitializer;
 import com.mycompany.SkySong.testsupport.BaseIT;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -16,6 +18,16 @@ public class RoleManagerTest extends BaseIT {
 
     @Autowired
     private SqlDatabaseCleaner cleaner;
+
+    @BeforeEach
+    void setUp() throws Exception {
+        initializer.setup("data_sql/test-setup.sql");
+    }
+
+    @AfterEach
+    void cleanUp() {
+        cleaner.clean();
+    }
 
     @Test
     void shouldThrowExceptionWhenRoleNotExist() {
