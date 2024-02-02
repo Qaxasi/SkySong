14140d7346f60ac9e172116ce84ccbb475175c1d
Champel Mariusz <champelmariusz@gmail.com>
2024-02-02 15:41:43 +0100

    test: Refactor test

--- src/test/java/com/mycompany/SkySong/auth/service/UserFactoryImplTest.java
+++ src/test/java/com/mycompany/SkySong/auth/service/UserFactoryImplTest.java
@@ -5,16 +5,12 @@ import com.mycompany.SkySong.auth.model.entity.Role;
 import com.mycompany.SkySong.auth.model.entity.UserRole;
 import com.mycompany.SkySong.shared.entity.User;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Profile;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.when;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @Profile("test")
@@ -26,21 +22,14 @@ public class UserFactoryImplTest {
     private PasswordService passwordService;
     @Test
     void shouldCreateUserWithGivenDetails() {
-        String username = "testUsername";
-        String email = "testEmail@gmail.com";
-        String password = "testPassword@123";
-        String encodedPassword = "encodedPassword";
+        RegisterRequest registerRequest = new RegisterRequest("User", "mail@mail.com", "Pass#3");
         Role role = new Role(UserRole.ROLE_USER);
-
-        RegisterRequest registerRequest = new RegisterRequest(username, email, password);
-
-        when(passwordService.encodePassword(password)).thenReturn(encodedPassword);
 
         User user = userFactory.createUser(registerRequest, role);
 
-        assertEquals(user.getUsername(), registerRequest.username());
-        assertEquals(user.getEmail(), registerRequest.email());
-        assertEquals(user.getPassword(), encodedPassword);
+        assertEquals("User", user.getUsername());
+        assertEquals("mail@mail.com", user.getEmail());
+        assertNotEquals("Pass#3", user.getPassword());
         assertTrue(user.getRoles().contains(role));
     }
 }
