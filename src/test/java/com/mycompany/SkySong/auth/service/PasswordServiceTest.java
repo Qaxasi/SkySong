87295709d15c99ff4b748dec3edf99864563c9e0
Champel Mariusz <champelmariusz@gmail.com>
2024-02-02 22:22:50 +0100

    test: Change @Profile to @ActiveProfiles

--- src/test/java/com/mycompany/SkySong/auth/service/PasswordServiceTest.java
+++ src/test/java/com/mycompany/SkySong/auth/service/PasswordServiceTest.java
@@ -3,13 +3,13 @@ package com.mycompany.SkySong.auth.service;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Profile;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.context.ActiveProfiles;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@Profile("test")
+@ActiveProfiles("test")
 public class PasswordServiceTest {
     @Autowired
     private PasswordService passwordService;
--- src/test/java/com/mycompany/SkySong/auth/service/UserFactoryTest.java
+++ src/test/java/com/mycompany/SkySong/auth/service/UserFactoryTest.java
@@ -7,12 +7,12 @@ import com.mycompany.SkySong.shared.entity.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Profile;
+import org.springframework.test.context.ActiveProfiles;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@Profile("test")
+@ActiveProfiles("test")
 public class UserFactoryTest {
     @Autowired
     private UserFactory userFactory;
