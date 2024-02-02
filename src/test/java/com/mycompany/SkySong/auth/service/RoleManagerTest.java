89ba6a574dc6d3b666e1114e74b4f2654b985fe3
Champel Mariusz <champelmariusz@gmail.com>
2024-02-02 18:03:48 +0100

    test: Return test class

---
+++ src/test/java/com/mycompany/SkySong/auth/service/RoleManagerTest.java
@@ -0,0 +1,42 @@
+package com.mycompany.SkySong.auth.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Profile;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Profile("test")
+public class RoleManagerTest {
+    private RoleManagerImpl userRoleManagerImpl;
+
+    @Test
+    void shouldThrowExceptionWhenRoleNotExist() {
+        when(roleDAO.findByName(any())).thenReturn(Optional.empty());
+
+        assertThrows(InternalErrorException.class, () -> userRoleManagerImpl.getRoleByName(any()));
+    }
+    @Test
+    void shouldReturnCorrectRoleWhenRoleExist() {
+        UserRole role = UserRole.ROLE_USER;
+        Role expectedRole = new Role(role);
+
+        when(roleDAO.findByName(role)).thenReturn(Optional.of(expectedRole));
+
+        Role actualRole = userRoleManagerImpl.getRoleByName(role);
+
+        assertEquals(actualRole, expectedRole);
+    }
+    @Test
+    void shouldReturnErrorMessageWhenRoleNotFound() {
+        UserRole role = UserRole.ROLE_USER;
+        String expectedMessage = "Error during registration";
+
+        when(messageService.getMessage("user.role.not-set")).thenReturn(expectedMessage);
+        when(roleDAO.findByName(role)).thenReturn(Optional.empty());
+
+        Exception exception =
+                assertThrows(InternalErrorException.class, () -> userRoleManagerImpl.getRoleByName(role));
+
+        assertEquals(expectedMessage, exception.getMessage());
+    }
+}
