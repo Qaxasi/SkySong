c365903a9dc381f93325732334930bad4289a2ee
Champel Mariusz <champelmariusz@gmail.com>
2024-02-02 15:13:55 +0100

    test: Add @SpringBootTest annotation and set test profile

--- src/test/java/com/mycompany/SkySong/auth/service/UserFactoryImplTest.java
+++ src/test/java/com/mycompany/SkySong/auth/service/UserFactoryImplTest.java
@@ -9,10 +9,14 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Profile;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
 
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Profile("test")
 public class UserFactoryImplTest {
 
     private UserFactoryImpl userFactory;
