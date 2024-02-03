00336aaa705e1e896f8466f1a409964459b65788
Champel Mariusz <champelmariusz@gmail.com>
2024-02-03 21:46:52 +0100

    test: Add test to verify exception for to short username

--- src/test/java/com/mycompany/SkySong/auth/service/UsernameValidationStrategyTest.java
+++ src/test/java/com/mycompany/SkySong/auth/service/UsernameValidationStrategyTest.java
@@ -1,8 +1,13 @@
 package com.mycompany.SkySong.auth.service;
 
+import com.mycompany.SkySong.shared.exception.CredentialValidationException;
 import com.mycompany.SkySong.shared.service.ApplicationMessageService;
 import com.mycompany.SkySong.shared.service.ApplicationMessageServiceImpl;
+import com.mycompany.SkySong.testsupport.auth.RegistrationRequests;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class UsernameValidationStrategyTest {
     private ApplicationMessageService message;
@@ -13,4 +18,11 @@ public class UsernameValidationStrategyTest {
         message = new ApplicationMessageServiceImpl();
         strategy = new UsernameValidationStrategy(message);
     }
+    @Test
+    void whenUsernameIsToShort_ThrowException() {
+        assertThrows(CredentialValidationException.class,
+                () -> strategy.validate(RegistrationRequests.USERNAME_TO_SHORT));
+    }
+
+
 }
