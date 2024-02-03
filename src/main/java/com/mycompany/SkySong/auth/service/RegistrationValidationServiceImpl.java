8b15a6e93eac15b686cf591f9c61aee6beb7fc2f
Champel Mariusz <champelmariusz@gmail.com>
2024-02-03 18:39:02 +0100

    refactor: Clear validateCredentials method and remove message dependency

--- src/main/java/com/mycompany/SkySong/auth/service/RegistrationValidationServiceImpl.java
+++ src/main/java/com/mycompany/SkySong/auth/service/RegistrationValidationServiceImpl.java
@@ -1,23 +1,12 @@
 package com.mycompany.SkySong.auth.service;
 
-import com.mycompany.SkySong.shared.exception.CredentialValidationException;
 import com.mycompany.SkySong.auth.model.dto.RegisterRequest;
-import com.mycompany.SkySong.shared.service.ApplicationMessageService;
-import com.mycompany.SkySong.shared.util.ValidationPatterns;
 import org.springframework.stereotype.Service;
 
 @Service
 class RegistrationValidationServiceImpl implements RegistrationValidationService {
-    private final ApplicationMessageService messageService;
-
-    public RegistrationValidationServiceImpl(ApplicationMessageService messageService) {
-        this.messageService = messageService;
-    }
 
     @Override
     public void validateCredentials(RegisterRequest registerRequest) {
-        validateUsername(registerRequest);
-        validateEmail(registerRequest);
-        validatePassword(registerRequest);
     }
 }
