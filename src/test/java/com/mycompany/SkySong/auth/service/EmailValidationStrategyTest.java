be26cedad2a11acbcffed86b3174fbd406919f5c
Champel Mariusz <champelmariusz@gmail.com>
2024-02-03 22:23:24 +0100

    test: Add dependencies

--- src/test/java/com/mycompany/SkySong/auth/service/EmailValidationStrategyTest.java
+++ src/test/java/com/mycompany/SkySong/auth/service/EmailValidationStrategyTest.java
@@ -1,4 +1,10 @@
 package com.mycompany.SkySong.auth.service;
 
+import com.mycompany.SkySong.shared.service.ApplicationMessageService;
+
 public class EmailValidationStrategyTest {
+
+    private ApplicationMessageService message;
+
+    private EmailValidationStrategy strategy;
 }
