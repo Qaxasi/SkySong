d083338e483b5b68475e827a7555f85f3a46711a
Champel Mariusz <champelmariusz@gmail.com>
2024-02-03 22:24:34 +0100

    test: Initialize dependencies

--- src/test/java/com/mycompany/SkySong/auth/service/EmailValidationStrategyTest.java
+++ src/test/java/com/mycompany/SkySong/auth/service/EmailValidationStrategyTest.java
@@ -1,10 +1,18 @@
 package com.mycompany.SkySong.auth.service;
 
 import com.mycompany.SkySong.shared.service.ApplicationMessageService;
+import com.mycompany.SkySong.shared.service.ApplicationMessageServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
 
 public class EmailValidationStrategyTest {
 
     private ApplicationMessageService message;
 
     private EmailValidationStrategy strategy;
+
+    @BeforeEach
+    void setUp() {
+        message = new ApplicationMessageServiceImpl();
+        strategy = new EmailValidationStrategy(message);
+    }
 }
