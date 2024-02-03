135d622dcef0d74c9a4605205a59d231de9eff8c
Champel Mariusz <champelmariusz@gmail.com>
2024-02-03 18:41:52 +0100

    test: Add RegistrationValidationStrategy list and create constructor

--- src/main/java/com/mycompany/SkySong/auth/service/RegistrationValidationServiceImpl.java
+++ src/main/java/com/mycompany/SkySong/auth/service/RegistrationValidationServiceImpl.java
@@ -3,8 +3,16 @@ package com.mycompany.SkySong.auth.service;
 import com.mycompany.SkySong.auth.model.dto.RegisterRequest;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 class RegistrationValidationServiceImpl implements RegistrationValidationService {
+
+    private final List<RegistrationValidationStrategy> strategies;
+
+    public RegistrationValidationServiceImpl(List<RegistrationValidationStrategy> strategies) {
+        this.strategies = strategies;
+    }
 
     @Override
     public void validateCredentials(RegisterRequest registerRequest) {
