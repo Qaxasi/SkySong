1ef1cbf0575c19c137ccf763dd559c6bf8d34bb8
Champel Mariusz <champelmariusz@gmail.com>
2024-02-03 18:30:01 +0100

    feat: Implement RegistrationValidationStrategy and add ApplicationMessageService dependency

--- src/main/java/com/mycompany/SkySong/auth/service/EmailValidationStrategy.java
+++ src/main/java/com/mycompany/SkySong/auth/service/EmailValidationStrategy.java
@@ -1,4 +1,18 @@
 package com.mycompany.SkySong.auth.service;
 
-public class EmailValidationStrategy {
+import com.mycompany.SkySong.auth.model.dto.RegisterRequest;
+import com.mycompany.SkySong.shared.exception.CredentialValidationException;
+import com.mycompany.SkySong.shared.service.ApplicationMessageService;
+
+public class EmailValidationStrategy implements RegistrationValidationStrategy {
+
+    private final ApplicationMessageService message;
+
+    public EmailValidationStrategy(ApplicationMessageService message) {
+        this.message = message;
+    }
+    @Override
+    public void validate(RegisterRequest registerRequest) throws CredentialValidationException {
+
+    }
 }
