b39d0482e4c1b099a24d9daa296a7f112557de43
Champel Mariusz <champelmariusz@gmail.com>
2024-02-03 23:37:37 +0100

    test: Remove constants

--- src/test/java/com/mycompany/SkySong/testsupport/auth/RegistrationRequests.java
+++ src/test/java/com/mycompany/SkySong/testsupport/auth/RegistrationRequests.java
@@ -14,13 +14,6 @@ public class RegistrationRequests {
     public static final RegisterRequest EXIST_USERNAME =
             new RegisterRequest("Mark", "sam@mail.com", "Password#3");
 
-    public static final RegisterRequest INVALID_USERNAME =
-            new RegisterRequest("invalid#", "sam@mail.com", "Password#3");
-    public static final RegisterRequest INVALID_EMAIL =
-            new RegisterRequest("Sam", "invalid", "Password#3");
-    public static final RegisterRequest INVALID_PASSWORD =
-            new RegisterRequest("Sam", "sam@mail.com", "invalid");
-
     public static RegisterRequest REGISTER(String username) {
         return new RegisterRequest(username, "new@mail.com", "Password#3");
     }
