82a7666e673bd84d3e5c2923eee554bd112518dd
Champel Mariusz <champelmariusz@gmail.com>
2024-02-03 20:56:46 +0100

    test: Add dependencies for message service and validation strategy and initialize them

--- src/test/java/com/mycompany/SkySong/auth/service/UsernameValidationStrategyTest.java
+++ src/test/java/com/mycompany/SkySong/auth/service/UsernameValidationStrategyTest.java
@@ -1,4 +1,16 @@
 package com.mycompany.SkySong.auth.service;
 
+import com.mycompany.SkySong.shared.service.ApplicationMessageService;
+import com.mycompany.SkySong.shared.service.ApplicationMessageServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
+
 public class UsernameValidationStrategyTest {
+    private ApplicationMessageService message;
+    private UsernameValidationStrategy strategy;
+
+    @BeforeEach
+    void setUp() {
+        message = new ApplicationMessageServiceImpl();
+        strategy = new UsernameValidationStrategy(message);
+    }
 }
