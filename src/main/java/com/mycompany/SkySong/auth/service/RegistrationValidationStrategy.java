e336c7e5d68890be57068fec27c2821a7ca8349f
Champel Mariusz <champelmariusz@gmail.com>
2024-02-03 15:47:45 +0100

    refactor: Rename interface

--- src/main/java/com/mycompany/SkySong/auth/service/RegistrationValidationStrategy.java
+++ src/main/java/com/mycompany/SkySong/auth/service/RegistrationValidationStrategy.java
@@ -3,6 +3,6 @@ package com.mycompany.SkySong.auth.service;
 import com.mycompany.SkySong.auth.model.dto.RegisterRequest;
 import com.mycompany.SkySong.shared.exception.CredentialValidationException;
 
-public interface ValidationStrategy {
+public interface RegistrationValidationStrategy {
     void validate(RegisterRequest registerRequest) throws CredentialValidationException;
 }
