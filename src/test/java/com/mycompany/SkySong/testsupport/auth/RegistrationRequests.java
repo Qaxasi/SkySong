bb6722291ad3396458c7f989cdbfdde52249d7bc
Champel Mariusz <champelmariusz@gmail.com>
2024-02-03 23:29:31 +0100

    test: Add constants for RegisterRequest

--- src/test/java/com/mycompany/SkySong/testsupport/auth/RegistrationRequests.java
+++ src/test/java/com/mycompany/SkySong/testsupport/auth/RegistrationRequests.java
@@ -26,4 +26,38 @@ public class RegistrationRequests {
     }
     public static final String MALFORMED_REQUEST =
             "{\"username\": \"New\", \"email\": \"new@mail.com\", \"password\": \"Password#3\"";
+
+    public static final RegisterRequest PASSWORD_TO_SHORT =
+            new RegisterRequest("User", "mail@mail.com", "Pass#3");
+
+    public static final RegisterRequest PASSWORD_NO_UPPERCASE_LETTER =
+            new RegisterRequest("User", "mail@mail.com", "password#3");
+
+    public static final RegisterRequest PASSWORD_NO_LOWERCASE_LETTER =
+            new RegisterRequest("User", "mail@mail.com", "PASSWORD#3");
+
+    public static final RegisterRequest PASSWORD_NO_SPECIAL_CHARACTER =
+            new RegisterRequest("User", "mail@mail.com", "Password3");
+
+    public static final RegisterRequest PASSWORD_NO_NUMBER =
+            new RegisterRequest("User", "mail@mail.com", "Password#d");
+
+    public static final RegisterRequest USERNAME_TO_SHORT =
+            new RegisterRequest("Us", "mail@mail.com", "Password#3");
+
+    public static final RegisterRequest USERNAME_TO_LONG =
+            new RegisterRequest("testVeryLongUsernameFormat", "mail@mail.com", "Password#3");
+
+    public static final RegisterRequest USERNAME_WITH_SPECIAL_CHARACTER =
+            new RegisterRequest("User", "mail@mail.com", "Password#3");
+
+    public static final RegisterRequest EMAIL_INVALID_FORMAT =
+            new RegisterRequest("User", "mail-mail.com", "Password#3");
+
+    public static final RegisterRequest EMAIL_TO_SHORT =
+            new RegisterRequest("User", "m@m.c", "Password#3");
+
+    public static final RegisterRequest EMAIL_TO_LONG =
+            new RegisterRequest("User", "qwertyuiopasdfghjklzxcvbn@mail.com", "Password#3");
+
 }
