890a1504fb1e72761eaff73d2d87c960eb7af9cd
Champel Mariusz <champelmariusz@gmail.com>
2024-02-03 22:56:44 +0100

    test: Add dependencies and ilitialize them

--- src/test/java/com/mycompany/SkySong/auth/service/PasswordValidationStrategyTest.java
+++ src/test/java/com/mycompany/SkySong/auth/service/PasswordValidationStrategyTest.java
@@ -1,4 +1,19 @@
 package com.mycompany.SkySong.auth.service;
 
+import com.mycompany.SkySong.shared.service.ApplicationMessageService;
+import com.mycompany.SkySong.shared.service.ApplicationMessageServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
+
 public class PasswordValidationStrategyTest {
+
+    private ApplicationMessageService message;
+
+    private PasswordValidationStrategy strategy;
+
+    @BeforeEach
+    void setUp() {
+        message = new ApplicationMessageServiceImpl();
+        strategy = new PasswordValidationStrategy(message);
+    }
+
 }
