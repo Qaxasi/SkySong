2ea302ac92456e21f9844258f6bbf6ea230a7974
Champel Mariusz <champelmariusz@gmail.com>
2024-02-04 14:09:12 +0100

    test: Rename field

--- src/test/java/com/mycompany/SkySong/auth/service/RegistrationValidationServiceTest.java
+++ src/test/java/com/mycompany/SkySong/auth/service/RegistrationValidationServiceTest.java
@@ -1,14 +1,19 @@
 package com.mycompany.SkySong.auth.service;
 
+import com.mycompany.SkySong.testsupport.auth.RegistrationRequests;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("test")
 public class RegistrationValidationServiceTest {
 
     @Autowired
-    private RegistrationValidationService registrationValidationService;
+    private RegistrationValidationService validator;
 
 }
+
