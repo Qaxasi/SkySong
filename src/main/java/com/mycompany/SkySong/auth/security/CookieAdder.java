fb36ab61712fdeffa16198d73c6ca8f1644864e1
Champel Mariusz <champelmariusz@gmail.com>
2024-02-04 18:34:30 +0100

    refactor: Delete unnecessary classes and interfaces

--- src/main/java/com/mycompany/SkySong/auth/security/CookieAdder.java
+++
@@ -1,7 +0,0 @@
-package com.mycompany.SkySong.auth.security;
-
-import jakarta.servlet.http.HttpServletResponse;
-
-public interface CookieAdder {
-    void addCookie(HttpServletResponse response, String name, String value, int maxAge);
-}
--- src/main/java/com/mycompany/SkySong/auth/security/CookieDeleter.java
+++
@@ -1,8 +0,0 @@
-package com.mycompany.SkySong.auth.security;
-
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-
-public interface CookieDeleter {
-    void deleteCookie(HttpServletRequest request, HttpServletResponse response, String name);
-}
