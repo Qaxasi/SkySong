d99f8cbc1b0632302a326b032132c00bff6b9bef
Champel Mariusz <champelmariusz@gmail.com>
2024-02-05 15:58:52 +0100

    feat: Add fields and annotations to JWTAuthResponse for token handling

--- src/main/java/com/mycompany/SkySong/auth/model/dto/JWTAuthResponse.java
+++ src/main/java/com/mycompany/SkySong/auth/model/dto/JWTAuthResponse.java
@@ -1,4 +1,15 @@
 package com.mycompany.SkySong.auth.model.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class JWTAuthResponse {
+    private String accessToken;
+    private String tokenType = "Bearer";
 }
