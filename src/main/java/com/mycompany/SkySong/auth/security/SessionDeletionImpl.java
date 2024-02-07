e2af3c6a0b09093d706a5dcbf945713de4e1be66
Champel Mariusz <champelmariusz@gmail.com>
2024-02-07 15:12:02 +0100

    feat: Inject dependencies

--- src/main/java/com/mycompany/SkySong/auth/security/SessionDeletionImpl.java
+++ src/main/java/com/mycompany/SkySong/auth/security/SessionDeletionImpl.java
@@ -1,6 +1,15 @@
 package com.mycompany.SkySong.auth.security;
 
+import com.mycompany.SkySong.auth.repository.SessionDAO;
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class SessionDeletionImpl implements SessionDeletion {
+
+    @Autowired
+    private SessionDAO sessionDAO;
+
+    @Autowired
+    private SecureTokenGenerator tokenGenerator;
 
     @Override
     public void deleteSession(String sessionId) {
