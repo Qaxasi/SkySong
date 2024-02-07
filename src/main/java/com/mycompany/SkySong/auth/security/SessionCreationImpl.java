2752385b1389a7100af3d64909e54f1e65efae02
Champel Mariusz <champelmariusz@gmail.com>
2024-02-07 15:27:01 +0100

    feat: Annotate class as a spring service

--- src/main/java/com/mycompany/SkySong/auth/security/SessionCreationImpl.java
+++ src/main/java/com/mycompany/SkySong/auth/security/SessionCreationImpl.java
@@ -1,5 +1,8 @@
 package com.mycompany.SkySong.auth.security;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class SessionCreationImpl implements SessionCreation {
     @Override
     public String createSession(Integer userID) {
