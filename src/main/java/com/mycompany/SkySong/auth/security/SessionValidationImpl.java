3b56f7770ac6bfe93c5d34dcb980903ee8caa33b
Champel Mariusz <champelmariusz@gmail.com>
2024-02-07 15:23:17 +0100

    feat: Implement validateSession method

--- src/main/java/com/mycompany/SkySong/auth/security/SessionValidationImpl.java
+++ src/main/java/com/mycompany/SkySong/auth/security/SessionValidationImpl.java
@@ -1,8 +1,11 @@
 package com.mycompany.SkySong.auth.security;
 
+import com.mycompany.SkySong.auth.model.entity.Session;
 import com.mycompany.SkySong.auth.repository.SessionDAO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Date;
 
 @Service
 public class SessionValidationImpl implements SessionValidation {
@@ -12,9 +15,12 @@ public class SessionValidationImpl implements SessionValidation {
 
     @Autowired
     private SecureTokenGenerator tokenGenerator;
-    
+
     @Override
     public boolean validateSession(String sessionId) {
-        return false;
+        String hashedSessionId = tokenGenerator.generateHashedToken(sessionId);
+        Session session = sessionDAO.findById(hashedSessionId).orElse(null);
+
+        return session != null && session.getExpiresAt().after(new Date());
     }
 }
