d365c36d75e1e09658b2ab60fe2936b787e114de
Champel Mariusz <champelmariusz@gmail.com>
2024-02-07 15:29:45 +0100

    feat: Implement createSession method

--- src/main/java/com/mycompany/SkySong/auth/security/SessionCreationImpl.java
+++ src/main/java/com/mycompany/SkySong/auth/security/SessionCreationImpl.java
@@ -1,8 +1,11 @@
 package com.mycompany.SkySong.auth.security;
 
+import com.mycompany.SkySong.auth.model.entity.Session;
 import com.mycompany.SkySong.auth.repository.SessionDAO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Date;
 
 @Service
 public class SessionCreationImpl implements SessionCreation {
@@ -15,6 +18,16 @@ public class SessionCreationImpl implements SessionCreation {
 
     @Override
     public String createSession(Integer userID) {
-        return null;
+        String token = tokenGenerator.generateToken();
+        String hashedToken = tokenGenerator.generateHashedToken(token);
+
+        Session session = new Session();
+        session.setSessionId(hashedToken);
+        session.setUserId(userID);
+        session.setCreateAt(new Date());
+        session.setExpiresAt(new Date(System.currentTimeMillis() + 1000));
+
+        sessionDAO.save(session);
+        return token;
     }
 }
