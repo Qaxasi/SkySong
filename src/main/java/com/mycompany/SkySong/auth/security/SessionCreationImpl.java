34cd00ab6bb8b78e7633080455088fa35e4e09c1
Champel Mariusz <champelmariusz@gmail.com>
2024-02-07 15:26:44 +0100

    feat: Create class and implement SessionCreation interface

---
+++ src/main/java/com/mycompany/SkySong/auth/security/SessionCreationImpl.java
@@ -0,0 +1,8 @@
+package com.mycompany.SkySong.auth.security;
+
+public class SessionCreationImpl implements SessionCreation {
+    @Override
+    public String createSession(Integer userID) {
+        return null;
+    }
+}
