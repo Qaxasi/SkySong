41ef8f829a98c101e051193ce98d37a7fe3c60c7
Champel Mariusz <champelmariusz@gmail.com>
2024-02-07 18:30:23 +0100

    refactor: Remove SecuredTokenGenerator dependency

--- src/main/java/com/mycompany/SkySong/auth/service/LoginServiceImpl.java
+++ src/main/java/com/mycompany/SkySong/auth/service/LoginServiceImpl.java
@@ -1,7 +1,6 @@
 package com.mycompany.SkySong.auth.service;
 
 import com.mycompany.SkySong.auth.model.dto.LoginRequest;
-import com.mycompany.SkySong.auth.security.SecureTokenGenerator;
 import com.mycompany.SkySong.shared.service.ApplicationMessageService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.BadCredentialsException;
@@ -13,13 +12,11 @@ import org.springframework.stereotype.Service;
 @Service
 class LoginServiceImpl implements LoginService {
 
-    private final SecureTokenGenerator tokenGenerator;
     private final ApplicationMessageService messageService;
     private final UserAuthenticationService userAuth;
 
-    public LoginServiceImpl(SecureTokenGenerator tokenGenerator, ApplicationMessageService messageService,
+    public LoginServiceImpl(ApplicationMessageService messageService,
                             UserAuthenticationService userAuth) {
-        this.tokenGenerator = tokenGenerator;
         this.messageService = messageService;
         this.userAuth = userAuth;
     }
