6fc22da6e3926390284996c059c194ff16d1ba2d
Champel Mariusz <champelmariusz@gmail.com>
2024-02-07 15:43:26 +0100

    test: Add dependencies

--- src/main/java/com/mycompany/SkySong/auth/security/SecureTokenGenerator.java
+++ src/main/java/com/mycompany/SkySong/auth/security/SecureTokenGenerator.java
@@ -1,4 +1,10 @@
 package com.mycompany.SkySong.auth.security;
 
+import java.security.SecureRandom;
+import java.util.Base64;
+
 public class SecureTokenGenerator {
+
+    private final SecureRandom secureRandom = new SecureRandom();
+    private final Base64.Encoder encoder = Base64.getUrlEncoder();
 }
