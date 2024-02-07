64664cd9170feede83d51729ce7da55230ea04ea
Champel Mariusz <champelmariusz@gmail.com>
2024-02-07 15:18:12 +0100

    feat: Inject dependencies

--- src/main/java/com/mycompany/SkySong/auth/security/SessionValidationImpl.java
+++ src/main/java/com/mycompany/SkySong/auth/security/SessionValidationImpl.java
@@ -1,9 +1,18 @@
 package com.mycompany.SkySong.auth.security;
 
+import com.mycompany.SkySong.auth.repository.SessionDAO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class SessionValidationImpl implements SessionValidation {
+
+    @Autowired
+    private SessionDAO sessionDAO;
+
+    @Autowired
+    private SecureTokenGenerator tokenGenerator;
+    
     @Override
     public boolean validateSession(String sessionId) {
         return false;
