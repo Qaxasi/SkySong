d76c3e6a0c81b752ca70a321a9840f0922952de9
Champel Mariusz <champelmariusz@gmail.com>
2024-02-07 15:48:07 +0100

    feat: Add SecureTokenGenerator dependency

--- src/main/java/com/mycompany/SkySong/auth/service/LoginServiceImpl.java
+++ src/main/java/com/mycompany/SkySong/auth/service/LoginServiceImpl.java
@@ -1,7 +1,8 @@
 package com.mycompany.SkySong.auth.service;
 
 import com.mycompany.SkySong.auth.model.dto.LoginRequest;
-    import com.mycompany.SkySong.shared.service.ApplicationMessageService;
+import com.mycompany.SkySong.auth.security.SecureTokenGenerator;
+import com.mycompany.SkySong.shared.service.ApplicationMessageService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.Authentication;
@@ -12,11 +13,13 @@ import org.springframework.stereotype.Service;
 @Service
 class LoginServiceImpl implements LoginService {
 
+    private final SecureTokenGenerator tokenGenerator;
     private final ApplicationMessageService messageService;
     private final UserAuthenticationService userAuth;
 
-    public LoginServiceImpl(ApplicationMessageService messageService,
+    public LoginServiceImpl(SecureTokenGenerator tokenGenerator, ApplicationMessageService messageService,
                             UserAuthenticationService userAuth) {
+        this.tokenGenerator = tokenGenerator;
         this.messageService = messageService;
         this.userAuth = userAuth;
     }
