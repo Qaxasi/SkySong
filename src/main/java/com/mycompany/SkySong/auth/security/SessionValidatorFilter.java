02f5efba2abd666944cccb3b34672f7e18a72b10
Champel Mariusz <champelmariusz@gmail.com>
2024-02-07 21:28:23 +0100

    feat: Create class and extend OncePerRequestFilter

---
+++ src/main/java/com/mycompany/SkySong/auth/security/SessionValidatorFilter.java
@@ -0,0 +1,17 @@
+package com.mycompany.SkySong.auth.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+public class SessionValidatorFilter extends OncePerRequestFilter {
+}
