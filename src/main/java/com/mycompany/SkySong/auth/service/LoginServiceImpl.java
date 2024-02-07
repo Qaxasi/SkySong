e3a1be174c353ef278f2de9a29b1b6f8048f9b97
Champel Mariusz <champelmariusz@gmail.com>
2024-02-07 18:59:08 +0100

    feat: Inject UserDAO

--- src/main/java/com/mycompany/SkySong/auth/service/LoginServiceImpl.java
+++ src/main/java/com/mycompany/SkySong/auth/service/LoginServiceImpl.java
@@ -2,12 +2,17 @@ package com.mycompany.SkySong.auth.service;
 
 import com.mycompany.SkySong.auth.model.dto.LoginRequest;
 import com.mycompany.SkySong.auth.security.SessionCreation;
+import com.mycompany.SkySong.shared.entity.User;
+import com.mycompany.SkySong.shared.repository.UserDAO;
 import com.mycompany.SkySong.shared.service.ApplicationMessageService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Slf4j
 @Service
@@ -16,12 +21,14 @@ class LoginServiceImpl implements LoginService {
     private final SessionCreation sessionCreation;
     private final ApplicationMessageService messageService;
     private final UserAuthenticationService userAuth;
+    private final UserDAO userDAO;
 
     public LoginServiceImpl(SessionCreation sessionCreation, ApplicationMessageService messageService,
-                            UserAuthenticationService userAuth) {
+                            UserAuthenticationService userAuth, UserDAO userDAO) {
         this.sessionCreation = sessionCreation;
         this.messageService = messageService;
         this.userAuth = userAuth;
+        this.userDAO = userDAO;
     }
 
     @Override
