e6fff33b4df2ad51aea917c0b8c11296e62522a3
Champel Mariusz <champelmariusz@gmail.com>
2024-02-07 21:25:44 +0100

    refactor: Delete unused import

--- src/main/java/com/mycompany/SkySong/auth/controller/LogoutController.java
+++ src/main/java/com/mycompany/SkySong/auth/controller/LogoutController.java
@@ -1,10 +1,8 @@
 package com.mycompany.SkySong.auth.controller;
 
-
 import com.mycompany.SkySong.auth.security.SessionDeletion;
 import com.mycompany.SkySong.shared.dto.ApiResponse;
 import com.mycompany.SkySong.shared.service.ApplicationMessageService;
-import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -22,6 +20,7 @@ public class LogoutController {
         this.messageService = messageService;
         this.sessionDeletion = sessionDeletion;
     }
+    
     @PostMapping("/logout")
     public ResponseEntity<?> logout(@CookieValue("session_id") String token) {
         sessionDeletion.deleteSession(token);
