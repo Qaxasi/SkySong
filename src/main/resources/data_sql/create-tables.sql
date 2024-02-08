520ccc5f9110aa29b7e7fb4a03a3bf2366302346
Champel Mariusz <champelmariusz@gmail.com>
2024-02-08 20:51:02 +0100

    feat: Add sessions table to database schema

--- src/main/resources/data_sql/create-tables.sql
+++ src/main/resources/data_sql/create-tables.sql
@@ -18,3 +18,10 @@
      FOREIGN KEY (user_id) REFERENCES users (id),
      FOREIGN KEY (role_id) REFERENCES roles (id)
  );
+
+ CREATE TABLE sessions (
+     session_id VARCHAR(255) PRIMARY KEY,
+     user_id INTEGER NOT NULL,
+     create_at TIMESTAMP NOT NULL,
+     expires_at TIMESTAMP NOT NULL
+ );
