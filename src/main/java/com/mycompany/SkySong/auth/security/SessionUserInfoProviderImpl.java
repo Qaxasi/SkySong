0bb50da0d36d7ee17e596b73f8d104b44908715e
Champel Mariusz <champelmariusz@gmail.com>
2024-02-08 16:29:03 +0100

    feat: Create class and implement SessionUserInfoProvider interface

---
+++ src/main/java/com/mycompany/SkySong/auth/security/SessionUserInfoProviderImpl.java
@@ -0,0 +1,9 @@
+package com.mycompany.SkySong.auth.security;
+
+public class SessionUserInfoProviderImpl implements SessionUserInfoProvider {
+    
+    @Override
+    public String getUsernameForSession(String sessionId) {
+        return null;
+    }
+}
