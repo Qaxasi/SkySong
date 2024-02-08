70ce1a006fbf4dbec183a86c019a09df7a2cc6f5
Champel Mariusz <champelmariusz@gmail.com>
2024-02-08 16:53:56 +0100

    feat: Add dependencies

--- src/main/java/com/mycompany/SkySong/auth/security/SessionUserInfoProviderImpl.java
+++ src/main/java/com/mycompany/SkySong/auth/security/SessionUserInfoProviderImpl.java
@@ -1,9 +1,23 @@
 package com.mycompany.SkySong.auth.security;
 
+import com.mycompany.SkySong.auth.repository.SessionDAO;
+import com.mycompany.SkySong.shared.repository.UserDAO;
 import org.springframework.stereotype.Service;
 
 @Service
 public class SessionUserInfoProviderImpl implements SessionUserInfoProvider {
+
+    private final SessionDAO sessionDAO;
+
+    private final UserDAO userDAO;
+
+    private final SecureTokenGenerator tokenGenerator;
+
+    public SessionUserInfoProviderImpl(SessionDAO sessionDAO, UserDAO userDAO, SecureTokenGenerator tokenGenerator) {
+        this.sessionDAO = sessionDAO;
+        this.userDAO = userDAO;
+        this.tokenGenerator = tokenGenerator;
+    }
 
     @Override
     public String getUsernameForSession(String sessionId) {
