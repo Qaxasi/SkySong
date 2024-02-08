0303cf900f168919368c5458552a9463ee3508fe
Champel Mariusz <champelmariusz@gmail.com>
2024-02-08 16:29:20 +0100

    feat: Annotate class as a spring service

--- src/main/java/com/mycompany/SkySong/auth/security/SessionUserInfoProviderImpl.java
+++ src/main/java/com/mycompany/SkySong/auth/security/SessionUserInfoProviderImpl.java
@@ -1,7 +1,10 @@
 package com.mycompany.SkySong.auth.security;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class SessionUserInfoProviderImpl implements SessionUserInfoProvider {
-    
+
     @Override
     public String getUsernameForSession(String sessionId) {
         return null;
