376e2137ed241856019fd70f69a2d02e1e446d69
Champel Mariusz <champelmariusz@gmail.com>
2024-02-08 18:37:06 +0100

    refactor: Refactor filter yo authenticate user based on valid session

--- src/main/java/com/mycompany/SkySong/auth/security/SessionValidatorFilter.java
+++ src/main/java/com/mycompany/SkySong/auth/security/SessionValidatorFilter.java
@@ -6,6 +6,9 @@ import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.jetbrains.annotations.NotNull;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -48,7 +51,17 @@ public class SessionValidatorFilter extends OncePerRequestFilter {
                 .orElse(null);
 
 
-        if (sessionId == null || !session.validateSession(sessionId)) {
+        if (sessionId != null && session.validateSession(sessionId)) {
+
+            String username = userInfoProvider.getUsernameForSession(sessionId);
+            UserDetails details = userDetails.loadUserByUsername(username);
+
+            UsernamePasswordAuthenticationToken authentication =
+                    new UsernamePasswordAuthenticationToken(details, null, details.getAuthorities());
+
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        } else {
             response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
             return;
         }
