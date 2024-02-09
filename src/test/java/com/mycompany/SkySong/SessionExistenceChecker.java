3cd9fd7abe58d75613da38a5e9dd4b95d2d4bf1c
Champel Mariusz <champelmariusz@gmail.com>
2024-02-09 16:24:07 +0100

    test: Add dependency

--- src/test/java/com/mycompany/SkySong/SessionExistenceChecker.java
+++ src/test/java/com/mycompany/SkySong/SessionExistenceChecker.java
@@ -2,6 +2,14 @@ package com.mycompany.SkySong;
 
 import org.springframework.stereotype.Component;
 
+import javax.sql.DataSource;
+
 @Component
 public class SessionExistenceChecker {
+
+    private final DataSource dataSource;
+
+    public SessionExistenceChecker(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 }
