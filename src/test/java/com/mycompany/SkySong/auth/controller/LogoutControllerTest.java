3ba7ddceb1bf831027a042b6e2cefbde1bee3d4a
Champel Mariusz <champelmariusz@gmail.com>
2024-02-09 14:55:32 +0100

    test: Add test for logout without session cookie

--- src/test/java/com/mycompany/SkySong/auth/controller/LogoutControllerTest.java
+++ src/test/java/com/mycompany/SkySong/auth/controller/LogoutControllerTest.java
@@ -3,11 +3,18 @@ package com.mycompany.SkySong.auth.controller;
 import com.mycompany.SkySong.SqlDatabaseCleaner;
 import com.mycompany.SkySong.SqlDatabaseInitializer;
 import com.mycompany.SkySong.testsupport.BaseIT;
+import com.mycompany.SkySong.testsupport.auth.LoginRequests;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+
+import static com.mycompany.SkySong.testsupport.JsonUtils.asJsonString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @AutoConfigureMockMvc
 public class LogoutControllerTest extends BaseIT {
@@ -28,5 +35,11 @@ public class LogoutControllerTest extends BaseIT {
     @AfterEach
     void cleanUp() {
         cleaner.clean();
+    }
+
+    @Test
+    void whenLogoutWithoutCookie_ReturnUnauthorized() throws Exception {
+        mockMvc.perform(post("/api/v1/users/logout"))
+                .andExpect(status().isUnauthorized());
     }
 }
