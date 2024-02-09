a2f6473a8ed4d3efe8209f6cf9628a7a448c2275
Champel Mariusz <champelmariusz@gmail.com>
2024-02-09 14:38:29 +0100

    test: Add @AutoConfigureMockMvc annotation

--- src/test/java/com/mycompany/SkySong/auth/controller/LogoutControllerTest.java
+++ src/test/java/com/mycompany/SkySong/auth/controller/LogoutControllerTest.java
@@ -1,6 +1,9 @@
 package com.mycompany.SkySong.auth.controller;
 
 import com.mycompany.SkySong.testsupport.BaseIT;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 
+@AutoConfigureMockMvc
 public class LogoutControllerTest extends BaseIT {
+    
 }
