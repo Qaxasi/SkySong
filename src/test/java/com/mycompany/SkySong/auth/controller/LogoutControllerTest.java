f78533b5821b2164f3398f07c7d49a932f2e230b
Champel Mariusz <champelmariusz@gmail.com>
2024-02-09 14:40:08 +0100

    test: Inject dependency

--- src/test/java/com/mycompany/SkySong/auth/controller/LogoutControllerTest.java
+++ src/test/java/com/mycompany/SkySong/auth/controller/LogoutControllerTest.java
@@ -1,9 +1,17 @@
 package com.mycompany.SkySong.auth.controller;
 
+import com.mycompany.SkySong.SqlDatabaseCleaner;
+import com.mycompany.SkySong.SqlDatabaseInitializer;
 import com.mycompany.SkySong.testsupport.BaseIT;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.test.web.servlet.MockMvc;
 
 @AutoConfigureMockMvc
 public class LogoutControllerTest extends BaseIT {
-    
+
+    @Autowired
+    private MockMvc mockMvc;
 }
