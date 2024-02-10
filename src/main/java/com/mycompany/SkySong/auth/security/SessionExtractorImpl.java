909e071749dcaaabbf9a07908d8faf9842d2d7cb
Champel Mariusz <champelmariusz@gmail.com>
2024-02-10 13:54:31 +0100

    feat: Implement getSessionIdFromRequest method

--- src/main/java/com/mycompany/SkySong/auth/security/SessionExtractorImpl.java
+++ src/main/java/com/mycompany/SkySong/auth/security/SessionExtractorImpl.java
@@ -1,11 +1,21 @@
 package com.mycompany.SkySong.auth.security;
 
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 
+import java.util.Arrays;
+import java.util.Optional;
+
 public class SessionExtractorImpl implements SessionExtractor {
-    
+
     @Override
     public String getSessionIdFromRequest(HttpServletRequest request) {
-        return null;
+        return Optional.ofNullable(request.getCookies())
+                .stream()
+                .flatMap(Arrays::stream)
+                .filter(cookie -> "session_id".equals(cookie.getName()))
+                .findFirst()
+                .map(Cookie::getValue)
+                .orElse(null);
     }
 }
