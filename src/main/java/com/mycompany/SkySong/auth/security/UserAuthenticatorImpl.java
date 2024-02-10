154c7a23eda3c4964545c7d60f7c7e24704b60bd
Champel Mariusz <champelmariusz@gmail.com>
2024-02-10 14:08:51 +0100

    feat: Create class and implement UserAuthenticator interface

---
+++ src/main/java/com/mycompany/SkySong/auth/security/UserAuthenticatorImpl.java
@@ -0,0 +1,9 @@
+package com.mycompany.SkySong.auth.security;
+
+public class UserAuthenticatorImpl implements UserAuthenticator {
+    
+    @Override
+    public void authenticateUser(String sessionId) {
+
+    }
+}
