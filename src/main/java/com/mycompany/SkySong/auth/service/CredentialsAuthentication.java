5400441ce1d4e030541f9f964e08065177da2e5e
Champel Mariusz <champelmariusz@gmail.com>
2024-02-10 16:31:12 +0100

    refactor: Rename interface

--- src/main/java/com/mycompany/SkySong/auth/service/CredentialsAuthentication.java
+++ src/main/java/com/mycompany/SkySong/auth/service/CredentialsAuthentication.java
@@ -3,6 +3,6 @@ package com.mycompany.SkySong.auth.service;
 import com.mycompany.SkySong.auth.model.dto.LoginRequest;
 import org.springframework.security.core.Authentication;
 
-public interface UserAuthenticationService {
+public interface CredentialsAuthentication {
     Authentication authenticateUser(LoginRequest loginRequest);
 }
