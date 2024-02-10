dbf84f922d543b6c4faef8791ec7242e27058251
Champel Mariusz <champelmariusz@gmail.com>
2024-02-10 19:47:28 +0100

    feat: Add session records for users in database setup script

--- src/main/resources/data_sql/test-setup.sql
+++ src/main/resources/data_sql/test-setup.sql
@@ -33,3 +33,14 @@ INSERT INTO user_roles (user_id, role_id)
 SELECT u.id, r.id
 FROM users u, roles r
 WHERE u.username = 'testAdmin' AND r.name = 'ROLE_ADMIN';
+
+INSERT INTO sessions (session_id, user_id, create_at, expires_at)
+VALUES
+('ZkX8l8qgPRXen35UIFwINREhPAlgJeDTp0dz_Vb5ueI=', (SELECT id FROM users WHERE username = 'User')
+,NOW(), DATE_ADD(NOW(), INTERVAL 24 HOUR)),
+
+('PBqIWGB-xFyONvAqah2zsGhB-o2uzeTDTqipZ_OALZM=', (SELECT id FROM users WHERE username = 'Mark'),
+NOW(), DATE_ADD(NOW(), INTERVAL 24 HOUR)),
+
+('FOZcMfuRoswxlcPb5L5G4zLbb6j1i7LuYdfvyhfJYwg=', (SELECT id FROM users WHERE username = 'testAdmin'),
+ NOW(), DATE_ADD(NOW(), INTERVAL 24 HOUR));
