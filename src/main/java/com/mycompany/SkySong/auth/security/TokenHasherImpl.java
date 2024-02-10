86809023d9c766652603c7b73e73e93934ed20d8
Champel Mariusz <champelmariusz@gmail.com>
2024-02-10 12:58:46 +0100

    feat: Add Base64 URL encoder

--- src/main/java/com/mycompany/SkySong/auth/security/TokenHasherImpl.java
+++ src/main/java/com/mycompany/SkySong/auth/security/TokenHasherImpl.java
@@ -1,6 +1,10 @@
 package com.mycompany.SkySong.auth.security;
 
+import java.util.Base64;
+
 public class TokenHasherImpl implements TokenHasher {
+
+    private final Base64.Encoder encoder = Base64.getUrlEncoder();
 
     @Override
     public String generateHashedToken(String token) {
