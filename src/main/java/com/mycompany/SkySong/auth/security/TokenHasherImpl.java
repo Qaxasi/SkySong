7c3569938bcdbe13af929b4cd53193ffece634f8
Champel Mariusz <champelmariusz@gmail.com>
2024-02-10 12:53:42 +0100

    feat: Create class and implement TokenHasher interface

---
+++ src/main/java/com/mycompany/SkySong/auth/security/TokenHasherImpl.java
@@ -0,0 +1,9 @@
+package com.mycompany.SkySong.auth.security;
+
+public class TokenHasherImpl implements TokenHasher {
+
+    @Override
+    public String generateHashedToken(String token) {
+        return null;
+    }
+}
