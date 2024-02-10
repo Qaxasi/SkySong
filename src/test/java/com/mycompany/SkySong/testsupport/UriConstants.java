d8ca6acb3590da4eadaee080638956e10259dcb3
Champel Mariusz <champelmariusz@gmail.com>
2024-02-10 18:12:37 +0100

    refactor: Delete unnecessary classes

--- src/test/java/com/mycompany/SkySong/testsupport/UriConstants.java
+++
@@ -1,7 +0,0 @@
-package com.mycompany.SkySong.testsupport;
-
-public class UriConstants {
-    public static final String LOGIN_URI = "/api/v1/users/login";
-    public static final String LOGOUT_URI = "/api/v1/users/logout";
-    public static final String REGISTRATION_URI = "/api/v1/users/register";
-}
