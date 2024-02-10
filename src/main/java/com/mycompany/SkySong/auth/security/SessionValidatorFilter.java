82a8afc76b128c3c6dcaea5e3a0ff35356ed5779
Champel Mariusz <champelmariusz@gmail.com>
2024-02-10 14:15:08 +0100

    refactor: Delete unnecessary dependencies

--- src/main/java/com/mycompany/SkySong/auth/security/SessionValidatorFilter.java
+++ src/main/java/com/mycompany/SkySong/auth/security/SessionValidatorFilter.java
@@ -7,9 +7,6 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.security.authentication.InsufficientAuthenticationException;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -19,8 +16,6 @@ import java.io.IOException;
 public class SessionValidatorFilter extends OncePerRequestFilter {
 
     private final SessionValidation session;
-    private final CustomUserDetailsService userDetails;
-    private final SessionUserInfoProvider userInfoProvider;
     private final CustomAuthenticationEntryPoint authEntryPoint;
     private final ApplicationMessageService message;
     private final SessionExtractor sessionExtractor;
