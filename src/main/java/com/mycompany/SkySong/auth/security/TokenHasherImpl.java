54088bef27e174df2bc97671e563a582390d5322
Champel Mariusz <champelmariusz@gmail.com>
2024-02-10 12:59:09 +0100

    feat: Annotate class as a spring service

--- src/main/java/com/mycompany/SkySong/auth/security/TokenHasherImpl.java
+++ src/main/java/com/mycompany/SkySong/auth/security/TokenHasherImpl.java
@@ -1,7 +1,10 @@
 package com.mycompany.SkySong.auth.security;
 
+import org.springframework.stereotype.Service;
+
 import java.util.Base64;
 
+@Service
 public class TokenHasherImpl implements TokenHasher {
 
     private final Base64.Encoder encoder = Base64.getUrlEncoder();
