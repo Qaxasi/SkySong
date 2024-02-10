51e59690d6d83857deca188d5b74a04f832f5473
Champel Mariusz <champelmariusz@gmail.com>
2024-02-10 14:10:05 +0100

    feat: Add dependencies

--- src/main/java/com/mycompany/SkySong/auth/security/UserAuthenticatorImpl.java
+++ src/main/java/com/mycompany/SkySong/auth/security/UserAuthenticatorImpl.java
@@ -1,7 +1,15 @@
 package com.mycompany.SkySong.auth.security;
 
 public class UserAuthenticatorImpl implements UserAuthenticator {
-    
+
+    private final SessionUserInfoProvider userInfoProvider;
+    private final CustomUserDetailsService userDetails;
+
+    public UserAuthenticatorImpl(SessionUserInfoProvider userInfoProvider, CustomUserDetailsService userDetails) {
+        this.userInfoProvider = userInfoProvider;
+        this.userDetails = userDetails;
+    }
+
     @Override
     public void authenticateUser(String sessionId) {
 
