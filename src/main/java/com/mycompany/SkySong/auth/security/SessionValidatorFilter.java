018ded13556d34f01b8518e49487c0fc6e1be19e
Champel Mariusz <champelmariusz@gmail.com>
2024-02-10 14:12:33 +0100

    feat: Add dependency

--- src/main/java/com/mycompany/SkySong/auth/security/SessionValidatorFilter.java
+++ src/main/java/com/mycompany/SkySong/auth/security/SessionValidatorFilter.java
@@ -3,7 +3,6 @@ package com.mycompany.SkySong.auth.security;
 import com.mycompany.SkySong.shared.service.ApplicationMessageService;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
-import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.jetbrains.annotations.NotNull;
@@ -15,8 +14,6 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.Optional;
 
 @Component
 public class SessionValidatorFilter extends OncePerRequestFilter {
@@ -27,17 +24,19 @@ public class SessionValidatorFilter extends OncePerRequestFilter {
     private final CustomAuthenticationEntryPoint authEntryPoint;
     private final ApplicationMessageService message;
     private final SessionExtractor sessionExtractor;
+    private final UserAuthenticator authenticator;
 
     public SessionValidatorFilter(SessionValidation session,
                                   CustomUserDetailsService userDetails,
                                   SessionUserInfoProvider userInfoProvider,
-                                  CustomAuthenticationEntryPoint authEntryPoint, ApplicationMessageService message, SessionExtractor sessionExtractor) {
+                                  CustomAuthenticationEntryPoint authEntryPoint, ApplicationMessageService message, SessionExtractor sessionExtractor, UserAuthenticator authenticator) {
         this.session = session;
         this.userDetails = userDetails;
         this.userInfoProvider = userInfoProvider;
         this.authEntryPoint = authEntryPoint;
         this.message = message;
         this.sessionExtractor = sessionExtractor;
+        this.authenticator = authenticator;
     }
 
     @Override
