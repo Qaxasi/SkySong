ee17de75985640f4ed79809acfc86c0b2645d109
Champel Mariusz <champelmariusz@gmail.com>
2024-02-13 23:43:39 +0100

    test: Initialize TokenGenerator in test setup

--- src/test/java/com/mycompany/SkySong/auth/security/TokenGeneratorTest.java
+++ src/test/java/com/mycompany/SkySong/auth/security/TokenGeneratorTest.java
@@ -1,4 +1,13 @@
 package com.mycompany.SkySong.auth.security;
 
+import org.junit.jupiter.api.BeforeEach;
+
 public class TokenGeneratorTest {
+
+    private TokenGenerator tokenGenerator;
+
+    @BeforeEach
+    void setUp() {
+        tokenGenerator = new TokenGeneratorImpl();
+    }
 }
