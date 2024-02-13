8e7aa9043a92ec92d526d6bdf0a38fb8978fbc93
Champel Mariusz <champelmariusz@gmail.com>
2024-02-13 22:44:06 +0100

    test: Initialize TokenHasher in test setup

--- src/test/java/com/mycompany/SkySong/auth/security/TokenHasherTest.java
+++ src/test/java/com/mycompany/SkySong/auth/security/TokenHasherTest.java
@@ -1,4 +1,15 @@
 package com.mycompany.SkySong.auth.security;
 
+import org.junit.jupiter.api.BeforeEach;
+
 public class TokenHasherTest {
+
+    private TokenHasher tokenHasher;
+
+    @BeforeEach
+    void setUp() {
+        tokenHasher = new TokenHasherImpl();
+    }
+
+    
 }
