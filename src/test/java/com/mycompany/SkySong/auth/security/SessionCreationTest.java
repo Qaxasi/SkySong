95aaa95a84d064fbd6dcd082a515682544654f8c
Champel Mariusz <champelmariusz@gmail.com>
2024-02-14 00:22:42 +0100

    test: Inject dependencies

--- src/test/java/com/mycompany/SkySong/auth/security/SessionCreationTest.java
+++ src/test/java/com/mycompany/SkySong/auth/security/SessionCreationTest.java
@@ -1,6 +1,10 @@
 package com.mycompany.SkySong.auth.security;
 
 import com.mycompany.SkySong.testsupport.BaseIT;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class SessionCreationTest extends BaseIT {
+
+    @Autowired
+    private SessionCreation session;
 }
