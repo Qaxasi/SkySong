2aaf468ad8875c7214f23a60f8836295bd6d318f
Champel Mariusz <champelmariusz@gmail.com>
2024-02-14 14:36:05 +0100

    test: Add test to verify raw token is not stored in DB

--- src/test/java/com/mycompany/SkySong/auth/security/SessionCreationTest.java
+++ src/test/java/com/mycompany/SkySong/auth/security/SessionCreationTest.java
@@ -1,12 +1,16 @@
 package com.mycompany.SkySong.auth.security;
 
+import com.mycompany.SkySong.SessionExistenceChecker;
 import com.mycompany.SkySong.SqlDatabaseCleaner;
 import com.mycompany.SkySong.SqlDatabaseInitializer;
+import com.mycompany.SkySong.auth.model.entity.Session;
 import com.mycompany.SkySong.testsupport.BaseIT;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -14,6 +18,8 @@ public class SessionCreationTest extends BaseIT {
 
     @Autowired
     private SessionCreation session;
+    @Autowired
+    private SessionExistenceChecker checker;
 
     @Autowired
     private SqlDatabaseInitializer initializer;
@@ -34,5 +40,11 @@ public class SessionCreationTest extends BaseIT {
     void whenCreatingSession_ReturnsNonNullToken() {
         String token = session.createSession(10);
         assertThat(token).isNotNull();
+    }
+
+    @Test
+    void whenCreatingSession_NotStoreRawTokenInDatabase() {
+        String token = session.createSession(10);
+        assertThat(checker.sessionExist(token)).isFalse();
     }
 }
