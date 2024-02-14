cd170920043a542afd84df1e5394cd003a416a65
Champel Mariusz <champelmariusz@gmail.com>
2024-02-14 20:35:05 +0100

    test: Setup SessionExtractor and mock HttpServletRequest in tests

--- src/test/java/com/mycompany/SkySong/auth/security/SessionExtractorTest.java
+++ src/test/java/com/mycompany/SkySong/auth/security/SessionExtractorTest.java
@@ -1,8 +1,22 @@
 package com.mycompany.SkySong.auth.security;
 
+import jakarta.servlet.http.HttpServletRequest;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
 public class SessionExtractorTest {
+
+    private SessionExtractor sessionExtractor;
+
+    @Mock
+    private HttpServletRequest request;
+
+    @BeforeEach
+    void setUp() {
+        sessionExtractor = new SessionExtractorImpl();
+    }
+
 }
