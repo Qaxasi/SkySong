f75c74602829d35976036a77ef75e9415c01758c
Champel Mariusz <champelmariusz@gmail.com>
2024-02-14 18:42:47 +0100

    test: Implement database setup and cleanup for tests

--- src/test/java/com/mycompany/SkySong/auth/security/SessionDeletionTest.java
+++ src/test/java/com/mycompany/SkySong/auth/security/SessionDeletionTest.java
@@ -4,6 +4,8 @@ import com.mycompany.SkySong.SessionExistenceChecker;
 import com.mycompany.SkySong.SqlDatabaseCleaner;
 import com.mycompany.SkySong.SqlDatabaseInitializer;
 import com.mycompany.SkySong.testsupport.BaseIT;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 
 public class SessionDeletionTest extends BaseIT {
@@ -17,4 +19,14 @@ public class SessionDeletionTest extends BaseIT {
     private SqlDatabaseInitializer initializer;
     @Autowired
     private SqlDatabaseCleaner cleaner;
+
+    @BeforeEach
+    void setUp() throws Exception {
+        initializer.setup("data_sql/test-setup.sql");
+    }
+
+    @AfterEach
+    void cleanUp() {
+        cleaner.clean();
+    }
 }
