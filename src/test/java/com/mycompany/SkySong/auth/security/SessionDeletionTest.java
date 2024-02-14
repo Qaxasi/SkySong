32e75b7f5d84493a598bc9385cf3071bddabc5ae
Champel Mariusz <champelmariusz@gmail.com>
2024-02-14 18:41:16 +0100

    test: Inject dependencies

--- src/test/java/com/mycompany/SkySong/auth/security/SessionDeletionTest.java
+++ src/test/java/com/mycompany/SkySong/auth/security/SessionDeletionTest.java
@@ -1,6 +1,20 @@
 package com.mycompany.SkySong.auth.security;
 
+import com.mycompany.SkySong.SessionExistenceChecker;
+import com.mycompany.SkySong.SqlDatabaseCleaner;
+import com.mycompany.SkySong.SqlDatabaseInitializer;
 import com.mycompany.SkySong.testsupport.BaseIT;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class SessionDeletionTest extends BaseIT {
+
+    @Autowired
+    private SessionDeletion sessionDeletion;
+    @Autowired
+    private SessionExistenceChecker checker;
+
+    @Autowired
+    private SqlDatabaseInitializer initializer;
+    @Autowired
+    private SqlDatabaseCleaner cleaner;
 }
