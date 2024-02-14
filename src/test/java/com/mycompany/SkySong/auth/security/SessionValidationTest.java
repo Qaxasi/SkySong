6d229fe029ee461f26e6a5abf22f8c57b1ae021e
Champel Mariusz <champelmariusz@gmail.com>
2024-02-14 23:58:24 +0100

    test: Add test for validating a valid session returns true

--- src/test/java/com/mycompany/SkySong/auth/security/SessionValidationTest.java
+++ src/test/java/com/mycompany/SkySong/auth/security/SessionValidationTest.java
@@ -5,7 +5,10 @@ import com.mycompany.SkySong.SqlDatabaseInitializer;
 import com.mycompany.SkySong.testsupport.BaseIT;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class SessionValidationTest extends BaseIT {
 
@@ -25,5 +28,10 @@ public class SessionValidationTest extends BaseIT {
     @AfterEach
     void cleanUp() {
         cleaner.clean();
+    }
+
+    @Test
+    void whenValidSession_ReturnsTrue() {
+        assertTrue(validation.validateSession("jrYa_WLToysV-r08qLhwUZncJLY8OPgT"));
     }
 }
