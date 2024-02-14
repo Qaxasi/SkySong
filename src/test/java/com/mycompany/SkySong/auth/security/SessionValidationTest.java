595011b21e2ac8ff761f14a49640d5b2783af75f
Champel Mariusz <champelmariusz@gmail.com>
2024-02-14 21:44:47 +0100

    test: Inject dependencies

--- src/test/java/com/mycompany/SkySong/auth/security/SessionValidationTest.java
+++ src/test/java/com/mycompany/SkySong/auth/security/SessionValidationTest.java
@@ -1,6 +1,21 @@
 package com.mycompany.SkySong.auth.security;
 
+import com.mycompany.SkySong.SqlDatabaseCleaner;
+import com.mycompany.SkySong.SqlDatabaseInitializer;
 import com.mycompany.SkySong.testsupport.BaseIT;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class SessionValidationTest extends BaseIT {
+
+    @Autowired
+    private SessionValidation validation;
+
+    @Autowired
+    private SqlDatabaseInitializer initializer;
+    @Autowired
+    private SqlDatabaseCleaner cleaner;
+
+   
 }
