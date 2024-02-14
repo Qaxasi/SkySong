dafbd33bd946186fda0d16a14b9bf1e9a09011d4
Champel Mariusz <champelmariusz@gmail.com>
2024-02-14 16:09:33 +0100

    feat: Add method to get session

--- src/test/java/com/mycompany/SkySong/SessionGetter.java
+++ src/test/java/com/mycompany/SkySong/SessionGetter.java
@@ -1,8 +1,14 @@
 package com.mycompany.SkySong;
 
+import com.mycompany.SkySong.auth.model.entity.Session;
 import org.springframework.stereotype.Component;
 
 import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Optional;
 
 @Component
 public class SessionGetter {
@@ -11,5 +17,29 @@ public class SessionGetter {
 
     public SessionGetter(DataSource dataSource) {
         this.dataSource = dataSource;
+    }
+
+    public Optional<Session> getSession(String sessionId) {
+        try {
+            String query = "SELECT * FROM sessions WHERE session_id = ?";
+            try (Connection connection = dataSource.getConnection();
+                 PreparedStatement statement = connection.prepareStatement(query)) {
+
+                statement.setString(1, sessionId);
+                ResultSet resultSet = statement.executeQuery();
+
+                if (resultSet.next()) {
+                    Session session = new Session();
+                    session.setSessionId(resultSet.getString("session_id"));
+                    session.setUserId(resultSet.getInt("user_id"));
+                    session.setCreateAt(resultSet.getTimestamp("create_at"));
+                    session.setExpiresAt(resultSet.getTimestamp("expires_at"));
+                    return Optional.of(session);
+                }
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException("Error during getting session " + e.getMessage(), e);
+        }
+        return Optional.empty();
     }
 }
