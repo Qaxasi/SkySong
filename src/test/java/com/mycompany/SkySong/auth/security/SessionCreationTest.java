78626229cbaf7271127534a9eb0294255399f7bc
Champel Mariusz <champelmariusz@gmail.com>
2024-02-14 14:27:16 +0100

    test: Add test for non-null session token creation

--- src/test/java/com/mycompany/SkySong/auth/security/SessionCreationTest.java
+++ src/test/java/com/mycompany/SkySong/auth/security/SessionCreationTest.java
@@ -8,6 +8,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class SessionCreationTest extends BaseIT {
 
     @Autowired
@@ -26,5 +28,11 @@ public class SessionCreationTest extends BaseIT {
     @AfterEach
     void cleanUp() {
         cleaner.clean();
+    }
+
+    @Test
+    void whenCreatingSession_ReturnsNonNullToken() {
+        String token = session.createSession(10);
+        assertThat(token).isNotNull();
     }
 }
