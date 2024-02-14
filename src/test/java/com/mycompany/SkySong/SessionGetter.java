288f20aac74064c908f7c5878abba2d902315c4f
Champel Mariusz <champelmariusz@gmail.com>
2024-02-14 16:08:18 +0100

    feat: Add DataSource dependency

--- src/test/java/com/mycompany/SkySong/SessionGetter.java
+++ src/test/java/com/mycompany/SkySong/SessionGetter.java
@@ -2,6 +2,14 @@ package com.mycompany.SkySong;
 
 import org.springframework.stereotype.Component;
 
+import javax.sql.DataSource;
+
 @Component
 public class SessionGetter {
+
+    private final DataSource dataSource;
+
+    public SessionGetter(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 }
