ca2fe66c690252f51723c507c2de647df07a9b64
Champel Mariusz <champelmariusz@gmail.com>
2024-02-14 20:24:54 +0100

    test: Remove BaseIT extension

--- src/test/java/com/mycompany/SkySong/auth/security/SessionExtractorTest.java
+++ src/test/java/com/mycompany/SkySong/auth/security/SessionExtractorTest.java
@@ -1,6 +1,4 @@
 package com.mycompany.SkySong.auth.security;
 
-import com.mycompany.SkySong.testsupport.BaseIT;
-
-public class SessionExtractorTest extends BaseIT {
+public class SessionExtractorTest {
 }
