8b2ab3923709cf2fbad4aed4af17a2fcbd0e30f7
Champel Mariusz <champelmariusz@gmail.com>
2024-02-15 15:42:47 +0100

    test: Add method to create expired session

--- src/test/java/com/mycompany/SkySong/testsupport/auth/security/SessionTestHelper.java
+++ src/test/java/com/mycompany/SkySong/testsupport/auth/security/SessionTestHelper.java
@@ -1,8 +1,11 @@
 package com.mycompany.SkySong.testsupport.auth.security;
 
+import com.mycompany.SkySong.auth.model.entity.Session;
 import com.mycompany.SkySong.auth.security.TokenGenerator;
 import com.mycompany.SkySong.auth.security.TokenHasher;
 import org.springframework.stereotype.Component;
+
+import java.util.Date;
 
 @Component
 public class SessionTestHelper {
@@ -15,4 +18,17 @@ public class SessionTestHelper {
         this.tokenGenerator = tokenGenerator;
     }
 
+    public String createExpiredSession(Integer userID) {
+        String token = tokenGenerator.generateToken();
+        String hashedToken = tokenHasher.generateHashedToken(token);
+
+        Session session = new Session();
+        session.setSessionId(hashedToken);
+        session.setUserId(userID);
+        session.setCreateAt(new Date());
+        session.setExpiresAt(new Date(System.currentTimeMillis() + (-10000)));
+
+        sessionDAO.save(session);
+        return token;
+    }
 }
