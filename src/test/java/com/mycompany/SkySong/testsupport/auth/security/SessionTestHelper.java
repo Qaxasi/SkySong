70776b31e0cf227db2c87dd17c29a238f9e6c652
Champel Mariusz <champelmariusz@gmail.com>
2024-02-15 15:40:40 +0100

    test: Add dependency

--- src/test/java/com/mycompany/SkySong/testsupport/auth/security/SessionTestHelper.java
+++ src/test/java/com/mycompany/SkySong/testsupport/auth/security/SessionTestHelper.java
@@ -1,5 +1,6 @@
 package com.mycompany.SkySong.testsupport.auth.security;
 
+import com.mycompany.SkySong.auth.security.TokenGenerator;
 import com.mycompany.SkySong.auth.security.TokenHasher;
 import org.springframework.stereotype.Component;
 
@@ -7,8 +8,11 @@ import org.springframework.stereotype.Component;
 public class SessionTestHelper {
 
     private final TokenHasher tokenHasher;
+    private final TokenGenerator tokenGenerator;
 
-    public SessionTestHelper(TokenHasher tokenHasher) {
+    public SessionTestHelper(TokenHasher tokenHasher, TokenGenerator tokenGenerator) {
         this.tokenHasher = tokenHasher;
+        this.tokenGenerator = tokenGenerator;
     }
+
 }
