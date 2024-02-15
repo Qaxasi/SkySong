c02ae9c5c79f2ea7792bc8dc6ce8e369168f6faa
Champel Mariusz <champelmariusz@gmail.com>
2024-02-15 16:45:01 +0100

    feat: Add @Testconteiners annotation

--- src/test/java/com/mycompany/SkySong/testsupport/BaseIT.java
+++ src/test/java/com/mycompany/SkySong/testsupport/BaseIT.java
@@ -3,9 +3,11 @@ package com.mycompany.SkySong.testsupport;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("test")
+@Testcontainers
 public class BaseIT {
 
     private static final MySQLContainer<?> container;
