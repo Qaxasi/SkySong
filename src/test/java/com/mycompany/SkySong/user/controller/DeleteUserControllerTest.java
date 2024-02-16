6558378c88ea5052dab6cac4c3f0f368a0cd0842
Champel Mariusz <champelmariusz@gmail.com>
2024-02-16 17:40:41 +0100

    test: Annotate class with AutoConfigureMockMvc

--- src/test/java/com/mycompany/SkySong/user/controller/DeleteUserControllerTest.java
+++ src/test/java/com/mycompany/SkySong/user/controller/DeleteUserControllerTest.java
@@ -9,6 +9,7 @@ import com.mycompany.SkySong.user.service.DeleteUserService;
 import com.mycompany.SkySong.testsupport.controller.DeleteRequestAssertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
@@ -18,6 +19,7 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import static org.mockito.Mockito.when;
 
+@AutoConfigureMockMvc
 public class DeleteUserControllerTest extends BaseIT {
 
     @Autowired
