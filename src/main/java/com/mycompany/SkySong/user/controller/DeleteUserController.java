9b4b540b818d5a489e51c2da9eb3cbe7be2749c5
Champel Mariusz <champelmariusz@gmail.com>
2024-02-16 15:11:30 +0100

    style: Improve readability

--- src/main/java/com/mycompany/SkySong/user/controller/DeleteUserController.java
+++ src/main/java/com/mycompany/SkySong/user/controller/DeleteUserController.java
@@ -12,12 +12,16 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/v1/users")
 public class DeleteUserController {
+
     private final DeleteUserService deleteUserService;
+
     private final ApplicationMessageService messageService;
+
     public DeleteUserController(DeleteUserService deleteUserService, ApplicationMessageService messageService) {
         this.deleteUserService = deleteUserService;
         this.messageService = messageService;
     }
+    
     @DeleteMapping({"/","/{userId}"})
     public ResponseEntity<ApiResponse> delete(@PathVariable(required = false) Long userId) throws DatabaseException {
         if (userId == null) {
