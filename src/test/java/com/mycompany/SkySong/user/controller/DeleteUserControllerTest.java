89454f8a0a25f67baea573b3b27127b405025991
Champel Mariusz <champelmariusz@gmail.com>
2024-02-16 23:04:51 +0100

    test: Add database setup and cleanup in test class

--- src/test/java/com/mycompany/SkySong/user/controller/DeleteUserControllerTest.java
+++ src/test/java/com/mycompany/SkySong/user/controller/DeleteUserControllerTest.java
@@ -1,14 +1,16 @@
 package com.mycompany.SkySong.user.controller;
 
+import com.mycompany.SkySong.SqlDatabaseCleaner;
+import com.mycompany.SkySong.SqlDatabaseInitializer;
 import com.mycompany.SkySong.auth.security.*;
 import com.mycompany.SkySong.shared.config.SecurityConfig;
 import com.mycompany.SkySong.shared.exception.UserNotFoundException;
 import com.mycompany.SkySong.shared.service.ApplicationMessageService;
 import com.mycompany.SkySong.testsupport.AuthenticationTestHelper;
 import com.mycompany.SkySong.testsupport.BaseIT;
-import com.mycompany.SkySong.user.service.DeleteUserService;
-import com.mycompany.SkySong.testsupport.controller.DeleteRequestAssertions;
 import jakarta.servlet.http.Cookie;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -20,6 +22,8 @@ import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @AutoConfigureMockMvc
 public class DeleteUserControllerTest extends BaseIT {
@@ -28,6 +32,21 @@ public class DeleteUserControllerTest extends BaseIT {
     private MockMvc mockMvc;
     @Autowired
     private AuthenticationTestHelper authHelper;
+
+    @Autowired
+    private SqlDatabaseInitializer initializer;
+    @Autowired
+    private SqlDatabaseCleaner cleaner;
+
+    @BeforeEach
+    void setUp() throws Exception {
+        initializer.setup("data_sql/test-setup.sql");
+    }
+
+    @AfterEach
+    void cleanUp() {
+        cleaner.clean();
+    }
 
     @Test
     void shouldReturnStatusNotFoundWhenUserDeletionGivenInvalidUserId() throws Exception {
