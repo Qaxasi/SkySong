98c54cf96cd399b43b0c704529c0f71df4da151e
Champel Mariusz <champelmariusz@gmail.com>
2024-02-18 15:20:38 +0100

    test: Add fetchByUsername method to retrieve user ID

--- src/test/java/com/mycompany/SkySong/UserIdFetcher.java
+++ src/test/java/com/mycompany/SkySong/UserIdFetcher.java
@@ -3,6 +3,11 @@ package com.mycompany.SkySong;
 import org.springframework.stereotype.Component;
 
 import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Optional;
 
 @Component
 public class UserIdFetcher {
@@ -11,5 +16,24 @@ public class UserIdFetcher {
 
     public UserIdFetcher(DataSource dataSource) {
         this.dataSource = dataSource;
+    }
+
+    public Integer fetchByUsername(String username) {
+        try {
+            String query = "SELECT id FROM users WHERE username = ?";
+            try (Connection connection = dataSource.getConnection();
+                 PreparedStatement statement = connection.prepareStatement(query)) {
+
+                statement.setString(1, username);
+                ResultSet resultSet = statement.executeQuery();
+
+                if (resultSet.next()) {
+                    return resultSet.getInt("id");
+                }
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException("Error fetching user ID: " + e.getMessage(), e);
+        }
+        return null;
     }
 }
