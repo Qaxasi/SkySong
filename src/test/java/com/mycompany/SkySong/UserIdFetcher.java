cac1c46849164d2d29a8911a5735606b8a40efdb
Champel Mariusz <champelmariusz@gmail.com>
2024-02-18 14:56:21 +0100

    test: Add DataSource dependency

--- src/test/java/com/mycompany/SkySong/UserIdFetcher.java
+++ src/test/java/com/mycompany/SkySong/UserIdFetcher.java
@@ -2,6 +2,14 @@ package com.mycompany.SkySong;
 
 import org.springframework.stereotype.Component;
 
+import javax.sql.DataSource;
+
 @Component
 public class UserIdFetcher {
+
+    private final DataSource dataSource;
+
+    public UserIdFetcher(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 }
