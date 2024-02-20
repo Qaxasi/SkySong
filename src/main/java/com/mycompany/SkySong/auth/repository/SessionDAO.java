f1a3931f4becbfdcb27e841e226d476089cea0cd
Champel Mariusz <champelmariusz@gmail.com>
2024-02-20 14:35:31 +0100

    feat: Add insertion method to SessionDAO

--- src/main/java/com/mycompany/SkySong/auth/repository/SessionDAO.java
+++ src/main/java/com/mycompany/SkySong/auth/repository/SessionDAO.java
@@ -1,7 +1,17 @@
 package com.mycompany.SkySong.auth.repository;
 
+import org.jdbi.v3.sqlobject.customizer.Bind;
+import org.jdbi.v3.sqlobject.statement.SqlUpdate;
 import org.springframework.stereotype.Repository;
+
+import java.sql.Timestamp;
 
 @Repository
 public interface SessionDAO {
+
+    @SqlUpdate("INSERT INTO sessions (session_id, user_id, create_at, expires_at) VALUES (:sessionId, :userId, :createAt, :expiresAt)")
+    void insert(@Bind("sessionId") String sessionId, @Bind("userId") int userId,
+                @Bind("createAt") Timestamp createAt, @Bind("expiresAt") Timestamp expiresAt);
+
+
 }
