3cf15a618d4354da0a08b2eeee1bb49d48d91164
Champel Mariusz <champelmariusz@gmail.com>
2024-02-20 16:00:37 +0100

    refactor: Update findByName method for direct role querying

--- src/main/java/com/mycompany/SkySong/auth/repository/RoleDAO.java
+++ src/main/java/com/mycompany/SkySong/auth/repository/RoleDAO.java
@@ -2,11 +2,17 @@ package com.mycompany.SkySong.auth.repository;
 
 import com.mycompany.SkySong.auth.model.entity.Role;
 import com.mycompany.SkySong.auth.model.entity.UserRole;
+import org.jdbi.v3.sqlobject.config.RegisterBeanMapper;
+import org.jdbi.v3.sqlobject.customizer.Bind;
+import org.jdbi.v3.sqlobject.statement.SqlQuery;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
 public interface RoleDAO {
-    Optional<Role> findByName(UserRole roleName);
+
+    @SqlQuery("SELECT * FROM roles WHERE name = :name")
+    @RegisterBeanMapper((Role.class))
+    Optional<Role> findByName(@Bind("name") UserRole roleName);
 }
