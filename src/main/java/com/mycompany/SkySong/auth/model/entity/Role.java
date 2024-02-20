d07c024598fd244e22b45e0554b2adec2e4e3b00
Champel Mariusz <champelmariusz@gmail.com>
2024-02-20 11:38:25 +0100

    refactor: Remove JPA dependencies

--- src/main/java/com/mycompany/SkySong/auth/model/entity/Role.java
+++ src/main/java/com/mycompany/SkySong/auth/model/entity/Role.java
@@ -1,28 +1,18 @@
 package com.mycompany.SkySong.auth.model.entity;
 
-import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.util.Set;
-
 @Getter
 @Setter
 @NoArgsConstructor
-@Entity
-@Table(name = "roles")
+@AllArgsConstructor
 public class Role {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    @Enumerated(EnumType.STRING)
     private UserRole name;
-
-    @ManyToMany(mappedBy = "roles")
-    private Set<User> users;
 
     public Role(UserRole name) {
         this.name = name;
--- src/main/java/com/mycompany/SkySong/auth/model/entity/User.java
+++ src/main/java/com/mycompany/SkySong/auth/model/entity/User.java
@@ -1,7 +1,5 @@
 package com.mycompany.SkySong.auth.model.entity;
 
-import com.mycompany.SkySong.auth.model.entity.Role;
-import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,29 +9,14 @@ import java.util.Set;
 
 @Setter
 @Getter
-@Entity
 @NoArgsConstructor
 @AllArgsConstructor
-@Table(name = "users")
 public class User {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
-
-    @Column(nullable = false, unique = true)
     private String username;
-
-    @Column(nullable = false, unique = true)
     private String email;
-
-    @Column(nullable = false)
     private String password;
-
-    @ManyToMany(fetch = FetchType.EAGER, cascade =  CascadeType.MERGE)
-    @JoinTable(name = "user_roles",
-            joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
-            inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id"))
     private Set<Role> roles;
 
     public User(String username, String email, String password, Set<Role> roles) {
