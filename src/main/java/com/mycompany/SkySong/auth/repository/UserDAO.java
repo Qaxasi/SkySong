bc76b7c00b4e13fd7c4810bcea284ebdbe672c32
Champel Mariusz <champelmariusz@gmail.com>
2024-02-20 17:12:18 +0100

    refactor: Refactor UserDAO to use direct SQL queries and JDBI mappings

--- src/main/java/com/mycompany/SkySong/auth/repository/UserDAO.java
+++ src/main/java/com/mycompany/SkySong/auth/repository/UserDAO.java
@@ -1,14 +1,27 @@
 package com.mycompany.SkySong.auth.repository;
 
 import com.mycompany.SkySong.auth.model.entity.User;
+import org.jdbi.v3.sqlobject.config.RegisterBeanMapper;
+import org.jdbi.v3.sqlobject.customizer.Bind;
+import org.jdbi.v3.sqlobject.statement.SqlQuery;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
 public interface UserDAO {
-    Optional<User> findByEmail(String email);
-    Optional<User> findByUsername(String username);
-    Boolean existsByUsername(String username);
-    Boolean existsByEmail(String email);
+
+    @SqlQuery("SELECT * FROM users WHERE email = :email")
+    @RegisterBeanMapper(User.class)
+    Optional<User> findByEmail(@Bind("email") String email);
+
+    @SqlQuery("SELECT * FROM users WHERE username = :username")
+    @RegisterBeanMapper(User.class)
+    Optional<User> findByUsername(@Bind("username") String username);
+
+    @SqlQuery("SELECT COUNT(*) FROM users WHERE username = :username")
+    int existsByUsername(@Bind("username") String username);
+
+    @SqlQuery("SELECT COUNT(*) FROM users WHERE email = :email")
+    int existsByEmail(@Bind("email") String email);
 }
