d8bc390c5ee416f5a861bc4b07448f2c1df3942b
Champel Mariusz <champelmariusz@gmail.com>
2024-02-20 14:48:12 +0100

    feat: Add findById method to SessionDAO

--- src/main/java/com/mycompany/SkySong/auth/repository/SessionDAO.java
+++ src/main/java/com/mycompany/SkySong/auth/repository/SessionDAO.java
@@ -1,10 +1,13 @@
 package com.mycompany.SkySong.auth.repository;
 
+import com.mycompany.SkySong.auth.model.entity.Session;
+import org.jdbi.v3.sqlobject.config.RegisterBeanMapper;
 import org.jdbi.v3.sqlobject.customizer.Bind;
 import org.jdbi.v3.sqlobject.statement.SqlUpdate;
 import org.springframework.stereotype.Repository;
 
 import java.sql.Timestamp;
+import java.util.Optional;
 
 @Repository
 public interface SessionDAO {
@@ -13,5 +16,7 @@ public interface SessionDAO {
     void insert(@Bind("sessionId") String sessionId, @Bind("userId") int userId,
                 @Bind("createAt") Timestamp createAt, @Bind("expiresAt") Timestamp expiresAt);
 
-
+    @SqlUpdate("SELECT * FROM sessions WHERE session_id = :sessionId")
+    @RegisterBeanMapper(Session.class)
+    Optional<Session> findById(@Bind("sessionId") String sessionId);
 }
