d2a4d4cbe6104081ee8978c0b82ce08a5bd21fd1
Champel Mariusz <champelmariusz@gmail.com>
2024-02-20 22:01:58 +0100

    style: Improve readability

--- src/main/java/com/mycompany/SkySong/auth/service/CredentialExistenceCheckerImpl.java
+++ src/main/java/com/mycompany/SkySong/auth/service/CredentialExistenceCheckerImpl.java
@@ -7,22 +7,27 @@ import org.springframework.stereotype.Service;
 
 @Service
 class CredentialExistenceCheckerImpl implements CredentialExistenceChecker {
+
     private final UserDAO userDAO;
     private final ApplicationMessageService messageService;
+
     public CredentialExistenceCheckerImpl(UserDAO userDAO, ApplicationMessageService messageService) {
         this.userDAO = userDAO;
         this.messageService = messageService;
     }
+
     @Override
     public void checkForExistingCredentials(RegisterRequest registerRequest) {
         checkForExistingUsername(registerRequest);
         checkForExistingEmail(registerRequest);
     }
+
     private void checkForExistingUsername(RegisterRequest registerRequest) {
         if (userDAO.existsByUsername(registerRequest.username()) > 0) {
             throw new CredentialValidationException(messageService.getMessage("username.exist"));
         }
     }
+    
     private void checkForExistingEmail(RegisterRequest registerRequest) {
         if (userDAO.existsByEmail(registerRequest.email()) > 0) {
             throw new CredentialValidationException(messageService.getMessage("email.exist"));
