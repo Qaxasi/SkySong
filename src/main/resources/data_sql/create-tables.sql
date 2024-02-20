5c498f26310ee6b26235f5342daf35bb61102d10
Champel Mariusz <champelmariusz@gmail.com>
2024-02-20 20:40:58 +0100

    style: Remove comma

--- src/main/resources/data_sql/create-tables.sql
+++ src/main/resources/data_sql/create-tables.sql
@@ -2,7 +2,7 @@
      id INT AUTO_INCREMENT PRIMARY KEY,
      username VARCHAR(50) NOT NULL UNIQUE,
      email VARCHAR(100) NOT NULL UNIQUE,
-     password VARCHAR(255) NOT NULL,
+     password VARCHAR(255) NOT NULL
  );
 
  CREATE TABLE roles (
