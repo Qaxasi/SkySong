06374584bcffc452b77ff82eb4d04b9c54beb1b4
Champel Mariusz <champelmariusz@gmail.com>
2024-02-20 16:57:12 +0100

    refactor: Update SQL script

--- src/main/resources/data_sql/create-tables.sql
+++ src/main/resources/data_sql/create-tables.sql
@@ -3,8 +3,7 @@
      username VARCHAR(50) NOT NULL UNIQUE,
      email VARCHAR(100) NOT NULL UNIQUE,
      password VARCHAR(255) NOT NULL,
-     enabled BOOLEAN NOT NULL DEFAULT TRUE
-     );
+ );
 
  CREATE TABLE roles (
      id INT AUTO_INCREMENT PRIMARY KEY,
