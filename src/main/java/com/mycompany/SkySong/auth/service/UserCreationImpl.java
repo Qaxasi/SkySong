7a42555000dff422539e2b135c0a80b502eab7b4
Champel Mariusz <champelmariusz@gmail.com>
2024-02-22 00:35:47 +0100

    feat: Create class and implement UserCreation interface

---
+++ src/main/java/com/mycompany/SkySong/auth/service/UserCreationImpl.java
@@ -0,0 +1,10 @@
+package com.mycompany.SkySong.auth.service;
+
+import com.mycompany.SkySong.auth.model.dto.RegisterRequest;
+
+public class UserCreationImpl implements UserCreation {
+    @Override
+    public void createUser(RegisterRequest registerRequest) {
+        
+    }
+}
