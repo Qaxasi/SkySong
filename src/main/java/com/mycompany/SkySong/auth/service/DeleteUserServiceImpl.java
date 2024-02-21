0f7c88f14231fc0bcec1bf1015c2d1bc1a3cdcd1
Champel Mariusz <champelmariusz@gmail.com>
2024-02-21 14:48:58 +0100

    refactor: Update deleteUser method to remove user roles before deletion

--- src/main/java/com/mycompany/SkySong/auth/service/DeleteUserServiceImpl.java
+++ src/main/java/com/mycompany/SkySong/auth/service/DeleteUserServiceImpl.java
@@ -5,7 +5,6 @@ import com.mycompany.SkySong.auth.exception.UserNotFoundException;
 import com.mycompany.SkySong.auth.model.dto.ApiResponse;
 import com.mycompany.SkySong.auth.repository.UserDAO;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -19,20 +18,20 @@ class DeleteUserServiceImpl implements DeleteUserService {
         this.userDAO = userDAO;
         this.messageService = messageService;
     }
+
     @Transactional
     @Override
     public ApiResponse deleteUser(int userId) throws DatabaseException {
-        try {
-            return userDAO.findById(userId).map(user -> {
-                userDAO.delete(user);
-                log.info("User with ID {} deleted successfully", userId);
-                return new ApiResponse(messageService.getMessage("user.delete.success", userId));
-            }).orElseThrow(() -> {
-                log.info("User with ID {} does not exist", userId);
-                return new UserNotFoundException(messageService.getMessage("user.delete.not-found", userId));
-            });
-        } catch (DataAccessException e) {
-                throw new DatabaseException(messageService.getMessage("user.delete.error"));
-        }
+        return userDAO.findById(userId)
+                .map(user -> {
+                    userDAO.deleteUserRoles(userId);
+                    userDAO.delete(user);
+                    log.info("User with ID {} deleted successfully", userId);
+                    return new ApiResponse(messageService.getMessage("user.delete.success", userId));
+                }).orElseThrow(() -> {
+                    log.info("User with ID {} does not exist", userId);
+                    return new UserNotFoundException(messageService.getMessage("user.delete.not-found", userId));
+                });
     }
 }
+
