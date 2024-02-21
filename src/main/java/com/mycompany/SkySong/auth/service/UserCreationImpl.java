71f908c680efa230d07e3349c425b0d7451341e5
Champel Mariusz <champelmariusz@gmail.com>
2024-02-22 00:36:49 +0100

    feat: Add dependencies

--- src/main/java/com/mycompany/SkySong/auth/service/UserCreationImpl.java
+++ src/main/java/com/mycompany/SkySong/auth/service/UserCreationImpl.java
@@ -1,10 +1,22 @@
 package com.mycompany.SkySong.auth.service;
 
 import com.mycompany.SkySong.auth.model.dto.RegisterRequest;
+import com.mycompany.SkySong.auth.repository.UserDAO;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserCreationImpl implements UserCreation {
+
+    private final UserDAO userDAO;
+    private final RoleManager roleManager;
+    private final UserFactory userFactory;
+
+    public UserCreationImpl(UserDAO userDAO, RoleManager roleManager, UserFactory userFactory) {
+        this.userDAO = userDAO;
+        this.roleManager = roleManager;
+        this.userFactory = userFactory;
+    }
+
     @Override
     public void createUser(RegisterRequest registerRequest) {
 
