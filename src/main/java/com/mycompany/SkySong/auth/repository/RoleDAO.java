df9bfeab50e4ee48c16d6d11f52216511bb977f9
Champel Mariusz <champelmariusz@gmail.com>
2024-02-21 12:04:28 +0100

    feat: Add findRolesByUserId method to RoleDAO

--- src/main/java/com/mycompany/SkySong/auth/repository/RoleDAO.java
+++ src/main/java/com/mycompany/SkySong/auth/repository/RoleDAO.java
@@ -7,10 +7,15 @@ import org.jdbi.v3.sqlobject.customizer.Bind;
 import org.jdbi.v3.sqlobject.statement.SqlQuery;
 
 import java.util.Optional;
+import java.util.Set;
 
 public interface RoleDAO {
 
     @SqlQuery("SELECT * FROM roles WHERE name = :name")
     @RegisterBeanMapper(Role.class)
     Optional<Role> findByName(@Bind("name") UserRole roleName);
+
+    @SqlQuery("SELECT r.name FROM roles r JOIN user_roles ur ON r.id = ur.role_id WHERE ur.user_id = :userId")
+    @RegisterBeanMapper(Role.class)
+    Set<Role> findRolesByUserId(@Bind("userId") Integer userId);
 }
