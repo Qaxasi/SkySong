f97990a793ecb78872c394785c1d51230cd70324
Champel Mariusz <champelmariusz@gmail.com>
2024-02-22 00:37:28 +0100

    feat: Implement createUser method

--- src/main/java/com/mycompany/SkySong/auth/service/UserCreationImpl.java
+++ src/main/java/com/mycompany/SkySong/auth/service/UserCreationImpl.java
@@ -1,8 +1,12 @@
 package com.mycompany.SkySong.auth.service;
 
 import com.mycompany.SkySong.auth.model.dto.RegisterRequest;
+import com.mycompany.SkySong.auth.model.entity.Role;
+import com.mycompany.SkySong.auth.model.entity.User;
+import com.mycompany.SkySong.auth.model.entity.UserRole;
 import com.mycompany.SkySong.auth.repository.UserDAO;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class UserCreationImpl implements UserCreation {
@@ -17,8 +21,13 @@ public class UserCreationImpl implements UserCreation {
         this.userFactory = userFactory;
     }
 
+    @Transactional
     @Override
     public void createUser(RegisterRequest registerRequest) {
+        Role role = roleManager.getRoleByName(UserRole.ROLE_USER);
+        User user = userFactory.createUser(registerRequest, role);
 
+        int userId = userDAO.save(user);
+        userDAO.assignRoleToUser(userId, role.getId());
     }
 }
