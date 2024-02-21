bb31e535b41c6a91295811c0e0f8474298a3d82e
Champel Mariusz <champelmariusz@gmail.com>
2024-02-22 00:36:06 +0100

    feat: Annotate class as a spring service

--- src/main/java/com/mycompany/SkySong/auth/service/UserCreationImpl.java
+++ src/main/java/com/mycompany/SkySong/auth/service/UserCreationImpl.java
@@ -1,10 +1,12 @@
 package com.mycompany.SkySong.auth.service;
 
 import com.mycompany.SkySong.auth.model.dto.RegisterRequest;
+import org.springframework.stereotype.Service;
 
+@Service
 public class UserCreationImpl implements UserCreation {
     @Override
     public void createUser(RegisterRequest registerRequest) {
-        
+
     }
 }
