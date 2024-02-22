ed78aeebd6c32388002e80da102f7f6aff54bcd2
Champel Mariusz <champelmariusz@gmail.com>
2024-02-22 15:46:23 +0100

    test: Add test to verify exception when email exist

--- src/test/java/com/mycompany/SkySong/auth/service/EmailExistenceValidationStrategyTest.java
+++ src/test/java/com/mycompany/SkySong/auth/service/EmailExistenceValidationStrategyTest.java
@@ -1,11 +1,16 @@
 package com.mycompany.SkySong.auth.service;
 
+import com.mycompany.SkySong.auth.exception.CredentialValidationException;
+import com.mycompany.SkySong.testsupport.auth.common.RegistrationRequests;
 import com.mycompany.SkySong.testsupport.common.BaseIT;
 import com.mycompany.SkySong.testsupport.common.SqlDatabaseCleaner;
 import com.mycompany.SkySong.testsupport.common.SqlDatabaseInitializer;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class EmailExistenceValidationStrategyTest extends BaseIT {
 
@@ -25,5 +30,11 @@ public class EmailExistenceValidationStrategyTest extends BaseIT {
     @AfterEach
     void cleanUp() {
         cleaner.clean();
+    }
+
+    @Test
+    void whenEmailExist_ThrowException() {
+        assertThrows(CredentialValidationException.class,
+                () -> strategy.validate(RegistrationRequests.EXIST_EMAIL));
     }
 }
