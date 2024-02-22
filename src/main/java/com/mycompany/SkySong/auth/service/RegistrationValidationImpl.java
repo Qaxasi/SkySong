be98fbb4c697cc1b37e6f79e30ff19853d14ce3c
Champel Mariusz <champelmariusz@gmail.com>
2024-02-22 11:52:03 +0100

    feat: Create class and implement RegistrationValidation interface

---
+++ src/main/java/com/mycompany/SkySong/auth/service/RegistrationValidationImpl.java
@@ -0,0 +1,13 @@
+package com.mycompany.SkySong.auth.service;
+
+import com.mycompany.SkySong.auth.model.dto.RegisterRequest;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RegistrationValidationImpl implements RegistrationValidation {
+
+    @Override
+    public void validateRequest(RegisterRequest registerRequest) {
+
+    }
+}
