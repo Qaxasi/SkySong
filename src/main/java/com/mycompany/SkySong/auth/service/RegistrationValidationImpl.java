0225ce90c1765b3146374ff11fd04df06e22b20b
Champel Mariusz <champelmariusz@gmail.com>
2024-02-22 15:04:44 +0100

    refactor: Rename class

--- src/main/java/com/mycompany/SkySong/auth/service/RegistrationValidationImpl.java
+++ src/main/java/com/mycompany/SkySong/auth/service/RegistrationValidationImpl.java
@@ -6,16 +6,16 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
-class CredentialsValidationServiceImpl implements CredentialsValidationService {
+class RegistrationValidationImpl implements RegistrationValidation {
 
     private final List<RegistrationValidationStrategy> strategies;
 
-    public CredentialsValidationServiceImpl(List<RegistrationValidationStrategy> strategies) {
+    public RegistrationValidationImpl(List<RegistrationValidationStrategy> strategies) {
         this.strategies = strategies;
     }
 
     @Override
-    public void validateCredentials(RegisterRequest registerRequest) {
+    public void validateRequest(RegisterRequest registerRequest) {
         for (RegistrationValidationStrategy strategy : strategies) {
             strategy.validate(registerRequest);
         }
