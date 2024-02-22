d281cd4e3f23cac1a618527394d6b02c7c7ea79c
Champel Mariusz <champelmariusz@gmail.com>
2024-02-22 15:38:10 +0100

    test: Inject dependencies

--- src/test/java/com/mycompany/SkySong/auth/service/UsernameExistenceValidationStrategyTest.java
+++ src/test/java/com/mycompany/SkySong/auth/service/UsernameExistenceValidationStrategyTest.java
@@ -1,6 +1,24 @@
 package com.mycompany.SkySong.auth.service;
 
+import com.mycompany.SkySong.auth.exception.CredentialValidationException;
+import com.mycompany.SkySong.testsupport.auth.common.RegistrationRequests;
 import com.mycompany.SkySong.testsupport.common.BaseIT;
+import com.mycompany.SkySong.testsupport.common.SqlDatabaseCleaner;
+import com.mycompany.SkySong.testsupport.common.SqlDatabaseInitializer;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class UsernameExistenceValidationStrategyTest extends BaseIT {
+    
+    @Autowired
+    private UsernameExistenceValidationStrategy strategy;
+
+    @Autowired
+    private SqlDatabaseInitializer initializer;
+    @Autowired
+    private SqlDatabaseCleaner cleaner;
 }
