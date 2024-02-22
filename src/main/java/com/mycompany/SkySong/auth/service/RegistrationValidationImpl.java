866e5f7662fe591e7011aae1cd2e6e261fe43f3d
Champel Mariusz <champelmariusz@gmail.com>
2024-02-22 11:52:21 +0100

    feat: Add dependencies

--- src/main/java/com/mycompany/SkySong/auth/service/RegistrationValidationImpl.java
+++ src/main/java/com/mycompany/SkySong/auth/service/RegistrationValidationImpl.java
@@ -6,6 +6,15 @@ import org.springframework.stereotype.Service;
 @Service
 public class RegistrationValidationImpl implements RegistrationValidation {
 
+    private final CredentialsValidationService validation;
+    private final CredentialsExistenceChecker checker;
+
+    public RegistrationValidationImpl(CredentialsValidationService validation,
+                                      CredentialsExistenceChecker checker) {
+        this.validation = validation;
+        this.checker = checker;
+    }
+
     @Override
     public void validateRequest(RegisterRequest registerRequest) {
 
