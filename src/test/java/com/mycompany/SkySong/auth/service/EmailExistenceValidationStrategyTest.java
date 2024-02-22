1d3b42e9994a736dc01a8fa1fbfb5d2b48795d51
Champel Mariusz <champelmariusz@gmail.com>
2024-02-22 15:45:04 +0100

    test: Inject dependencies

--- src/test/java/com/mycompany/SkySong/auth/service/EmailExistenceValidationStrategyTest.java
+++ src/test/java/com/mycompany/SkySong/auth/service/EmailExistenceValidationStrategyTest.java
@@ -1,6 +1,17 @@
 package com.mycompany.SkySong.auth.service;
 
 import com.mycompany.SkySong.testsupport.common.BaseIT;
+import com.mycompany.SkySong.testsupport.common.SqlDatabaseCleaner;
+import com.mycompany.SkySong.testsupport.common.SqlDatabaseInitializer;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class EmailExistenceValidationStrategyTest extends BaseIT {
+
+    @Autowired
+    private EmailExistenceValidationStrategy strategy;
+
+    @Autowired
+    private SqlDatabaseInitializer initializer;
+    @Autowired
+    private SqlDatabaseCleaner cleaner;
 }
