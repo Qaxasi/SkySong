39f044ee8d7db00224bc02157489ed5b6f2b576f
Champel Mariusz <champelmariusz@gmail.com>
2024-02-22 14:51:26 +0100

    feat: Add dependencies

--- src/main/java/com/mycompany/SkySong/auth/service/UsernameExistenceValidationStrategy.java
+++ src/main/java/com/mycompany/SkySong/auth/service/UsernameExistenceValidationStrategy.java
@@ -2,11 +2,20 @@ package com.mycompany.SkySong.auth.service;
 
 import com.mycompany.SkySong.auth.exception.CredentialValidationException;
 import com.mycompany.SkySong.auth.model.dto.RegisterRequest;
+import com.mycompany.SkySong.auth.repository.UserDAO;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UsernameExistenceValidationStrategy implements RegistrationValidationStrategy {
-    
+
+    private final ApplicationMessageService message;
+    private final UserDAO userDAO;
+
+    public UsernameExistenceValidationStrategy(ApplicationMessageService message, UserDAO userDAO) {
+        this.message = message;
+        this.userDAO = userDAO;
+    }
+
     @Override
     public void validate(RegisterRequest registerRequest) throws CredentialValidationException {
 
