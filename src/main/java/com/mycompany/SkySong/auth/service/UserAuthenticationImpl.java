b58500b6ca3bf8be1ee6fb77d6a4ac6de6d6b2b5
Champel Mariusz <champelmariusz@gmail.com>
2024-02-23 16:57:20 +0100

    feat: Create class and implement UserAuthentication interface

---
+++ src/main/java/com/mycompany/SkySong/auth/service/UserAuthenticationImpl.java
@@ -0,0 +1,14 @@
+package com.mycompany.SkySong.auth.service;
+
+import com.mycompany.SkySong.auth.model.dto.LoginRequest;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+public class UserAuthenticationImpl implements UserAuthentication {
+    @Override
+    public Authentication authenticateUser(LoginRequest loginRequest) {
+        return null;
+    }
+}
