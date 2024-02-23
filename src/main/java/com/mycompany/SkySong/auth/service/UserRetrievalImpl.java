4a51fb61c0371ef1063271c78ef8e28bc92144f8
Champel Mariusz <champelmariusz@gmail.com>
2024-02-23 19:07:23 +0100

    feat: Implement findUserByAuthentication method

--- src/main/java/com/mycompany/SkySong/auth/service/UserRetrievalImpl.java
+++ src/main/java/com/mycompany/SkySong/auth/service/UserRetrievalImpl.java
@@ -3,6 +3,7 @@ package com.mycompany.SkySong.auth.service;
 import com.mycompany.SkySong.auth.model.entity.User;
 import com.mycompany.SkySong.auth.repository.UserDAO;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -16,6 +17,8 @@ public class UserRetrievalImpl implements UserRetrieval {
 
     @Override
     public User findUserByAuthentication(Authentication authentication) {
-        return null;
+        return userDAO.findByUsername(authentication.getName())
+                .orElseThrow(() -> new UsernameNotFoundException(
+                        "User not found with username: " + authentication.getName()));
     }
 }
