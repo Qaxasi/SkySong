fe61dc7c76bbd048a6eede5d1b96105c1cb93f46
Champel Mariusz <champelmariusz@gmail.com>
2024-02-23 19:59:27 +0100

    test: Inject dependency

--- src/test/java/com/mycompany/SkySong/auth/service/UserRetrievalTest.java
+++ src/test/java/com/mycompany/SkySong/auth/service/UserRetrievalTest.java
@@ -1,6 +1,10 @@
 package com.mycompany.SkySong.auth.service;
 
 import com.mycompany.SkySong.testsupport.common.BaseIT;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class UserRetrievalTest extends BaseIT {
+
+    @Autowired
+    private UserRetrieval retrieval;
 }
