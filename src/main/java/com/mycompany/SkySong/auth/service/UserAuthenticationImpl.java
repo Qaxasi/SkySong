6043a97f8e5bf6e1599b39e7b8180d1b8f76c1d5
Champel Mariusz <champelmariusz@gmail.com>
2024-02-23 16:57:42 +0100

    feat: Annotate class as a spring service

--- src/main/java/com/mycompany/SkySong/auth/service/UserAuthenticationImpl.java
+++ src/main/java/com/mycompany/SkySong/auth/service/UserAuthenticationImpl.java
@@ -5,7 +5,9 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
 
+@Service
 public class UserAuthenticationImpl implements UserAuthentication {
     @Override
     public Authentication authenticateUser(LoginRequest loginRequest) {
