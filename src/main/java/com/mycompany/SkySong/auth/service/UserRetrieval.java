296489ef163fdcc5c9daff6225f02ee42514bf7d
Champel Mariusz <champelmariusz@gmail.com>
2024-02-23 20:23:50 +0100

    refactor: Change method parameter

--- src/main/java/com/mycompany/SkySong/auth/service/UserRetrieval.java
+++ src/main/java/com/mycompany/SkySong/auth/service/UserRetrieval.java
@@ -1,8 +1,7 @@
 package com.mycompany.SkySong.auth.service;
 
 import com.mycompany.SkySong.auth.model.entity.User;
-import org.springframework.security.core.Authentication;
 
 public interface UserRetrieval {
-    User findUserByAuthentication(Authentication authentication);
+    User findUserByAuthentication(String username);
 }
