61f9d04d3165ffb489ee9c0b392e28b04598e175
Champel Mariusz <champelmariusz@gmail.com>
2024-02-23 20:10:42 +0100

    test: Add database setup and cleanup in test

--- src/test/java/com/mycompany/SkySong/auth/service/UserRetrievalTest.java
+++ src/test/java/com/mycompany/SkySong/auth/service/UserRetrievalTest.java
@@ -3,6 +3,8 @@ package com.mycompany.SkySong.auth.service;
 import com.mycompany.SkySong.testsupport.common.BaseIT;
 import com.mycompany.SkySong.testsupport.common.SqlDatabaseCleaner;
 import com.mycompany.SkySong.testsupport.common.SqlDatabaseInitializer;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 
 public class UserRetrievalTest extends BaseIT {
@@ -16,4 +18,13 @@ public class UserRetrievalTest extends BaseIT {
     @Autowired
     private UserRetrieval retrieval;
 
+    @BeforeEach
+    void setUp() throws Exception {
+        initializer.setup("data_sql/test-setup.sql");
+    }
+
+    @AfterEach
+    void cleanUp() {
+        cleaner.clean();
+    }
 }
