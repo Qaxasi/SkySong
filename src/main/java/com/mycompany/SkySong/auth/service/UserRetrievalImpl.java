fc5bc4b05acf4c30d68edadc129f30f87d68a8f3
Champel Mariusz <champelmariusz@gmail.com>
2024-02-23 19:06:29 +0100

    feat: Add dependency

--- src/main/java/com/mycompany/SkySong/auth/service/UserRetrievalImpl.java
+++ src/main/java/com/mycompany/SkySong/auth/service/UserRetrievalImpl.java
@@ -3,11 +3,17 @@ package com.mycompany.SkySong.auth.service;
 import com.mycompany.SkySong.auth.model.entity.User;
 import com.mycompany.SkySong.auth.repository.UserDAO;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserRetrievalImpl implements UserRetrieval {
+
+    private final UserDAO userDAO;
+
+    public UserRetrievalImpl(UserDAO userDAO) {
+        this.userDAO = userDAO;
+    }
+
     @Override
     public User findUserByAuthentication(Authentication authentication) {
         return null;
