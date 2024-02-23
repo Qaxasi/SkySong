42dac1b00e2b663cc7cd4e34dcc1cee6da858b07
Champel Mariusz <champelmariusz@gmail.com>
2024-02-23 20:09:50 +0100

    test: Inject dependencies

--- src/test/java/com/mycompany/SkySong/auth/service/UserRetrievalTest.java
+++ src/test/java/com/mycompany/SkySong/auth/service/UserRetrievalTest.java
@@ -1,10 +1,19 @@
 package com.mycompany.SkySong.auth.service;
 
 import com.mycompany.SkySong.testsupport.common.BaseIT;
+import com.mycompany.SkySong.testsupport.common.SqlDatabaseCleaner;
+import com.mycompany.SkySong.testsupport.common.SqlDatabaseInitializer;
 import org.springframework.beans.factory.annotation.Autowired;
 
 public class UserRetrievalTest extends BaseIT {
 
     @Autowired
+    private SqlDatabaseInitializer initializer;
+
+    @Autowired
+    private SqlDatabaseCleaner cleaner;
+
+    @Autowired
     private UserRetrieval retrieval;
+
 }
