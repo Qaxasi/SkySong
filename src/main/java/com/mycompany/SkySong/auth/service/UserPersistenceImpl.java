a12f5fd7a40006b3c798acdfa1845b5fb54434ff
Champel Mariusz <champelmariusz@gmail.com>
2024-02-24 20:03:35 +0100

    feat: Implement saveUser method

--- src/main/java/com/mycompany/SkySong/auth/service/UserPersistenceImpl.java
+++ src/main/java/com/mycompany/SkySong/auth/service/UserPersistenceImpl.java
@@ -1,5 +1,6 @@
 package com.mycompany.SkySong.auth.service;
 
+import com.mycompany.SkySong.auth.model.entity.Role;
 import com.mycompany.SkySong.auth.model.entity.User;
 import com.mycompany.SkySong.auth.repository.UserDAO;
 import org.springframework.stereotype.Service;
@@ -15,6 +16,10 @@ public class UserPersistenceImpl implements UserPersistence {
 
     @Override
     public void saveUser(User user) {
+        int userId = userDAO.save(user);
 
+        for (Role role : user.getRoles()) {
+            userDAO.assignRoleToUser(userId, role.getId());
+        }
     }
 }
