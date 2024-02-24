f1ef0ae93f0cf1631e6cacfb2f3d6172d38d8166
Champel Mariusz <champelmariusz@gmail.com>
2024-02-24 19:25:03 +0100

    test: Inject dependencies

--- src/test/java/com/mycompany/SkySong/auth/service/UserWithRoleSetupTest.java
+++ src/test/java/com/mycompany/SkySong/auth/service/UserWithRoleSetupTest.java
@@ -14,4 +14,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class UserWithRoleSetupTest extends BaseIT {
+
+    @Autowired
+    private UserWithRoleSetup userSetup;
+
+    @Autowired
+    private SqlDatabaseInitializer initializer;
+    @Autowired
+    private SqlDatabaseCleaner cleaner;
 }
