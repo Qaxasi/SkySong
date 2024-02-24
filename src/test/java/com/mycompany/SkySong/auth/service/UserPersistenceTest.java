6b061b39acf9d67017c32d18558f0258987eafa5
Champel Mariusz <champelmariusz@gmail.com>
2024-02-24 20:07:12 +0100

    test: Inject dependencies

--- src/test/java/com/mycompany/SkySong/auth/service/UserPersistenceTest.java
+++ src/test/java/com/mycompany/SkySong/auth/service/UserPersistenceTest.java
@@ -1,6 +1,20 @@
 package com.mycompany.SkySong.auth.service;
 
 import com.mycompany.SkySong.testsupport.common.BaseIT;
+import com.mycompany.SkySong.testsupport.common.SqlDatabaseCleaner;
+import com.mycompany.SkySong.testsupport.common.SqlDatabaseInitializer;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class UserPersistenceTest extends BaseIT {
+
+    @Autowired
+    private UserPersistence userPersistence;
+
+    @Autowired
+    private SqlDatabaseInitializer initializer;
+    @Autowired
+    private SqlDatabaseCleaner cleaner;
+
 }
