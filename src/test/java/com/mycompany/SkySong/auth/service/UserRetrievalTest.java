ee17e22b5edd0484e61cf5892e232d6ce10e39c8
Champel Mariusz <champelmariusz@gmail.com>
2024-02-24 13:09:22 +0100

    test: Add test for existing user retrieval

--- src/test/java/com/mycompany/SkySong/auth/service/UserRetrievalTest.java
+++ src/test/java/com/mycompany/SkySong/auth/service/UserRetrievalTest.java
@@ -1,11 +1,15 @@
 package com.mycompany.SkySong.auth.service;
 
+import com.mycompany.SkySong.auth.model.entity.User;
 import com.mycompany.SkySong.testsupport.common.BaseIT;
 import com.mycompany.SkySong.testsupport.common.SqlDatabaseCleaner;
 import com.mycompany.SkySong.testsupport.common.SqlDatabaseInitializer;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class UserRetrievalTest extends BaseIT {
 
@@ -26,5 +30,11 @@ public class UserRetrievalTest extends BaseIT {
     @AfterEach
     void cleanUp() {
         cleaner.clean();
+    }
+
+    @Test
+    void whenUserExist_ReturnUser() {
+        User user = retrieval.findByAuthUsername("Mark");
+        assertThat(user).isNotNull();
     }
 }
