5723965f739589df6a6750dd08cf57277d2ae782
Champel Mariusz <champelmariusz@gmail.com>
2024-02-24 18:31:01 +0100

    feat: Add dependencies

--- src/main/java/com/mycompany/SkySong/auth/service/UserWithRoleSetupImpl.java
+++ src/main/java/com/mycompany/SkySong/auth/service/UserWithRoleSetupImpl.java
@@ -1,8 +1,22 @@
 package com.mycompany.SkySong.auth.service;
 
+import com.mycompany.SkySong.auth.model.dto.RegisterRequest;
+import com.mycompany.SkySong.auth.model.entity.User;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserWithRoleSetupImpl implements UserWithRoleSetup {
 
+    private final RoleManager roleManager;
+    private final UserFactory userFactory;
+
+    public UserWithRoleSetupImpl(RoleManager roleManager, UserFactory userFactory) {
+        this.roleManager = roleManager;
+        this.userFactory = userFactory;
+    }
+
+    @Override
+    public User setupUserWithRole(RegisterRequest request) {
+        return null;
+    }
 }
