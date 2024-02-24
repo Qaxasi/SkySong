5a3972ac45087dfb707ce1e4b1d32da75b67fb1a
Champel Mariusz <champelmariusz@gmail.com>
2024-02-24 19:24:50 +0100

    test: Create test class and extend with BaseIT

--- src/test/java/com/mycompany/SkySong/auth/service/UserWithRoleSetupTest.java
+++ src/test/java/com/mycompany/SkySong/auth/service/UserWithRoleSetupTest.java
@@ -1,4 +1,17 @@
 package com.mycompany.SkySong.auth.service;
 
-public class UserWithRoleSetupTest {
+import com.mycompany.SkySong.auth.model.entity.User;
+import com.mycompany.SkySong.auth.model.entity.UserRole;
+import com.mycompany.SkySong.testsupport.auth.common.RegistrationRequests;
+import com.mycompany.SkySong.testsupport.common.BaseIT;
+import com.mycompany.SkySong.testsupport.common.SqlDatabaseCleaner;
+import com.mycompany.SkySong.testsupport.common.SqlDatabaseInitializer;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class UserWithRoleSetupTest extends BaseIT {
 }
