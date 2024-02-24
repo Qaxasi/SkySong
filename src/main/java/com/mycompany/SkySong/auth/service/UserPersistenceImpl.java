a41547b8a4d8e8aba20b68af8ebc3d2925b058c3
Champel Mariusz <champelmariusz@gmail.com>
2024-02-24 20:00:41 +0100

    feat: Create new class and implement UserPersistence interface

---
+++ src/main/java/com/mycompany/SkySong/auth/service/UserPersistenceImpl.java
@@ -0,0 +1,12 @@
+package com.mycompany.SkySong.auth.service;
+
+import com.mycompany.SkySong.auth.model.entity.User;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserPersistenceImpl implements UserPersistence {
+    @Override
+    public void saveUser(User user) {
+
+    }
+}
