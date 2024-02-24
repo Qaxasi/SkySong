f274e0610881489c8a77de5523fd69d62ecc7997
Champel Mariusz <champelmariusz@gmail.com>
2024-02-24 20:01:10 +0100

    feat: Inject dependencies

--- src/main/java/com/mycompany/SkySong/auth/service/UserPersistenceImpl.java
+++ src/main/java/com/mycompany/SkySong/auth/service/UserPersistenceImpl.java
@@ -1,10 +1,18 @@
 package com.mycompany.SkySong.auth.service;
 
 import com.mycompany.SkySong.auth.model.entity.User;
+import com.mycompany.SkySong.auth.repository.UserDAO;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserPersistenceImpl implements UserPersistence {
+
+    private final UserDAO userDAO;
+
+    public UserPersistenceImpl(UserDAO userDAO) {
+        this.userDAO = userDAO;
+    }
+
     @Override
     public void saveUser(User user) {
 
