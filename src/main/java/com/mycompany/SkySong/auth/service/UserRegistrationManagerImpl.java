5b9876d648a379852b5a1f6374abb0575e347080
Champel Mariusz <champelmariusz@gmail.com>
2024-02-24 20:23:43 +0100

    refactor: Refactor setupNewUser method

--- src/main/java/com/mycompany/SkySong/auth/service/UserRegistrationManagerImpl.java
+++ src/main/java/com/mycompany/SkySong/auth/service/UserRegistrationManagerImpl.java
@@ -1,7 +1,6 @@
 package com.mycompany.SkySong.auth.service;
 
 import com.mycompany.SkySong.auth.model.dto.RegisterRequest;
-import com.mycompany.SkySong.auth.model.entity.Role;
 import com.mycompany.SkySong.auth.model.entity.User;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -19,11 +18,8 @@ public class UserRegistrationManagerImpl implements UserRegistrationManager {
 
     @Transactional
     @Override
-    public void setupNewUser(RegisterRequest registerRequest) {
-        Role role = roleManager.getRoleByName(UserRole.ROLE_USER);
-        User user = userFactory.createUser(registerRequest, role);
-
-        int userId = userDAO.save(user);
-        userDAO.assignRoleToUser(userId, role.getId());
+    public void setupNewUser(RegisterRequest request) {
+        User user = userSetup.setupUserWithRole(request);
+        userPersistence.saveUser(user);
     }
 }
