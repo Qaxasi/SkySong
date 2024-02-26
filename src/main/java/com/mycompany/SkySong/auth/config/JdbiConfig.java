9c271becf6869f88100aa9391f8efa3256540e3b
Champel Mariusz <champelmariusz@gmail.com>
2024-02-26 14:38:11 +0100

    refactor: Remove unused import

--- src/main/java/com/mycompany/SkySong/auth/config/JdbiConfig.java
+++ src/main/java/com/mycompany/SkySong/auth/config/JdbiConfig.java
@@ -7,7 +7,6 @@ import org.jdbi.v3.core.Jdbi;
 import org.jdbi.v3.sqlobject.SqlObjectPlugin;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Role;
 import org.springframework.jdbc.datasource.TransactionAwareDataSourceProxy;
 
 import javax.sql.DataSource;
