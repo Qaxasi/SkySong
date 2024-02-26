5317225b810a468e2f87468c0ba78f2ca62ef85c
Champel Mariusz <champelmariusz@gmail.com>
2024-02-26 12:03:52 +0100

    test: Implement createUser method

--- src/test/java/com/mycompany/SkySong/testsupport/auth/service/UserCreator.java
+++ src/test/java/com/mycompany/SkySong/testsupport/auth/service/UserCreator.java
@@ -1,7 +1,12 @@
 package com.mycompany.SkySong.testsupport.auth.service;
 
+import com.mycompany.SkySong.auth.model.entity.Role;
+import com.mycompany.SkySong.auth.model.entity.User;
+import com.mycompany.SkySong.auth.model.entity.UserRole;
 import com.mycompany.SkySong.auth.repository.RoleDAO;
 import org.springframework.stereotype.Component;
+
+import java.util.Set;
 
 @Component
 public class UserCreator {
@@ -10,5 +15,13 @@ public class UserCreator {
 
     public UserCreator(RoleDAO roleDAO) {
         this.roleDAO = roleDAO;
+    }
+
+    public User createUser(String username) {
+        Role role = roleDAO.findByName(UserRole.ROLE_USER)
+                .orElseThrow(() -> new AssertionError("Role not found"));
+
+        Set<Role> roles = Set.of(role);
+        return new User( username, "new@mail.mail", "Password#3", roles);
     }
 }
