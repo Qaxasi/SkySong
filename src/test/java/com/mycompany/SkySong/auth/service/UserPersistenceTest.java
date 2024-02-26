b30843fec9e71a05bbb4892884b60d5ffa3365a1
Champel Mariusz <champelmariusz@gmail.com>
2024-02-26 11:39:30 +0100

    test: Inject dependencies

--- src/test/java/com/mycompany/SkySong/auth/service/UserPersistenceTest.java
+++ src/test/java/com/mycompany/SkySong/auth/service/UserPersistenceTest.java
@@ -1,12 +1,19 @@
 package com.mycompany.SkySong.auth.service;
 
+import com.mycompany.SkySong.auth.model.entity.User;
+import com.mycompany.SkySong.auth.model.entity.UserRole;
 import com.mycompany.SkySong.testsupport.auth.common.UserExistenceChecker;
+import com.mycompany.SkySong.testsupport.auth.service.UserCreator;
+import com.mycompany.SkySong.testsupport.auth.service.UserRoleChecker;
 import com.mycompany.SkySong.testsupport.common.BaseIT;
 import com.mycompany.SkySong.testsupport.common.SqlDatabaseCleaner;
 import com.mycompany.SkySong.testsupport.common.SqlDatabaseInitializer;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class UserPersistenceTest extends BaseIT {
 
@@ -14,6 +21,10 @@ public class UserPersistenceTest extends BaseIT {
     private UserPersistence userPersistence;
     @Autowired
     private UserExistenceChecker userChecker;
+    @Autowired
+    private UserCreator userCreator;
+    @Autowired
+    private UserRoleChecker roleChecker;
 
     @Autowired
     private SqlDatabaseInitializer initializer;
@@ -29,5 +40,4 @@ public class UserPersistenceTest extends BaseIT {
     void cleanUp() {
         cleaner.clean();
     }
-
 }
