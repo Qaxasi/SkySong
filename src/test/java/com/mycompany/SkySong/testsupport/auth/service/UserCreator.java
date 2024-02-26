d94ab4d8873ca5ce7cc453f1fe7ad8bb77e4e622
Champel Mariusz <champelmariusz@gmail.com>
2024-02-26 11:56:52 +0100

    test: Add dependency

--- src/test/java/com/mycompany/SkySong/testsupport/auth/service/UserCreator.java
+++ src/test/java/com/mycompany/SkySong/testsupport/auth/service/UserCreator.java
@@ -1,7 +1,14 @@
 package com.mycompany.SkySong.testsupport.auth.service;
 
+import com.mycompany.SkySong.auth.repository.RoleDAO;
 import org.springframework.stereotype.Component;
 
 @Component
 public class UserCreator {
+
+    private final RoleDAO roleDAO;
+
+    public UserCreator(RoleDAO roleDAO) {
+        this.roleDAO = roleDAO;
+    }
 }
