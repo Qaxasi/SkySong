29d0b2a8efa16888f4eb99737f82b02c96781720
Champel Mariusz <champelmariusz@gmail.com>
2024-02-27 21:51:50 +0100

    feat: Add dependency

--- src/main/java/com/mycompany/SkySong/auth/service/UserAuthenticationImpl.java
+++ src/main/java/com/mycompany/SkySong/auth/service/UserAuthenticationImpl.java
@@ -1,7 +1,9 @@
 package com.mycompany.SkySong.auth.service;
 
 import com.mycompany.SkySong.auth.model.dto.LoginRequest;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -11,9 +13,11 @@ import org.springframework.stereotype.Service;
 public class UserAuthenticationImpl implements UserAuthentication {
 
     private final AuthenticationManager authenticationManager;
+    private final ApplicationMessageService message;
 
-    public UserAuthenticationImpl(AuthenticationManager authenticationManager) {
+    public UserAuthenticationImpl(AuthenticationManager authenticationManager, ApplicationMessageService message) {
         this.authenticationManager = authenticationManager;
+        this.message = message;
     }
 
     @Override
