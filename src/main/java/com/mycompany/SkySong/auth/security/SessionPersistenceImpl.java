8489ba74935318443de4c2cc6071ed82ca811e53
Champel Mariusz <champelmariusz@gmail.com>
2024-02-27 17:54:06 +0100

    feat: Create class and implement SessionPersistence interface

---
+++ src/main/java/com/mycompany/SkySong/auth/security/SessionPersistenceImpl.java
@@ -0,0 +1,12 @@
+package com.mycompany.SkySong.auth.security;
+
+import com.mycompany.SkySong.auth.model.entity.Session;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SessionPersistenceImpl implements SessionPersistence {
+    @Override
+    public void saveSession(Session session) {
+
+    }
+}
