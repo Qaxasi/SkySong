f40af9ce73bafd152e710cd94255375774d4a5d8
Champel Mariusz <champelmariusz@gmail.com>
2024-02-27 22:09:33 +0100

    feat: Add dependency

--- src/main/java/com/mycompany/SkySong/auth/service/UserAuthProcessorImpl.java
+++ src/main/java/com/mycompany/SkySong/auth/service/UserAuthProcessorImpl.java
@@ -7,6 +7,14 @@ import org.springframework.stereotype.Service;
 @Service
 public class UserAuthProcessorImpl implements UserAuthProcessor {
 
+    private final UserAuthentication userAuth;
+    private final UserRetrieval userRetrieval;
+
+    public UserAuthProcessorImpl(UserAuthentication userAuth, UserRetrieval userRetrieval) {
+        this.userAuth = userAuth;
+        this.userRetrieval = userRetrieval;
+    }
+
     @Override
     public User authenticateAndRetrieveUser(LoginRequest request) {
         return null;
