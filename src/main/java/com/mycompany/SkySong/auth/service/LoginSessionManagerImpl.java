2be357d827441f052b86c2009a9631e81d862cfe
Champel Mariusz <champelmariusz@gmail.com>
2024-02-27 22:38:36 +0100

    feat: Add dependency

--- src/main/java/com/mycompany/SkySong/auth/service/LoginSessionManagerImpl.java
+++ src/main/java/com/mycompany/SkySong/auth/service/LoginSessionManagerImpl.java
@@ -1,10 +1,20 @@
 package com.mycompany.SkySong.auth.service;
 
 import com.mycompany.SkySong.auth.model.dto.LoginRequest;
+import com.mycompany.SkySong.auth.security.SessionPersistence;
 import org.springframework.stereotype.Service;
 
 @Service
 public class LoginSessionManagerImpl implements LoginSessionManager {
+    
+    private final SessionPersistence sessionPersistence;
+    private final UserSessionInitializer sessionInit;
+
+    public LoginSessionManagerImpl(SessionPersistence sessionPersistence, UserSessionInitializer sessionInit) {
+        this.sessionPersistence = sessionPersistence;
+        this.sessionInit = sessionInit;
+    }
+
     @Override
     public String initializeAndSaveSession(LoginRequest request) {
         return null;
