4d284c41825a9aad428dfc8ced03fb5b888f55c4
Champel Mariusz <champelmariusz@gmail.com>
2024-02-27 14:30:06 +0100

    test: Add dependency

--- src/test/java/com/mycompany/SkySong/testsupport/auth/service/UserLoginTestHelper.java
+++ src/test/java/com/mycompany/SkySong/testsupport/auth/service/UserLoginTestHelper.java
@@ -1,8 +1,14 @@
 package com.mycompany.SkySong.testsupport.auth.service;
 
+import com.mycompany.SkySong.auth.service.LoginService;
 import org.springframework.stereotype.Component;
 
 @Component
 public class UserLoginTestHelper {
 
+    private final LoginService login;
+
+    public UserLoginTestHelper(LoginService login) {
+        this.login = login;
+    }
 }
