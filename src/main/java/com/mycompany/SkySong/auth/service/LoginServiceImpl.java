c5a8eeba95ab876e51d7f2fde79b33b20aab41ea
Champel Mariusz <champelmariusz@gmail.com>
2024-02-27 21:42:48 +0100

    refactor: Simplify login method

--- src/main/java/com/mycompany/SkySong/auth/service/LoginServiceImpl.java
+++ src/main/java/com/mycompany/SkySong/auth/service/LoginServiceImpl.java
@@ -2,33 +2,25 @@ package com.mycompany.SkySong.auth.service;
 
 import com.mycompany.SkySong.auth.model.dto.LoginRequest;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
 class LoginServiceImpl implements LoginService {
 
-    private final LoginSessionManager loginSessionManager;
+    private final LoginSessionManager sessionManager;
     private final ApplicationMessageService messageService;
 
 
     public LoginServiceImpl(ApplicationMessageService messageService,
-                            LoginSessionManager loginSessionManager) {
-        this.loginSessionManager = loginSessionManager;
+                            LoginSessionManager sessionManager) {
+        this.sessionManager = sessionManager;
         this.messageService = messageService;
 
     }
 
     @Override
     public String login(LoginRequest request) {
-        try {
-            Authentication auth = userAuth.authenticateUser(request);
-            User user = userRetrieval.findByAuthUsername(auth.getName());
-            return sessionCreation.createSession(user.getId());
-        } catch (BadCredentialsException e) {
-            log.error("Error during login for user: {}", request.usernameOrEmail(), e);
-            throw new BadCredentialsException(messageService.getMessage("login.failure"));
-        }
+        return sessionManager.sessionManager(request);
     }
 }
