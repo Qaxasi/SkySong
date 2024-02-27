ffb807734d642a7b4bdf5e07b52785ca5a33c59b
Champel Mariusz <champelmariusz@gmail.com>
2024-02-27 18:53:15 +0100

    feat: Add dependency

--- src/main/java/com/mycompany/SkySong/auth/security/SessionPersistenceImpl.java
+++ src/main/java/com/mycompany/SkySong/auth/security/SessionPersistenceImpl.java
@@ -1,10 +1,18 @@
 package com.mycompany.SkySong.auth.security;
 
 import com.mycompany.SkySong.auth.model.entity.Session;
+import com.mycompany.SkySong.auth.repository.SessionDAO;
 import org.springframework.stereotype.Service;
 
 @Service
 public class SessionPersistenceImpl implements SessionPersistence {
+
+    private final SessionDAO sessionDAO;
+
+    public SessionPersistenceImpl(SessionDAO sessionDAO) {
+        this.sessionDAO = sessionDAO;
+    }
+
     @Override
     public void saveSession(Session session) {
 
