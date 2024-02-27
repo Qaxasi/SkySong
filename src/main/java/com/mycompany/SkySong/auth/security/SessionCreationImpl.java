dbaae844e5a177b7ccbce4406ab604eb9fbe9125
Champel Mariusz <champelmariusz@gmail.com>
2024-02-27 20:25:51 +0100

    refactor: Refactor createSession method to return Session

--- src/main/java/com/mycompany/SkySong/auth/security/SessionCreationImpl.java
+++ src/main/java/com/mycompany/SkySong/auth/security/SessionCreationImpl.java
@@ -1,28 +1,23 @@
 package com.mycompany.SkySong.auth.security;
 
 import com.mycompany.SkySong.auth.model.entity.Session;
-import com.mycompany.SkySong.auth.repository.SessionDAO;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
 
 @Service
 public class SessionCreationImpl implements SessionCreation {
-
-    private final SessionDAO sessionDAO;
-
     private final TokenGenerator tokenGenerator;
 
     private final TokenHasher tokenHasher;
 
-    public SessionCreationImpl(SessionDAO sessionDAO, TokenGenerator tokenGenerator, TokenHasher tokenHasher) {
-        this.sessionDAO = sessionDAO;
+    public SessionCreationImpl(TokenGenerator tokenGenerator, TokenHasher tokenHasher) {
         this.tokenGenerator = tokenGenerator;
         this.tokenHasher = tokenHasher;
     }
 
     @Override
-    public String createSession(Integer userID) {
+    public Session createSession(Integer userID) {
         String token = tokenGenerator.generateToken();
         String hashedToken = tokenHasher.generateHashedToken(token);
 
@@ -32,7 +27,6 @@ public class SessionCreationImpl implements SessionCreation {
         session.setCreateAt(new Date());
         session.setExpiresAt(new Date(System.currentTimeMillis() + (24 * 60 * 60 * 1000)));
 
-        sessionDAO.save(session);
-        return token;
+        return session;
     }
 }
