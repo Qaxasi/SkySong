468e0823723dcaf3fcb09dccab420ec103761734
Champel Mariusz <champelmariusz@gmail.com>
2024-02-28 15:28:02 +0100

    test: Refactor test to use Session object directly

--- src/test/java/com/mycompany/SkySong/auth/security/SessionCreationTest.java
+++ src/test/java/com/mycompany/SkySong/auth/security/SessionCreationTest.java
@@ -1,17 +1,8 @@
 package com.mycompany.SkySong.auth.security;
 
-import com.mycompany.SkySong.testsupport.auth.security.SessionExistenceChecker;
-import com.mycompany.SkySong.testsupport.auth.security.SessionFetcher;
-import com.mycompany.SkySong.testsupport.common.SqlDatabaseCleaner;
-import com.mycompany.SkySong.testsupport.common.SqlDatabaseInitializer;
 import com.mycompany.SkySong.auth.model.entity.Session;
-import com.mycompany.SkySong.testsupport.common.BaseIT;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
 import java.time.Duration;
 import java.time.Instant;
@@ -36,15 +27,11 @@ public class SessionCreationTest {
         Instant now = Instant.now();
         int userId = 10;
 
-        String token = sessionCreation.createSession(userId);
-        String hashedToken = tokenHasher.generateHashedToken(token);
+        Session session = sessionCreation.createSession(userId);
 
-        Session createdSession = sessionFetcher.getSession(hashedToken).orElseThrow(
-                () -> new AssertionError("Expected session not found in database."));
-        Instant expirationTime = createdSession.getExpiresAt().toInstant();
+        Instant expirationTime = session.getExpiresAt().toInstant();
 
         long hoursUntilExpiration = Duration.between(now, expirationTime).toHours();
         assertThat(hoursUntilExpiration).isBetween(23L, 25L);
     }
-
 }
