8d35cf682232eec4af8a774b4087c3d30b7aab61
Champel Mariusz <champelmariusz@gmail.com>
2024-02-28 16:53:16 +0100

    test: Add database setup and cleanup methods in test class

--- src/test/java/com/mycompany/SkySong/auth/service/UserAuthProcessorTest.java
+++ src/test/java/com/mycompany/SkySong/auth/service/UserAuthProcessorTest.java
@@ -3,6 +3,8 @@ package com.mycompany.SkySong.auth.service;
 import com.mycompany.SkySong.testsupport.common.BaseIT;
 import com.mycompany.SkySong.testsupport.common.SqlDatabaseCleaner;
 import com.mycompany.SkySong.testsupport.common.SqlDatabaseInitializer;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 
 public class UserAuthProcessorTest extends BaseIT {
@@ -14,4 +16,14 @@ public class UserAuthProcessorTest extends BaseIT {
     private SqlDatabaseInitializer initializer;
     @Autowired
     private SqlDatabaseCleaner cleaner;
+
+    @BeforeEach
+    void setUp() throws Exception {
+        initializer.setup("data_sql/test-setup.sql");
+    }
+
+    @AfterEach
+    void cleanUp() {
+        cleaner.clean();
+    }
 }
