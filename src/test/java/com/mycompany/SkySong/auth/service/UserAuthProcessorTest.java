787a6c7ef8e6426b0c86013b666d6ed63333aa29
Champel Mariusz <champelmariusz@gmail.com>
2024-02-28 17:19:40 +0100

    test: Add test for existing user fetch in authProcessor

--- src/test/java/com/mycompany/SkySong/auth/service/UserAuthProcessorTest.java
+++ src/test/java/com/mycompany/SkySong/auth/service/UserAuthProcessorTest.java
@@ -1,11 +1,16 @@
 package com.mycompany.SkySong.auth.service;
 
+import com.mycompany.SkySong.auth.model.entity.User;
+import com.mycompany.SkySong.testsupport.auth.common.LoginRequests;
 import com.mycompany.SkySong.testsupport.common.BaseIT;
 import com.mycompany.SkySong.testsupport.common.SqlDatabaseCleaner;
 import com.mycompany.SkySong.testsupport.common.SqlDatabaseInitializer;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class UserAuthProcessorTest extends BaseIT {
 
@@ -25,5 +30,11 @@ public class UserAuthProcessorTest extends BaseIT {
     @AfterEach
     void cleanUp() {
         cleaner.clean();
+    }
+
+    @Test
+    void whenFetchUser_UserExist() {
+        User user = authProcessor.fetchUserByAuthentication(LoginRequests.LOGIN("User"));
+        assertThat(user).isNotNull();
     }
 }
