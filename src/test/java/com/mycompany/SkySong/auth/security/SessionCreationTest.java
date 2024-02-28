2d956fe565459bbdf2d60b7a6e3f735ea79aa23e
Champel Mariusz <champelmariusz@gmail.com>
2024-02-28 15:22:53 +0100

    test: Remove dependency

--- src/test/java/com/mycompany/SkySong/auth/security/SessionCreationTest.java
+++ src/test/java/com/mycompany/SkySong/auth/security/SessionCreationTest.java
@@ -10,6 +10,8 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.time.Duration;
 import java.time.Instant;
@@ -17,9 +19,6 @@ import java.time.Instant;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class SessionCreationTest {
-
-    @Autowired
-    private SessionCreation sessionCreation;
 
     @Test
     void whenCreatingSession_SetsCorrectExpirationTime() {
