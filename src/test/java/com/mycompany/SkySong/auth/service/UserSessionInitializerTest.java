9f0b9b68dff9f2e786cccff7ed85e778bf1a56d4
Champel Mariusz <champelmariusz@gmail.com>
2024-02-28 17:40:57 +0100

    test: Inject dependencies

--- src/test/java/com/mycompany/SkySong/auth/service/UserSessionInitializerTest.java
+++ src/test/java/com/mycompany/SkySong/auth/service/UserSessionInitializerTest.java
@@ -1,6 +1,17 @@
 package com.mycompany.SkySong.auth.service;
 
 import com.mycompany.SkySong.testsupport.common.BaseIT;
+import com.mycompany.SkySong.testsupport.common.SqlDatabaseCleaner;
+import com.mycompany.SkySong.testsupport.common.SqlDatabaseInitializer;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class UserSessionInitializerTest extends BaseIT {
+
+    @Autowired
+    private UserSessionInitializer sessionInitializer;
+
+    @Autowired
+    private SqlDatabaseInitializer initializer;
+    @Autowired
+    private SqlDatabaseCleaner cleaner;
 }
