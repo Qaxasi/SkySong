2eb54d7549265d58ffe409f85ea678d821656397
Champel Mariusz <champelmariusz@gmail.com>
2024-02-29 22:34:19 +0100

    test: Add test class and extend with BaseIT

---
+++ src/test/java/com/mycompany/SkySong/auth/service/SessionPersistenceTest.java
@@ -0,0 +1,11 @@
+package com.mycompany.SkySong.auth.service;
+
+import com.mycompany.SkySong.auth.security.SessionPersistence;
+import com.mycompany.SkySong.testsupport.auth.security.SessionExistenceChecker;
+import com.mycompany.SkySong.testsupport.common.BaseIT;
+import com.mycompany.SkySong.testsupport.common.SqlDatabaseCleaner;
+import com.mycompany.SkySong.testsupport.common.SqlDatabaseInitializer;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class SessionPersistenceTest extends BaseIT {
+}
