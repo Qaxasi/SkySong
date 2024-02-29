faaac6a1888ea7a8bf17585f7f52c96a2856f149
Champel Mariusz <champelmariusz@gmail.com>
2024-02-29 23:34:57 +0100

    test: Add dependency

--- src/test/java/com/mycompany/SkySong/auth/service/SessionPersistenceTest.java
+++ src/test/java/com/mycompany/SkySong/auth/service/SessionPersistenceTest.java
@@ -1,12 +1,15 @@
 package com.mycompany.SkySong.auth.service;
 
+import com.mycompany.SkySong.auth.model.entity.Session;
 import com.mycompany.SkySong.auth.security.SessionPersistence;
 import com.mycompany.SkySong.testsupport.auth.security.SessionExistenceChecker;
+import com.mycompany.SkySong.testsupport.auth.service.TestSessionCreator;
 import com.mycompany.SkySong.testsupport.common.BaseIT;
 import com.mycompany.SkySong.testsupport.common.SqlDatabaseCleaner;
 import com.mycompany.SkySong.testsupport.common.SqlDatabaseInitializer;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
 public class SessionPersistenceTest extends BaseIT {
@@ -15,6 +18,7 @@ public class SessionPersistenceTest extends BaseIT {
     private SessionPersistence sessionPersistence;
     @Autowired
     private SessionExistenceChecker sessionChecker;
+    private TestSessionCreator sessionCreation;
 
     @Autowired
     private SqlDatabaseInitializer initializer;
@@ -23,6 +27,7 @@ public class SessionPersistenceTest extends BaseIT {
 
     @BeforeEach
     void setUp() throws Exception {
+        sessionCreation = new TestSessionCreator();
         initializer.setup("data_sql/test-setup.sql");
     }
 
