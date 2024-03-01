b9527691b3c862b5d06b8aec1414c0907afc087d
Champel Mariusz <champelmariusz@gmail.com>
2024-03-01 23:35:56 +0100

    test: Change constants from static final to instance variable

--- src/test/java/com/mycompany/SkySong/testsupport/auth/common/LoginRequests.java
+++ src/test/java/com/mycompany/SkySong/testsupport/auth/common/LoginRequests.java
@@ -4,24 +4,24 @@ import com.mycompany.SkySong.auth.model.dto.LoginRequest;
 
 public class LoginRequests {
 
-    public static final LoginRequest loginRegisteredUser =
+    public LoginRequest loginRegisteredUser =
             new LoginRequest("New", "Password#3");
-    public static final LoginRequest validCredentials =
+    public LoginRequest validCredentials =
             new LoginRequest("User", "Password#3");
-    public static final LoginRequest invalidPassword =
+    public LoginRequest invalidPassword =
             new LoginRequest("User", "invalid");
-    public static final LoginRequest invalidUsername =
+    public LoginRequest invalidUsername =
             new LoginRequest("invalid", "Password#3");
-    public static final LoginRequest invalidEmail =
+    public LoginRequest invalidEmail =
             new LoginRequest("invalid@mail.com", "Password#3");
-    public static final LoginRequest emptyCredentials =
+    public LoginRequest emptyCredentials =
             new LoginRequest("", "");
 
 
-    public static final String malformedJson =
+    public String malformedJson =
             "{\"usernameOrEmail\": \"mail@mail.com\",\"password\": \"Password#3\"";
 
-    public static LoginRequest login(String username) {
+    public LoginRequest login(String username) {
         return new LoginRequest(username, "Password#3");
     }
     public LoginRequest loginInvalidPassword(String username) {
