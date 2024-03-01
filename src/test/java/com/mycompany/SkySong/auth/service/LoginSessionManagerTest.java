12de19e231dd81b5ed6ab136d1cacb19585e6d37
Champel Mariusz <champelmariusz@gmail.com>
2024-03-01 16:06:58 +0100

    test: Add test to verify session ID is not null after initialization

--- src/test/java/com/mycompany/SkySong/auth/service/LoginSessionManagerTest.java
+++ src/test/java/com/mycompany/SkySong/auth/service/LoginSessionManagerTest.java
@@ -1,11 +1,15 @@
 package com.mycompany.SkySong.auth.service;
 
+import com.mycompany.SkySong.testsupport.auth.common.LoginRequests;
 import com.mycompany.SkySong.testsupport.common.BaseIT;
 import com.mycompany.SkySong.testsupport.common.SqlDatabaseCleaner;
 import com.mycompany.SkySong.testsupport.common.SqlDatabaseInitializer;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class LoginSessionManagerTest extends BaseIT {
 
@@ -25,5 +29,11 @@ public class LoginSessionManagerTest extends BaseIT {
     @AfterEach
     void cleanUp() {
         cleaner.clean();
+    }
+
+    @Test
+    void whenInitializeAndSaveSession_SessionIdNotNull() {
+        String sessionId = sessionManager.initializeAndSaveSession(LoginRequests.VALID_CREDENTIALS);
+        assertThat(sessionId).isNotNull();
     }
 }
