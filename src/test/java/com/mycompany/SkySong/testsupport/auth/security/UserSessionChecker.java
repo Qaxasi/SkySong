512729364230e51d9832090f9a7267cb19709111
Champel Mariusz <champelmariusz@gmail.com>
2024-03-01 19:34:42 +0100

    test: Add dependency

--- src/test/java/com/mycompany/SkySong/testsupport/auth/security/UserSessionChecker.java
+++ src/test/java/com/mycompany/SkySong/testsupport/auth/security/UserSessionChecker.java
@@ -2,6 +2,14 @@ package com.mycompany.SkySong.testsupport.auth.security;
 
 import org.springframework.stereotype.Component;
 
+import javax.sql.DataSource;
+
 @Component
 public class UserSessionChecker {
+
+    private final DataSource dataSource;
+
+    public UserSessionChecker(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 }
