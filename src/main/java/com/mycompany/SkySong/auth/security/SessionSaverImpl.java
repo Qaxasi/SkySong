1cf54c473683a9bdcf07587a77446e4436c7aa49
Champel Mariusz <champelmariusz@gmail.com>
2024-03-02 13:52:49 +0100

    feat: Annotate saveSession with Transactional

--- src/main/java/com/mycompany/SkySong/auth/security/SessionSaverImpl.java
+++ src/main/java/com/mycompany/SkySong/auth/security/SessionSaverImpl.java
@@ -3,6 +3,7 @@ package com.mycompany.SkySong.auth.security;
 import com.mycompany.SkySong.auth.model.entity.Session;
 import com.mycompany.SkySong.auth.repository.SessionDAO;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class SessionSaverImpl implements SessionSaver {
@@ -14,6 +15,7 @@ public class SessionSaverImpl implements SessionSaver {
     }
 
     @Override
+    @Transactional
     public void saveSession(Session session) {
         sessionDAO.save(session);
     }
