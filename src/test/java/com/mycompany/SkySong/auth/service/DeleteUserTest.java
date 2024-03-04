bb795d65068aee20dcf43d07072b2846153c7b77
Champel Mariusz <champelmariusz@gmail.com>
2024-03-04 21:29:50 +0100

    refactor: Remove unused imports

--- src/test/java/com/mycompany/SkySong/auth/service/DeleteUserTest.java
+++ src/test/java/com/mycompany/SkySong/auth/service/DeleteUserTest.java
@@ -8,7 +8,6 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 import static org.junit.Assert.assertThrows;
 
