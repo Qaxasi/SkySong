20910306bb69ddd180afa6652364aec47f687379
Champel Mariusz <champelmariusz@gmail.com>
2024-03-04 17:43:00 +0100

    feat: Create new class and annotate as a spring service

--- src/main/java/com/mycompany/SkySong/auth/service/sdsdsd.java
+++
@@ -1,8 +0,0 @@
-package com.mycompany.SkySong.auth.service;
-
-import com.mycompany.SkySong.auth.exception.DatabaseException;
-import com.mycompany.SkySong.auth.model.dto.ApiResponse;
-
-public interface DeleteUserService {
-    ApiResponse deleteUser(int userId) throws DatabaseException;
-}
