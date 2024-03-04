d238950e7c4274912cc9b801675a7513cd103a53
Champel Mariusz <champelmariusz@gmail.com>
2024-03-04 17:43:28 +0100

    feat: Add dependency

--- src/main/java/com/mycompany/SkySong/auth/service/DeleteUser.java
+++ src/main/java/com/mycompany/SkySong/auth/service/DeleteUser.java
@@ -1,7 +1,16 @@
 package com.mycompany.SkySong.auth.service;
 
+import com.mycompany.SkySong.auth.exception.UserNotFoundException;
+import com.mycompany.SkySong.auth.model.entity.User;
+import com.mycompany.SkySong.auth.repository.UserDAO;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class DeleteUser {
+
+    private final UserDAO userDAO;
+    public DeleteUser(UserDAO userDAO) {
+        this.userDAO = userDAO;
+    }
 }
