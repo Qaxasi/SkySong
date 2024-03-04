7eca469f08f015432d18f0fe317a5183f7b7704c
Champel Mariusz <champelmariusz@gmail.com>
2024-03-04 20:53:29 +0100

    test: Add test to verify exception when user not exist

--- src/main/java/com/mycompany/SkySong/auth/service/UserRetrieval.java
+++
@@ -1,7 +0,0 @@
-package com.mycompany.SkySong.auth.service;
-
-import com.mycompany.SkySong.auth.model.entity.User;
-
-public interface UserRetrieval {
-    User findByAuthUsername(String username);
-}
--- src/test/java/com/mycompany/SkySong/auth/service/DeleteUserTest.java
+++ src/test/java/com/mycompany/SkySong/auth/service/DeleteUserTest.java
@@ -1,11 +1,16 @@
 package com.mycompany.SkySong.auth.service;
 
+import com.mycompany.SkySong.auth.exception.UserNotFoundException;
 import com.mycompany.SkySong.testsupport.common.BaseIT;
 import com.mycompany.SkySong.testsupport.common.SqlDatabaseCleaner;
 import com.mycompany.SkySong.testsupport.common.SqlDatabaseInitializer;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import static org.junit.Assert.assertThrows;
 
 public class DeleteUserTest extends BaseIT {
 
@@ -27,4 +32,8 @@ public class DeleteUserTest extends BaseIT {
         cleaner.clean();
     }
 
+    @Test
+    void whenUserNotExist_ThrowException() {
+        assertThrows(UserNotFoundException.class, () -> deleteUser.deleteUserById(100));
+    }
 }
