89335a19de81651508ed2cfbacd3febc087db568
Champel Mariusz <champelmariusz@gmail.com>
2024-03-04 21:34:06 +0100

    test: Inject dependencies and rename variable

--- src/test/java/com/mycompany/SkySong/auth/service/DeleteUserTest.java
+++ src/test/java/com/mycompany/SkySong/auth/service/DeleteUserTest.java
@@ -1,6 +1,8 @@
 package com.mycompany.SkySong.auth.service;
 
 import com.mycompany.SkySong.auth.exception.UserNotFoundException;
+import com.mycompany.SkySong.testsupport.auth.common.UserExistenceChecker;
+import com.mycompany.SkySong.testsupport.auth.common.UserIdFetcher;
 import com.mycompany.SkySong.testsupport.common.BaseIT;
 import com.mycompany.SkySong.testsupport.common.SqlDatabaseCleaner;
 import com.mycompany.SkySong.testsupport.common.SqlDatabaseInitializer;
@@ -14,7 +16,11 @@ import static org.junit.Assert.assertThrows;
 public class DeleteUserTest extends BaseIT {
 
     @Autowired
-    private DeleteUser deleteUser;
+    private DeleteUser delete;
+    @Autowired
+    private UserIdFetcher idFetcher;
+    @Autowired
+    private UserExistenceChecker userChecker;
 
     @Autowired
     private SqlDatabaseInitializer initializer;
@@ -33,6 +39,6 @@ public class DeleteUserTest extends BaseIT {
 
     @Test
     void whenUserNotExist_ThrowException() {
-        assertThrows(UserNotFoundException.class, () -> deleteUser.deleteUserById(100));
+        assertThrows(UserNotFoundException.class, () -> delete.deleteUserById(100));
     }
 }
