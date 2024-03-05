91cc3711e687adfa133b75da3e1e10be41511006
Champel Mariusz <champelmariusz@gmail.com>
2024-03-05 19:21:02 +0100

    refactor: Change class access to public

--- src/main/java/com/mycompany/SkySong/auth/security/SessionSaver.java
+++
@@ -1,7 +0,0 @@
-package com.mycompany.SkySong.auth.security;
-
-import com.mycompany.SkySong.auth.model.entity.Session;
-
-public interface SessionSaver {
-    void saveSession(Session session);
-}
--- src/main/java/com/mycompany/SkySong/auth/service/RegistrationValidationImpl.java
+++ src/main/java/com/mycompany/SkySong/auth/service/RegistrationValidationImpl.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
-class RegistrationValidationImpl implements RegistrationValidation {
+public class RegistrationValidationImpl implements RegistrationValidation {
 
     private final List<RegistrationValidationStrategy> strategies;
 
