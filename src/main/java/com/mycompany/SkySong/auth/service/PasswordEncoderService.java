7afc3742fbbc704d0060a30386034aeef473b69f
Champel Mariusz <champelmariusz@gmail.com>
2024-03-06 15:39:20 +0100

    refactor: Remove interface implementation and rename class

--- src/main/java/com/mycompany/SkySong/auth/service/PasswordEncoderService.java
+++ src/main/java/com/mycompany/SkySong/auth/service/PasswordEncoderService.java
@@ -3,10 +3,10 @@ package com.mycompany.SkySong.auth.service;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 @Service
-public class PasswordService {
+public class PasswordEncoderService {
     private final PasswordEncoder passwordEncoder;
 
-    public PasswordService(PasswordEncoder passwordEncoder) {
+    public PasswordEncoderService(PasswordEncoder passwordEncoder) {
         this.passwordEncoder = passwordEncoder;
     }
 
