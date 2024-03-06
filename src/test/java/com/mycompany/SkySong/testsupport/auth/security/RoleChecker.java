e501ec0351fd894ce1c268a069ff58f036d0c0c6
Champel Mariusz <champelmariusz@gmail.com>
2024-03-06 01:50:58 +0100

    test: Create utility class and annotate as a spring component

---
+++ src/test/java/com/mycompany/SkySong/testsupport/auth/security/RoleChecker.java
@@ -0,0 +1,13 @@
+package com.mycompany.SkySong.testsupport.auth.security;
+
+import org.springframework.stereotype.Component;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+@Component
+public class RoleChecker {
+}
