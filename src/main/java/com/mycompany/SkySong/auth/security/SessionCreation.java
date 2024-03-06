920debcf8006f44b5c72f0805a878cd1db49ec45
Champel Mariusz <champelmariusz@gmail.com>
2024-03-06 15:12:04 +0100

    refactor: Update SessionCreation interface to require token for session creation

--- src/main/java/com/mycompany/SkySong/auth/security/SessionCreation.java
+++ src/main/java/com/mycompany/SkySong/auth/security/SessionCreation.java
@@ -3,5 +3,5 @@ package com.mycompany.SkySong.auth.security;
 import com.mycompany.SkySong.auth.model.entity.Session;
 
 public interface SessionCreation {
-    Session createSession(Integer userID);
+    Session createSession(String token, Integer userID);
 }
