a0129be8187494191e2deb74e743b090472406dc
Champel Mariusz <champelmariusz@gmail.com>
2024-03-25 18:14:37 +0100

    feat: Inject dependency

--- src/main/java/com/mycompany/SkySong/auth/service/UserSessionCreator.java
+++ src/main/java/com/mycompany/SkySong/auth/service/UserSessionCreator.java
@@ -1,6 +1,24 @@
 package com.mycompany.SkySong.auth.service;
+import com.mycompany.SkySong.auth.repository.SessionDAO;
+import com.mycompany.SkySong.auth.repository.UserDAO;
+import com.mycompany.SkySong.auth.security.SessionCreation;
+import com.mycompany.SkySong.auth.security.TokenGenerator;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserSessionCreator {
+
+    private final SessionCreation sessionCreation;
+    private final UserAuthentication userAuth;
+    private final TokenGenerator tokenGenerator;
+    private final SessionDAO sessionDAO;
+    private final UserDAO userDAO;
+
+    public UserSessionCreator(SessionCreation sessionCreation, UserAuthentication userAuth, TokenGenerator tokenGenerator, SessionDAO sessionDAO, UserDAO userDAO) {
+        this.sessionCreation = sessionCreation;
+        this.userAuth = userAuth;
+        this.tokenGenerator = tokenGenerator;
+        this.sessionDAO = sessionDAO;
+        this.userDAO = userDAO;
+    }
 }
