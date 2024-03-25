cad497eda19b1e74d59557444ee8b840208eeb70
Champel Mariusz <champelmariusz@gmail.com>
2024-03-25 18:49:03 +0100

    style: Improve readability

--- src/main/java/com/mycompany/SkySong/auth/service/PasswordEncoderService.java
+++ src/main/java/com/mycompany/SkySong/auth/service/PasswordEncoderService.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class PasswordEncoderService {
-    
+
     private final PasswordEncoder passwordEncoder;
 
     public PasswordEncoderService(PasswordEncoder passwordEncoder) {
