309026e5d6e8c7d191e1ca69288d9c015050d52d
Champel Mariusz <champelmariusz@gmail.com>
2024-03-26 19:37:14 +0100

    feat: Implement registerUser method

--- src/main/java/com/mycompany/SkySong/auth/service/UserRegistration.java
+++ src/main/java/com/mycompany/SkySong/auth/service/UserRegistration.java
@@ -1,8 +1,14 @@
 package com.mycompany.SkySong.auth.service;
 
+import com.mycompany.SkySong.auth.exception.RoleNotFoundException;
+import com.mycompany.SkySong.auth.model.dto.RegisterRequest;
+import com.mycompany.SkySong.auth.model.entity.Role;
+import com.mycompany.SkySong.auth.model.entity.User;
+import com.mycompany.SkySong.auth.model.entity.UserRole;
 import com.mycompany.SkySong.auth.repository.RoleDAO;
 import com.mycompany.SkySong.auth.repository.UserDAO;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class UserRegistration {
@@ -15,5 +21,19 @@ public class UserRegistration {
         this.userFactory = userFactory;
         this.roleDAO = roleDAO;
         this.userDAO = userDAO;
+    }
+
+    @Transactional
+    public void registerUser(RegisterRequest request) {
+        Role role = roleDAO.findByName(UserRole.ROLE_USER).orElseThrow(
+                () -> new RoleNotFoundException("Role not found"));
+
+        User user = userFactory.createUser(request, role);
+
+        int userId = userDAO.save(user);
+
+        for (Role roles : user.getRoles()) {
+            userDAO.assignRoleToUser(userId, roles.getId());
+        }
     }
 }
