27c526f5f9f9d4bd52e59756d65b0bb78e060199
Champel Mariusz <champelmariusz@gmail.com>
2024-03-27 19:48:02 +0100

    test: Inject dependencies and add database setup and cleanup methods

--- src/test/java/com/mycompany/SkySong/auth/service/UserSessionCreatorTest.java
+++ src/test/java/com/mycompany/SkySong/auth/service/UserSessionCreatorTest.java
@@ -1,6 +1,28 @@
 package com.mycompany.SkySong.auth.service;
 
 import com.mycompany.SkySong.testsupport.common.BaseIT;
+import com.mycompany.SkySong.testsupport.common.SqlDatabaseCleaner;
+import com.mycompany.SkySong.testsupport.common.SqlDatabaseInitializer;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class UserSessionCreatorTest extends BaseIT {
+    @Autowired
+    private UserSessionCreator userSessionCreator;
+
+    @Autowired
+    private SqlDatabaseInitializer initializer;
+    @Autowired
+    private SqlDatabaseCleaner cleaner;
+
+    @BeforeEach
+    void setUp() throws Exception {
+        initializer.setup("data_sql/test-setup.sql");
+    }
+
+    @AfterEach
+    void cleanUp() {
+        cleaner.clean();
+    }
 }
