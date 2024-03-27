4a21d70237d50172228daf0bf8ba06437a1c1588
Champel Mariusz <champelmariusz@gmail.com>
2024-03-27 21:12:02 +0100

    test: Inject dependencies

--- src/test/java/com/mycompany/SkySong/auth/service/UserSessionCreatorTest.java
+++ src/test/java/com/mycompany/SkySong/auth/service/UserSessionCreatorTest.java
@@ -1,15 +1,26 @@
 package com.mycompany.SkySong.auth.service;
 
+import com.mycompany.SkySong.auth.model.entity.Session;
+import com.mycompany.SkySong.testsupport.auth.common.LoginRequests;
+import com.mycompany.SkySong.testsupport.auth.security.UserSessionChecker;
 import com.mycompany.SkySong.testsupport.common.BaseIT;
 import com.mycompany.SkySong.testsupport.common.SqlDatabaseCleaner;
 import com.mycompany.SkySong.testsupport.common.SqlDatabaseInitializer;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class UserSessionCreatorTest extends BaseIT {
+    
     @Autowired
     private UserSessionCreator userSessionCreator;
+    @Autowired
+    private LoginRequests loginHelper;
+    @Autowired
+    private UserSessionChecker sessionChecker;
 
     @Autowired
     private SqlDatabaseInitializer initializer;
