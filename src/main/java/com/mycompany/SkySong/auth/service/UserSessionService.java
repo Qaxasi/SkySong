f859bccced41432c96da5ec48a3ce6d40c362be9
Champel Mariusz <champelmariusz@gmail.com>
2024-03-29 21:28:25 +0100

    feat: Add dependencies

--- src/main/java/com/mycompany/SkySong/auth/service/UserSessionService.java
+++ src/main/java/com/mycompany/SkySong/auth/service/UserSessionService.java
@@ -1,7 +1,16 @@
 package com.mycompany.SkySong.auth.service;
 
+import com.mycompany.SkySong.auth.security.TokenGenerator;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserSessionService {
+
+    private final TokenGenerator tokenGenerator;
+    private final UserSessionCreator userSessionCreator;
+
+    public UserSessionService(TokenGenerator tokenGenerator, UserSessionCreator userSessionCreator) {
+        this.tokenGenerator = tokenGenerator;
+        this.userSessionCreator = userSessionCreator;
+    }
 }
