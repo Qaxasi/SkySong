14b8f14bfde24fd7fc053564c545e1cfb580b911
Champel Mariusz <champelmariusz@gmail.com>
2024-03-29 22:01:01 +0100

    test: Create test class and extend with BaseIT

---
+++ src/test/java/com/mycompany/SkySong/auth/service/UserSessionServiceTest.java
@@ -0,0 +1,15 @@
+package com.mycompany.SkySong.auth.service;
+
+import com.mycompany.SkySong.testsupport.auth.common.LoginRequests;
+import com.mycompany.SkySong.testsupport.common.BaseIT;
+import com.mycompany.SkySong.testsupport.common.SqlDatabaseCleaner;
+import com.mycompany.SkySong.testsupport.common.SqlDatabaseInitializer;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class UserSessionServiceTest extends BaseIT {
+}
