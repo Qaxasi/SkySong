fab52944d1a2a776f0a91797858bcc6bfd575991
Champel Mariusz <champelmariusz@gmail.com>
2024-04-04 20:56:45 +0200

    test: Inject dependencies

--- src/test/java/com/mycompany/SkySong/testsupport/auth/common/TestUserFactory.java
+++ src/test/java/com/mycompany/SkySong/testsupport/auth/common/TestUserFactory.java
@@ -1,9 +1,25 @@
 package com.mycompany.SkySong.testsupport.auth.common;
 
+import com.mycompany.SkySong.auth.exception.RoleNotFoundException;
+import com.mycompany.SkySong.auth.model.entity.Role;
+import com.mycompany.SkySong.auth.model.entity.User;
+import com.mycompany.SkySong.auth.model.entity.UserRole;
+import com.mycompany.SkySong.auth.repository.RoleDAO;
+import com.mycompany.SkySong.auth.repository.UserDAO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 import java.util.Set;
 
 @Component
 public class TestUserFactory {
+
+    @Autowired
+    private UserDAO userDAO;
+    @Autowired
+    private RoleDAO roleDAO;
+    @Autowired
+    private PasswordEncoder encoder;
+    
 }
