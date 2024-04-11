969d0a4d834bbe474c3c1f449e7739e47fcd07b6
Champel Mariusz <champelmariusz@gmail.com>
2024-04-11 21:05:42 +0200

    test: Create class and annotate as a spring component

---
+++ src/test/java/com/mycompany/SkySong/testsupport/auth/common/UserSaver.java
@@ -0,0 +1,11 @@
+package com.mycompany.SkySong.testsupport.auth.common;
+
+import com.mycompany.SkySong.auth.model.entity.User;
+import com.mycompany.SkySong.auth.repository.UserDAO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+@Component
+public class UserSaver {
+}
