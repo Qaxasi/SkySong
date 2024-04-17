55de2795bf03c0cb5f41847f8de4abe6897cd142
Champel Mariusz <champelmariusz@gmail.com>
2024-04-17 22:32:35 +0200

    test: Create session builder class

---
+++ src/test/java/com/mycompany/SkySong/testsupport/auth/common/SessionBuilder.java
@@ -0,0 +1,10 @@
+package com.mycompany.SkySong.testsupport.auth.common;
+
+import com.mycompany.SkySong.user.Session;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class SessionBuilder {
+}
