3494ba90eb141fb8eaca42edfb6b21b01e8afa71
Champel Mariusz <champelmariusz@gmail.com>
2024-04-25 18:52:07 +0200

    test: Create utility class and annotate as a spring component

---
+++ src/test/java/com/mycompany/SkySong/testsupport/auth/common/UserFixture.java
@@ -0,0 +1,11 @@
+package com.mycompany.SkySong.testsupport.auth.common;
+
+import com.mycompany.SkySong.user.*;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.function.Consumer;
+
+@Component
+public class UserFixture {
+}
