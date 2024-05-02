1fd6a2c66216fad27e45ff64a25bc4c59a6c5c2f
Champel Mariusz <champelmariusz@gmail.com>
2024-05-02 14:47:23 +0200

    refactor: Delete unnecessary classes

--- src/main/java/com/mycompany/SkySong/registration/domain/service/RegistrationValidationStrategy.java
+++
@@ -1,8 +0,0 @@
-package com.mycompany.SkySong.registration.domain.service;
-
-import com.mycompany.SkySong.registration.domain.exception.CredentialValidationException;
-import com.mycompany.SkySong.registration.application.dto.RegisterRequest;
-
-public interface RegistrationValidationStrategy {
-    void validate(RegisterRequest registerRequest) throws CredentialValidationException;
-}
