8861cc766406053b2b95cf412584a97afc9d0dae
Champel Mariusz <champelmariusz@gmail.com>
2024-05-02 11:48:18 +0200

    feat: Extend RoleDAO in RoleRepositoryPort

--- src/main/java/com/mycompany/SkySong/registration/domain/ports/RoleRepositoryPort.java
+++ src/main/java/com/mycompany/SkySong/registration/domain/ports/RoleRepositoryPort.java
@@ -2,11 +2,12 @@ package com.mycompany.SkySong.registration.domain.ports;
 
 import com.mycompany.SkySong.registration.domain.model.Role;
 import com.mycompany.SkySong.registration.domain.model.UserRole;
+import com.mycompany.SkySong.user.RoleDAO;
 
 import java.util.Optional;
 import java.util.Set;
 
-public interface RoleRepositoryPort {
+public interface RoleRepositoryPort extends RoleDAO {
     Optional<Role> findByName(UserRole roleName);
     Set<Role> findRolesByUserId(Integer userId);
 }
