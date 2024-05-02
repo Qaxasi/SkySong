858e5d772d22675faafa4b842eedfbdf16d88fda
Champel Mariusz <champelmariusz@gmail.com>
2024-05-02 11:49:00 +0200

    feat: Extend UserDAO in UserRepositoryPort

--- src/main/java/com/mycompany/SkySong/registration/domain/ports/UserRepositoryPort.java
+++ src/main/java/com/mycompany/SkySong/registration/domain/ports/UserRepositoryPort.java
@@ -1,8 +1,9 @@
 package com.mycompany.SkySong.registration.domain.ports;
 
 import com.mycompany.SkySong.registration.domain.model.User;
+import com.mycompany.SkySong.user.UserDAO;
 
-public interface UserRepositoryPort {
+public interface UserRepositoryPort extends UserDAO {
 
     int save(User user);
     void assignRoleToUser(int userId, int roleId);
