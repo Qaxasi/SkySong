b981a2be90e822ddff462d7778203b70c6539795
Champel Mariusz <champelmariusz@gmail.com>
2024-05-02 13:37:47 +0200

    refactor: Remove validation and message dependencies

--- src/main/java/com/mycompany/SkySong/registration/domain/service/UserRegistrationService.java
+++ src/main/java/com/mycompany/SkySong/registration/domain/service/UserRegistrationService.java
@@ -1,25 +1,15 @@
 package com.mycompany.SkySong.registration.domain.service;
 
 import com.mycompany.SkySong.common.dto.ApiResponse;
-import com.mycompany.SkySong.common.utils.ApplicationMessageLoader;
 import com.mycompany.SkySong.registration.application.dto.RegisterRequest;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
-@Slf4j
 @Service
 public class UserRegistrationService {
 
-    private final RegistrationValidation validation;
-    private final ApplicationMessageLoader message;
     private final UserRegistration registration;
 
-    public UserRegistrationService(RegistrationValidation validation,
-                                   ApplicationMessageLoader message,
-                                   UserRegistration registration) {
-
-        this.validation = validation;
-        this.message = message;
+    public UserRegistrationService(UserRegistration registration) {
         this.registration = registration;
     }
 
