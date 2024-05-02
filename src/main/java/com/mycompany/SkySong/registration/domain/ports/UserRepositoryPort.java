f74103b9f8ba71e1ef4bd52c2bdf03b8d28da94a
Champel Mariusz <champelmariusz@gmail.com>
2024-05-02 11:53:43 +0200

    style: Improve readability

--- src/main/java/com/mycompany/SkySong/registration/domain/ports/UserRepositoryPort.java
+++ src/main/java/com/mycompany/SkySong/registration/domain/ports/UserRepositoryPort.java
@@ -4,7 +4,6 @@ import com.mycompany.SkySong.registration.domain.model.User;
 import com.mycompany.SkySong.user.UserDAO;
 
 public interface UserRepositoryPort extends UserDAO {
-
     int save(User user);
     void assignRoleToUser(int userId, int roleId);
 }
