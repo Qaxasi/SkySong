0b3c3f01566199a868bea40ee66affae83b794e9
Champel Mariusz <champelmariusz@gmail.com>
2024-05-02 14:59:53 +0200

    feat: Add validate method to validate registration request

--- src/main/java/com/mycompany/SkySong/registration/domain/service/UserRegistration.java
+++ src/main/java/com/mycompany/SkySong/registration/domain/service/UserRegistration.java
@@ -38,4 +38,25 @@ public class UserRegistration {
         }
         return new ApiResponse("User registered successfully.");
     }
+
+    private void validate(RegisterRequest request) {
+        if (!request.username().matches("^[a-zA-Z0-9]{3,20}$")) {
+            throw new CredentialValidationException("Invalid username format. The username can contain only letters " +
+                    "and numbers, and should be between 3 and 20 characters long.");
+        }
+        if (!request.email().matches("^[a-zA-Z0-9._%+-]{1,15}@[a-zA-Z0-9.-]{2,15}\\.[a-zA-Z]{2,6}$")) {
+            throw new CredentialValidationException("Invalid email address format. The email should follow the standard " +
+                    "format (e.g., user@example.com) and be between 6 and 30 characters long.");
+        }
+        if (!request.password().matches("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}$")) {
+            throw new CredentialValidationException("Invalid password format. The password must contain an least " +
+                    "8 characters, including uppercase letters, lowercase letters, numbers, and special characters.");
+        }
+        if (userDAO.existsByUsername(request.username())) {
+            throw new CredentialValidationException("Username is already exist!.");
+        }
+        if (userDAO.existsByEmail(request.email())) {
+            throw new CredentialValidationException("mail is already exist!.");
+        }
+    }
 }
