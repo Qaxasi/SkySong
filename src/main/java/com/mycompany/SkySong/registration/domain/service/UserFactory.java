9624b9e33616c361082d87f8ab38268f0bb62ab0
Champel Mariusz <champelmariusz@gmail.com>
2024-05-03 10:34:11 +0200

    refactor: Remove Service annotation

--- src/main/java/com/mycompany/SkySong/registration/domain/service/UserFactory.java
+++ src/main/java/com/mycompany/SkySong/registration/domain/service/UserFactory.java
@@ -3,11 +3,9 @@ package com.mycompany.SkySong.registration.domain.service;
 import com.mycompany.SkySong.registration.application.dto.RegisterRequest;
 import com.mycompany.SkySong.registration.domain.model.Role;
 import com.mycompany.SkySong.registration.domain.model.User;
-import org.springframework.stereotype.Service;
 
 import java.util.Collections;
 
-@Service
 public class UserFactory {
 
     private final PasswordEncoderService passwordEncoder;
