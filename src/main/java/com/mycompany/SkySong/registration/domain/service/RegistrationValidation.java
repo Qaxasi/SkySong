7d24f1171d251f6f7b2d1f4e25f6ba6576b1fe25
Champel Mariusz <champelmariusz@gmail.com>
2024-05-03 09:37:49 +0200

    feat: Inject dependency by constructor

--- src/main/java/com/mycompany/SkySong/registration/domain/service/RegistrationValidation.java
+++ src/main/java/com/mycompany/SkySong/registration/domain/service/RegistrationValidation.java
@@ -1,4 +1,12 @@
 package com.mycompany.SkySong.registration.domain.service;
 
+import com.mycompany.SkySong.registration.domain.ports.UserRepositoryPort;
+
 class RegistrationValidation {
+
+    private final UserRepositoryPort userRepository;
+
+    RegistrationValidation(UserRepositoryPort userRepository) {
+        this.userRepository = userRepository;
+    }
 }
