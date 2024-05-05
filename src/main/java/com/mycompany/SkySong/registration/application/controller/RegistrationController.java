a4e1469aaa4ffa647f83b3d4f9791f54c1196b03
Champel Mariusz <champelmariusz@gmail.com>
2024-05-05 17:15:26 +0200

    refactor: Rename dependency

--- src/main/java/com/mycompany/SkySong/registration/application/controller/RegistrationController.java
+++ src/main/java/com/mycompany/SkySong/registration/application/controller/RegistrationController.java
@@ -2,7 +2,7 @@ package com.mycompany.SkySong.registration.application.controller;
 
 import com.mycompany.SkySong.common.dto.ApiResponse;
 import com.mycompany.SkySong.registration.application.dto.RegisterRequest;
-import com.mycompany.SkySong.registration.domain.service.RegistrationFacade;
+import com.mycompany.SkySong.registration.domain.service.UserRegistrationFacade;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,9 +12,9 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/v1/users")
 public class RegistrationController {
 
-    private final RegistrationFacade registration;
+    private final UserRegistrationFacade registration;
 
-    public RegistrationController(RegistrationFacade registration) {
+    public RegistrationController(UserRegistrationFacade registration) {
         this.registration = registration;
     }
 
