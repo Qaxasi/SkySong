6a95ad3e7d13e0a70f0c4d8c655763a6acdb8d56
Champel Mariusz <champelmariusz@gmail.com>
2024-05-05 16:40:48 +0200

    refactor: Rename class

--- src/main/java/com/mycompany/SkySong/registration/domain/service/UserRegistrationService.java
+++ src/main/java/com/mycompany/SkySong/registration/domain/service/UserRegistrationService.java
@@ -1,11 +1,11 @@
 package com.mycompany.SkySong.registration.domain.service;
 
-class RegistrationService {
+class UserRegistrationService {
 
     private final UserRegistration registration;
     private final RequestValidation validation;
 
-    RegistrationService(UserRegistration registration, RequestValidation validation) {
+    UserRegistrationService(UserRegistration registration, RequestValidation validation) {
         this.registration = registration;
         this.validation = validation;
     }
