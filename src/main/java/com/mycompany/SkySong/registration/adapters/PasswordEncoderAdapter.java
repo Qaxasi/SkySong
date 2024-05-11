a719963816d62b24c0b2423dfadc9b97072f0df2
Champel Mariusz <champelmariusz@gmail.com>
2024-05-11 21:48:58 +0200

    feat: Add PasswordEncoder dependency

--- src/main/java/com/mycompany/SkySong/registration/adapters/PasswordEncoderAdapter.java
+++ src/main/java/com/mycompany/SkySong/registration/adapters/PasswordEncoderAdapter.java
@@ -2,10 +2,18 @@ package com.mycompany.SkySong.registration.adapters;
 
 
 import com.mycompany.SkySong.registration.domain.ports.PasswordEncoderPort;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 class PasswordEncoderAdapter implements PasswordEncoderPort {
+
+    private final PasswordEncoder encoder;
+
+    PasswordEncoderAdapter(PasswordEncoder encoder) {
+        this.encoder = encoder;
+    }
+
     @Override
     public String encode(CharSequence password) {
         return null;
