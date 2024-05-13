15cd4ce08584d437ab1e7308861d2417b61c3438
Champel Mariusz <champelmariusz@gmail.com>
2024-05-13 20:37:03 +0200

    feat: Implement authentication logic in UserAuthenticationAdapter

--- src/main/java/com/mycompany/SkySong/login/adapters/UserAuthenticationAdapter.java
+++ src/main/java/com/mycompany/SkySong/login/adapters/UserAuthenticationAdapter.java
@@ -3,7 +3,10 @@ package com.mycompany.SkySong.login.adapters;
 import com.mycompany.SkySong.login.application.dto.LoginRequest;
 import com.mycompany.SkySong.login.domain.ports.UserAuthenticationPort;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -17,6 +20,14 @@ class UserAuthenticationAdapter implements UserAuthenticationPort {
 
     @Override
     public Authentication authenticateUser(LoginRequest request) {
-        return null;
+        try {
+            Authentication auth = authManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(
+                            request.usernameOrEmail(), request.password()));
+            SecurityContextHolder.getContext().setAuthentication(auth);
+            return auth;
+        } catch (BadCredentialsException e) {
+            throw new BadCredentialsException("Logout failed due to an internal error.");
+        }
     }
 }
