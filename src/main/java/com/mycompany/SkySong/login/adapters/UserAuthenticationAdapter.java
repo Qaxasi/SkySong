7ea113072b5d5fb99d972ab9fb5168f758b0a52f
Champel Mariusz <champelmariusz@gmail.com>
2024-05-13 20:32:58 +0200

    feat: Add AuthenticationManager dependency

--- src/main/java/com/mycompany/SkySong/login/adapters/UserAuthenticationAdapter.java
+++ src/main/java/com/mycompany/SkySong/login/adapters/UserAuthenticationAdapter.java
@@ -3,15 +3,17 @@ package com.mycompany.SkySong.login.adapters;
 import com.mycompany.SkySong.login.application.dto.LoginRequest;
 import com.mycompany.SkySong.login.domain.ports.UserAuthenticationPort;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 @Service
 class UserAuthenticationAdapter implements UserAuthenticationPort {
+
+    private final AuthenticationManager authManager;
+
+    UserAuthenticationAdapter(AuthenticationManager authManager) {
+        this.authManager = authManager;
+    }
 
     @Override
     public Authentication authenticateUser(LoginRequest request) {
