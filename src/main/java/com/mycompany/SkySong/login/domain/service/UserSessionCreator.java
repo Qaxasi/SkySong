694dbad58497b1625222d4132c4abe2635575882
Champel Mariusz <champelmariusz@gmail.com>
2024-05-14 22:37:41 +0200

    refactor: Refactor createUserSession method to directly create and save sessions

--- src/main/java/com/mycompany/SkySong/login/domain/service/UserSessionCreator.java
+++ src/main/java/com/mycompany/SkySong/login/domain/service/UserSessionCreator.java
@@ -30,12 +30,8 @@ public class UserSessionCreator {
         this.userDAO = userDAO;
     }
 
-    public void createUserSession(LoginRequest request, String sessionToken) {
-        Authentication auth = userAuth.authenticateUser(request);
-        User user = userDAO.findByUsername(auth.getName()).orElseThrow(
-                () -> new UserNotFoundException("User not found with username :" + auth.getName()));
-
-        Session session = sessionCreation.createSession(sessionToken, user.getId());
+    public void createUserSession(String sessionToken, int userId) {
+        Session session = createSession(sessionToken, userId);
         sessionDAO.save(session);
     }
 
