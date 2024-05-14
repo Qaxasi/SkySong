8abc1c2610286d71537a560292c0caaeb4e2e8e4
Champel Mariusz <champelmariusz@gmail.com>
2024-05-14 22:43:36 +0200

    refactor: Remove dependencies

--- src/main/java/com/mycompany/SkySong/login/domain/service/UserSessionCreator.java
+++ src/main/java/com/mycompany/SkySong/login/domain/service/UserSessionCreator.java
@@ -1,33 +1,16 @@
 package com.mycompany.SkySong.login.domain.service;
 
-import com.mycompany.SkySong.common.exception.UserNotFoundException;
 import com.mycompany.SkySong.common.utils.TokenHasher;
-import com.mycompany.SkySong.login.application.dto.LoginRequest;
-import com.mycompany.SkySong.login.domain.ports.UserRepositoryPort;
 import com.mycompany.SkySong.user.Session;
-import com.mycompany.SkySong.registration.domain.model.User;
-import com.mycompany.SkySong.user.SessionDAO;
-import com.mycompany.SkySong.user.UserDAO;
-import org.springframework.security.core.Authentication;
-
 import java.util.Date;
 
 public class UserSessionCreator {
-    private final TokenHasher tokenHasher;
-    private final SessionCreation sessionCreation;
-    private final UserRepositoryPort userRepository;
-    private final SessionDAO sessionDAO;
-    private final UserDAO userDAO;
 
-    public UserSessionCreator(TokenHasher tokenHasher, SessionCreation sessionCreation,
-                              UserRepositoryPort userRepository,
-                              SessionDAO sessionDAO,
-                              UserDAO userDAO) {
+    private final TokenHasher tokenHasher;
+
+    public UserSessionCreator(TokenHasher tokenHasher) {
         this.tokenHasher = tokenHasher;
-        this.sessionCreation = sessionCreation;
-        this.userRepository = userRepository;
-        this.sessionDAO = sessionDAO;
-        this.userDAO = userDAO;
+
     }
 
     public void createUserSession(String sessionToken, int userId) {
