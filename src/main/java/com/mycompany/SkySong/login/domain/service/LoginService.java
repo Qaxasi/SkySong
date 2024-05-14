364f4a0935d5c988f64125b965390cce3170661e
Champel Mariusz <champelmariusz@gmail.com>
2024-05-14 22:28:16 +0200

    refactor: Update login method to authenticate and verify user before session creation

--- src/main/java/com/mycompany/SkySong/login/domain/service/LoginService.java
+++ src/main/java/com/mycompany/SkySong/login/domain/service/LoginService.java
@@ -5,7 +5,6 @@ import com.mycompany.SkySong.login.application.dto.LoginRequest;
 import com.mycompany.SkySong.login.domain.ports.UserAuthenticationPort;
 import com.mycompany.SkySong.login.domain.ports.UserRepositoryPort;
 import com.mycompany.SkySong.registration.domain.model.User;
-import org.springframework.security.core.Authentication;
 
 class LoginService {
 
@@ -25,8 +24,12 @@ class LoginService {
    }
 
     public String login(LoginRequest request) {
+       String username = userAuth.authenticateUser(request);
+       User user = userRepository.findByUsername(username).orElseThrow(
+                () -> new UserNotFoundException("User not found with username :" + username));
+
         String sessionToken = tokenGenerator.generateToken();
-        userSessionCreator.createUserSession(request, sessionToken);
+        userSessionCreator.createUserSession(sessionToken, user.getId());
         return sessionToken;
     }
 }
