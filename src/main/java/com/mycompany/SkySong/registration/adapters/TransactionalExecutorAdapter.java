588dab8de00b6e0087347895210c12f89c3d4f7b
Champel Mariusz <champelmariusz@gmail.com>
2024-05-20 20:11:57 +0200

    refactor: Change TransactionalExecutorAdapter to package-private

--- src/main/java/com/mycompany/SkySong/registration/adapters/TransactionalExecutorAdapter.java
+++ src/main/java/com/mycompany/SkySong/registration/adapters/TransactionalExecutorAdapter.java
@@ -4,7 +4,7 @@ import com.mycompany.SkySong.registration.domain.ports.TransactionalExecutorPort
 
 import java.util.function.Supplier;
 
-public class TransactionalExecutorAdapter implements TransactionalExecutorPort {
+class TransactionalExecutorAdapter implements TransactionalExecutorPort {
     @Override
     public <T> T execute(Supplier<T> action) {
         return null;
