f68fead4b9e6beeea54cb7bbe011ac5288cda425
Champel Mariusz <champelmariusz@gmail.com>
2024-05-20 20:13:18 +0200

    feat: Add TransactionTemplate dependency

--- src/main/java/com/mycompany/SkySong/registration/adapters/TransactionalExecutorAdapter.java
+++ src/main/java/com/mycompany/SkySong/registration/adapters/TransactionalExecutorAdapter.java
@@ -1,10 +1,18 @@
 package com.mycompany.SkySong.registration.adapters;
 
 import com.mycompany.SkySong.registration.domain.ports.TransactionalExecutorPort;
+import org.springframework.transaction.support.TransactionTemplate;
 
 import java.util.function.Supplier;
 
 class TransactionalExecutorAdapter implements TransactionalExecutorPort {
+
+    private final TransactionTemplate transactionTemplate;
+
+    TransactionalExecutorAdapter(TransactionTemplate transactionTemplate) {
+        this.transactionTemplate = transactionTemplate;
+    }
+
     @Override
     public <T> T execute(Supplier<T> action) {
         return null;
