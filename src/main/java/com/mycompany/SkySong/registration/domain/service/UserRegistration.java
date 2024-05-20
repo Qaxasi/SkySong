c7381f65fd472b79d88237efee10e359e8e7afb3
Champel Mariusz <champelmariusz@gmail.com>
2024-05-20 20:33:29 +0200

    refactor: Refactor UserRegistration to use TransactionalExecutorPort for transaction management

--- src/main/java/com/mycompany/SkySong/registration/domain/service/UserRegistration.java
+++ src/main/java/com/mycompany/SkySong/registration/domain/service/UserRegistration.java
@@ -7,24 +7,29 @@ import com.mycompany.SkySong.registration.domain.model.Role;
 import com.mycompany.SkySong.registration.domain.model.User;
 import com.mycompany.SkySong.registration.domain.model.UserRole;
 import com.mycompany.SkySong.registration.domain.ports.RoleRepositoryPort;
+import com.mycompany.SkySong.registration.domain.ports.TransactionalExecutorPort;
+import com.mycompany.SkySong.registration.domain.ports.UserRepositoryPort;
 
 class UserRegistration {
 
     private final UserFactory userFactory;
     private final RoleRepositoryPort roleRepository;
     private final UserRepositoryPort userRepository;
+    private final TransactionalExecutorPort transactionalExecutor;
 
 
     UserRegistration(UserFactory userFactory,
-                            RoleRepositoryPort roleRepository,
-                            UserRepositoryPort userRepository) {
+                     RoleRepositoryPort roleRepository,
+                     UserRepositoryPort userRepository,
+                     TransactionalExecutorPort transactionalExecutor) {
         this.userFactory = userFactory;
         this.roleRepository = roleRepository;
         this.userRepository = userRepository;
+        this.transactionalExecutor = transactionalExecutor;
     }
 
     ApiResponse registerUser(RegisterRequest request) {
-        return transactionTemplate.execute(status -> {
+        return transactionalExecutor.execute(() -> {
             Role role = roleRepository.findByName(UserRole.ROLE_USER).orElseThrow(
                     () -> new RoleNotFoundException("Role not found"));
 
