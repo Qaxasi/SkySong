96632bb9ce385871b417603fd490cc4aabe58e60
Champel Mariusz <champelmariusz@gmail.com>
2024-05-21 20:39:30 +0200

    feat: Add TransactionalExecutorPort dependency

--- src/main/java/com/mycompany/SkySong/user/delete/domain/service/DeleteUser.java
+++ src/main/java/com/mycompany/SkySong/user/delete/domain/service/DeleteUser.java
@@ -4,17 +4,21 @@ import com.mycompany.SkySong.common.dto.ApiResponse;
 import com.mycompany.SkySong.common.exception.UserNotFoundException;
 import com.mycompany.SkySong.registration.domain.model.User;
 import com.mycompany.SkySong.user.delete.domain.ports.SessionRepositoryPort;
+import com.mycompany.SkySong.user.delete.domain.ports.TransactionalExecutorPort;
 import com.mycompany.SkySong.user.delete.domain.ports.UserRepositoryPort;
 
 class DeleteUser {
 
     private final UserRepositoryPort userRepository;
     private final SessionRepositoryPort sessionRepository;
+    private final TransactionalExecutorPort transactionalExecutor;
 
     DeleteUser(UserRepositoryPort userRepository,
-               SessionRepositoryPort sessionRepository) {
+               SessionRepositoryPort sessionRepository,
+               TransactionalExecutorPort transactionalExecutor) {
         this.userRepository = userRepository;
         this.sessionRepository = sessionRepository;
+        this.transactionalExecutor = transactionalExecutor;
     }
 
     ApiResponse deleteUserById(int userId) {
