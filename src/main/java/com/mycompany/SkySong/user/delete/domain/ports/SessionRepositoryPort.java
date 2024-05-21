adb1fe2af424c23eef85ea44fcb71b855b83f03b
Champel Mariusz <champelmariusz@gmail.com>
2024-05-21 19:37:53 +0200

    style: Improve readability

--- src/main/java/com/mycompany/SkySong/user/delete/domain/ports/SessionRepositoryPort.java
+++ src/main/java/com/mycompany/SkySong/user/delete/domain/ports/SessionRepositoryPort.java
@@ -3,5 +3,5 @@ package com.mycompany.SkySong.user.delete.domain.ports;
 import com.mycompany.SkySong.common.dao.SessionDAO;
 
 public interface SessionRepositoryPort extends SessionDAO {
-    void deleteUserSessions( int userId);
+    void deleteUserSessions(int userId);
 }
