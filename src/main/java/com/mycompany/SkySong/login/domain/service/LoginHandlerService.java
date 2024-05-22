0fbcfd8c952c6d75bae3dfa32a5cb780616e1f04
Champel Mariusz <champelmariusz@gmail.com>
2024-05-22 21:11:50 +0200

    feat: Create class and annotate as a spring service

---
+++ src/main/java/com/mycompany/SkySong/login/domain/service/LoginHandlerService.java
@@ -0,0 +1,8 @@
+package com.mycompany.SkySong.login.domain.service;
+
+import com.mycompany.SkySong.login.application.dto.LoginRequest;
+import org.springframework.stereotype.Service;
+
+@Service
+public class LoginHandlerService {
+}
