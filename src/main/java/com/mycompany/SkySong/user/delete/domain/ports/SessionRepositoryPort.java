537b3dfd58afc1aca51199cf84d4f5c82e9a4b15
Champel Mariusz <champelmariusz@gmail.com>
2024-05-27 18:54:42 +0200

    refactor: Delete unnecessary interfaces

--- src/main/java/com/mycompany/SkySong/user/delete/domain/ports/SessionRepositoryPort.java
+++
@@ -1,7 +0,0 @@
-package com.mycompany.SkySong.user.delete.domain.ports;
-
-import com.mycompany.SkySong.common.dao.SessionDAO;
-
-public interface SessionRepositoryPort extends SessionDAO {
-    void deleteUserSessions(int userId);
-}
--- src/main/java/com/mycompany/SkySong/user/delete/domain/ports/TransactionalExecutorPort.java
+++
@@ -1,7 +0,0 @@
-package com.mycompany.SkySong.user.delete.domain.ports;
-
-import java.util.function.Supplier;
-
-public interface TransactionalExecutorPort {
-    <T> T execute(Supplier<T> action);
-}
