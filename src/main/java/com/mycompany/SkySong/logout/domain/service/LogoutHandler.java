16f2de77ecf1c4035969615a80d516ac6844d8d9
Champel Mariusz <champelmariusz@gmail.com>
2024-05-29 19:59:28 +0200

    feat: Create LogoutHandler class

---
+++ src/main/java/com/mycompany/SkySong/logout/domain/service/LogoutHandler.java
@@ -0,0 +1,7 @@
+package com.mycompany.SkySong.logout.domain.service;
+
+import com.mycompany.SkySong.common.utils.TokenHasher;
+import com.mycompany.SkySong.logout.domain.ports.SessionRepositoryPort;
+
+class LogoutHandler {
+}
