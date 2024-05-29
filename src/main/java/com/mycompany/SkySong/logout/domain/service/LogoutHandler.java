1d4b94e047cd81ae117bbf395088b5267ffbff71
Champel Mariusz <champelmariusz@gmail.com>
2024-05-29 20:00:01 +0200

    feat: Add dependency

--- src/main/java/com/mycompany/SkySong/logout/domain/service/LogoutHandler.java
+++ src/main/java/com/mycompany/SkySong/logout/domain/service/LogoutHandler.java
@@ -4,4 +4,12 @@ import com.mycompany.SkySong.common.utils.TokenHasher;
 import com.mycompany.SkySong.logout.domain.ports.SessionRepositoryPort;
 
 class LogoutHandler {
+
+    private final SessionRepositoryPort sessionRepositoryPort;
+    private final TokenHasher tokenHasher;
+    LogoutHandler(SessionRepositoryPort sessionRepositoryPort,
+                  TokenHasher tokenHasher) {
+        this.sessionRepositoryPort = sessionRepositoryPort;
+        this.tokenHasher = tokenHasher;
+    }
 }
