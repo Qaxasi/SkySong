ac1b3e0b3855aa9932345716ee0cab56cf15d469
Champel Mariusz <champelmariusz@gmail.com>
2024-05-30 21:08:22 +0200

    refactor: Rename interface

--- src/main/java/com/mycompany/SkySong/registration/domain/ports/UserRepository.java
+++ src/main/java/com/mycompany/SkySong/registration/domain/ports/UserRepository.java
@@ -3,7 +3,7 @@ package com.mycompany.SkySong.registration.domain.ports;
 import com.mycompany.SkySong.registration.domain.model.User;
 import com.mycompany.SkySong.common.dao.UserDAO;
 
-public interface UserRepositoryPort extends UserDAO {
+public interface UserRepository extends UserDAO {
     int save(User user);
     void assignRoleToUser(int userId, int roleId);
 }
