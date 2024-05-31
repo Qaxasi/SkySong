f039f44125055570ba7659112d4b28043e1dbe3c
Champel Mariusz <champelmariusz@gmail.com>
2024-05-31 23:11:18 +0200

    feat: Create UserCreator class

---
+++ src/main/java/com/mycompany/SkySong/registration/domain/service/UserCreator.java
@@ -0,0 +1,15 @@
+package com.mycompany.SkySong.registration.domain.service;
+
+import com.mycompany.SkySong.registration.application.dto.RegisterRequest;
+import com.mycompany.SkySong.registration.domain.exception.RoleNotFoundException;
+import com.mycompany.SkySong.registration.domain.model.Role;
+import com.mycompany.SkySong.registration.domain.model.User;
+import com.mycompany.SkySong.registration.domain.model.UserRole;
+import com.mycompany.SkySong.registration.domain.ports.PasswordEncoder;
+import com.mycompany.SkySong.registration.domain.ports.RoleRepository;
+
+import java.util.Collections;
+
+class UserCreator {
+    
+}
