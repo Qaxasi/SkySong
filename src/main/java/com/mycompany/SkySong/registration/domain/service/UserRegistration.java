dfe3ca36eb2474338fd5d582447aa3126ae33ad6
Champel Mariusz <champelmariusz@gmail.com>
2024-06-01 17:36:43 +0200

    feat: Add registerUser method to UserRegistration

--- src/main/java/com/mycompany/SkySong/registration/domain/service/UserRegistration.java
+++ src/main/java/com/mycompany/SkySong/registration/domain/service/UserRegistration.java
@@ -1,5 +1,8 @@
 package com.mycompany.SkySong.registration.domain.service;
 
+import com.mycompany.SkySong.common.dto.ApiResponse;
+import com.mycompany.SkySong.registration.application.dto.RegisterRequest;
+import com.mycompany.SkySong.registration.domain.model.User;
 import com.mycompany.SkySong.registration.domain.ports.UserSaver;
 
 class UserRegistration {
@@ -12,5 +15,12 @@ class UserRegistration {
         this.validation = validation;
         this.userCreator = userCreator;
         this.userSaver = userSaver;
+    }
+
+    ApiResponse registerUser(RegisterRequest request) {
+        validation.validate(request);
+        User user = userCreator.createUser(request);
+        userSaver.saveUser(user);
+        return new ApiResponse("User registered successfully.");
     }
 }
