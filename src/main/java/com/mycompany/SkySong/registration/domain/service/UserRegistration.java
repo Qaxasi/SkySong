87fad5678cb044ce02d81a7a7db2aeec9bd03d9b
Champel Mariusz <champelmariusz@gmail.com>
2024-06-01 17:35:12 +0200

    feat: Add dependency

--- src/main/java/com/mycompany/SkySong/registration/domain/service/UserRegistration.java
+++ src/main/java/com/mycompany/SkySong/registration/domain/service/UserRegistration.java
@@ -1,4 +1,16 @@
 package com.mycompany.SkySong.registration.domain.service;
 
+import com.mycompany.SkySong.registration.domain.ports.UserSaver;
+
 class UserRegistration {
+
+    private final RequestValidation validation;
+    private final UserCreator userCreator;
+    private final UserSaver userSaver;
+
+    UserRegistration(RequestValidation validation, UserCreator userCreator, UserSaver userSaver) {
+        this.validation = validation;
+        this.userCreator = userCreator;
+        this.userSaver = userSaver;
+    }
 }
