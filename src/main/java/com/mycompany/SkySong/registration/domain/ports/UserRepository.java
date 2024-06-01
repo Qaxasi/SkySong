d84e48a839ff56fc1d344571fda120ca0a54d566
Champel Mariusz <champelmariusz@gmail.com>
2024-06-01 17:04:07 +0200

    feat: Add existsByUsername and existsByEmail methods to UserRepository interface

--- src/main/java/com/mycompany/SkySong/registration/domain/ports/UserRepository.java
+++ src/main/java/com/mycompany/SkySong/registration/domain/ports/UserRepository.java
@@ -2,8 +2,12 @@ package com.mycompany.SkySong.registration.domain.ports;
 
 import com.mycompany.SkySong.registration.domain.model.User;
 import com.mycompany.SkySong.common.dao.UserDAO;
+import org.jdbi.v3.sqlobject.customizer.Bind;
+import org.jdbi.v3.sqlobject.statement.SqlQuery;
 
 public interface UserRepository extends UserDAO {
     int save(User user);
     void assignRoleToUser(int userId, int roleId);
+    boolean existsByUsername(String username);
+    boolean existsByEmail( String email);
 }
