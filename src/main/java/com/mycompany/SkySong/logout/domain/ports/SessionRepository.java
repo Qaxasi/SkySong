1165c918a11f37057b0f87a45153c21569e6062e
Champel Mariusz <champelmariusz@gmail.com>
2024-06-02 15:46:19 +0200

    refactor: Rename interface

--- src/main/java/com/mycompany/SkySong/logout/domain/ports/SessionRepository.java
+++ src/main/java/com/mycompany/SkySong/logout/domain/ports/SessionRepository.java
@@ -1,6 +1,6 @@
 package com.mycompany.SkySong.logout.domain.ports;
 
 import com.mycompany.SkySong.common.dao.SessionDAO;
-public interface SessionRepositoryPort extends SessionDAO {
+public interface SessionRepository extends SessionDAO {
     void deleteById(String sessionId);
 }
