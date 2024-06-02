7454bae6c54f4cf94533d850edafeaf8991e00c5
Champel Mariusz <champelmariusz@gmail.com>
2024-06-02 15:54:26 +0200

    refactor: Refactor LogoutHandler to use SessionRepository and SessionTokenHasher

--- src/main/java/com/mycompany/SkySong/logout/domain/service/LogoutHandler.java
+++ src/main/java/com/mycompany/SkySong/logout/domain/service/LogoutHandler.java
@@ -1,21 +1,21 @@
 package com.mycompany.SkySong.logout.domain.service;
 
-import com.mycompany.SkySong.common.utils.TokenHasher;
-import com.mycompany.SkySong.logout.domain.ports.SessionRepositoryPort;
+import com.mycompany.SkySong.logout.domain.ports.SessionRepository;
+import com.mycompany.SkySong.logout.domain.ports.SessionTokenHasher;
 
 class LogoutHandler {
 
-    private final SessionRepositoryPort sessionRepositoryPort;
-    private final TokenHasher tokenHasher;
-    
-    LogoutHandler(SessionRepositoryPort sessionRepositoryPort,
-                  TokenHasher tokenHasher) {
-        this.sessionRepositoryPort = sessionRepositoryPort;
+    private final SessionRepository sessionRepository;
+    private final SessionTokenHasher tokenHasher;
+
+    LogoutHandler(SessionRepository sessionRepository,
+                  SessionTokenHasher tokenHasher) {
+        this.sessionRepository = sessionRepository;
         this.tokenHasher = tokenHasher;
     }
 
     public void deleteSession(String sessionId) {
         String hashedSessionId = tokenHasher.hashToken(sessionId);
-        sessionRepositoryPort.deleteById(hashedSessionId);
+        sessionRepository.deleteById(hashedSessionId);
     }
 }
