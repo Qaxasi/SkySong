6267793fc6befbb75693a1683e7142a36de1d5c1
Champel Mariusz <champelmariusz@gmail.com>
2024-06-06 21:02:26 +0200

    refactor: Delete unnecessary class

--- src/main/java/com/mycompany/SkySong/logout/domain/ports/SessionTokenHasher.java
+++
@@ -1,7 +0,0 @@
-package com.mycompany.SkySong.logout.domain.ports;
-
-import com.mycompany.SkySong.common.infrastructure.TokenHasher;
-
-public interface SessionTokenHasher extends TokenHasher {
-    String hashToken(String token);
-}
