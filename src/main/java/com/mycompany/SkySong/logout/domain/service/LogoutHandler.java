aa9ba8156ec35ef351b38cf572c48d1686a28760
Champel Mariusz <champelmariusz@gmail.com>
2024-06-06 21:09:40 +0200

    refactor: Remove SessionTokenHasher dependency

--- src/main/java/com/mycompany/SkySong/logout/domain/service/LogoutHandler.java
+++ src/main/java/com/mycompany/SkySong/logout/domain/service/LogoutHandler.java
@@ -1,21 +1,16 @@
 package com.mycompany.SkySong.logout.domain.service;
 
 import com.mycompany.SkySong.logout.domain.ports.SessionRepository;
-import com.mycompany.SkySong.logout.domain.ports.SessionTokenHasher;
 
 class LogoutHandler {
 
     private final SessionRepository sessionRepository;
-    private final SessionTokenHasher tokenHasher;
 
-    LogoutHandler(SessionRepository sessionRepository,
-                  SessionTokenHasher tokenHasher) {
+    LogoutHandler(SessionRepository sessionRepository) {
         this.sessionRepository = sessionRepository;
-        this.tokenHasher = tokenHasher;
     }
 
     public void deleteSession(String sessionId) {
-        String hashedSessionId = tokenHasher.hashToken(sessionId);
-        sessionRepository.deleteById(hashedSessionId);
+        sessionRepository.deleteById(sessionId);
     }
 }
