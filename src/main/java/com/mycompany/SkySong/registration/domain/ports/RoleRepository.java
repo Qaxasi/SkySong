8ae94e8a77a74cccabfb80ec451ac61f51da2db4
Champel Mariusz <champelmariusz@gmail.com>
2024-06-11 21:21:54 +0200

    refactor: Remove RoleDAO extension from RoleRepository

--- src/main/java/com/mycompany/SkySong/registration/domain/ports/RoleRepository.java
+++ src/main/java/com/mycompany/SkySong/registration/domain/ports/RoleRepository.java
@@ -2,10 +2,9 @@ package com.mycompany.SkySong.registration.domain.ports;
 
 import com.mycompany.SkySong.registration.domain.model.Role;
 import com.mycompany.SkySong.registration.domain.model.UserRole;
-import com.mycompany.SkySong.common.dao.RoleDAO;
 
 import java.util.Optional;
 
-public interface RoleRepository extends RoleDAO {
+public interface RoleRepository {
     Optional<Role> findByName(UserRole roleName);
 }
