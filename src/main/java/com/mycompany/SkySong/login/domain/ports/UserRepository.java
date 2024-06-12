b40bfa5869371427fa3355422363da874fdb5f68
Champel Mariusz <champelmariusz@gmail.com>
2024-06-12 21:53:53 +0200

    refactor: Remove UserDAO extension from interface

--- src/main/java/com/mycompany/SkySong/login/domain/ports/UserRepository.java
+++ src/main/java/com/mycompany/SkySong/login/domain/ports/UserRepository.java
@@ -1,10 +1,9 @@
 package com.mycompany.SkySong.login.domain.ports;
 
 import com.mycompany.SkySong.registration.domain.model.User;
-import com.mycompany.SkySong.common.dao.UserDAO;
 
 import java.util.Optional;
 
-public interface UserRepository extends UserDAO {
+public interface UserRepository {
     Optional<User> findByUsername(String username);
 }
