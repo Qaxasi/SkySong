82c9052de0f95d80175ee71d707f1c4860dc825f
Champel Mariusz <champelmariusz@gmail.com>
2024-06-15 21:52:42 +0200

    refactor: Update import path

--- src/main/java/com/mycompany/SkySong/registration/domain/ports/UserSaver.java
+++ src/main/java/com/mycompany/SkySong/registration/domain/ports/UserSaver.java
@@ -1,6 +1,6 @@
 package com.mycompany.SkySong.registration.domain.ports;
 
-import com.mycompany.SkySong.registration.domain.model.User;
+import com.mycompany.SkySong.common.entity.User;
 
 public interface UserSaver {
     void saveUser(User user);
