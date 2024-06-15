7dd8dac7b724daafb74569cd6fddbbd015eb0a02
Champel Mariusz <champelmariusz@gmail.com>
2024-06-15 23:03:02 +0200

    refactor: Rename interface

--- src/main/java/com/mycompany/SkySong/registration/domain/ports/RegistrationRoleRepository.java
+++ src/main/java/com/mycompany/SkySong/registration/domain/ports/RegistrationRoleRepository.java
@@ -1,10 +1,10 @@
 package com.mycompany.SkySong.registration.domain.ports;
 
-import com.mycompany.SkySong.registration.domain.model.Role;
+import com.mycompany.SkySong.common.entity.Role;
 import com.mycompany.SkySong.registration.domain.model.UserRole;
 
 import java.util.Optional;
 
-public interface RoleRepository {
+public interface RegistrationRoleRepository {
     Optional<Role> findByName(UserRole roleName);
 }
