400e38ae8e9d6811d071d16d857efdc323c45c88
Champel Mariusz <champelmariusz@gmail.com>
2024-06-15 23:07:26 +0200

    style: Improve readability

--- src/main/java/com/mycompany/SkySong/registration/domain/ports/RegistrationUserRepository.java
+++ src/main/java/com/mycompany/SkySong/registration/domain/ports/RegistrationUserRepository.java
@@ -2,5 +2,5 @@ package com.mycompany.SkySong.registration.domain.ports;
 
 public interface RegistrationUserRepository {
     boolean existsByUsername(String username);
-    boolean existsByEmail( String email);
+    boolean existsByEmail(String email);
 }
