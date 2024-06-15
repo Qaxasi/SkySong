70757440f91c9d119acb5df4058bc076c0d69d6d
Champel Mariusz <champelmariusz@gmail.com>
2024-06-15 23:01:24 +0200

    refactor: Change findByName method parameter type from UserRole to String

--- src/main/java/com/mycompany/SkySong/common/dao/RoleDAO.java
+++ src/main/java/com/mycompany/SkySong/common/dao/RoleDAO.java
@@ -1,6 +1,5 @@
 package com.mycompany.SkySong.common.dao;
 
-import com.mycompany.SkySong.registration.domain.model.UserRole;
 import com.mycompany.SkySong.common.entity.Role;
 import org.jdbi.v3.sqlobject.config.RegisterBeanMapper;
 import org.jdbi.v3.sqlobject.customizer.Bind;
@@ -15,7 +14,7 @@ public interface RoleDAO {
 
     @SqlQuery("SELECT * FROM roles WHERE name = :name")
     @RegisterBeanMapper(Role.class)
-    Optional<Role> findByName(@Bind("name") UserRole roleName);
+    Optional<Role> findByName(@Bind("name") String roleName);
 
     @SqlQuery("SELECT r.name FROM roles r JOIN user_roles ur ON r.id = ur.role_id WHERE ur.user_id = :userId")
     @RegisterBeanMapper(Role.class)
