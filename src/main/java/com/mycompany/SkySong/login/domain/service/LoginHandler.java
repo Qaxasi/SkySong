07e6fb608b81d91f176cc6277e2c6600a44d0e22
Champel Mariusz <champelmariusz@gmail.com>
2024-06-15 17:01:03 +0200

    feat: Add dependency

--- src/main/java/com/mycompany/SkySong/login/domain/service/LoginHandler.java
+++ src/main/java/com/mycompany/SkySong/login/domain/service/LoginHandler.java
@@ -3,13 +3,16 @@ package com.mycompany.SkySong.login.domain.service;
 import com.mycompany.SkySong.common.exception.UserNotFoundException;
 import com.mycompany.SkySong.login.application.dto.LoginRequest;
 import com.mycompany.SkySong.common.entity.User;
+import com.mycompany.SkySong.login.domain.ports.UserAuthentication;
 
 class LoginHandler {
     private final UserSessionCreator sessionCreator;
+    private final UserAuthentication authentication;
 
-    LoginHandler(UserSessionCreator sessionCreator) {
+    LoginHandler(UserSessionCreator sessionCreator, UserAuthentication authentication) {
         this.sessionCreator = sessionCreator;
-   }
+        this.authentication = authentication;
+    }
 
     public String login(LoginRequest request) {
        String username = userAuth.authenticateUser(request.usernameOrEmail(), request.password());
