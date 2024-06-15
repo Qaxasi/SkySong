4ff84650e72ebbc3c000ea4f7a348b6af697a683
Champel Mariusz <champelmariusz@gmail.com>
2024-06-15 22:45:47 +0200

    refactor: Update import paths

--- src/main/java/com/mycompany/SkySong/registration/adapters/TransactionUserSaver.java
+++ src/main/java/com/mycompany/SkySong/registration/adapters/TransactionUserSaver.java
@@ -1,8 +1,8 @@
 package com.mycompany.SkySong.registration.adapters;
 
 import com.mycompany.SkySong.common.dao.UserDAO;
-import com.mycompany.SkySong.registration.domain.model.Role;
-import com.mycompany.SkySong.registration.domain.model.User;
+import com.mycompany.SkySong.common.entity.Role;
+import com.mycompany.SkySong.common.entity.User;
 import com.mycompany.SkySong.registration.domain.ports.UserSaver;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.support.TransactionTemplate;
