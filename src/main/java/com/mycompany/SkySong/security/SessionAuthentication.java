7e0102a3b128685aceb2128aab372e74a2281a5b
Champel Mariusz <champelmariusz@gmail.com>
2024-06-16 21:07:27 +0200

    feat: Add method to retrieve username from session ID

--- src/main/java/com/mycompany/SkySong/security/SessionAuthentication.java
+++ src/main/java/com/mycompany/SkySong/security/SessionAuthentication.java
@@ -1,9 +1,16 @@
-package com.mycompany.SkySong.application.security;
+package com.mycompany.SkySong.security;
 
+import com.mycompany.SkySong.common.entity.Session;
+import com.mycompany.SkySong.common.entity.User;
+import com.mycompany.SkySong.infrastructure.persistence.dao.SessionDAO;
+import com.mycompany.SkySong.infrastructure.persistence.dao.UserDAO;
+import com.mycompany.SkySong.security.exception.SessionNotFoundException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class SessionAuthentication {
@@ -24,5 +31,14 @@ public class SessionAuthentication {
                 new UsernamePasswordAuthenticationToken(details, null, details.getAuthorities());
 
         SecurityContextHolder.getContext().setAuthentication(authentication);
+    }
+
+    private String getUsernameForSession(String sessionId) {
+        return Optional.of(sessionId)
+                .flatMap(sessionDAO::findById)
+                .map(Session::getUserId)
+                .flatMap(userDAO::findById)
+                .map(User::getUsername)
+                .orElseThrow(() -> new SessionNotFoundException("No user associated with session ID:" + sessionId));
     }
 }
