91d281d22e7f4e346908c878f76609342c973540
Champel Mariusz <champelmariusz@gmail.com>
2024-06-16 15:57:13 +0200

    refactor: Move class to appropriate package

--- src/main/java/com/mycompany/SkySong/login/domain/exception/UserNotFoundException.java
+++ src/main/java/com/mycompany/SkySong/login/domain/exception/UserNotFoundException.java
@@ -1,4 +1,6 @@
-package com.mycompany.SkySong.common.exception;
+package com.mycompany.SkySong.login.domain.exception;
+
+import com.mycompany.SkySong.common.exception.ApplicationRuntimeException;
 
 public class UserNotFoundException extends ApplicationRuntimeException {
     public UserNotFoundException(String message) {
