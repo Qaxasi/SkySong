3489b85fc8a0fef1b2a38a54cef344b7c1007d45
Champel Mariusz <champelmariusz@gmail.com>
2024-06-16 17:49:19 +0200

    feat: Extend SessionDAO with LoginSessionRepository and LogoutSessionRepository

--- src/main/java/com/mycompany/SkySong/common/dao/SessionDAO.java
+++ src/main/java/com/mycompany/SkySong/common/dao/SessionDAO.java
@@ -1,6 +1,8 @@
 package com.mycompany.SkySong.common.dao;
 
 import com.mycompany.SkySong.common.entity.Session;
+import com.mycompany.SkySong.login.domain.ports.LoginSessionRepository;
+import com.mycompany.SkySong.logout.domain.ports.LogoutSessionRepository;
 import org.jdbi.v3.sqlobject.config.RegisterBeanMapper;
 import org.jdbi.v3.sqlobject.customizer.Bind;
 import org.jdbi.v3.sqlobject.customizer.BindBean;
@@ -9,7 +11,7 @@ import org.jdbi.v3.sqlobject.statement.SqlUpdate;
 
 import java.util.Optional;
 
-public interface SessionDAO {
+public interface SessionDAO extends LoginSessionRepository, LogoutSessionRepository {
 
     @SqlUpdate("INSERT INTO sessions (session_id, user_id, create_at, expires_at) VALUES (:sessionId, :userId, :createAt, :expiresAt)")
     void save(@BindBean Session session);
