c8e0a87110ab2da7e2ae92f2e316101ad45e0ff8
Champel Mariusz <champelmariusz@gmail.com>
2024-06-16 13:53:02 +0200

    refactor: Rename interface

--- src/main/java/com/mycompany/SkySong/login/domain/ports/LoginUserRepository.java
+++ src/main/java/com/mycompany/SkySong/login/domain/ports/LoginUserRepository.java
@@ -1,9 +1,9 @@
 package com.mycompany.SkySong.login.domain.ports;
 
-import com.mycompany.SkySong.registration.domain.model.User;
+import com.mycompany.SkySong.common.entity.User;
 
 import java.util.Optional;
 
-public interface UserRepository {
+public interface LoginUserRepository {
     Optional<User> findByUsername(String username);
 }
