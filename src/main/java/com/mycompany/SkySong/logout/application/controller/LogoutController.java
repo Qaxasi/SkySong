8d66bb066a6b5dd44a2a6f65a491c79239af4753
Champel Mariusz <champelmariusz@gmail.com>
2024-06-16 13:32:20 +0200

    style: Improve readability

--- src/main/java/com/mycompany/SkySong/logout/application/controller/LogoutController.java
+++ src/main/java/com/mycompany/SkySong/logout/application/controller/LogoutController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class LogoutController {
 
     private final LogoutSessionRepository sessionRepository;
+    
     public LogoutController(LogoutSessionRepository sessionRepository) {
         this.sessionRepository = sessionRepository;
     }
