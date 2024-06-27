42912cc904b5c38fc5e0cc0c9801125dfc18cca4
Champel Mariusz <champelmariusz@gmail.com>
2024-06-27 21:56:32 +0200

    refactor: Change DeleteUserHandler class to public

--- src/main/java/com/mycompany/SkySong/user/delete/domain/service/DeleteUserHandler.java
+++ src/main/java/com/mycompany/SkySong/user/delete/domain/service/DeleteUserHandler.java
@@ -3,7 +3,7 @@ package com.mycompany.SkySong.user.delete.domain.service;
 import com.mycompany.SkySong.common.dto.ApiResponse;
 import com.mycompany.SkySong.user.delete.domain.ports.DeleteUser;
 
-class DeleteUserHandler {
+public class DeleteUserHandler {
 
     private final DeleteUser deleteUser;
 
