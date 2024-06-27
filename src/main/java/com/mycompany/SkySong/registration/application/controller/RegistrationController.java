38ea97a4e5da3c59757fadc70c61fd2850f4348e
Champel Mariusz <champelmariusz@gmail.com>
2024-06-27 20:59:43 +0200

    refactor: Update RegistrationController to use UserRegistration dependency

--- src/main/java/com/mycompany/SkySong/registration/application/controller/RegistrationController.java
+++ src/main/java/com/mycompany/SkySong/registration/application/controller/RegistrationController.java
@@ -1,7 +1,7 @@
 package com.mycompany.SkySong.registration.application.controller;
 
 import com.mycompany.SkySong.common.dto.ApiResponse;
-import com.mycompany.SkySong.registration.domain.service.UserRegistrationFacade;
+import com.mycompany.SkySong.registration.domain.service.UserRegistration;
 import com.mycompany.SkySong.registration.dto.RegisterRequest;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
@@ -12,15 +12,15 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/v1/users")
 public class RegistrationController {
 
-    private final UserRegistrationFacade registration;
+    private final UserRegistration registration;
 
-    public RegistrationController(UserRegistrationFacade registration) {
+    public RegistrationController(UserRegistration registration) {
         this.registration = registration;
     }
 
     @PostMapping("/register")
     public ResponseEntity<ApiResponse> register(@Valid @RequestBody RegisterRequest request) {
-        ApiResponse registrationResponse = registration.register(request);
+        ApiResponse registrationResponse = registration.registerUser(request);
         return new ResponseEntity<>(registrationResponse, HttpStatus.CREATED);
     }
 }
