bd593428a66b43a9d8852a4507338a50f539fbf9
Champel Mariusz <champelmariusz@gmail.com>
2024-06-27 21:28:02 +0200

    feat: Add UserRegistrationFactory class for creating UserRegistration instances

---
+++ src/main/java/com/mycompany/SkySong/registration/domain/service/UserRegistrationFactory.java
@@ -0,0 +1,19 @@
+package com.mycompany.SkySong.registration.domain.service;
+
+import com.mycompany.SkySong.registration.domain.ports.PasswordEncoder;
+import com.mycompany.SkySong.registration.domain.ports.RegistrationRoleRepository;
+import com.mycompany.SkySong.registration.domain.ports.RegistrationUserRepository;
+import com.mycompany.SkySong.registration.domain.ports.UserSaver;
+
+
+public class UserRegistrationSetup {
+
+    public UserRegistration createUserRegistration(RegistrationUserRepository userRepository,
+                                                          RegistrationRoleRepository roleRepository,
+                                                          PasswordEncoder encoder,
+                                                          UserSaver userSaver)  {
+        RequestValidation validation = new RequestValidation(userRepository);
+        UserCreator userCreator = new UserCreator(encoder, roleRepository);
+        return new UserRegistration(validation, userCreator, userSaver);
+    }
+}
