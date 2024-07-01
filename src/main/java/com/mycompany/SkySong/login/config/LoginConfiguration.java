8044db8eb8de588c716a0b6220ebc69a9444a7f0
Champel Mariusz <champelmariusz@gmail.com>
2024-07-01 19:15:30 +0200

    refactor: Use LoginHandlerFactory to refactor loginHandler bean creation

--- src/main/java/com/mycompany/SkySong/login/config/LoginConfiguration.java
+++ src/main/java/com/mycompany/SkySong/login/config/LoginConfiguration.java
@@ -1,20 +1,20 @@
-package com.mycompany.SkySong.login.domain.service;
+package com.mycompany.SkySong.login.config;
 
 import com.mycompany.SkySong.login.domain.ports.LoginSessionRepository;
 import com.mycompany.SkySong.login.domain.ports.LoginUserRepository;
 import com.mycompany.SkySong.login.domain.ports.UserAuthentication;
+import com.mycompany.SkySong.login.domain.service.LoginHandler;
+import com.mycompany.SkySong.login.domain.service.LoginHandlerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 class LoginConfiguration {
-
     @Bean
-    LoginHandler loginHandler(UserAuthentication auth,
-                              LoginSessionRepository sessionRepository,
+    LoginHandler loginHandler(LoginSessionRepository sessionRepository,
+                              UserAuthentication userAuth,
                               LoginUserRepository userRepository) {
-        SessionTokenGenerator tokenGenerator = new SessionTokenGenerator();
-        UserSessionCreator sessionCreator = new UserSessionCreator(sessionRepository, tokenGenerator);
-        return new LoginHandler(sessionCreator, auth, userRepository);
+        LoginHandlerFactory factory = new LoginHandlerFactory();
+        return factory.createLoginHandler(sessionRepository, userAuth, userRepository);
     }
 }
