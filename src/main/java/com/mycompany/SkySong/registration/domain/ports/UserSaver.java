b996b04c8df4d784af96cc72f08250465af6b342
Champel Mariusz <champelmariusz@gmail.com>
2024-07-03 20:55:04 +0200

    refactor: Change method parameter

--- src/main/java/com/mycompany/SkySong/registration/domain/ports/UserSaver.java
+++ src/main/java/com/mycompany/SkySong/registration/domain/ports/UserSaver.java
@@ -1,7 +1,7 @@
 package com.mycompany.SkySong.registration.domain.ports;
 
-import com.mycompany.SkySong.common.entity.User;
+import com.mycompany.SkySong.registration.dto.UserRegistrationDTO;
 
 public interface UserSaver {
-    void saveUser(User user);
+    void saveUser(UserRegistrationDTO user);
 }
