5c55c9833a00172bcdfa34a2ce5e54f1db64af92
Champel Mariusz <champelmariusz@gmail.com>
2024-07-03 21:45:03 +0200

    feat: Add UserRegistrationMapper dependency and update saveUser method

--- src/main/java/com/mycompany/SkySong/registration/adapters/TransactionUserSaver.java
+++ src/main/java/com/mycompany/SkySong/registration/adapters/TransactionUserSaver.java
@@ -1,9 +1,11 @@
 package com.mycompany.SkySong.registration.adapters;
 
+import com.mycompany.SkySong.common.entity.User;
 import com.mycompany.SkySong.infrastructure.persistence.dao.UserDAO;
 import com.mycompany.SkySong.common.entity.Role;
-import com.mycompany.SkySong.common.entity.User;
 import com.mycompany.SkySong.registration.domain.ports.UserSaver;
+import com.mycompany.SkySong.registration.dto.UserRegistrationDTO;
+import com.mycompany.SkySong.registration.xyz.UserRegistrationMapper;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.support.TransactionTemplate;
 
@@ -12,16 +14,20 @@ class TransactionUserSaver implements UserSaver {
 
     private final UserDAO userDAO;
     private final TransactionTemplate transactionTemplate;
+    private final UserRegistrationMapper userMapper;
 
     public TransactionUserSaver(UserDAO userDAO,
-                                TransactionTemplate transactionTemplate) {
+                                TransactionTemplate transactionTemplate,
+                                UserRegistrationMapper userMapper) {
         this.userDAO = userDAO;
         this.transactionTemplate = transactionTemplate;
+        this.userMapper = userMapper;
     }
 
     @Override
-    public void saveUser(User user) {
+    public void saveUser(UserRegistrationDTO userDto) {
         transactionTemplate.executeWithoutResult(status -> {
+            User user = userMapper.toEntity(userDto);
             int userId = userDAO.save(user);
 
             for (Role roles : user.getRoles()) {
