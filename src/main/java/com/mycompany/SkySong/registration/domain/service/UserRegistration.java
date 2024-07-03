43a0ad5566c84814b51b86c01539d73ba5c436f7
Champel Mariusz <champelmariusz@gmail.com>
2024-07-03 21:36:43 +0200

    feat: Add UserRegistrationMapper dependency and update registerUser method

--- src/main/java/com/mycompany/SkySong/registration/domain/service/UserRegistration.java
+++ src/main/java/com/mycompany/SkySong/registration/domain/service/UserRegistration.java
@@ -1,6 +1,7 @@
 package com.mycompany.SkySong.registration.domain.service;
 
 import com.mycompany.SkySong.common.dto.ApiResponse;
+import com.mycompany.SkySong.registration.xyz.UserRegistrationMapper;
 import com.mycompany.SkySong.registration.dto.RegisterRequest;
 import com.mycompany.SkySong.common.entity.User;
 import com.mycompany.SkySong.registration.domain.ports.UserSaver;
@@ -9,18 +10,23 @@ public class UserRegistration {
 
     private final RequestValidation validation;
     private final UserCreator userCreator;
+    private final UserRegistrationMapper userMapper;
     private final UserSaver userSaver;
 
-    UserRegistration(RequestValidation validation, UserCreator userCreator, UserSaver userSaver) {
+    UserRegistration(RequestValidation validation,
+                     UserCreator userCreator,
+                     UserRegistrationMapper userRegistrationMapper,
+                     UserSaver userSaver) {
         this.validation = validation;
         this.userCreator = userCreator;
+        this.userMapper = userRegistrationMapper;
         this.userSaver = userSaver;
     }
 
     public ApiResponse registerUser(RegisterRequest request) {
         validation.validate(request);
         User user = userCreator.createUser(request);
-        userSaver.saveUser(user);
+        userSaver.saveUser(userMapper.toDto(user));
         return new ApiResponse("User registered successfully.");
     }
 }
