eb0eda93ef2441e9dad1aaf89376fb24ee910870
Champel Mariusz <champelmariusz@gmail.com>
2024-07-04 22:15:51 +0200

    refactor: Remove roles mapping from toEntity method in UserMapper

--- src/main/java/com/mycompany/SkySong/common/mapper/UserMapper.java
+++ src/main/java/com/mycompany/SkySong/common/mapper/UserMapper.java
@@ -2,18 +2,16 @@ package com.mycompany.SkySong.common.mapper;
 
 import com.mycompany.SkySong.common.entity.User;
 import com.mycompany.SkySong.registration.dto.UserDTO;
+import com.mycompany.SkySong.registration.mapper.RoleMapper;
 
 import java.util.stream.Collectors;
 
 public class UserMapper {
     public User toEntity(UserDTO userDTO) {
         User user = new User();
-        user.setId(user.getId());
-        user.setUsername(user.getUsername());
-        user.setEmail(user.getEmail());
-        user.setRoles(userDTO.roles().stream()
-                .map(roleMapper::toEntity)
-                .collect(Collectors.toSet()));
+        user.setId(userDTO.id());
+        user.setUsername(userDTO.username());
+        user.setEmail(userDTO.email());
         return user;
     }
     public UserDTO toDto(User user) {
