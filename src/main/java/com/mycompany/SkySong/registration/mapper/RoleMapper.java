6e8b3edbcb46fac9d79d82634116045424bb5582
Champel Mariusz <champelmariusz@gmail.com>
2024-07-04 22:13:33 +0200

    refactor: Move class to appropriate package

--- src/main/java/com/mycompany/SkySong/registration/mapper/RoleMapper.java
+++ src/main/java/com/mycompany/SkySong/registration/mapper/RoleMapper.java
@@ -1,15 +1,17 @@
-package com.mycompany.SkySong.common.mapper;
+package com.mycompany.SkySong.registration.mapper;
 
 import com.mycompany.SkySong.common.entity.Role;
 import com.mycompany.SkySong.registration.dto.RoleDTO;
 
 public class RoleMapper {
+
     public Role toEntity(RoleDTO roleDTO) {
         Role role = new Role();
         role.setId(roleDTO.id());
         role.setName(roleDTO.name());
         return role;
     }
+    
     public RoleDTO toDto(Role role) {
         return new RoleDTO(role.getId(), role.getName());
     }
