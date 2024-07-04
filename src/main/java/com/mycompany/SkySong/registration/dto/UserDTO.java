1ed6321da6e1b51fbece20b29de0c909b6a2d481
Champel Mariusz <champelmariusz@gmail.com>
2024-07-04 22:12:48 +0200

    refactor: Remove roles field from UserDTO

--- src/main/java/com/mycompany/SkySong/registration/dto/UserDTO.java
+++ src/main/java/com/mycompany/SkySong/registration/dto/UserDTO.java
@@ -1,5 +1,3 @@
 package com.mycompany.SkySong.registration.dto;
 
-import java.util.Set;
-
-public record UserDTO(Integer id, String username, String email, Set<RoleDTO> roles) {}
+public record UserDTO(Integer id, String username, String email) {}
