b843257953fc9172e5b612564ba0332d6e8af9a3
Champel Mariusz <champelmariusz@gmail.com>
2024-07-06 17:38:53 +0200

    refactor: Add UserRegistrationMapper as a parameter to RegistrationConfiguration

--- src/main/java/com/mycompany/SkySong/registration/config/RegistrationConfiguration.java
+++ src/main/java/com/mycompany/SkySong/registration/config/RegistrationConfiguration.java
@@ -1,11 +1,13 @@
 package com.mycompany.SkySong.registration.config;
 
+import com.mycompany.SkySong.registration.mapper.RoleMapper;
 import com.mycompany.SkySong.registration.domain.ports.PasswordEncoder;
 import com.mycompany.SkySong.registration.domain.ports.RegistrationRoleRepository;
 import com.mycompany.SkySong.registration.domain.ports.RegistrationUserRepository;
 import com.mycompany.SkySong.registration.domain.ports.UserSaver;
 import com.mycompany.SkySong.registration.domain.service.UserRegistration;
 import com.mycompany.SkySong.registration.domain.service.UserRegistrationFactory;
+import com.mycompany.SkySong.registration.mapper.UserRegistrationMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -15,8 +17,9 @@ class RegistrationConfiguration {
     UserRegistration registration(RegistrationUserRepository userRepository,
                                   RegistrationRoleRepository roleRepository,
                                   PasswordEncoder encoder,
+                                  UserRegistrationMapper userMapper,
                                   UserSaver userSaver) {
         UserRegistrationFactory registrationFactory = new UserRegistrationFactory();
-        return registrationFactory.createUserRegistration(userRepository, roleRepository, encoder, userSaver);
+        return registrationFactory.createUserRegistration(userRepository, roleRepository, encoder, userMapper, userSaver);
     }
 }
