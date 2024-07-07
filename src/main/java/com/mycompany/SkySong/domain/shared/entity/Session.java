fbbf150e3de72f29e469a9ec55df23ee9fe729a4
Champel Mariusz <champelmariusz@gmail.com>
2024-07-07 21:06:09 +0200

    refactor: Remove Lombok annotations and add explicit methods in Session entity

--- src/main/java/com/mycompany/SkySong/domain/shared/entity/Session.java
+++ src/main/java/com/mycompany/SkySong/domain/shared/entity/Session.java
@@ -1,20 +1,52 @@
-package com.mycompany.SkySong.infrastructure.entity;
-
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+package com.mycompany.SkySong.domain.shared.entity;
 
 import java.util.Date;
 
-@Setter
-@Getter
-@NoArgsConstructor
-@AllArgsConstructor
 public class Session {
 
     private String sessionId;
     private Integer userId;
     private Date createAt;
     private Date expiresAt;
+
+    public Session() {}
+
+    public Session(String sessionId, Integer userId, Date createAt, Date expiresAt) {
+        this.sessionId = sessionId;
+        this.userId = userId;
+        this.createAt = createAt;
+        this.expiresAt = expiresAt;
+    }
+
+    public String getSessionId() {
+        return sessionId;
+    }
+
+    public void setSessionId(String sessionId) {
+        this.sessionId = sessionId;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public Date getCreateAt() {
+        return createAt;
+    }
+
+    public void setCreateAt(Date createAt) {
+        this.createAt = createAt;
+    }
+
+    public Date getExpiresAt() {
+        return expiresAt;
+    }
+
+    public void setExpiresAt(Date expiresAt) {
+        this.expiresAt = expiresAt;
+    }
 }
