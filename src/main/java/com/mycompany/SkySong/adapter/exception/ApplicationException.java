ee0272f8e9c09b75075d3aeb9bafa6cc700f8e9f
Champel Mariusz <champelmariusz@gmail.com>
2024-07-17 17:42:58 +0200

    refactor: Delete unnecessary classes

--- src/main/java/com/mycompany/SkySong/adapter/exception/ApplicationException.java
+++
@@ -1,7 +0,0 @@
-package com.mycompany.SkySong.common.exception;
-
-public class ApplicationException extends Exception {
-    public ApplicationException(String message) {
-        super(message);
-    }
-}
