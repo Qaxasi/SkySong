cf0862f57548087fc6842c89b089fac83f835450
Champel Mariusz <champelmariusz@gmail.com>
2024-07-19 16:39:17 +0200

    feat: Add LoginConfiguration with LoginHandler and UserSessionCreator beans

--- src/main/java/com/mycompany/SkySong/infrastructure/config/login/LoginConfiguration.java
+++ src/main/java/com/mycompany/SkySong/infrastructure/config/login/LoginConfiguration.java
@@ -1,20 +1,26 @@
-//package com.mycompany.SkySong.infrastructure.config.login;
-//
-//import com.mycompany.SkySong.domain.login.ports.LoginSessionRepository;
-//import com.mycompany.SkySong.domain.login.ports.LoginUserRepository;
-//import com.mycompany.SkySong.domain.login.ports.UserAuthentication;
-//import com.mycompany.SkySong.application.login.usecase.LoginHandler;
-//import com.mycompany.SkySong.domain.login.service.LoginHandlerFactory;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//
-//@Configuration
-//class LoginConfiguration {
-//    @Bean
-//    LoginHandler loginHandler(LoginSessionRepository sessionRepository,
-//                              UserAuthentication userAuth,
-//                              LoginUserRepository userRepository) {
-//        LoginHandlerFactory factory = new LoginHandlerFactory();
-//        return factory.createLoginHandler(sessionRepository, userAuth, userRepository);
-//    }
-//}
+package com.mycompany.SkySong.infrastructure.config.login;
+
+import com.mycompany.SkySong.application.login.usecase.LoginHandler;
+import com.mycompany.SkySong.domain.login.ports.LoginSessionRepository;
+import com.mycompany.SkySong.domain.login.ports.LoginUserRepository;
+import com.mycompany.SkySong.domain.login.ports.UserAuthentication;
+import com.mycompany.SkySong.domain.login.service.UserSessionCreator;
+import com.mycompany.SkySong.domain.login.service.UserSessionCreatorFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class LoginConfiguration {
+
+    @Bean
+    public LoginHandler loginHandler(UserAuthentication auth,
+                              UserSessionCreator sessionCreator,
+                              LoginUserRepository userRepository) {
+        return new LoginHandler(sessionCreator, auth, userRepository);
+    }
+    @Bean
+    public UserSessionCreator sessionCreator(LoginSessionRepository sessionRepository) {
+        UserSessionCreatorFactory factory = new UserSessionCreatorFactory();
+        return factory.createUserSessionCreator(sessionRepository);
+    }
+}
