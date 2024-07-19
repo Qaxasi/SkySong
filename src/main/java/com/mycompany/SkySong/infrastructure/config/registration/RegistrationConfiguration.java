eb34dc0e2b94830318c48bd24e31692d4d5b7793
Champel Mariusz <champelmariusz@gmail.com>
2024-07-19 16:42:37 +0200

    refactor: Change RegistrationConfiguration class to public

--- src/main/java/com/mycompany/SkySong/infrastructure/config/registration/RegistrationConfiguration.java
+++ src/main/java/com/mycompany/SkySong/infrastructure/config/registration/RegistrationConfiguration.java
@@ -1,18 +1,19 @@
-package com.mycompany.SkySong.infrastructure.registration.config;
+package com.mycompany.SkySong.infrastructure.config.registration;
 
-import com.mycompany.SkySong.common.mapper.RoleMapper;
+import com.mycompany.SkySong.application.registration.mapper.RoleMapper;
+import com.mycompany.SkySong.application.registration.mapper.UserRegistrationMapper;
+import com.mycompany.SkySong.application.registration.usecase.UserRegistrationHandler;
 import com.mycompany.SkySong.domain.registration.ports.PasswordEncoder;
 import com.mycompany.SkySong.domain.registration.ports.RegistrationRoleRepository;
 import com.mycompany.SkySong.domain.registration.ports.RegistrationUserRepository;
 import com.mycompany.SkySong.domain.registration.ports.UserSaver;
-import com.mycompany.SkySong.domain.registration.service.UserRegistration;
-import com.mycompany.SkySong.domain.registration.service.UserRegistrationFactory;
-import com.mycompany.SkySong.common.mapper.UserRegistrationMapper;
+import com.mycompany.SkySong.domain.registration.service.RequestValidation;
+import com.mycompany.SkySong.domain.registration.service.UserCreator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-class RegistrationConfiguration {
+public class RegistrationConfiguration {
 
     @Bean
     public RoleMapper roleMapper() {
