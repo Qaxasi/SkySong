d13b8281816d6b5b137b7d0ebc67f1c4bd95a8d6
Champel Mariusz <champelmariusz@gmail.com>
2024-07-21 14:42:05 +0200

    refactor: Update error message

--- src/main/java/com/mycompany/SkySong/adapter/login/adapter/SpringAuthentication.java
+++ src/main/java/com/mycompany/SkySong/adapter/login/adapter/SpringAuthentication.java
@@ -24,7 +24,7 @@ class SpringAuthentication implements UserAuthentication {
             SecurityContextHolder.getContext().setAuthentication(auth);
             return auth.getName();
         } catch (BadCredentialsException e) {
-            throw new BadCredentialsException("    Login failed due to invalid credentials.");
+            throw new BadCredentialsException("Login failed due to invalid credentials.");
         }
     }
 }
