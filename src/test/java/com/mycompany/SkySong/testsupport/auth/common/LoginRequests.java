c291d215676abb28f661ea01072987f06087d314
Champel Mariusz <champelmariusz@gmail.com>
2024-07-22 14:10:29 +0200

    refactor: Rename LoginRequest instance

--- src/test/java/com/mycompany/SkySong/testsupport/auth/common/LoginRequests.java
+++ src/test/java/com/mycompany/SkySong/testsupport/auth/common/LoginRequests.java
@@ -1,6 +1,6 @@
 package com.mycompany.SkySong.testsupport.auth.common;
 
-import com.mycompany.SkySong.login.LoginRequest;
+import com.mycompany.SkySong.application.login.dto.LoginRequest;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -10,8 +10,8 @@ public class LoginRequests {
             new LoginRequest("New", "Password#3");
     public LoginRequest validCredentials =
             new LoginRequest("User", "Password#3");
-    public LoginRequest invalidPassword =
-            new LoginRequest("User", "invalid");
+    public LoginRequest nonExistingUser =
+            new LoginRequest("Invalid", "invalid");
     public LoginRequest invalidUsername =
             new LoginRequest("invalid", "Password#3");
     public LoginRequest invalidEmail =
