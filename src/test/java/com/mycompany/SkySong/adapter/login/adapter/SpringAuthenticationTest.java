7034a6f4a98263d362358548fef9e9aacc98914c
Champel Mariusz <champelmariusz@gmail.com>
2024-07-22 22:07:25 +0200

    test: Inject dependencies

--- src/test/java/com/mycompany/SkySong/adapter/login/adapter/SpringAuthenticationTest.java
+++ src/test/java/com/mycompany/SkySong/adapter/login/adapter/SpringAuthenticationTest.java
@@ -1,7 +1,25 @@
 package com.mycompany.SkySong.adapter.login.adapter;
 
+import com.mycompany.SkySong.testsupport.auth.common.UserFixture;
 import com.mycompany.SkySong.testsupport.common.BaseIT;
+import com.mycompany.SkySong.testsupport.common.SqlDatabaseCleaner;
+import com.mycompany.SkySong.testsupport.common.SqlDatabaseInitializer;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
 
 
 class SpringAuthenticationTest extends BaseIT {
+
+
+    @Autowired
+    private SpringAuthentication authentication;
+    @Autowired
+    private UserFixture userFixture;
+
+    @Autowired
+    private SqlDatabaseInitializer initializer;
+    @Autowired
+    private SqlDatabaseCleaner cleaner;
+    
 }
