b589f7084d8d643b1349390f491e693630f23823
Champel Mariusz <champelmariusz@gmail.com>
2024-07-24 21:14:43 +0200

    test: Add SessionTokenGenerator field and initialize in setUp method

--- src/test/java/com/mycompany/SkySong/domain/login/service/SessionTokenGeneratorTest.java
+++ src/test/java/com/mycompany/SkySong/domain/login/service/SessionTokenGeneratorTest.java
@@ -1,4 +1,16 @@
 package com.mycompany.SkySong.domain.login.service;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 class SessionTokenGeneratorTest {
+
+    private SessionTokenGenerator tokenGenerator;
+
+    @BeforeEach
+    void setUp() {
+        tokenGenerator = new SessionTokenGenerator();
+    }
 }
