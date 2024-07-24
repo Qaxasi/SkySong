7e4c9137eef5dd15787d14413663abe9cc1a5f98
Champel Mariusz <champelmariusz@gmail.com>
2024-07-24 21:49:41 +0200

    test: Update tests to use utility method for generating tokens

--- src/test/java/com/mycompany/SkySong/domain/login/service/SessionTokenGeneratorTest.java
+++ src/test/java/com/mycompany/SkySong/domain/login/service/SessionTokenGeneratorTest.java
@@ -16,21 +16,21 @@ class SessionTokenGeneratorTest {
 
     @Test
     void whenGeneratingToken_ReturnsNonEmptyString() {
-        String token = tokenGenerator.generateToken();
+        String token = generateToken();
         assertThat(token).isNotNull();
     }
 
     @Test
     void whenGeneratingMultipleTokens_ReturnsUniqueTokens() {
-        String firstToken = tokenGenerator.generateToken();
-        String secondToken = tokenGenerator.generateToken();
+        String firstToken = generateToken();
+        String secondToken = generateToken();
 
         assertThat(firstToken).isNotEqualTo(secondToken);
     }
 
     @Test
     void whenGeneratingToken_TokenHaveExpectedLength() {
-        String token = tokenGenerator.generateToken();
+        String token = generateToken();
 
         assertThat(token.length()).isSameAs(32);
     }
