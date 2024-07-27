a09d980f8b2bc3c572961e9fab299121ee9202d4
Champel Mariusz <champelmariusz@gmail.com>
2024-07-27 16:16:04 +0200

    test: Create InMemoryRoleDAO class and implement RoleDAO

---
+++ src/test/java/com/mycompany/SkySong/infrastructure/dao/InMemoryRoleDAO.java
@@ -0,0 +1,20 @@
+package com.mycompany.SkySong.infrastructure.dao;
+
+import com.mycompany.SkySong.domain.shared.entity.Role;
+import com.mycompany.SkySong.domain.shared.enums.UserRole;
+import com.mycompany.SkySong.infrastructure.persistence.dao.RoleDAO;
+
+import java.util.Optional;
+import java.util.Set;
+
+public class InMemoryRoleDAO implements RoleDAO {
+    @Override
+    public Optional<Role> findByName(UserRole roleName) {
+        return null;
+    }
+
+    @Override
+    public Set<Role> findRolesByUserId(Integer userId) {
+        return null;
+    }
+}
