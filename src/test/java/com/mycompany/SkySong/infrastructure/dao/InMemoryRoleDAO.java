bbedbed63b35852ba7275b1c9cb2126bab33bd42
Champel Mariusz <champelmariusz@gmail.com>
2024-07-27 16:26:14 +0200

    test: Add roles map to InMemoryRoleDAO

--- src/test/java/com/mycompany/SkySong/infrastructure/dao/InMemoryRoleDAO.java
+++ src/test/java/com/mycompany/SkySong/infrastructure/dao/InMemoryRoleDAO.java
@@ -4,10 +4,15 @@ import com.mycompany.SkySong.domain.shared.entity.Role;
 import com.mycompany.SkySong.domain.shared.enums.UserRole;
 import com.mycompany.SkySong.infrastructure.persistence.dao.RoleDAO;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
 
 public class InMemoryRoleDAO implements RoleDAO {
+
+    private final Map<Long, Role> roles = new HashMap<>();
+
     @Override
     public Optional<Role> findByName(UserRole roleName) {
         return null;
