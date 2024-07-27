a604b256e4934b508ee1f67701ae8736ae013636
Champel Mariusz <champelmariusz@gmail.com>
2024-07-27 16:15:26 +0200

    test: Create InMemoryUserDAO class and implement UserDAO

---
+++ src/test/java/com/mycompany/SkySong/infrastructure/dao/InMemoryUserDAO.java
@@ -0,0 +1,54 @@
+package com.mycompany.SkySong.infrastructure.dao;
+
+import com.mycompany.SkySong.domain.shared.entity.User;
+import com.mycompany.SkySong.infrastructure.persistence.dao.UserDAO;
+
+import java.util.Optional;
+
+public class InMemoryUserDAO implements UserDAO {
+
+    @Override
+    public int save(User user) {
+        return 0;
+    }
+
+    @Override
+    public Optional<User> findById(int id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<User> findByEmail(String email) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<User> findByUsername(String username) {
+        return Optional.empty();
+    }
+
+    @Override
+    public boolean existsByUsername(String username) {
+        return false;
+    }
+
+    @Override
+    public boolean existsByEmail(String email) {
+        return false;
+    }
+
+    @Override
+    public void delete(User user) {
+
+    }
+
+    @Override
+    public void deleteUserRoles(int userId) {
+
+    }
+
+    @Override
+    public void assignRoleToUser(int userId, int roleId) {
+
+    }
+}
