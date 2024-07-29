7904b27115e1b369ae033cfc72cf01261044bf8d
Champel Mariusz <champelmariusz@gmail.com>
2024-07-29 20:31:46 +0200

    test: Add InMemorySessionDAO implementing SessionDAO

---
+++ src/test/java/com/mycompany/SkySong/infrastructure/dao/InMemorySessionDAO.java
@@ -0,0 +1,26 @@
+package com.mycompany.SkySong.infrastructure.dao;
+
+import com.mycompany.SkySong.domain.shared.entity.Session;
+import com.mycompany.SkySong.infrastructure.persistence.dao.SessionDAO;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+public class InMemorySessionDAO implements SessionDAO {
+    @Override
+    public void save(Session session) {
+    }
+
+    @Override
+    public Optional<Session> findById(String sessionId) {
+    }
+
+    @Override
+    public void deleteById(String sessionId) {
+    }
+
+    @Override
+    public void deleteUserSessions(int userId) {
+    }
+}
