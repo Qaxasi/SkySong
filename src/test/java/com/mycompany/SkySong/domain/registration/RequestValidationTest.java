4c033537a98776ab50553a62f3e37139eebd88e4
Champel Mariusz <champelmariusz@gmail.com>
2024-07-30 21:22:30 +0200

    test: Add dependencies

--- src/test/java/com/mycompany/SkySong/domain/registration/RequestValidationTest.java
+++ src/test/java/com/mycompany/SkySong/domain/registration/RequestValidationTest.java
@@ -1,4 +1,16 @@
 package com.mycompany.SkySong.domain.registration;
 
+import com.mycompany.SkySong.domain.registration.service.RequestValidation;
+import com.mycompany.SkySong.infrastructure.dao.InMemoryRoleDAO;
+import com.mycompany.SkySong.infrastructure.dao.InMemoryUserDAO;
+import com.mycompany.SkySong.testsupport.auth.common.RegistrationRequests;
+import com.mycompany.SkySong.testsupport.auth.common.UserFixture;
+
 class RequestValidationTest {
+
+    private RegistrationRequests requests;
+    private InMemoryUserDAO userDAO;
+    private InMemoryRoleDAO roleDAO;
+    private UserFixture userFixture;
+    private RequestValidation validation;
 }
