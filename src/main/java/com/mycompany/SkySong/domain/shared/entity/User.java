d895678dc4f3805b408580a2409dacf3323bf5e2
Champel Mariusz <champelmariusz@gmail.com>
2024-08-02 22:10:11 +0200

    feat: Implement Builder pattern in User class

--- src/main/java/com/mycompany/SkySong/domain/shared/entity/User.java
+++ src/main/java/com/mycompany/SkySong/domain/shared/entity/User.java
@@ -1,5 +1,10 @@
 package com.mycompany.SkySong.domain.shared.entity;
 
+import com.mycompany.SkySong.domain.registration.exception.RoleNotFoundException;
+import com.mycompany.SkySong.domain.registration.ports.PasswordEncoder;
+import com.mycompany.SkySong.domain.shared.enums.UserRole;
+import com.mycompany.SkySong.infrastructure.persistence.dao.RoleDAO;
+
 import java.util.HashSet;
 import java.util.Set;
 
@@ -26,6 +31,53 @@ public class User {
         this.email = email;
         this.password = password;
         this.roles = roles;
+    }
+
+    public static class Builder {
+       private final RoleDAO roleDAO;
+       private final PasswordEncoder passwordEncoder;
+
+        private Integer id;
+        private String username;
+        private String email;
+        private String password;
+        private Set<Role> roles = new HashSet<>();
+
+       public Builder(RoleDAO roleDAO, PasswordEncoder passwordEncoder) {
+            this.roleDAO = roleDAO;
+            this.passwordEncoder = passwordEncoder;
+        }
+
+        public Builder withId(Integer id) {
+           this.id = id;
+           return this;
+        }
+
+        public Builder withUsername(String username) {
+           this.username = username;
+           return this;
+        }
+
+        public Builder withEmail(String email) {
+           this.email = email;
+           return this;
+        }
+
+        public Builder withPassword(String password) {
+           this.password = password;
+           return this;
+        }
+
+        public Builder withRole(UserRole roleName) {
+           Role role = roleDAO.findByName(roleName).orElseThrow(
+                   () -> new RoleNotFoundException("Role not found " + roleName));
+           this.roles.add(role);
+           return this;
+       }
+
+       public User build() {
+           return new User(this);
+       }
     }
 
     public Integer getId() {
