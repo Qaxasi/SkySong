e13d8ce97c996a7369e3ae9766f849f2b5184b1c
Champel Mariusz <champelmariusz@gmail.com>
2024-08-06 20:39:35 +0200

    test: Add dependencies to UserCreatorTest class

--- src/test/java/com/mycompany/SkySong/domain/registration/UserCreatorTest.java
+++ src/test/java/com/mycompany/SkySong/domain/registration/UserCreatorTest.java
@@ -1,4 +1,15 @@
 package com.mycompany.SkySong.domain.registration;
 
+import com.mycompany.SkySong.domain.registration.service.UserCreator;
+import com.mycompany.SkySong.infrastructure.dao.InMemoryRoleDAO;
+import com.mycompany.SkySong.infrastructure.dao.InMemoryUserDAO;
+import com.mycompany.SkySong.testsupport.auth.common.RegistrationRequests;
+
 class UserCreatorTest {
+
+    private InMemoryRoleDAO roleDAO;
+    private InMemoryUserDAO userDAO;
+    private RegistrationRequests requests;
+    private UserCreator userCreator;
+
 }
