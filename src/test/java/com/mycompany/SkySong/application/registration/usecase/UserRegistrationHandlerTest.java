2a1a314e1156af1c922ad14bfe8656e5d5c7f9b6
Champel Mariusz <champelmariusz@gmail.com>
2024-08-08 21:26:26 +0200

    test: Add dependencies into UserRegistrationHandlerTest

--- src/test/java/com/mycompany/SkySong/application/registration/usecase/UserRegistrationHandlerTest.java
+++ src/test/java/com/mycompany/SkySong/application/registration/usecase/UserRegistrationHandlerTest.java
@@ -1,6 +1,26 @@
 package com.mycompany.SkySong.application.registration.usecase;
 
+import com.mycompany.SkySong.testsupport.auth.common.RegistrationRequests;
+import com.mycompany.SkySong.testsupport.auth.common.UserExistenceChecker;
+import com.mycompany.SkySong.testsupport.auth.service.UserRoleChecker;
 import com.mycompany.SkySong.testsupport.common.BaseIT;
+import com.mycompany.SkySong.testsupport.common.SqlDatabaseCleaner;
+import com.mycompany.SkySong.testsupport.common.SqlDatabaseInitializer;
+import org.springframework.beans.factory.annotation.Autowired;
 
 class UserRegistrationHandlerTest extends BaseIT {
+
+    @Autowired
+    private UserRegistrationHandler registration;
+    @Autowired
+    private UserExistenceChecker userChecker;
+    @Autowired
+    private UserRoleChecker roleChecker;
+    private RegistrationRequests requests;
+
+    @Autowired
+    private SqlDatabaseInitializer initializer;
+    @Autowired
+    private SqlDatabaseCleaner cleaner;
+
 }
