23878a67322caf57ea77712d46644b61b0ec71a9
Champel Mariusz <champelmariusz@gmail.com>
2024-08-10 21:22:39 +0200

    test: Add test to verify correct mapping from RoleDTO to Role entity

--- src/test/java/com/mycompany/SkySong/application/registration/mapper/RoleMapperTest.java
+++ src/test/java/com/mycompany/SkySong/application/registration/mapper/RoleMapperTest.java
@@ -1,6 +1,12 @@
 package com.mycompany.SkySong.application.registration.mapper;
 
+import com.mycompany.SkySong.application.registration.dto.RoleDTO;
+import com.mycompany.SkySong.domain.shared.entity.Role;
+import com.mycompany.SkySong.domain.shared.enums.UserRole;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class RoleMapperTest {
 
@@ -9,5 +15,15 @@ class RoleMapperTest {
     @BeforeEach
     void setUp() {
         roleMapper = new RoleMapper();
+    }
+
+    @Test
+    void whenMappingRoleDtoToEntity_FieldsAreCorrectlySet() {
+        RoleDTO roleDTO = new RoleDTO(1, UserRole.ROLE_USER);
+
+        Role role = toEntity(roleDTO);
+
+        assertEquals(1, role.getId());
+        assertEquals(UserRole.ROLE_USER, role.getName());
     }
 }
