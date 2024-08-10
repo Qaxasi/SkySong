e8ed4887d8d64ee45daa48506bf0f6ceb2acdd90
Champel Mariusz <champelmariusz@gmail.com>
2024-08-10 22:16:23 +0200

    test: Add dependency and initialize in setup method

--- src/test/java/com/mycompany/SkySong/application/registration/mapper/UserRegistrationMapperTest.java
+++ src/test/java/com/mycompany/SkySong/application/registration/mapper/UserRegistrationMapperTest.java
@@ -1,4 +1,15 @@
 package com.mycompany.SkySong.application.registration.mapper;
 
+import org.junit.jupiter.api.BeforeEach;
+
 class UserRegistrationMapperTest {
+
+    private RoleMapper roleMapper;
+    private UserRegistrationMapper userMapper;
+
+    @BeforeEach
+    void setUp() {
+        roleMapper = new RoleMapper();
+        userMapper = new UserRegistrationMapper(roleMapper);
+    }
 }
