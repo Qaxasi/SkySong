97d10790d79b502813dfa42687486485b8d99768
Champel Mariusz <champelmariusz@gmail.com>
2024-08-10 21:18:17 +0200

    test: Add RoleMapper dependency and initialize in setup method

--- src/test/java/com/mycompany/SkySong/application/registration/mapper/RoleMapperTest.java
+++ src/test/java/com/mycompany/SkySong/application/registration/mapper/RoleMapperTest.java
@@ -1,4 +1,13 @@
 package com.mycompany.SkySong.application.registration.mapper;
 
+import org.junit.jupiter.api.BeforeEach;
+
 class RoleMapperTest {
+
+    private RoleMapper roleMapper;
+
+    @BeforeEach
+    void setUp() {
+        roleMapper = new RoleMapper();
+    }
 }
