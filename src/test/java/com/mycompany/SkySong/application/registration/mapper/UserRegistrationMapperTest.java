cdaf535b30421ad8fb5f047bbe3eca443f833d20
Champel Mariusz <champelmariusz@gmail.com>
2024-08-10 22:17:52 +0200

    test: Add test to verify correct mapping from UserRegistrationDto to User entity

--- src/test/java/com/mycompany/SkySong/application/registration/mapper/UserRegistrationMapperTest.java
+++ src/test/java/com/mycompany/SkySong/application/registration/mapper/UserRegistrationMapperTest.java
@@ -1,6 +1,13 @@
 package com.mycompany.SkySong.application.registration.mapper;
 
+import com.mycompany.SkySong.application.registration.dto.UserRegistrationDTO;
+import com.mycompany.SkySong.domain.shared.entity.User;
+import com.mycompany.SkySong.domain.shared.enums.UserRole;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class UserRegistrationMapperTest {
 
@@ -12,4 +19,18 @@ class UserRegistrationMapperTest {
         roleMapper = new RoleMapper();
         userMapper = new UserRegistrationMapper(roleMapper);
     }
+
+    @Test
+    void whenMappingDtoToEntity_EntityHasCorrectFields() {
+        UserRegistrationDTO userDto = createUserDto("Alex", "alex@mail.mail", "Password#3", UserRole.ROLE_USER);
+
+        User user = toEntity(userDto);
+
+        assertEquals("Alex", user.getUsername());
+        assertEquals("alex@mail.mail", user.getEmail());
+        assertEquals("Password#3", user.getPassword());
+        assertTrue(user.getRoles().stream().anyMatch(r -> r.getName().equals(UserRole.ROLE_USER)));
+
+    }
+    
 }
