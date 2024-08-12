5f8ecf6a506169a7ee9ed59710ac1f570a37969f
Champel Mariusz <champelmariusz@gmail.com>
2024-08-12 20:31:29 +0200

    feat: Add LoginResponse class

---
+++ src/main/java/com/mycompany/SkySong/application/login/dto/LoginResponse.java
@@ -0,0 +1,11 @@
+package com.mycompany.SkySong.application.login.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class LoginResponse {
+
+    @JsonProperty("access_token")
+    private String accessToken;
+    @JsonProperty("refresh_token")
+    private String refreshToken;
+}
