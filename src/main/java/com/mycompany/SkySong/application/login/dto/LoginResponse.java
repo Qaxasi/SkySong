c1b27762a9eab1998dfb640ff71db1bd48aa564b
Champel Mariusz <champelmariusz@gmail.com>
2024-08-12 20:32:16 +0200

    style: Improve readability

--- src/main/java/com/mycompany/SkySong/application/login/dto/LoginResponse.java
+++ src/main/java/com/mycompany/SkySong/application/login/dto/LoginResponse.java
@@ -3,7 +3,6 @@ package com.mycompany.SkySong.application.login.dto;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class LoginResponse {
-
     @JsonProperty("access_token")
     private String accessToken;
     @JsonProperty("refresh_token")
