013f632a01947b7a9bdd607fe790d597d7c4a4f5
Champel Mariusz <champelmariusz@gmail.com>
2024-08-13 22:28:45 +0200

    feat: Add custom exception

---
+++ src/main/java/com/mycompany/SkySong/domain/registration/exception/RegistrationException.java
@@ -0,0 +1,7 @@
+package com.mycompany.SkySong.domain.registration.exception;
+
+public class RegistrationException extends RuntimeException {
+    public RegistrationException(String message) {
+        super(message);
+    }
+}
