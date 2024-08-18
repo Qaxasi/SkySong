5bbd8a24cf98c2b7276f24cd4df35d8065f0f2fd
Champel Mariusz <champelmariusz@gmail.com>
2024-08-18 21:36:33 +0200

    feat: Implement authentication process and JWT/refresh token handling

--- src/main/java/com/mycompany/SkySong/adapter/login/service/AuthenticationHandler.java
+++ src/main/java/com/mycompany/SkySong/adapter/login/service/AuthenticationHandler.java
@@ -1,7 +1,13 @@
 package com.mycompany.SkySong.adapter.login.service;
 
+import com.mycompany.SkySong.adapter.login.dto.AuthenticationResponse;
+import com.mycompany.SkySong.adapter.security.CustomUserDetails;
 import com.mycompany.SkySong.adapter.security.jwt.JwtTokenManager;
+import com.mycompany.SkySong.application.login.dto.LoginRequest;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -14,5 +20,19 @@ public class AuthenticationHandler {
                                  JwtTokenManager tokenManager) {
         this.authManager = authManager;
         this.tokenManager = tokenManager;
+    }
+
+    public AuthenticationResponse authenticate(LoginRequest request) {
+        Authentication authentication = authManager.authenticate(
+                new UsernamePasswordAuthenticationToken(request.usernameOrEmail(), request.password()));
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        CustomUserDetails userDetails = (CustomUserDetails) authentication.getPrincipal();
+
+        String jwtToken = tokenManager.generateToken(userDetails);
+        String refreshToken = tokenManager.generateRefreshToken(userDetails);
+
+        return new AuthenticationResponse(jwtToken, refreshToken);
     }
 }
