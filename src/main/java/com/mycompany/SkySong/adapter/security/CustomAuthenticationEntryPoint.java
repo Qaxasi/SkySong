bb40bec555eebf92bfb37360d678fd6e7edb7d2d
Champel Mariusz <champelmariusz@gmail.com>
2024-08-18 21:02:00 +0200

    feat: Add JwtTokenManager class with configuration properties

--- src/main/java/com/mycompany/SkySong/adapter/security/CustomAuthenticationEntryPoint.java
+++ src/main/java/com/mycompany/SkySong/adapter/security/CustomAuthenticationEntryPoint.java
@@ -1,32 +1,46 @@
 package com.mycompany.SkySong.adapter.security;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mycompany.SkySong.adapter.login.exception.TokenExpiredException;
 import io.jsonwebtoken.ExpiredJwtException;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.logging.log4j.Logger;
 import org.json.JSONObject;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.MediaType;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 @Component
-public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
+@Slf4j
+public class AuthEntryPointJwt implements AuthenticationEntryPoint {
+
+
 
     @Override
-    public void commence(HttpServletRequest request,
-                         HttpServletResponse response,
-                         AuthenticationException authException) throws IOException {
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException)
+            throws IOException, ServletException {
+        log.error("Unauthorized error: {}", authException.getMessage());
 
-        String errorMessage = "Unauthorized access. Please log in.";
-
-        if (authException.getCause() instanceof ExpiredJwtException) {
-            errorMessage = "Token expired. Please refresh your token.";
-        }
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
         response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-        response.setContentType("application/json");
-        response.getWriter().write(new JSONObject()
-                .put("error", errorMessage)
-                .toString());
+
+        final Map<String, Object> body = new HashMap<>();
+        body.put("status", HttpServletResponse.SC_UNAUTHORIZED);
+        body.put("error", "Unauthorized");
+        body.put("message", authException.getMessage());
+        body.put("path", request.getServletPath());
+
+        final ObjectMapper mapper = new ObjectMapper();
+        mapper.writeValue(response.getOutputStream(), body);
     }
+
 }
---
+++ src/main/java/com/mycompany/SkySong/adapter/security/jwt/JwtTokenManager.java
@@ -0,0 +1,16 @@
+package com.mycompany.SkySong.adapter.security.jwt;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class JwtTokenManager {
+    @Value("${application.security.jwt.secret-key}")
+    private String secretKey;
+    @Value("${application.security.jwt.expiration}")
+    private long jwtExpiration;
+    @Value("${application.security.jwt.refresh-token.expiration}")
+    private long refreshJwtExpiration;
+}
