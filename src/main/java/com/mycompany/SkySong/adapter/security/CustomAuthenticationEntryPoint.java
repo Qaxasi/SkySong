352e3002e6bcbe7da2706f3f24f715e5ee5d4457
Champel Mariusz <champelmariusz@gmail.com>
2024-08-18 22:31:28 +0200

    refactor: Refactor CustomAuthenticationEntryPoint

--- src/main/java/com/mycompany/SkySong/adapter/security/CustomAuthenticationEntryPoint.java
+++ src/main/java/com/mycompany/SkySong/adapter/security/CustomAuthenticationEntryPoint.java
@@ -1,46 +1,28 @@
 package com.mycompany.SkySong.adapter.security;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.mycompany.SkySong.adapter.login.exception.TokenExpiredException;
-import io.jsonwebtoken.ExpiredJwtException;
-import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.logging.log4j.Logger;
 import org.json.JSONObject;
-import org.slf4j.LoggerFactory;
-import org.springframework.http.MediaType;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 
 @Component
 @Slf4j
-public class AuthEntryPointJwt implements AuthenticationEntryPoint {
-
-
+public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
 
     @Override
-    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException)
-            throws IOException, ServletException {
-        log.error("Unauthorized error: {}", authException.getMessage());
+    public void commence(HttpServletRequest request,
+                         HttpServletResponse response,
+                         AuthenticationException authException) throws IOException {
 
-        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
         response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-
-        final Map<String, Object> body = new HashMap<>();
-        body.put("status", HttpServletResponse.SC_UNAUTHORIZED);
-        body.put("error", "Unauthorized");
-        body.put("message", authException.getMessage());
-        body.put("path", request.getServletPath());
-
-        final ObjectMapper mapper = new ObjectMapper();
-        mapper.writeValue(response.getOutputStream(), body);
+        response.setContentType("application/json");
+        response.getWriter().write(new JSONObject()
+                .put("error", "Unauthorized access. Please log in.")
+                .toString());
     }
-
 }
