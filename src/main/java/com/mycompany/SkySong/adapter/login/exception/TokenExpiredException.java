6c678d8a8bd2614859551a23dc60d7da8bdff344
Champel Mariusz <champelmariusz@gmail.com>
2024-08-19 23:12:39 +0200

    feat: Add custom exception for JWT expired token

---
+++ src/main/java/com/mycompany/SkySong/adapter/login/exception/TokenExpiredException.java
@@ -0,0 +1,10 @@
+package com.mycompany.SkySong.adapter.login.exception;
+
+
+import org.springframework.security.core.AuthenticationException;
+
+public class TokenExpiredException extends AuthenticationException {
+    public TokenExpiredException(String msg) {
+        super(msg);
+    }
+}
