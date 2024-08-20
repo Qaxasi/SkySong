52e907c842993f6d7e248b1b7766fd51cc3d5d9a
Champel Mariusz <champelmariusz@gmail.com>
2024-08-20 22:38:16 +0200

    feat: Add refresh token method to RefreshTokenController for handling token refresh request

--- src/main/java/com/mycompany/SkySong/adapter/security/jwt/RefreshTokenController.java
+++ src/main/java/com/mycompany/SkySong/adapter/security/jwt/RefreshTokenController.java
@@ -1,5 +1,12 @@
 package com.mycompany.SkySong.adapter.security.jwt;
 
+import com.mycompany.SkySong.application.shared.dto.ApiResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseCookie;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,5 +20,20 @@ public class RefreshTokenController {
                                   CookieUtils cookieUtils) {
         this.tokenManager = tokenManager;
         this.cookieUtils = cookieUtils;
+    }
+
+    @PostMapping("/refresh-token")
+    public ResponseEntity<?> refreshToken(HttpServletRequest request) {
+        String refreshToken = getJwtFromCookies(request, "refreshToken");
+
+        if (refreshToken != null && validateRefreshToken(refreshToken)) {
+            String newJwtToken = generateAccessTokenFromRefreshToken(refreshToken);
+            ResponseCookie jwtCookie = generateCookie("jwtToken", newJwtToken, "/api");
+
+            return ResponseEntity.ok()
+                    .header(HttpHeaders.SET_COOKIE, jwtCookie.toString())
+                    .body(new ApiResponse("Your session has been successfully extended."));
+        }
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(new ApiResponse("Session renewal failed: please log in again."));
     }
 }
