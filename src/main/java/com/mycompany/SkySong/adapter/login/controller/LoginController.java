fcbe8f6726b45763916d2f07fb6c676619a013e0
Champel Mariusz <champelmariusz@gmail.com>
2024-08-20 21:56:01 +0200

    refactor: Remove LoginHandler dependency from LoginController

--- src/main/java/com/mycompany/SkySong/adapter/login/controller/LoginController.java
+++ src/main/java/com/mycompany/SkySong/adapter/login/controller/LoginController.java
@@ -1,10 +1,8 @@
 package com.mycompany.SkySong.adapter.login.controller;
 
 import com.mycompany.SkySong.application.shared.dto.ApiResponse;
-import com.mycompany.SkySong.application.login.usecase.LoginHandler;
 import com.mycompany.SkySong.application.login.dto.LoginRequest;
 import jakarta.servlet.http.Cookie;
-import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,13 +13,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/users")
 public class LoginController {
-
-    private final LoginHandler login;
-
-    public LoginController(LoginHandler login) {
-        this.login = login;
-    }
-
     @PostMapping("/login")
     public ResponseEntity<ApiResponse> login(@Valid @RequestBody LoginRequest request,
                                              HttpServletResponse response) {
