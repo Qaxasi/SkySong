07ec6b56a33e12b5b8c6fb551c058edaed381c31
Champel Mariusz <champelmariusz@gmail.com>
2024-08-20 22:00:23 +0200

    feat: Add dependencies into LoginController

--- src/main/java/com/mycompany/SkySong/adapter/login/controller/LoginController.java
+++ src/main/java/com/mycompany/SkySong/adapter/login/controller/LoginController.java
@@ -1,6 +1,8 @@
 package com.mycompany.SkySong.adapter.login.controller;
 
 import com.mycompany.SkySong.adapter.login.dto.AuthenticationResponse;
+import com.mycompany.SkySong.adapter.login.service.AuthenticationHandler;
+import com.mycompany.SkySong.adapter.security.jwt.CookieUtils;
 import com.mycompany.SkySong.application.shared.dto.ApiResponse;
 import com.mycompany.SkySong.application.login.dto.LoginRequest;
 import jakarta.validation.Valid;
@@ -15,6 +17,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/users")
 public class LoginController {
+
+    private final AuthenticationHandler authentication;
+    private final CookieUtils cookieUtils;
+
+    public LoginController(AuthenticationHandler authentication,
+                           CookieUtils cookieUtils) {
+        this.authentication = authentication;
+        this.cookieUtils = cookieUtils;
+    }
 
     @PostMapping("/login")
     public ResponseEntity<ApiResponse> login(@Valid @RequestBody LoginRequest request) {
