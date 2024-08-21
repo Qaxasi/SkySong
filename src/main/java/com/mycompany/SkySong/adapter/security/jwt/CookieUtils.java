f1d9d0d60ddeec56c5f436a083b4f0dc7bf75e75
Champel Mariusz <champelmariusz@gmail.com>
2024-08-21 22:12:08 +0200

    feat: Add getJwtFromCookies method to CookieUtils

--- src/main/java/com/mycompany/SkySong/adapter/security/jwt/CookieUtils.java
+++ src/main/java/com/mycompany/SkySong/adapter/security/jwt/CookieUtils.java
@@ -1,4 +1,17 @@
 package com.mycompany.SkySong.adapter.security.jwt;
 
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.web.util.WebUtils;
+
 public class CookieUtils {
+
+    public String getJwtFromCookies(HttpServletRequest request, String cookieName) {
+        Cookie cookie = WebUtils.getCookie(request, cookieName);
+        if (cookie != null) {
+            return cookie.getValue();
+        } else {
+            return null;
+        }
+    }
 }
