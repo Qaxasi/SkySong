6064b9fdd23148ed300bbf87ca49e0e3d8921a88
Champel Mariusz <champelmariusz@gmail.com>
2024-08-24 21:56:54 +0200

    refactor: Rename record

--- src/main/java/com/mycompany/SkySong/adapter/login/dto/AuthenticationResponse.java
+++
@@ -1,4 +0,0 @@
-package com.mycompany.SkySong.adapter.login.dto;
-
-public record AuthenticationResponse(String jwtToken, String refreshToken) {
-}
---
+++ src/main/java/com/mycompany/SkySong/adapter/login/dto/LoginResponse.java
@@ -0,0 +1,4 @@
+package com.mycompany.SkySong.adapter.login.dto;
+
+public record LoginResponse(String jwtToken, String refreshToken) {
+}
