471cfad91d07eb7d77e12986848d46e25ab653e2
Champel Mariusz <champelmariusz@gmail.com>
2024-08-26 22:30:27 +0200

    refactor: Add test to verify exception when login with invalid credentials

--- src/test/java/com/mycompany/SkySong/adapter/login/service/LoginHandlerTest.java
+++ src/test/java/com/mycompany/SkySong/adapter/login/service/LoginHandlerTest.java
@@ -1,15 +1,23 @@
 package com.mycompany.SkySong.adapter.login.service;
 
-import com.mycompany.SkySong.adapter.login.dto.LoginRequest;
+import com.mycompany.SkySong.infrastructure.dao.InMemoryRoleDAO;
+import com.mycompany.SkySong.infrastructure.dao.InMemoryUserDAO;
+import com.mycompany.SkySong.infrastructure.persistence.dao.RoleDAO;
+import com.mycompany.SkySong.infrastructure.persistence.dao.UserDAO;
+import com.mycompany.SkySong.testsupport.auth.common.UserBuilder;
 import com.mycompany.SkySong.testsupport.auth.common.UserFixture;
 import com.mycompany.SkySong.testsupport.common.BaseIT;
 import com.mycompany.SkySong.testsupport.common.SqlDatabaseCleaner;
 import com.mycompany.SkySong.testsupport.common.SqlDatabaseInitializer;
+import com.mycompany.SkySong.testsupport.utils.CustomPasswordEncoder;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.test.context.ActiveProfiles;
 
 import static org.junit.Assert.assertThrows;
 
@@ -32,6 +40,13 @@ class LoginHandlerTest extends BaseIT {
     @AfterEach
     void cleanUp() {
         cleaner.clean();
+    }
+
+    @Test
+    void whenInvalidCredentials_ThrowException() {
+        createUser("Alex", "Password#3");
+        assertThrows(BadCredentialsException.class,
+                () -> login("Invalid", "Invalid"));
     }
 
     private void createUser(String username, String password) {
