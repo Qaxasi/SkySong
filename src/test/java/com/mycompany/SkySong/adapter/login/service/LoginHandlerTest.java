9d6cbd3c4330da5cb1f3cd47947280638f67988f
Champel Mariusz <champelmariusz@gmail.com>
2024-08-26 21:14:52 +0200

    test: Add LoginHandlerTest class with dependencies and setup/cleanup methods

---
+++ src/test/java/com/mycompany/SkySong/adapter/login/service/LoginHandlerTest.java
@@ -0,0 +1,28 @@
+package com.mycompany.SkySong.adapter.login.service;
+
+import com.mycompany.SkySong.testsupport.common.BaseIT;
+import com.mycompany.SkySong.testsupport.common.SqlDatabaseCleaner;
+import com.mycompany.SkySong.testsupport.common.SqlDatabaseInitializer;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+
+class LoginHandlerTest extends BaseIT {
+
+    @Autowired
+    private LoginHandler login;
+    @Autowired
+    private SqlDatabaseInitializer initializer;
+    @Autowired
+    private SqlDatabaseCleaner cleaner;
+
+    @BeforeEach
+    void setUp() throws Exception {
+        initializer.setup("data_sql/test-setup.sql");
+    }
+
+    @AfterEach
+    void cleanUp() {
+        cleaner.clean();
+    }
+}
