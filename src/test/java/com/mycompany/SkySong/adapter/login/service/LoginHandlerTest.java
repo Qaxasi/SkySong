e56bced2d1b6af823fd9589a66856a876af01c83
Champel Mariusz <champelmariusz@gmail.com>
2024-08-26 21:19:06 +0200

    test: Add UserFixture dependency in LoginHandlerTest

--- src/test/java/com/mycompany/SkySong/adapter/login/service/LoginHandlerTest.java
+++ src/test/java/com/mycompany/SkySong/adapter/login/service/LoginHandlerTest.java
@@ -1,16 +1,20 @@
 package com.mycompany.SkySong.adapter.login.service;
 
+import com.mycompany.SkySong.testsupport.auth.common.UserFixture;
 import com.mycompany.SkySong.testsupport.common.BaseIT;
 import com.mycompany.SkySong.testsupport.common.SqlDatabaseCleaner;
 import com.mycompany.SkySong.testsupport.common.SqlDatabaseInitializer;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
 class LoginHandlerTest extends BaseIT {
 
     @Autowired
     private LoginHandler login;
+    @Autowired
+    private UserFixture userFixture;
     @Autowired
     private SqlDatabaseInitializer initializer;
     @Autowired
