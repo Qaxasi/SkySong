1634ea8527390bfcb16b934ece65ac12632f1386
Champel Mariusz <champelmariusz@gmail.com>
2024-08-28 22:21:16 +0200

    refactor: Update refresh token cookie path in LoginController

--- src/main/java/com/mycompany/SkySong/adapter/login/controller/LoginController.java
+++ src/main/java/com/mycompany/SkySong/adapter/login/controller/LoginController.java
@@ -35,7 +35,7 @@ public class LoginController {
                 "jwtToken", authResponse.jwtToken(), "/api", 600);
 
         ResponseCookie refreshCookie = cookieUtils.generateCookie(
-                "refreshToken", authResponse.refreshToken(), "/api/v1/auth/refresh-token", 86400);
+                "refreshToken", authResponse.refreshToken(), "/refresh-token", 86400);
 
         return ResponseEntity.ok()
                 .header(HttpHeaders.SET_COOKIE, jwtCookie.toString())
