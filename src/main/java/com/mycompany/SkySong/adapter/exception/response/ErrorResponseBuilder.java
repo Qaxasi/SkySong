e2d8f0f74912439ff585916eaa9673b0091784a0
Champel Mariusz <champelmariusz@gmail.com>
2024-08-30 21:14:23 +0200

    refactor: Refactor ErrorResponseBuilder to use ErrorResponse and add private constructor

--- src/main/java/com/mycompany/SkySong/adapter/exception/response/ErrorResponseBuilder.java
+++ src/main/java/com/mycompany/SkySong/adapter/exception/response/ErrorResponseBuilder.java
@@ -3,18 +3,19 @@ package com.mycompany.SkySong.adapter.exception.response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
-import java.util.HashMap;
 import java.util.Map;
 
 public class ErrorResponseBuilder {
-    public static ResponseEntity<Object> createErrorResponse(String errorMessage, HttpStatus status) {
-        Map<String, Object> response = new HashMap<>();
-        response.put("error", errorMessage);
-        return new ResponseEntity<>(response, status);
+
+    private ErrorResponseBuilder() {
     }
-    public static ResponseEntity<Object> createErrorResponse(Map<String, Object> errors, HttpStatus status) {
-        Map<String, Object> response = new HashMap<>();
-        response.put("errors", errors);
-        return new ResponseEntity<>(response, status);
+
+    public static ResponseEntity<Object> createErrorResponse(String errorMessage, HttpStatus status) {
+        ErrorResponse errorResponse = new ErrorResponse(errorMessage);
+        return new ResponseEntity<>(errorResponse, status);
+    }
+    public static ResponseEntity<Object> createErrorResponse(Map<String, String> errors, HttpStatus status) {
+        ErrorResponse errorResponse = new ErrorResponse(errors);
+        return new ResponseEntity<>(errorResponse, status);
     }
 }
