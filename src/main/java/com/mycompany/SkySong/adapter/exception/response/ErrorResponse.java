2c40b6fe61c32a5dd00f0eb23235a2d8b80d64b2
Champel Mariusz <champelmariusz@gmail.com>
2024-08-30 21:16:48 +0200

    feat: Add ErrorResponse class for structured error handling

---
+++ src/main/java/com/mycompany/SkySong/adapter/exception/response/ErrorResponse.java
@@ -0,0 +1,24 @@
+package com.mycompany.SkySong.adapter.exception.response;
+
+import java.util.Map;
+
+public class ErrorResponse {
+    private String error;
+    private Map<String, String> errors;
+
+    public ErrorResponse(String error) {
+        this.error = error;
+    }
+
+    public ErrorResponse(Map<String, String> errors) {
+        this.errors = errors;
+    }
+
+    public String getError() {
+        return error;
+    }
+
+    public Map<String, String> getErrors() {
+        return errors;
+    }
+}
