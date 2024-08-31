170512a2e85a9442c28e0a62eae11d7f4130a9e0
Champel Mariusz <champelmariusz@gmail.com>
2024-08-31 18:03:18 +0200

    test: Add custom TestRestTemplate configuration

---
+++ src/test/java/com/mycompany/SkySong/testsupport/common/CustomTestRestTemplateConfig.java
@@ -0,0 +1,40 @@
+package com.mycompany.SkySong.testsupport.common;
+
+import lombok.extern.slf4j.Slf4j;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.web.client.ResponseErrorHandler;
+
+import java.io.IOException;
+
+@TestConfiguration
+@Slf4j
+public class CustomTestRestTemplateConfig {
+
+    @Bean
+    public TestRestTemplate customTestRestTemplate() {
+        TestRestTemplate restTemplate = new TestRestTemplate();
+
+        restTemplate.getRestTemplate().setRequestFactory(new HttpComponentsClientHttpRequestFactory());
+
+        restTemplate.getRestTemplate().setErrorHandler(new ResponseErrorHandler() {
+            @Override
+            public boolean hasError(@NotNull ClientHttpResponse response) throws IOException {
+                HttpStatusCode statusCode = response.getStatusCode();
+                return (statusCode.is4xxClientError() || statusCode.is5xxServerError());
+            }
+
+            @Override
+            public void handleError(@NotNull ClientHttpResponse response) throws IOException {
+                log.error("Http error: " + response.getStatusCode());
+            }
+        });
+
+        return restTemplate;
+    }
+}
