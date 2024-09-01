70ecf684bf53b40d67eb6e2c5e00e6872214f24e
Champel Mariusz <champelmariusz@gmail.com>
2024-09-01 20:33:53 +0200

    test: Add test to verify status for missing token

--- src/test/java/com/mycompany/SkySong/adapter/refreshToken/controller/RefreshTokenControllerTest.java
+++ src/test/java/com/mycompany/SkySong/adapter/refreshToken/controller/RefreshTokenControllerTest.java
@@ -1,12 +1,25 @@
 package com.mycompany.SkySong.adapter.refreshToken.controller;
 
+import com.mycompany.SkySong.adapter.exception.response.ErrorResponse;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class RefreshTokenControllerTest  {
 
     @Autowired
     private TestRestTemplate restTemplate;
+
+    @Test
+    void whenMissingToken_ReturnForbidden() {
+        ResponseEntity<ErrorResponse> response = restTemplate.postForEntity(
+                "/api/v1/auth/refresh-token", null, ErrorResponse.class);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.FORBIDDEN);
+    }
 }
