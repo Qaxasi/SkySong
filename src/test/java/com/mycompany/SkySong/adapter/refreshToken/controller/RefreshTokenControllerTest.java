25ed097553c6b7330b39937ac5cdfdd9f45770a3
Champel Mariusz <champelmariusz@gmail.com>
2024-09-02 20:21:32 +0200

    test: Extend RefreshTokenControllerTest with BaseIT and setup fixtures

--- src/test/java/com/mycompany/SkySong/adapter/refreshToken/controller/RefreshTokenControllerTest.java
+++ src/test/java/com/mycompany/SkySong/adapter/refreshToken/controller/RefreshTokenControllerTest.java
@@ -1,22 +1,41 @@
 package com.mycompany.SkySong.adapter.refreshToken.controller;
 
 import com.mycompany.SkySong.adapter.exception.response.ErrorResponse;
+import com.mycompany.SkySong.infrastructure.persistence.dao.RoleDAO;
+import com.mycompany.SkySong.infrastructure.persistence.dao.UserDAO;
+import com.mycompany.SkySong.testsupport.auth.common.UserBuilder;
+import com.mycompany.SkySong.testsupport.auth.common.UserFixture;
+import com.mycompany.SkySong.testsupport.common.BaseIT;
+import com.mycompany.SkySong.testsupport.utils.CustomPasswordEncoder;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-class RefreshTokenControllerTest  {
+class RefreshTokenControllerTest extends BaseIT {
 
     @Autowired
     private TestRestTemplate restTemplate;
+    @Autowired
+    private UserDAO userDAO;
+    @Autowired
+    private RoleDAO roleDAO;
+    private UserFixture userFixture;
+
+    @BeforeEach
+    void setup() {
+        CustomPasswordEncoder encoder = new CustomPasswordEncoder(new BCryptPasswordEncoder());
+        UserBuilder userBuilder = new UserBuilder(encoder);
+
+        userFixture = new UserFixture(roleDAO, userDAO, userBuilder);
+    }
 
     @Test
     void whenInvalidToken_ReturnForbidden() {
