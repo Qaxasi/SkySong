df7758506f6b77394a971ed7d7891b82a1d25817
Champel Mariusz <champelmariusz@gmail.com>
2024-09-04 20:41:03 +0200

    refactor: Exclude null fields in ErrorResponse JSON serialization

--- src/main/java/com/mycompany/SkySong/adapter/exception/response/ErrorResponse.java
+++ src/main/java/com/mycompany/SkySong/adapter/exception/response/ErrorResponse.java
@@ -1,7 +1,10 @@
 package com.mycompany.SkySong.adapter.exception.response;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
 import java.util.Map;
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class ErrorResponse {
     private String error;
     private Map<String, String> errors;
