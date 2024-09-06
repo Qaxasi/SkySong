5fd236747de00b2fb787bd976091ca3e21145568
Champel Mariusz <champelmariusz@gmail.com>
2024-09-06 23:22:36 +0200

    feat: Create custom exception

---
+++ src/main/java/com/mycompany/SkySong/adapter/user/delete/adapters/UserNotFoundException.java
@@ -0,0 +1,7 @@
+package com.mycompany.SkySong.adapter.user.delete.adapters;
+
+public class UserNotFoundException extends RuntimeException {
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+}
