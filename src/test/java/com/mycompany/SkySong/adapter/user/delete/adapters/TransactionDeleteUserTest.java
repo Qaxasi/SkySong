7697058910f1961006f85689b35733da0ab62c28
Champel Mariusz <champelmariusz@gmail.com>
2024-09-07 17:28:26 +0200

    test: Add test to verify successful user deletion

--- src/test/java/com/mycompany/SkySong/adapter/user/delete/adapters/TransactionDeleteUserTest.java
+++ src/test/java/com/mycompany/SkySong/adapter/user/delete/adapters/TransactionDeleteUserTest.java
@@ -4,9 +4,10 @@ import com.mycompany.SkySong.adapter.user.delete.adapters.exception.UserNotFound
 import com.mycompany.SkySong.infrastructure.persistence.dao.RoleDAO;
 import com.mycompany.SkySong.infrastructure.persistence.dao.UserDAO;
 import com.mycompany.SkySong.testsupport.auth.common.UserBuilder;
+import com.mycompany.SkySong.testsupport.auth.common.UserExistenceChecker;
 import com.mycompany.SkySong.testsupport.auth.common.UserFixture;
+import com.mycompany.SkySong.testsupport.auth.common.UserIdFetcher;
 import com.mycompany.SkySong.testsupport.auth.security.RoleChecker;
-import com.mycompany.SkySong.testsupport.auth.service.UserRoleChecker;
 import com.mycompany.SkySong.testsupport.common.BaseIT;
 import com.mycompany.SkySong.testsupport.utils.CustomPasswordEncoder;
 import org.junit.jupiter.api.BeforeEach;
@@ -14,6 +15,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class TransactionDeleteUserTest extends BaseIT {
@@ -26,6 +28,10 @@ class TransactionDeleteUserTest extends BaseIT {
     private UserFixture userFixture;
     @Autowired
     private RoleChecker roleChecker;
+    @Autowired
+    private UserExistenceChecker userExistenceChecker;
+    @Autowired
+    private UserIdFetcher userIdFetcher;
 
     @BeforeEach
     void setup() {
@@ -38,5 +44,22 @@ class TransactionDeleteUserTest extends BaseIT {
     @Test
     void whenUserNotFound_ThrowException() {
         assertThrows(UserNotFoundException.class, () -> deleteUser.deleteEverythingById(1));
+    }
+
+    @Test
+    void whenUserDeleted_UserNotExist() {
+        createUserWithUsername("Alex");
+        int userId = fetchIdByUsername("Alex");
+
+        deleteUser.deleteEverythingById(userId);
+
+        assertThat(userExistenceChecker.userExist("Alex")).isFalse();
+    }
+
+    private int fetchIdByUsername(String username) {
+        return userIdFetcher.fetchByUsername(username);
+    }
+    private void createUserWithUsername(String username) {
+        userFixture.createUserWithUsername(username);
     }
 }
