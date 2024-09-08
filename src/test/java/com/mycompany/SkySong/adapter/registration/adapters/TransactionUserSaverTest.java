6469631d80ec1dd930367d3cb069003a7b0f9b23
Champel Mariusz <champelmariusz@gmail.com>
2024-09-08 21:33:31 +0200

    test: Add utility methods for creating userDTO

--- src/test/java/com/mycompany/SkySong/adapter/registration/adapters/TransactionUserSaverTest.java
+++ src/test/java/com/mycompany/SkySong/adapter/registration/adapters/TransactionUserSaverTest.java
@@ -1,9 +1,19 @@
 package com.mycompany.SkySong.adapter.registration.adapters;
 
+import com.mycompany.SkySong.application.registration.dto.RoleDTO;
+import com.mycompany.SkySong.application.registration.dto.UserRegistrationDTO;
+import com.mycompany.SkySong.domain.shared.enums.UserRole;
+import com.mycompany.SkySong.testsupport.auth.common.UserCountChecker;
 import com.mycompany.SkySong.testsupport.auth.common.UserExistenceChecker;
 import com.mycompany.SkySong.testsupport.auth.service.UserRoleChecker;
 import com.mycompany.SkySong.testsupport.common.BaseIT;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class TransactionUserSaverTest extends BaseIT {
     @Autowired
@@ -12,4 +22,13 @@ class TransactionUserSaverTest extends BaseIT {
     private UserExistenceChecker userExistenceChecker;
     @Autowired
     private UserRoleChecker userRoleChecker;
+
+    private UserRegistrationDTO createUserDtoWithUsername(String username) {
+        RoleDTO roleDTO = new RoleDTO(1, UserRole.ROLE_USER);
+        return new UserRegistrationDTO(username, "alex@mail.mail", "Password#3", Set.of(roleDTO));
+    }
+
+    private UserRegistrationDTO createUserDtoWithRoles(String username, Set<RoleDTO> roles) {
+        return new UserRegistrationDTO(username, "alex@mail.mail", "Password#3", roles);
+    }
 }
