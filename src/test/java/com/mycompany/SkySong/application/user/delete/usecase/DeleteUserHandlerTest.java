047445e1b6a6d9418087e39e7a6725d024608c96
Champel Mariusz <champelmariusz@gmail.com>
2024-09-09 21:26:20 +0200

    test: Refactor test class

--- src/test/java/com/mycompany/SkySong/application/user/delete/usecase/DeleteUserHandlerTest.java
+++ src/test/java/com/mycompany/SkySong/application/user/delete/usecase/DeleteUserHandlerTest.java
@@ -1,46 +1,46 @@
-package com.mycompany.SkySong.user;
+package com.mycompany.SkySong.application.user.delete.usecase;
 
-import com.mycompany.SkySong.common.dto.ApiResponse;
+import com.mycompany.SkySong.application.shared.dto.ApiResponse;
+import com.mycompany.SkySong.infrastructure.persistence.dao.RoleDAO;
+import com.mycompany.SkySong.infrastructure.persistence.dao.UserDAO;
+import com.mycompany.SkySong.testsupport.auth.common.UserBuilder;
 import com.mycompany.SkySong.testsupport.auth.common.UserFixture;
+import com.mycompany.SkySong.testsupport.auth.common.UserIdFetcher;
 import com.mycompany.SkySong.testsupport.common.BaseIT;
-import com.mycompany.SkySong.testsupport.common.SqlDatabaseCleaner;
-import com.mycompany.SkySong.testsupport.common.SqlDatabaseInitializer;
-import com.mycompany.SkySong.user.delete.domain.service.DeleteUserHandler;
-import org.junit.jupiter.api.AfterEach;
+import com.mycompany.SkySong.testsupport.utils.CustomPasswordEncoder;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class DeleteUserHandlerTest extends BaseIT {
 
     @Autowired
-    private DeleteUserHandler deleter;
-    @Autowired
+    private UserDeletionHandler userDeletion;
     private UserFixture userFixture;
-
     @Autowired
-    private SqlDatabaseInitializer initializer;
+    private RoleDAO roleDAO;
     @Autowired
-    private SqlDatabaseCleaner cleaner;
-
+    private UserDAO userDAO;
+    @Autowired
+    private UserIdFetcher userIdFetcher;
 
     @BeforeEach
-    void setUp() throws Exception {
-        initializer.setup("data_sql/test-setup.sql");
-    }
+    void setup() {
+        CustomPasswordEncoder encoder = new CustomPasswordEncoder(new BCryptPasswordEncoder());
+        UserBuilder userBuilder = new UserBuilder(encoder);
 
-    @AfterEach
-    void cleanUp() {
-        cleaner.clean();
+        userFixture = new UserFixture(roleDAO, userDAO, userBuilder);
     }
 
     @Test
     void whenUserDeleted_ReturnMessage() {
-        userFixture.createUserWithId(1);
-        ApiResponse response = deleter.deleteUser(1);
-        String expectedMessage = String.format("User with ID %d deleted successfully.", 1);
+        userFixture.createUserWithUsername("Alex");
+        int userId = userIdFetcher.fetchByUsername("Alex");
+        ApiResponse response = userDeletion.delete(userId);
+        String expectedMessage = String.format("User with ID %d deleted successfully.", userId);
         assertThat(response.message()).isEqualTo(expectedMessage);
     }
 }
