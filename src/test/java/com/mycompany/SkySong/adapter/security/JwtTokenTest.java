fd6e65a586e1139b9449269384da754a6481b041
Champel Mariusz <champelmariusz@gmail.com>
2024-09-13 21:56:53 +0200

    test: Add utility methods for tests in JwtTokenTest

--- src/test/java/com/mycompany/SkySong/adapter/security/JwtTokenTest.java
+++ src/test/java/com/mycompany/SkySong/adapter/security/JwtTokenTest.java
@@ -1,7 +1,14 @@
 package com.mycompany.SkySong.adapter.security;
 
 import com.mycompany.SkySong.adapter.security.jwt.JwtTokenManager;
+import com.mycompany.SkySong.adapter.security.user.CustomUserDetails;
 import org.junit.jupiter.api.BeforeEach;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 class JwtTokenTest {
 
@@ -13,5 +20,32 @@ class JwtTokenTest {
         long jwtExpiration = 100000L;
         long refreshJwtExpiration = 200000L;
         jwtManager = new JwtTokenManager(secretKey, jwtExpiration, refreshJwtExpiration);
+    }
+
+    private String generateTokenForUserWithUsername(String username) {
+        Set<GrantedAuthority> authorities = Set.of(new SimpleGrantedAuthority("ROLE_USER"));
+        CustomUserDetails customUserDetails = new CustomUserDetails(1, username, "alex@mail.mail", "Password#3", authorities);
+        return jwtManager.generateToken(customUserDetails);
+    }
+
+    private String generateTokenForUserWithRoles(List<String> roles) {
+        Set<GrantedAuthority> authorities = roles.stream()
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toSet());
+        CustomUserDetails customUserDetails = new CustomUserDetails(1, "Alex", "alex@mail.mail", "Password#3", authorities);
+        return jwtManager.generateToken(customUserDetails);
+    }
+
+    private CustomUserDetails getCustomUserDetails() {
+        Set<GrantedAuthority> authorities = Set.of(new SimpleGrantedAuthority("ROLE_USER"));
+        return new CustomUserDetails(1, "Alex", "alex@mail.mail", "Password#3", authorities);
+    }
+
+    private String extractUsername(String token) {
+        return jwtManager.extractUsername(token);
+    }
+
+    private List<String> extractRoles(String token) {
+        return jwtManager.extractRoles(token);
     }
 }
