ccaab0ed3a3d788bf065cb35e5e9ed0cf15d0ebc
Champel Mariusz <champelmariusz@gmail.com>
2024-09-14 16:09:51 +0200

    test: Add utility methods for creating jwt tokens

--- src/test/java/com/mycompany/SkySong/testsupport/auth/common/TestJwtTokenGenerator.java
+++ src/test/java/com/mycompany/SkySong/testsupport/auth/common/TestJwtTokenGenerator.java
@@ -2,8 +2,16 @@ package com.mycompany.SkySong.testsupport.auth.common;
 
 import com.mycompany.SkySong.adapter.security.jwt.JwtTokenManager;
 import com.mycompany.SkySong.adapter.security.user.CustomUserDetails;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 
+import java.security.Key;
+import java.security.KeyPair;
+import java.util.Date;
 import java.util.HashSet;
+import java.util.List;
 
 public class TestJwtTokenGenerator {
     private final String secretKey;
@@ -11,7 +19,7 @@ public class TestJwtTokenGenerator {
     private final long refreshJwtExpiration;
 
     public TestJwtTokenGenerator() {
-        this.secretKey = "wJ4ds7VbRmFHRP4fX5QbJmTcYZv5P1ZkVN7/kO4id8E=";
+        this.secretKey = "wJ4ds7VbRmFHRP4fX5QbJmTcYZv5P1ZkVN7skO4id8E=";
         this.jwtExpiration = 600000;
         this.refreshJwtExpiration = 86400000;
     }
@@ -32,5 +40,36 @@ public class TestJwtTokenGenerator {
                 new HashSet<>()
         );
         return jwtTokenManager.generateRefreshToken(userDetails);
+    }
+
+    public String generateValidAccessToken() {
+        return Jwts.builder()
+                .setSubject("Alex")
+                .claim("roles", List.of("ROLE_USER"))
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 60000))
+                .signWith(generateSignKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+    public String createExpiredToken() {
+        return Jwts.builder()
+                .setSubject("Alex")
+                .setExpiration(new Date(System.currentTimeMillis() - 1000))
+                .signWith(generateSignKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public String createUnsupportedToken() {
+        KeyPair keyPair = Keys.keyPairFor(SignatureAlgorithm.RS256);
+
+        return Jwts.builder()
+                .setSubject("Alex")
+                .signWith(keyPair.getPrivate(), SignatureAlgorithm.RS256)
+                .compact();
+    }
+
+    private Key generateSignKey() {
+        byte[] keyBytes = Decoders.BASE64URL.decode(secretKey);
+        return Keys.hmacShaKeyFor(keyBytes);
     }
 }
