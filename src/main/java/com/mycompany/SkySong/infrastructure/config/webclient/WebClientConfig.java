f6b45e48596afd1400cfa352d0c38bfee9dc8ef0
Champel Mariusz <champelmariusz@gmail.com>
2024-09-16 21:48:50 +0200

    feat: Add WebClientConfig configuration class

---
+++ src/main/java/com/mycompany/SkySong/infrastructure/config/webclient/WebClientConfig.java
@@ -0,0 +1,17 @@
+package com.mycompany.SkySong.infrastructure.config.webclient;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+public class WebClientConfig {
+    @Bean
+    public WebClient geocodingWebClient() {
+        return WebClient.builder()
+                .baseUrl("https://api.geoapify.com/v1/geocode")
+                .defaultHeaders(headers -> headers.setContentType(MediaType.APPLICATION_JSON))
+                .build();
+    }
+}
