fb83a49a186ae7231b8c42dface9571b2b042f9f
Champel Mariusz <champelmariusz@gmail.com>
2024-09-19 21:05:03 +0200

    refactor: Add constructor with message and cause in exception classes

--- src/main/java/com/mycompany/SkySong/adapter/geocoding/exception/InternalServerErrorException.java
+++ src/main/java/com/mycompany/SkySong/adapter/geocoding/exception/InternalServerErrorException.java
@@ -4,4 +4,7 @@ public class InternalServerErrorException extends RuntimeException {
     public InternalServerErrorException(String message) {
         super(message);
     }
+    public InternalServerErrorException(String message, Throwable cause) {
+        super(message, cause);
+    }
 }
--- src/main/java/com/mycompany/SkySong/adapter/geocoding/exception/TooManyRequestsException.java
+++ src/main/java/com/mycompany/SkySong/adapter/geocoding/exception/TooManyRequestsException.java
@@ -4,4 +4,7 @@ public class TooManyRequestsException extends RuntimeException {
     public TooManyRequestsException(String message) {
         super(message);
     }
+    public TooManyRequestsException(String message, Throwable cause) {
+        super(message, cause);
+    }
 }
