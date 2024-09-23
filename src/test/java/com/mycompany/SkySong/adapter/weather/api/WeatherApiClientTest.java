688e75dd6e4a0b8f1b6d6789fedac527d7b0b445
Champel Mariusz <champelmariusz@gmail.com>
2024-09-23 21:03:31 +0200

    test: Add test to verify WeatherApiClient throws exception on 503 error

--- src/test/java/com/mycompany/SkySong/adapter/weather/api/WeatherApiClientTest.java
+++ src/test/java/com/mycompany/SkySong/adapter/weather/api/WeatherApiClientTest.java
@@ -1,6 +1,7 @@
 package com.mycompany.SkySong.adapter.weather.api;
 
 import com.mycompany.SkySong.adapter.exception.common.AuthorizationException;
+import com.mycompany.SkySong.adapter.exception.common.ServiceUnavailableException;
 import com.mycompany.SkySong.adapter.exception.common.TooManyRequestsException;
 import com.mycompany.SkySong.adapter.weather.dto.WeatherResponse;
 import com.mycompany.SkySong.testutils.common.BaseWireMock;
@@ -83,6 +84,20 @@ public class WeatherApiClientTest extends BaseWireMock {
                         .withBody("\"message\": \"To many requests error\"")));
 
         assertThrows(TooManyRequestsException.class, () -> fetchWeatherData(52.2299, 21.0065));
+    }
+
+    @Test
+    void whenServerReturnsServiceUnavailable_ClientThrowsException() {
+        wireMockServer.stubFor(get(urlPathEqualTo("/v1/weather"))
+                .withQueryParam("lat", equalTo("52.2299"))
+                .withQueryParam("lon", equalTo("21.0065"))
+                .withQueryParam("appid", equalTo("test-api-key"))
+                .willReturn(aResponse()
+                        .withStatus(503)
+                        .withHeader("Content-Type", "application/json")
+                        .withBody("\"message\": \"Service unavailable error\"")));
+
+        assertThrows(ServiceUnavailableException.class, () -> fetchWeatherData(52.2299, 21.0065));
     }
 
     private WeatherResponse fetchWeatherData(double lat, double lon) {
