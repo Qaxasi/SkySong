a7028f19820c34972bc9c1ec75e0b685e7f06b2f
Champel Mariusz <champelmariusz@gmail.com>
2024-09-25 21:32:16 +0200

    feat: Add SpotifyRefreshTokenRequest dto

---
+++ src/main/java/com/mycompany/SkySong/adapter/spotify/dto/SpotifyRefreshTokenRequest.java
@@ -0,0 +1,18 @@
+package com.mycompany.SkySong.adapter.spotify.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+
+import java.util.List;
+
+public record SpotifyRefreshTokenRequest(@JsonProperty("grant_type") String grantType,
+                                         @JsonProperty("refresh_token") String refreshToken) {
+
+    public MultiValueMap<String, String> toMultivalueMap() {
+        MultiValueMap<String, String> formData = new LinkedMultiValueMap<>();
+        formData.put("grant_type", List.of(this.grantType));
+        formData.put("refresh_token", List.of(this.refreshToken));
+        return formData;
+    }
+}
