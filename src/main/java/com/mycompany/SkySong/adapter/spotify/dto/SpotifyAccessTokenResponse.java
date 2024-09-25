1dfa7a9aaf4bc7e7e1762558e70a5d43f3e8db46
Champel Mariusz <champelmariusz@gmail.com>
2024-09-25 20:44:01 +0200

    fix: Correct field name for refresh token in SpotifyAccessTokenResponse

--- src/main/java/com/mycompany/SkySong/adapter/spotify/dto/SpotifyAccessTokenResponse.java
+++ src/main/java/com/mycompany/SkySong/adapter/spotify/dto/SpotifyAccessTokenResponse.java
@@ -5,6 +5,6 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 public record SpotifyAccessTokenResponse(@JsonProperty("access_token") String accessToken,
                                          @JsonProperty("token_type") String tokenType,
                                          @JsonProperty("expires_in") int expirationTimeInSec,
-                                         @JsonProperty("refreshToken") String refreshToken,
+                                         @JsonProperty("refresh_token") String refreshToken,
                                          @JsonProperty("scope") String scope) {
 }
