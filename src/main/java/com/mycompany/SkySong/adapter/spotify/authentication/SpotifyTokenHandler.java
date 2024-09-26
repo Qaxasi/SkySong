77e69d2f9dada00fda940012bf0cd7d0570d10ea
Champel Mariusz <champelmariusz@gmail.com>
2024-09-26 23:34:42 +0200

    refactor: Switch SpotifyTokenHandler methods for synchronous execution for token handling

--- src/main/java/com/mycompany/SkySong/adapter/spotify/authentication/SpotifyTokenHandler.java
+++ src/main/java/com/mycompany/SkySong/adapter/spotify/authentication/SpotifyTokenHandler.java
@@ -1,6 +1,5 @@
 package com.mycompany.SkySong.adapter.spotify.authentication;
 
-import com.mycompany.SkySong.adapter.exception.common.AuthorizationException;
 import com.mycompany.SkySong.adapter.exception.common.RequestTimeoutException;
 import com.mycompany.SkySong.adapter.spotify.dto.SpotifyAccessTokenResponse;
 import com.mycompany.SkySong.adapter.spotify.dto.SpotifyAccessTokenRequest;
@@ -10,13 +9,11 @@ import com.mycompany.SkySong.adapter.spotify.exception.TokenRequestServerExcepti
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.reactive.function.client.WebClient;
-import reactor.core.publisher.Mono;
 
 import java.time.Duration;
 import java.util.concurrent.TimeoutException;
@@ -39,42 +36,44 @@ public class SpotifyTokenHandler {
         this.webClient = webClient;
     }
 
-    public Mono<SpotifyAccessTokenResponse> getAccessToken(String authorizationCode) {
+    public SpotifyAccessTokenResponse getAccessToken(String authorizationCode) {
         MultiValueMap<String, String> formData = new SpotifyAccessTokenRequest("authorization_code", authorizationCode, redirectUri).toMultiValueMap();
         return sendTokenRequest(formData);
     }
 
-    public Mono<SpotifyAccessTokenResponse> refreshAccessToken(String refreshToken) {
+    public SpotifyAccessTokenResponse refreshAccessToken(String refreshToken) {
         MultiValueMap<String, String> formData = new SpotifyRefreshTokenRequest("refresh_token", refreshToken).toMultiValueMap();
         return sendTokenRequest(formData);
     }
 
-    private Mono<SpotifyAccessTokenResponse> sendTokenRequest(MultiValueMap<String, String> bodyData) {
-        return webClient.post()
-                .uri("/api/token")
-                .headers(headers -> {
-                    headers.setBasicAuth(spotifyClientId, spotifyClientSecret);
-                    headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
-                })
-                .bodyValue(bodyData)
-                .retrieve()
-                .onStatus(HttpStatusCode::is4xxClientError, response -> response.bodyToMono(String.class).flatMap(errorBody -> {
-                    log.error("Client error while retrieving token. Status: {}, Body: {}", response.statusCode(), errorBody);
-                    if (response.statusCode() == HttpStatus.UNAUTHORIZED) {
-                        return Mono.error(new AuthorizationException("Authorization exception: " + errorBody));
-                    } else {
-                        return Mono.error(new TokenRequestClientException("Client error: " + errorBody));
-                    }
-                }))
-                .onStatus(HttpStatusCode::is5xxServerError, response -> response.bodyToMono(String.class).flatMap(errorBody -> {
-                    log.error("Server error while retrieving token. Status: {}, Body: {}", response.statusCode(), errorBody);
-                    return Mono.error(new TokenRequestServerException("Server error: " + errorBody));
-                }))
-                .bodyToMono(SpotifyAccessTokenResponse.class)
-                .timeout(Duration.ofSeconds(10))
-                .onErrorResume(TimeoutException.class, e -> {
-                    log.error("Timeout while retrieving token", e);
-                    throw new RequestTimeoutException("Request timed out while retrieving token", e);
-                });
+    public SpotifyAccessTokenResponse sendTokenRequest(MultiValueMap<String, String> bodyData) {
+        try {
+            return webClient.post()
+                    .uri("/api/token")
+                    .headers(headers -> {
+                        headers.setBasicAuth(spotifyClientId, spotifyClientSecret);
+                        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+                    })
+                    .bodyValue(bodyData)
+                    .retrieve()
+                    .onStatus(HttpStatusCode::is4xxClientError, response -> {
+                        String errorBody = response.bodyToMono(String.class).block();
+                        log.error("Client error while retrieving token. Status: {}, Body: {}", response.statusCode(), errorBody);
+                        throw new TokenRequestClientException("Client error: " + errorBody);
+                    })
+                    .onStatus(HttpStatusCode::is5xxServerError, response -> {
+                        String errorBody = response.bodyToMono(String.class).block();
+                        log.error("Server error while retrieving token. Status: {}, Body: {}", response.statusCode(), errorBody);
+                        throw new TokenRequestServerException("Server error: " + errorBody);
+                    })
+                    .bodyToMono(SpotifyAccessTokenResponse.class)
+                    .timeout(Duration.ofSeconds(5))
+                    .block();
+        } catch (RuntimeException ex) {
+            if (ex.getCause() instanceof TimeoutException) {
+                throw new RequestTimeoutException("Request timed out while retrieving token", ex);
+            }
+            throw ex;
+        }
     }
 }
