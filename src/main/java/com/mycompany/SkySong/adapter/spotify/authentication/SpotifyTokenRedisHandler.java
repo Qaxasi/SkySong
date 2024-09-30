097f6c6d1f01a6f4d8e9b0fc1381a38ff62cb2d3
Champel Mariusz <champelmariusz@gmail.com>
2024-09-30 22:25:36 +0200

    feat: Add class for managing refresh tokens in Redis

---
+++ src/main/java/com/mycompany/SkySong/adapter/spotify/authentication/SpotifyTokenRedisHandler.java
@@ -0,0 +1,26 @@
+package com.mycompany.SkySong.adapter.spotify.authentication;
+
+import org.springframework.data.redis.core.RedisTemplate;
+
+public class SpotifyTokenRedisHandler {
+
+    private final RedisTemplate<String, String> redisTemplate;
+
+    public SpotifyTokenRedisHandler(RedisTemplate<String, String> redisTemplate) {
+        this.redisTemplate = redisTemplate;
+    }
+
+    public void saveRefreshToken(int userId, String refreshToken) {
+        String redisKey = generateRedisKey(userId);
+        redisTemplate.opsForValue().set(redisKey, refreshToken);
+    }
+
+    public String getRefreshToken(int userId) {
+        String redisKey = generateRedisKey(userId);
+        return redisTemplate.opsForValue().get(redisKey);
+    }
+
+    private String generateRedisKey(int userId) {
+        return "spotify:refresh_token:" + userId;
+    }
+}
