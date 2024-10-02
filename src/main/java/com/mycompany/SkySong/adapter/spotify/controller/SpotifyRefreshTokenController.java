8237525651474c10b64ddc8d6dbd474896a5c49b
Champel Mariusz <champelmariusz@gmail.com>
2024-10-02 20:21:20 +0200

    feat: Update SpotifyRefreshTokenController to handle JWT token from cookie and set refreshed access token in cookie

--- src/main/java/com/mycompany/SkySong/adapter/spotify/controller/SpotifyRefreshTokenController.java
+++ src/main/java/com/mycompany/SkySong/adapter/spotify/controller/SpotifyRefreshTokenController.java
@@ -1,25 +1,37 @@
 package com.mycompany.SkySong.adapter.spotify.controller;
 
-import com.mycompany.SkySong.adapter.spotify.authentication.SpotifyTokenHandler;
+import com.mycompany.SkySong.adapter.spotify.authentication.SpotifyRefreshTokenHandler;
 import com.mycompany.SkySong.adapter.spotify.dto.SpotifyAccessTokenResponse;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/spotify/token")
 public class SpotifyRefreshTokenController {
 
-    private final SpotifyTokenHandler tokenHandler;
+    private final SpotifyRefreshTokenHandler refreshTokenHandler;
 
-    public SpotifyRefreshTokenController(SpotifyTokenHandler tokenHandler) {
-        this.tokenHandler = tokenHandler;
+    public SpotifyRefreshTokenController(SpotifyRefreshTokenHandler refreshTokenHandler) {
+        this.refreshTokenHandler = refreshTokenHandler;
     }
 
     @PostMapping("/refresh")
-    public ResponseEntity<SpotifyAccessTokenResponse> refreshAccessToken(@RequestParam("refresh_token") String token) {
-        return ResponseEntity.ok(tokenHandler.refreshAccessToken(token));
+    public ResponseEntity<String> refreshAccessToken(@CookieValue(name = "jwtToken") String jwtToken,
+                                                     HttpServletResponse response) {
+        if (jwtToken == null || jwtToken.isEmpty()) {
+            throw new IllegalArgumentException("Jwt token is missing or invalid");
+        }
+
+        SpotifyAccessTokenResponse newAccessToken = refreshTokenHandler.refreshSpotifyAccessToken(jwtToken);
+
+        Cookie accessTokenCookie = new Cookie("spotifyAccessToken", newAccessToken.accessToken());
+        accessTokenCookie.setHttpOnly(true);
+        accessTokenCookie.setPath("/api/v1/spotify/");
+        accessTokenCookie.setMaxAge(3600);
+        response.addCookie(accessTokenCookie);
+
+        return ResponseEntity.ok("Access token refreshed successfully.");
     }
 }
