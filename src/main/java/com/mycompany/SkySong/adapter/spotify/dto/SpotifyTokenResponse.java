ffdc2e12dcddfce8bf796172b76574f92cd7a982
Champel Mariusz <champelmariusz@gmail.com>
2024-10-03 21:25:12 +0200

    refactor: Rename dto

---
+++ src/main/java/com/mycompany/SkySong/adapter/spotify/dto/SpotifyTokenResponse.java
@@ -0,0 +1,11 @@
+package com.mycompany.SkySong.adapter.spotify.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public record SpotifyTokenResponse(@JsonProperty("access_token") String accessToken,
+                                   @JsonProperty("token_type") String tokenType,
+                                   @JsonProperty("expires_in") int expirationTimeInSec,
+                                   @JsonProperty("refresh_token") String refreshToken,
+                                   @JsonProperty("scope") String scope) {
+}
+    
