7376e246faa8e7fb8fab9cee442987eba3dd941e
Champel Mariusz <champelmariusz@gmail.com>
2024-10-07 21:25:03 +0200

    feat: Add constructor validation in SpotifyRefreshTokenRequest

--- src/main/java/com/mycompany/SkySong/adapter/spotify/authentication/dto/SpotifyRefreshTokenRequest.java
+++ src/main/java/com/mycompany/SkySong/adapter/spotify/authentication/dto/SpotifyRefreshTokenRequest.java
@@ -9,6 +9,15 @@ import java.util.List;
 public record SpotifyRefreshTokenRequest(@JsonProperty("grant_type") String grantType,
                                          @JsonProperty("refresh_token") String refreshToken) {
 
+    public SpotifyRefreshTokenRequest {
+        if (grantType == null || grantType.isEmpty()) {
+            throw new IllegalArgumentException("Grant type cannot be null or empty");
+        }
+        if (refreshToken == null || refreshToken.isEmpty()) {
+            throw new IllegalArgumentException("Refresh token cannot be null or empty");
+        }
+    }
+
     public MultiValueMap<String, String> toMultiValueMap() {
         MultiValueMap<String, String> formData = new LinkedMultiValueMap<>();
         formData.put("grant_type", List.of(this.grantType));
