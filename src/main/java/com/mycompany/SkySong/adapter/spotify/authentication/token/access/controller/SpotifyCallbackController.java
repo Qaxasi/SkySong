1b793092dd02c8fcdf3f3a6ec30683818f53eed0
Champel Mariusz <champelmariusz@gmail.com>
2024-10-07 23:28:48 +0200

    refactor: Remove unnecessary validation from SpotifyCallbackController

--- src/main/java/com/mycompany/SkySong/adapter/spotify/authentication/token/access/controller/SpotifyCallbackController.java
+++ src/main/java/com/mycompany/SkySong/adapter/spotify/authentication/token/access/controller/SpotifyCallbackController.java
@@ -3,7 +3,6 @@ package com.mycompany.SkySong.adapter.spotify.authentication.token.access.contro
 import com.mycompany.SkySong.adapter.spotify.authentication.token.access.handler.SpotifyAccessTokenHandler;
 import com.mycompany.SkySong.adapter.utils.CookieUtils;
 import com.mycompany.SkySong.application.shared.dto.ApiResponse;
-import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
@@ -24,19 +23,14 @@ public class SpotifyCallbackController {
 
     @GetMapping("/callback")
     public ResponseEntity<ApiResponse> handleSpotifyCallback(@RequestParam("code") String authCode,
-                                                             @CookieValue(name = "jwtToken") String jwtToken,
-                                                             HttpServletResponse response) {
-        if (authCode == null || authCode.isEmpty()) {
-            throw new IllegalArgumentException("Authorization code is missing or invalid");
-        }
-        if (jwtToken == null || jwtToken.isEmpty()) {
+                                                             @CookieValue(name = "jwtToken") String jwtToken) {
+        if (jwtToken.isEmpty()) {
             throw new IllegalArgumentException("Jwt token is missing or invalid");
         }
 
         String accessToken = tokenHandler.retrieveSpotifyAccessToken(authCode, jwtToken);
 
         ResponseCookie cookie = cookieUtils.generateCookie("spotifyAccessToken", accessToken, "/api/v1/spotify", 3600);
-
 
         return ResponseEntity.ok()
                 .header(HttpHeaders.SET_COOKIE, cookie.toString())
