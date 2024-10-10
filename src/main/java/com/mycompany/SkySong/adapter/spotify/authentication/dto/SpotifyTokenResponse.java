203fecec346a40f2039dad0ae7a532a162a8aee9
Champel Mariusz <champelmariusz@gmail.com>
2024-10-10 22:48:15 +0200

    refactor: Remove unused fields and add validation for others in SpotifyTokenResponse

--- src/main/java/com/mycompany/SkySong/adapter/spotify/authentication/dto/SpotifyTokenResponse.java
+++ src/main/java/com/mycompany/SkySong/adapter/spotify/authentication/dto/SpotifyTokenResponse.java
@@ -3,9 +3,19 @@ package com.mycompany.SkySong.adapter.spotify.authentication.dto;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public record SpotifyTokenResponse(@JsonProperty("access_token") String accessToken,
-                                   @JsonProperty("token_type") String tokenType,
-                                   @JsonProperty("expires_in") int expirationTimeInSec,
                                    @JsonProperty("refresh_token") String refreshToken,
                                    @JsonProperty("scope") String scope) {
+
+    public SpotifyTokenResponse {
+        if (accessToken == null || accessToken.isEmpty()) {
+            throw new IllegalArgumentException("Access token cannot be null or empty");
+        }
+        if (refreshToken == null || refreshToken.isEmpty()) {
+            throw new IllegalArgumentException("Refresh token cannot be null or empty");
+        }
+        if (scope == null || scope.isEmpty()) {
+            throw new IllegalArgumentException("Scope cannot be null or empty");
+        }
+     }
 }
     
