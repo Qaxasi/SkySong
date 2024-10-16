edec0ef5b9c4786dc6d40055a3aaded8660ab48c
Champel Mariusz <champelmariusz@gmail.com>
2024-10-16 22:08:25 +0200

    refactor: Rename dto and remove validation

--- src/main/java/com/mycompany/SkySong/adapter/spotify/authentication/dto/SpotifyTokenResponse.java
+++ src/main/java/com/mycompany/SkySong/adapter/spotify/authentication/dto/SpotifyTokenResponse.java
@@ -1,26 +1,8 @@
 package com.mycompany.SkySong.adapter.spotify.authentication.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.mycompany.SkySong.shared.utils.Result;
 
-public record SpotifyAccessTokenResponse(@JsonProperty("access_token") String accessToken,
-                                         @JsonProperty("refresh_token") String refreshToken,
-                                         @JsonProperty("scope") String scope) {
-
-    public static Result<SpotifyAccessTokenResponse> fromApiResponse(String accessToken,
-                                                                     String refreshToken,
-                                                                     String scope) {
-
-        if (accessToken == null || accessToken.isEmpty()) {
-            return Result.failure("Access token cannot be null or empty.");
-        }
-        if (refreshToken == null || refreshToken.isEmpty()) {
-            return Result.failure("Refresh token cannot be null or empty");
-        }
-        if (scope == null || scope.isEmpty()) {
-            return Result.failure("Scope cannot be null or empty");
-        }
-        return Result.success(new SpotifyAccessTokenResponse(accessToken, refreshToken, scope));
-     }
-}
+public record SpotifyTokenResponse(@JsonProperty("access_token") String accessToken,
+                                   @JsonProperty("refresh_token") String refreshToken,
+                                   @JsonProperty("scope") String scope) {}
     
