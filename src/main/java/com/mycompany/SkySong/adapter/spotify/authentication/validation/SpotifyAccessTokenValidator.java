79469ddc17d413e6ae4b07be77e83c6201e2d4d4
Champel Mariusz <champelmariusz@gmail.com>
2024-10-20 22:52:56 +0200

    feat: Add ErrorType to validations

--- src/main/java/com/mycompany/SkySong/adapter/spotify/authentication/validation/SpotifyAccessTokenValidator.java
+++ src/main/java/com/mycompany/SkySong/adapter/spotify/authentication/validation/SpotifyAccessTokenValidator.java
@@ -2,31 +2,32 @@ package com.mycompany.SkySong.adapter.spotify.authentication.validation;
 
 import com.mycompany.SkySong.adapter.spotify.authentication.dto.SpotifyAccessTokenRequest;
 import com.mycompany.SkySong.adapter.spotify.authentication.dto.SpotifyTokenResponse;
+import com.mycompany.SkySong.shared.utils.ErrorType;
 import com.mycompany.SkySong.shared.utils.Result;
 
 public class SpotifyAccessTokenValidator {
     public Result<Void> validateRequest(SpotifyAccessTokenRequest request) {
         if (request.grantType() == null || request.grantType().isEmpty()) {
-            Result.failure("Grant type cannot be null or empty");
+            Result.failure("Grant type cannot be null or empty", ErrorType.BAD_REQUEST);
         }
         if (request.authCode() == null || request.authCode().isEmpty()) {
-            Result.failure("Authorization code cannot be null or empty");
+            Result.failure("Authorization code cannot be null or empty", ErrorType.BAD_REQUEST);
         }
         if (request.redirectUri() == null || request.redirectUri().isEmpty()) {
-            Result.failure("Redirect uri cannot be null or empty");
+            Result.failure("Redirect uri cannot be null or empty", ErrorType.BAD_REQUEST);
         }
         return Result.success(null);
     }
 
     public Result<Void> validateResponse(SpotifyTokenResponse response) {
         if (response.accessToken() == null || response.accessToken().isEmpty()) {
-            return Result.failure("Access token cannot be null or empty");
+            return Result.failure("Access token cannot be null or empty",ErrorType.UNPROCESSABLE_ENTITY);
         }
         if (response.refreshToken() == null || response.refreshToken().isEmpty()) {
-            return Result.failure("Refresh token cannot be null or empty");
+            return Result.failure("Refresh token cannot be null or empty", ErrorType.UNPROCESSABLE_ENTITY);
         }
         if (response.scope() == null || response.scope().isEmpty()) {
-            return Result.failure("Scope cannot be null or empty");
+            return Result.failure("Scope cannot be null or empty", ErrorType.UNPROCESSABLE_ENTITY);
         }
 
         return Result.success(null);
--- src/main/java/com/mycompany/SkySong/adapter/spotify/authentication/validation/SpotifyRefreshTokenValidator.java
+++ src/main/java/com/mycompany/SkySong/adapter/spotify/authentication/validation/SpotifyRefreshTokenValidator.java
@@ -2,25 +2,26 @@ package com.mycompany.SkySong.adapter.spotify.authentication.validation;
 
 import com.mycompany.SkySong.adapter.spotify.authentication.dto.SpotifyRefreshTokenRequest;
 import com.mycompany.SkySong.adapter.spotify.authentication.dto.SpotifyTokenResponse;
+import com.mycompany.SkySong.shared.utils.ErrorType;
 import com.mycompany.SkySong.shared.utils.Result;
 
 public class SpotifyRefreshTokenValidator {
     public Result<Void> validateRequest(SpotifyRefreshTokenRequest request) {
         if (request.grantType() == null || request.grantType().isEmpty()) {
-            Result.failure("Grant type cannot be null or empty");
+            Result.failure("Grant type cannot be null or empty", ErrorType.BAD_REQUEST);
         }
         if (request.refreshToken() == null || request.refreshToken().isEmpty()) {
-            Result.failure("Refresh token cannot be null or empty");
+            Result.failure("Refresh token cannot be null or empty", ErrorType.BAD_REQUEST);
         }
         return Result.success(null);
     }
 
     public Result<Void> validateResponse(SpotifyTokenResponse response) {
         if (response.accessToken() == null || response.accessToken().isEmpty()) {
-            return Result.failure("Access token cannot be null or empty");
+            return Result.failure("Access token cannot be null or empty", ErrorType.UNPROCESSABLE_ENTITY);
         }
         if (response.scope() == null || response.scope().isEmpty()) {
-            return Result.failure("Scope cannot be null or empty");
+            return Result.failure("Scope cannot be null or empty", ErrorType.UNPROCESSABLE_ENTITY);
         }
 
         return Result.success(null);
