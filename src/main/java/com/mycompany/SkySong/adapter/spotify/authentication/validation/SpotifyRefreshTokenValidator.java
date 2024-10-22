006a7964cae98792d001a35478d59f120d44bd58
Champel Mariusz <champelmariusz@gmail.com>
2024-10-22 23:14:11 +0200

    feat: Annotate class as a spring service and add logging in SpotifyRefreshTokenValidator

--- src/main/java/com/mycompany/SkySong/adapter/spotify/authentication/validation/SpotifyRefreshTokenValidator.java
+++ src/main/java/com/mycompany/SkySong/adapter/spotify/authentication/validation/SpotifyRefreshTokenValidator.java
@@ -4,23 +4,31 @@ import com.mycompany.SkySong.adapter.spotify.authentication.dto.SpotifyRefreshTo
 import com.mycompany.SkySong.adapter.spotify.authentication.dto.SpotifyTokenResponse;
 import com.mycompany.SkySong.shared.utils.ErrorType;
 import com.mycompany.SkySong.shared.utils.Result;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
 
+@Service
+@Slf4j
 public class SpotifyRefreshTokenValidator {
     public Result<Void> validateRequest(SpotifyRefreshTokenRequest request) {
         if (request.grantType() == null || request.grantType().isEmpty()) {
-            Result.failure("Grant type cannot be null or empty", ErrorType.BAD_REQUEST);
+            log.error("Request validation failed: grant type is null or empty");
+            return Result.failure("Grant type cannot be null or empty", ErrorType.BAD_REQUEST);
         }
         if (request.refreshToken() == null || request.refreshToken().isEmpty()) {
-            Result.failure("Refresh token cannot be null or empty", ErrorType.BAD_REQUEST);
+            log.error("Request validation failed: refresh token is null or empty");
+            return Result.failure("Refresh token cannot be null or empty", ErrorType.BAD_REQUEST);
         }
         return Result.success(null);
     }
 
     public Result<Void> validateResponse(SpotifyTokenResponse response) {
         if (response.accessToken() == null || response.accessToken().isEmpty()) {
+            log.error("Response validation failed: access token is null or empty");
             return Result.failure("Access token cannot be null or empty", ErrorType.UNPROCESSABLE_ENTITY);
         }
         if (response.scope() == null || response.scope().isEmpty()) {
+            log.error("Response validation failed: scope is null or empty");
             return Result.failure("Scope cannot be null or empty", ErrorType.UNPROCESSABLE_ENTITY);
         }
 
