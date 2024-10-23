b58434bc8b087be668fecdc371333c42ae28a989
Champel Mariusz <champelmariusz@gmail.com>
2024-10-23 23:29:10 +0200

    test: Add test to verify successful response validation

---
+++ src/test/java/com/mycompany/SkySong/adapter/spotify/authentication/validation/SpotifyAccessTokenValidatorTest.java
@@ -0,0 +1,30 @@
+package com.mycompany.SkySong.adapter.spotify.authentication.validation;
+
+import com.mycompany.SkySong.adapter.spotify.authentication.dto.SpotifyTokenResponse;
+import com.mycompany.SkySong.shared.utils.Result;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SpotifyAccessTokenValidatorTest {
+
+    private SpotifyAccessTokenValidator validator;
+
+    @BeforeEach
+    void setup() {
+        validator = new SpotifyAccessTokenValidator();
+    }
+
+    @Test
+    void whenResponseValid_ValidationSuccess() {
+        SpotifyTokenResponse response = new SpotifyTokenResponse("accessToken", "refreshToken", "scope");
+        Result<Void> result = validateResponse(response);
+        assertThat(result.success()).isTrue();
+
+    }
+
+    private Result<Void> validateResponse(SpotifyTokenResponse response) {
+        return validator.validateResponse(response);
+    }
+}
