445597c14c28654630c49d8d6b1883e2d9acbcf1
Champel Mariusz <champelmariusz@gmail.com>
2024-10-24 21:45:55 +0200

    test: Add test to verify validation fails when refresh token is empty

--- src/test/java/com/mycompany/SkySong/adapter/spotify/authentication/validation/SpotifyAccessTokenValidatorTest.java
+++ src/test/java/com/mycompany/SkySong/adapter/spotify/authentication/validation/SpotifyAccessTokenValidatorTest.java
@@ -46,6 +46,16 @@ public class SpotifyAccessTokenValidatorTest {
         assertThat(result.errorType()).isEqualTo(ErrorType.UNPROCESSABLE_ENTITY);
     }
 
+    @Test
+    void whenRefreshTokenIsEmpty_ValidationFails() {
+        SpotifyTokenResponse response = new SpotifyTokenResponse("accessToken", "", "scope");
+
+        Result<Void> result = validateResponse(response);
+
+        assertThat(result.success()).isFalse();
+        assertThat(result.errorType()).isEqualTo(ErrorType.UNPROCESSABLE_ENTITY);
+    }
+
     private Result<Void> validateResponse(SpotifyTokenResponse response) {
         return validator.validateResponse(response);
     }
