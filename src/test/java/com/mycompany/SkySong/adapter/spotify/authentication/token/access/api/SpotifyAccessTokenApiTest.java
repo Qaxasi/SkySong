31cafee190384cdc5453ca5f24a9c8b3eb0d18b0
Champel Mariusz <champelmariusz@gmail.com>
2024-11-10 21:56:14 +0100

    test: Add test to verify successful access token retrieval

---
+++ src/test/java/com/mycompany/SkySong/adapter/spotify/authentication/token/access/api/SpotifyAccessTokenApiTest.java
@@ -0,0 +1,56 @@
+package com.mycompany.SkySong.adapter.spotify.authentication.token.access.api;
+
+import com.mycompany.SkySong.adapter.spotify.authentication.dto.SpotifyAccessTokenRequest;
+import com.mycompany.SkySong.adapter.spotify.authentication.dto.SpotifyTokenResponse;
+import com.mycompany.SkySong.adapter.spotify.authentication.token.access.validation.SpotifyAccessTokenValidator;
+import com.mycompany.SkySong.shared.utils.Result;
+import com.mycompany.SkySong.testutils.common.BaseWireMock;
+import com.mycompany.SkySong.testutils.utils.JsonFileLoader;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.io.IOException;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.post;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class SpotifyAccessTokenApiTest extends BaseWireMock {
+    private SpotifyAccessTokenApi tokenApi;
+    private String tokenResponse;
+
+    @BeforeEach
+    void setup() throws IOException {
+        WebClient webClient = WebClient.builder()
+                .baseUrl("http://localhost:8080/v1/spotify/auth")
+                .build();
+
+        tokenResponse = JsonFileLoader.loadJson("spotify_access_token_response.json");
+
+
+        SpotifyAccessTokenValidator validator = new SpotifyAccessTokenValidator();
+        tokenApi = new SpotifyAccessTokenApi("clientId", "clientSecret", webClient, validator);
+    }
+
+    @Test
+    void whenRequestSuccessful_ReturnNonEmptyAccessToken() {
+        wireMockServer.stubFor(post("/v1/spotify/auth/api/token")
+                .willReturn(aResponse()
+                        .withStatus(200)
+                        .withHeader("Content-Type", "application/json")
+                        .withBody(tokenResponse)));
+
+        Result<SpotifyTokenResponse> response = sendTokenRequest();
+
+        assertThat(response.data().accessToken())
+                .isNotBlank()
+                .isNotNull();
+    }
+
+    private Result<SpotifyTokenResponse> sendTokenRequest() {
+        SpotifyAccessTokenRequest request = new SpotifyAccessTokenRequest(
+                "authorization_code", "code", "http://localhost:8080/callback");
+        return tokenApi.fetchAccessToken(request);
+    }
+}
