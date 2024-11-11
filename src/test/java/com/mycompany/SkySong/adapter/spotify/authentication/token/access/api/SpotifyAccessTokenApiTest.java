9bf529b886cb10c36534bb9de16ef4bc3df7e035
Champel Mariusz <champelmariusz@gmail.com>
2024-11-11 20:40:06 +0100

    test: Add test to verify exception handling for 429 error

--- src/test/java/com/mycompany/SkySong/adapter/spotify/authentication/token/access/api/SpotifyAccessTokenApiTest.java
+++ src/test/java/com/mycompany/SkySong/adapter/spotify/authentication/token/access/api/SpotifyAccessTokenApiTest.java
@@ -3,6 +3,7 @@ package com.mycompany.SkySong.adapter.spotify.authentication.token.access.api;
 import com.mycompany.SkySong.adapter.exception.common.ApiAccessDeniedException;
 import com.mycompany.SkySong.adapter.exception.common.ApiAuthenticationException;
 import com.mycompany.SkySong.adapter.exception.common.ApiRequestTimeoutException;
+import com.mycompany.SkySong.adapter.exception.common.ApiTooManyRequestsException;
 import com.mycompany.SkySong.adapter.spotify.authentication.dto.SpotifyAccessTokenRequest;
 import com.mycompany.SkySong.adapter.spotify.authentication.dto.SpotifyTokenResponse;
 import com.mycompany.SkySong.adapter.spotify.authentication.token.access.validation.SpotifyAccessTokenValidator;
@@ -81,6 +82,17 @@ class SpotifyAccessTokenApiTest extends BaseWireMock {
                         .withBody("\"message\": \"Forbidden\"")));
 
         assertThrows(ApiAccessDeniedException.class, this::sendTokenRequest);
+    }
+
+    @Test
+    void whenRequestFailsWith429Error_ThrowException() {
+        wireMockServer.stubFor(post("/v1/spotify/auth/api/token")
+                .willReturn(aResponse()
+                        .withStatus(429)
+                        .withHeader("Content-Type", "application/json")
+                        .withBody("\"message\": \"To many requests\"")));
+
+        assertThrows(ApiTooManyRequestsException.class, this::sendTokenRequest);
     }
 
     private Result<SpotifyTokenResponse> sendTokenRequest() {
