38adf7153141afc686aa4b7bc2b42ca6c3020bb3
Champel Mariusz <champelmariusz@gmail.com>
2024-11-17 20:52:21 +0100

    refactor: Remove scope field from SpotifyTokenResponse

--- src/main/java/com/mycompany/SkySong/adapter/spotify/authentication/dto/SpotifyTokenResponse.java
+++ src/main/java/com/mycompany/SkySong/adapter/spotify/authentication/dto/SpotifyTokenResponse.java
@@ -3,6 +3,4 @@ package com.mycompany.SkySong.adapter.spotify.authentication.dto;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public record SpotifyTokenResponse(@JsonProperty("access_token") String accessToken,
-                                   @JsonProperty("refresh_token") String refreshToken,
-                                   @JsonProperty("scope") String scope) {}
-    
+                                   @JsonProperty("refresh_token") String refreshToken) {}
