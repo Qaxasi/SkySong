293738789cefc347a0e06133cf93534fa8f41001
Champel Mariusz <champelmariusz@gmail.com>
2025-02-23 20:05:06 +0100

    feat: Add WeatherEmotion enum

---
+++ src/main/java/com/mycompany/SkySong/domain/music/playlist/algorytm/model/WeatherEmotion.java
@@ -0,0 +1,12 @@
+package com.mycompany.SkySong.domain.music.playlist.algorytm.model;
+
+public enum WeatherEmotion {
+    SUNNY_HAPPY,
+    RELAXED_CLOUDY,
+    MELANCHOLIC_RAINY,
+    FREEZING_COLD,
+    CHILL_WINDY,
+    NIGHT_CALM,
+    ENERGETIC_STORM,
+    FOGGY_MYSTIC
+}
