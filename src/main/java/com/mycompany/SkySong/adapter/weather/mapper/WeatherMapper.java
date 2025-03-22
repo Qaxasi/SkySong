5724fb0b470a445fc06f95d861c389d25c0d98a9
Champel Mariusz <champelmariusz@gmail.com>
2025-03-22 20:27:42 +0100

    feat: Add rain and snow volume to Weather mapping

--- src/main/java/com/mycompany/SkySong/adapter/weather/mapper/WeatherMapper.java
+++ src/main/java/com/mycompany/SkySong/adapter/weather/mapper/WeatherMapper.java
@@ -6,11 +6,16 @@ import com.mycompany.SkySong.domain.weather.model.Weather;
 
 public class WeatherMapper {
     public Weather mapToModel(WeatherApiResponse weather) {
+        double rainVolume = weather.rain() != null ? weather.rain().rainVolume() : 0.0;
+        double snowVolume = weather.snow() != null ? weather.snow().snowVolume() : 0.0;
+
         return new Weather(
                 weather.atmosphericConditions().temperature(),
                 weather.atmosphericConditions().humidity(),
                 weather.clouds().cloudCoverage(),
                 weather.wind().speed(),
+                rainVolume,
+                snowVolume,
                 weather.daytime().sunrise(),
                 weather.daytime().sunset(),
                 weather.conditions().stream()
