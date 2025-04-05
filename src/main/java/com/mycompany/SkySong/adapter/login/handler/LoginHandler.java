cc59a7c6f384fd1a9227aa2b1f3f9e87c2441c64
Champel Mariusz <champelmariusz@gmail.com>
2025-04-05 17:15:36 +0200

    refactor: Add logs to login handler

--- src/main/java/com/mycompany/SkySong/adapter/login/handler/LoginHandler.java
+++ src/main/java/com/mycompany/SkySong/adapter/login/handler/LoginHandler.java
@@ -4,6 +4,7 @@ import com.mycompany.SkySong.adapter.login.dto.LoginDto;
 import com.mycompany.SkySong.adapter.login.dto.LoginResponse;
 import com.mycompany.SkySong.adapter.security.user.CustomUserDetails;
 import com.mycompany.SkySong.adapter.security.jwt.JwtTokenManager;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -11,6 +12,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 public class LoginHandler {
 
@@ -24,6 +26,7 @@ public class LoginHandler {
     }
 
     public LoginResponse login(LoginDto loginDto) {
+        log.debug("Login attempt for username/email - {}", loginDto.usernameOrEmail());
         try {
             Authentication authentication = authManager.authenticate(
                     new UsernamePasswordAuthenticationToken(loginDto.usernameOrEmail(), loginDto.password()));
@@ -31,12 +34,14 @@ public class LoginHandler {
             SecurityContextHolder.getContext().setAuthentication(authentication);
 
             CustomUserDetails userDetails = (CustomUserDetails) authentication.getPrincipal();
+            log.info("User '{}' (id:{}) authenticated successfully", userDetails.getUsername(), userDetails.id());
 
             String jwtToken = tokenManager.generateToken(userDetails);
             String refreshToken = tokenManager.generateRefreshToken(userDetails);
 
             return new LoginResponse(jwtToken, refreshToken);
         } catch (BadCredentialsException e) {
+            log.warn("Failed login attempt for user/email - {}", loginDto.usernameOrEmail());
             throw new BadCredentialsException("Invalid username or password.");
         }
     }
