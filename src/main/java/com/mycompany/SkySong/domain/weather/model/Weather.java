da3aa8bf12ed2ee7ad88b0c46fe59d5f5359eddd
Champel Mariusz <champelmariusz@gmail.com>
2025-04-06 16:32:38 +0200

    refactor: Remove conditions field from Weather model

--- src/main/java/com/mycompany/SkySong/domain/weather/model/Weather.java
+++ src/main/java/com/mycompany/SkySong/domain/weather/model/Weather.java
@@ -1,16 +1,13 @@
 package com.mycompany.SkySong.domain.weather.model;
 
-import java.util.List;
-
 public record Weather (
         double temperature,
         int humidity,
         int cloudCoverage,
         double windSpeed,
         double rainVolume,
-
+        double snowVolume,
         int sunrise,
-        int sunset,
-        List<String> conditions
+        int sunset
 ) {
 }
