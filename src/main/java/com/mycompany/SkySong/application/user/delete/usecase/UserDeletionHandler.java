ebbebde33367b5fc4025b5b873997b4fbd56e7f4
Champel Mariusz <champelmariusz@gmail.com>
2025-04-23 21:41:12 +0200

    refactor: Add logging in UserDeletionHandler

--- src/main/java/com/mycompany/SkySong/application/user/delete/usecase/UserDeletionHandler.java
+++ src/main/java/com/mycompany/SkySong/application/user/delete/usecase/UserDeletionHandler.java
@@ -1,18 +1,23 @@
 package com.mycompany.SkySong.application.user.delete.usecase;
 
 import com.mycompany.SkySong.application.shared.dto.ApiResponse;
-import com.mycompany.SkySong.domain.user.delete.ports.DeleteUser;
+import com.mycompany.SkySong.application.user.delete.ports.UserDeletion;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
 
+@Service
+@Slf4j
 public class UserDeletionHandler {
 
-    private final DeleteUser deleteUser;
+    private final UserDeletion userDeletion;
 
-    public UserDeletionHandler(DeleteUser deleteUser) {
-        this.deleteUser = deleteUser;
+    public UserDeletionHandler(UserDeletion userDeletion) {
+        this.userDeletion = userDeletion;
     }
 
     public ApiResponse delete(int userId) {
-        deleteUser.deleteEverythingById(userId);
-        return new ApiResponse(String.format("User with ID %d deleted successfully.", userId));
+        userDeletion.deleteEverythingById(userId);
+        log.info("User with id: {} deleted successfully", userId);
+        return new ApiResponse("User deleted successfully.");
     }
 }
