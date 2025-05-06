82f427b0b2de3c92e8f48620b759f0c024554bda
Champel Mariusz <champelmariusz@gmail.com>
2025-05-06 22:15:13 +0200

    refactor: update controller response type

--- src/main/java/com/mycompany/SkySong/adapter/user/registration/controller/RegistrationController.java
+++ src/main/java/com/mycompany/SkySong/adapter/user/registration/controller/RegistrationController.java
@@ -5,6 +5,7 @@ import com.mycompany.SkySong.adapter.user.registration.dto.RegistrationRequest;
 import com.mycompany.SkySong.application.user.registration.dto.UserRegistrationDto;
 import com.mycompany.SkySong.application.user.registration.usecase.UserRegistration;
 import com.mycompany.SkySong.shared.response.ApiResponse;
+import com.mycompany.SkySong.shared.response.BaseResponse;
 import com.mycompany.SkySong.shared.result.Result;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
@@ -25,7 +26,7 @@ public class RegistrationController {
     }
 
     @PostMapping("/register")
-    public ResponseEntity<Object> register(@Valid @RequestBody final RegistrationRequest request) {
+    public ResponseEntity<BaseResponse> register(@Valid @RequestBody final RegistrationRequest request) {
         final UserRegistrationDto dto = mapper.toDto(request);
         final Result<ApiResponse> result = registration.registerUser(dto);
 
