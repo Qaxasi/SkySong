6a36e2a839535c65c8860e4c11e2f3a39758600a
Champel Mariusz <champelmariusz@gmail.com>
2025-05-19 20:00:04 +0200

    refactor: add DomainException

---
+++ src/main/java/com/mycompany/SkySong/domain/shared/exception/DomainException.java
@@ -0,0 +1,7 @@
+package com.mycompany.SkySong.domain.shared.exception;
+
+public class DomainException extends RuntimeException {
+    public DomainException(String message) {
+        super(message);
+    }
+}
