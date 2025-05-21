c58e1d94900f770bfd4704d0d701722054936656
Champel Mariusz <champelmariusz@gmail.com>
2025-05-21 20:13:51 +0200

    refactor: delegate user id validation to use case

--- src/main/java/com/mycompany/SkySong/adapter/user/deletion/controller/UserDeletionController.java
+++ src/main/java/com/mycompany/SkySong/adapter/user/deletion/controller/UserDeletionController.java
@@ -1,9 +1,6 @@
 package com.mycompany.SkySong.adapter.user.deletion.controller;
 
-import com.mycompany.SkySong.application.shared.dto.ApiResponse;
 import com.mycompany.SkySong.application.user.delete.usecase.UserDeletionUseCase;
-import com.mycompany.SkySong.shared.error.ErrorResponse;
-import com.mycompany.SkySong.shared.error.ErrorType;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -20,17 +17,6 @@ public class UserDeletionController {
 
     @DeleteMapping({"/","/{userId}"})
     public ResponseEntity<Object> delete(@PathVariable(required = false) final Integer userId) {
-        if (userId == null) {
-            return ResponseEntity
-                    .status(HttpStatus.UNPROCESSABLE_ENTITY)
-                    .body(new ErrorResponse(
-                            "User ID is required and cannot be empty.",
-                            ErrorType.USER_REQUEST_INVALID.name(),
-                            ErrorType.USER_REQUEST_INVALID.getHttpStatus().value()));
-
-        }
-
-        ApiResponse deleteResponse = userDeletion.delete(userId);
-        return new ResponseEntity<>(deleteResponse, HttpStatus.OK);
+        return new ResponseEntity<>(userDeletion.delete(userId), HttpStatus.OK);
     }
 }
