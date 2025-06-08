0ba24efc0bea87ab3ddf9717d86c99a5ff7e4d88
Champel Mariusz <champelmariusz@gmail.com>
2025-06-08 18:20:55 +0200

    refactor: migrate UserRegistrationValidator to use UserRegistrationInput

--- src/main/java/com/mycompany/SkySong/identity/application/registration/validator/UserRegistrationValidator.java
+++ src/main/java/com/mycompany/SkySong/identity/application/registration/validator/UserRegistrationValidator.java
@@ -1,7 +1,7 @@
 package com.mycompany.SkySong.identity.application.registration.validator;
 
+import com.mycompany.SkySong.identity.application.registration.dto.UserRegistrationInput;
 import com.mycompany.SkySong.identity.application.registration.exception.UserRegistrationValidationException;
-import com.mycompany.SkySong.domain.user.registration.model.UserRegistrationData;
 import com.mycompany.SkySong.identity.application.registration.ports.RegistrationUserRepository;
 import com.mycompany.SkySong.shared.error.ErrorType;
 
@@ -12,36 +12,36 @@ public class UserRegistrationValidator {
         this.userRepository = userRepository;
     }
 
-    public void validate(UserRegistrationData data) {
-        validateFormat(data);
-        validateUniqueness(data);
+    public void validate(final UserRegistrationInput input) {
+        validateFormat(input);
+        validateUniqueness(input);
     }
 
-    private void validateFormat(final UserRegistrationData data) {
-        if (!data.username().matches("^[a-zA-Z0-9]{3,20}$")) {
+    private void validateFormat(final UserRegistrationInput input) {
+        if (!input.username().matches("^[a-zA-Z0-9]{3,20}$")) {
             throw new UserRegistrationValidationException("Invalid username format. The username can contain only letters " +
                     "and numbers, and should be between 3 and 20 characters long.",
                     ErrorType.INVALID_USERNAME_FORMAT);
         }
-        if (!data.email().matches("^[a-zA-Z0-9._%+-]{1,15}@[a-zA-Z0-9.-]{2,15}\\.[a-zA-Z]{2,6}$")) {
+        if (!input.email().matches("^[a-zA-Z0-9._%+-]{1,15}@[a-zA-Z0-9.-]{2,15}\\.[a-zA-Z]{2,6}$")) {
             throw new UserRegistrationValidationException("Invalid email address format. The email should follow the standard " +
                     "format (e.g., user@example.com) and be between 6 and 30 characters long.",
                     ErrorType.INVALID_EMAIL_FORMAT);
         }
-        if (!data.password().matches("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}$")) {
+        if (!input.password().matches("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}$")) {
             throw new UserRegistrationValidationException("Invalid password format. The password must contain an least " +
                     "8 characters, including uppercase letters, lowercase letters, numbers, and special characters.",
                     ErrorType.INVALID_PASSWORD_FORMAT);
         }
     }
 
-    private void validateUniqueness(final UserRegistrationData data) {
-        if (userRepository.existsByUsername(data.username())) {
+    private void validateUniqueness(final UserRegistrationInput input) {
+        if (userRepository.existsByUsername(input.username())) {
             throw new UserRegistrationValidationException(
                     "Username already exist!.",
                     ErrorType.USERNAME_EXIST);
         }
-        if (userRepository.existsByEmail(data.email())) {
+        if (userRepository.existsByEmail(input.email())) {
             throw new UserRegistrationValidationException(
                     "Email already exist!.",
                     ErrorType.EMAIL_EXIST);
