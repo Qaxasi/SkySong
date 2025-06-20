72897d6f3bbc1d667c0d121ae1a9a33e6bc2d1b7
Champel Mariusz <champelmariusz@gmail.com>
2025-06-20 15:16:15 +0200

    refactor: add UserRole parameter to RoleProvider port

--- src/main/java/com/mycompany/SkySong/identity/adapter/registration/out/persistence/SqlDefaultRoleProvider.java
+++ src/main/java/com/mycompany/SkySong/identity/adapter/registration/out/persistence/SqlDefaultRoleProvider.java
@@ -1,17 +1,12 @@
 package com.mycompany.SkySong.identity.adapter.registration.out.persistence;
 
-import com.mycompany.SkySong.identity.application.registration.exception.UserRoleConfigurationException;
 import com.mycompany.SkySong.identity.application.registration.ports.DefaultRoleProvider;
 import com.mycompany.SkySong.identity.domain.Role;
 import com.mycompany.SkySong.identity.domain.UserRole;
 import com.mycompany.SkySong.infrastructure.persistence.sql.RoleDAO;
-import com.mycompany.SkySong.shared.error.ErrorType;
-import com.mycompany.SkySong.shared.logging.ApplicationLogger;
 import org.springframework.stereotype.Component;
 
 import java.util.Optional;
-
-import static com.mycompany.SkySong.shared.logging.ApplicationLogger.Context.context;
 
 @Component
 public class SqlDefaultRoleProvider implements DefaultRoleProvider {
@@ -22,8 +17,8 @@ public class SqlDefaultRoleProvider implements DefaultRoleProvider {
     }
 
     @Override
-    public Optional<Role> provideDefaultRole() {
-        return roleDAO.findByName(UserRole.ROLE_USER);
+    public Optional<Role> provideRole(UserRole role) {
+        return roleDAO.findByName(role);
     }
 }
 
--- src/main/java/com/mycompany/SkySong/identity/application/registration/ports/RoleProvider.java
+++ src/main/java/com/mycompany/SkySong/identity/application/registration/ports/RoleProvider.java
@@ -1,9 +1,10 @@
 package com.mycompany.SkySong.identity.application.registration.ports;
 
 import com.mycompany.SkySong.identity.domain.Role;
+import com.mycompany.SkySong.identity.domain.UserRole;
 
 import java.util.Optional;
 
-public interface DefaultRoleProvider {
-    Optional<Role> provideDefaultRole();
+public interface RoleProvider {
+    Optional<Role> provideRole(UserRole role);
 }
