cd7159999b24837e8c9ce9c71050f92025934d55
Champel Mariusz <champelmariusz@gmail.com>
2025-08-20 19:38:36 +0200

    refactor(registration): replace @NotEmpty with @NotBlank and add @Email in RegistrationRequest

--- src/main/java/com/mycompany/SkySong/identity/registration/adapter/in/RegistrationRequest.java
+++ src/main/java/com/mycompany/SkySong/identity/registration/adapter/in/RegistrationRequest.java
@@ -1,13 +1,13 @@
 package com.mycompany.SkySong.identity.registration.adapter.in;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 
-public record RegistrationRequest(@NotEmpty(message = "The username field cannot be empty")
+public record RegistrationRequest(@NotBlank(message = "The username field cannot be empty")
                                   @JsonProperty String username,
-                                  @NotEmpty(message = "The email field cannot be empty")
+                                  @NotBlank(message = "The email field cannot be empty")
+                                  @Email(message = "Invalid email address. Use the format name@domain (e.g., john.doe@example.com).")
                                   @JsonProperty String email,
-                                  @NotEmpty(message = "The password field cannot be empty")
-                                  @JsonProperty String password) {
-
-}
+                                  @NotBlank(message = "The password field cannot be empty")
+                                  @JsonProperty String password) {}
