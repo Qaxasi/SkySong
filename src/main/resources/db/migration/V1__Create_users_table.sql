5006c3b5e1942285b7f65c51406bd72ef6cb2229
Champel Mariusz <champelmariusz@gmail.com>
2025-08-21 21:41:24 +0200

    feat(db): add account status flags to user in sql script

--- src/main/resources/db/migration/V1__Create_users_table.sql
+++ src/main/resources/db/migration/V1__Create_users_table.sql
@@ -2,5 +2,7 @@ CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(50) NOT NULL UNIQUE,
     email VARCHAR(100) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL
-    );
+    password VARCHAR(255) NOT NULL,
+    enabled TINYINT(1) NOT NULL DEFAULT 1,
+    locked TINYINT(1) NOT NULL DEFAULT 0
+ );
