b28ddf331a5e1c645c4057533d80f31394eb82f8
Champel Mariusz <champelmariusz@gmail.com>
2025-08-21 21:48:14 +0200

    feat(db): set FK actions and add role_id index in sql script

--- src/main/resources/db/migration/V3__Create_user_roles_table.sql
+++ src/main/resources/db/migration/V3__Create_user_roles_table.sql
@@ -2,6 +2,9 @@ CREATE TABLE user_roles (
     user_id INT NOT NULL,
     role_id INT NOT NULL,
     PRIMARY KEY (user_id, role_id),
-    FOREIGN KEY (user_id) REFERENCES users (id),
-    FOREIGN KEY (role_id) REFERENCES roles (id)
+    KEY idx_user_roles_role_id (role_id),
+    CONSTRAINT fk_user_roles_user
+      FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+    CONSTRAINT fk_user_roles_role
+      FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE RESTRICT
 );
