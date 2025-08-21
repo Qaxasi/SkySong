03a2983bc9657fd0c6d7ceba75d84f6446d7603b
Champel Mariusz <champelmariusz@gmail.com>
2025-08-21 21:55:41 +0200

    fix(sql): update users insert script to match new added columns

--- src/main/resources/db/migration/V6__Create_admin_account.sql
+++ src/main/resources/db/migration/V6__Create_admin_account.sql
@@ -1,2 +1,2 @@
-INSERT INTO users (username, email, password) VALUES
-('Admin', 'admin@gmail.com', '$2a$10$tnbX1aE1Y5GGNBBSYgDJd.gyKCQT1dkXUezImX6OSE4TrMnEyyPKu');
+INSERT INTO users (username, email, password, enabled, locked) VALUES
+('Admin', 'admin@gmail.com', '$2a$10$tnbX1aE1Y5GGNBBSYgDJd.gyKCQT1dkXUezImX6OSE4TrMnEyyPKu', 1, 0);
