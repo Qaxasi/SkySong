d3e1a8d4d501fca35ad3ef122394c2db07db678b
Champel Mariusz <champelmariusz@gmail.com>
2025-08-27 21:37:48 +0200

    refactor(user-auth): use HttpErrorMapper to errors response, add cookie retrieval error mapping

--- src/main/java/com/mycompany/SkySong/identity/authentication/adapter/in/web/refresh/TokenRefresherController.java
+++ src/main/java/com/mycompany/SkySong/identity/authentication/adapter/in/web/refresh/TokenRefresherController.java
@@ -1,14 +1,17 @@
 package com.mycompany.SkySong.identity.authentication.adapter.in.web.refresh;
 
+import com.mycompany.SkySong.infrastructure.http.ErrorView;
+import com.mycompany.SkySong.infrastructure.http.HttpErrorMapper;
 import com.mycompany.SkySong.infrastructure.security.jwt.JwtAccessTokenProperties;
 import com.mycompany.SkySong.infrastructure.security.refreshToken.RefreshTokenProperties;
 import com.mycompany.SkySong.identity.authentication.application.refresh.service.TokenRefresher;
 import com.mycompany.SkySong.infrastructure.cookie.CookieUtils;
-import com.mycompany.SkySong.shared.response.BaseResponse;
-import com.mycompany.SkySong.shared.response.SuccessResponse;
+import com.mycompany.SkySong.shared.error.ErrorType;
+import com.mycompany.SkySong.infrastructure.http.response.BaseResponse;
+import com.mycompany.SkySong.infrastructure.http.response.SuccessResponse;
+import com.mycompany.SkySong.shared.result.Result;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,44 +23,45 @@ import org.springframework.web.bind.annotation.RestController;
 public class TokenRefresherController {
 
     private final CookieUtils cookieUtils;
-    private final TokenRefresher accessTokenRefresher;
+    private final TokenRefresher tokenRefresher;
     private final JwtAccessTokenProperties accessTokenProperties;
     private final RefreshTokenProperties refreshTokenProperties;
+    private final HttpErrorMapper errorMapper;
 
     public TokenRefresherController(final CookieUtils cookieUtils,
-                                    final TokenRefresher accessTokenRefresher,
+                                    final TokenRefresher tokenRefresher,
                                     final JwtAccessTokenProperties accessTokenProperties,
-                                    final RefreshTokenProperties refreshTokenProperties) {
+                                    final RefreshTokenProperties refreshTokenProperties,
+                                    final HttpErrorMapper errorMapper) {
         this.cookieUtils = cookieUtils;
-        this.accessTokenRefresher = accessTokenRefresher;
+        this.tokenRefresher = tokenRefresher;
         this.accessTokenProperties = accessTokenProperties;
         this.refreshTokenProperties = refreshTokenProperties;
+        this.errorMapper = errorMapper;
     }
 
     @PostMapping("/refresh")
     public ResponseEntity<BaseResponse> refreshTokens(final HttpServletRequest request) {
         return cookieUtils.getCookieValue(request, refreshTokenProperties.cookie().name())
-                .fold(error -> ResponseEntity
-                        .status(HttpStatus.UNAUTHORIZED)
-                        .body(error.toErrorResponse()),
+                .mapError((type, message) ->
+                        type == ErrorType.COOKIE_NOT_FOUND
+                                ? Result.failure("Invalid refresh token", ErrorType.INVALID_REFRESH_TOKEN)
+                                : Result.failure(message, type))
+                .flatMap(refreshToken -> tokenRefresher.refresh(refreshToken))
+                .fold(
+                        error -> errorMapper.from(ErrorView.fromResult(error)),
 
-                        refreshToken -> accessTokenRefresher.refresh(refreshToken)
-                                .fold(error ->
-                                        ResponseEntity
-                                                .status(error.errorType().getHttpStatus())
-                                                .body(error.toErrorResponse()),
+                        tokens -> {
+                            final ResponseCookie accessTokenCookie = cookieUtils.generateCookie(
+                                    accessTokenProperties.cookie(), tokens.accessToken().value());
 
-                                        data -> {
-                                            final ResponseCookie accessTokenCookie = cookieUtils.generateCookie(
-                                                    accessTokenProperties.cookie(), data.accessToken().value());
+                            final ResponseCookie refreshTokenCookie = cookieUtils.generateCookie(
+                                    refreshTokenProperties.cookie(), tokens.refreshToken().value());
 
-                                            final ResponseCookie refreshTokenCookie = cookieUtils.generateCookie(
-                                                    refreshTokenProperties.cookie(), data.refreshToken().value());
-
-                                            return ResponseEntity.ok()
-                                                    .header(HttpHeaders.SET_COOKIE, accessTokenCookie.toString())
-                                                    .header(HttpHeaders.SET_COOKIE, refreshTokenCookie.toString())
-                                                    .body(new SuccessResponse("Your session has been successfully extended."));
-                                        }));
+                            return ResponseEntity.ok()
+                                    .header(HttpHeaders.SET_COOKIE, accessTokenCookie.toString())
+                                    .header(HttpHeaders.SET_COOKIE, refreshTokenCookie.toString())
+                                    .body(new SuccessResponse("Your session has been successfully extended."));
+                        });
     }
 }
