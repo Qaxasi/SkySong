74ee8ffcac6b7ad8bf91c2eb8fc408c87898cdc6
Champel Mariusz <champelmariusz@gmail.com>
2025-08-31 22:41:47 +0200

    fix(config): correct property key and script path

--- src/main/java/com/mycompany/SkySong/infrastructure/config/redis/RedisScriptProperties.java
+++ src/main/java/com/mycompany/SkySong/infrastructure/config/redis/RedisScriptProperties.java
@@ -3,7 +3,7 @@ package com.mycompany.SkySong.infrastructure.config.redis;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.core.io.Resource;
 
-@ConfigurationProperties(prefix = "redis.scripts")
+@ConfigurationProperties(prefix = "redis.session.script")
 public record RedisScriptProperties(Resource save,
-                                    Resource rotate) {
+                                    Resource rotateAndUpdate) {
 }
