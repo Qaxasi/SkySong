7aeec7a0ce008f1bd9f8047b5ad931620205d5e1
Champel Mariusz <champelmariusz@gmail.com>
2025-09-01 15:06:23 +0200

    fix(redis-lua): correct key usage in delete_user_sessions script and rename local variable

--- src/main/resources/redis-scripts/delete_user_sessions.lua
+++ src/main/resources/redis-scripts/delete_user_sessions.lua
@@ -1,18 +1,18 @@
 
 -- KEYS[1] = tokenSetKey (user set key)
--- KEYS[2] = sessionKeyPrefix (user session key prefix)
+-- KEYS[2] = mainKeyPrefix (user main key prefix)
 
 if (#KEYS ~= 2) then
     return redis.error_reply("delete_user_session script: wrong arity")
 end
 
 local tokenSetKey = KEYS[1]
-local sessionKeyPrefix = KEYS[2]
+local mainKeyPrefix = KEYS[2]
 
-local tokens = redis.call('SMEMBERS', KEYS[1])
+local tokens = redis.call('SMEMBERS', tokenSetKey)
 for _, token in ipairs(tokens) do
-    redis.call('UNLINK', KEYS[2] .. token)
+    redis.call('UNLINK', mainKeyPrefix .. token)
 end
 
-redis.call('UNLINK', KEYS[1])
+redis.call('UNLINK', tokenSetKey)
 return #tokens
