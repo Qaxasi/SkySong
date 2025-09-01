035ff5b47baf15936877755e2a66f9f26c2168c0
Champel Mariusz <champelmariusz@gmail.com>
2025-09-01 14:19:33 +0200

    refactor(config): change path for Redis script

--- src/main/java/com/mycompany/SkySong/infrastructure/config/redis/RedisScriptProperties.java
+++ src/main/java/com/mycompany/SkySong/infrastructure/config/redis/RedisScriptProperties.java
@@ -3,7 +3,7 @@ package com.mycompany.SkySong.infrastructure.config.redis;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.core.io.Resource;
 
-@ConfigurationProperties(prefix = "redis.session.script")
+@ConfigurationProperties(prefix = "redis.script.session")
 public record RedisScriptProperties(Resource save,
                                     Resource rotateAndUpdate,
                                     Resource deleteUserSessions) {
