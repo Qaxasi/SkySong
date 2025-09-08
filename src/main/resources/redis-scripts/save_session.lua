5859bb87ff1c30e8ec9d5fe246691a71e0b23ed9
Champel Mariusz <champelmariusz@gmail.com>
2025-09-08 22:14:10 +0200

    refactor(redis-lua): change variable name

--- src/main/resources/redis-scripts/save_session.lua
+++ src/main/resources/redis-scripts/save_session.lua
@@ -1,16 +1,17 @@
 
 -- KEYS[1] = mainKey
 -- KEYS[2] = tokensSetKey
--- ARGV[1] = json payload (String)
--- ARGV[2] = ttlSeconds (String/number)
--- ARGV[3] = token (String)
+-- ARGV[1] = json payload
+-- ARGV[2] = ttlSeconds
+-- ARGV[3] = token
 
 if (#KEYS ~= 2 or #ARGV ~= 3) then
     return redis.error_reply("saveSessionScript: wrong arity")
 end
 
 local mainKey = KEYS[1]
-local tokenSet = KEYS[2]
+local tokenSetKey = KEYS[2]
+
 local payload = ARGV[1]
 local ttl = tonumber(ARGV[2])
 local token = ARGV[3]
@@ -20,12 +21,11 @@ if (not ttl or ttl <= 0) then
 end
 
 redis.call('SET', mainKey, payload, 'EX', ttl)
+redis.call('SADD', tokenSetKey, token)
 
-redis.call('SADD', tokenSet, token)
-
-local curTtl = redis.call('TTL', tokenSet)
+local curTtl = redis.call('TTL', tokenSetKey)
 if (curTtl < 0 or curTtl < ttl) then
-    redis.call('EXPIRE', tokenSet, ttl)
+    redis.call('EXPIRE', tokenSetKey, ttl)
 end
 
 return 1
