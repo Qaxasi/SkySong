a54173f18b64700a273f117c6e3c20e2bd416348
Champel Mariusz <champelmariusz@gmail.com>
2025-09-11 16:13:12 +0200

    feat(auth-token): add sessionVersion to AccessTokenClaims

--- src/main/java/com/mycompany/SkySong/identity/authentication/application/shared/dto/AccessTokenClaims.java
+++ src/main/java/com/mycompany/SkySong/identity/authentication/application/shared/dto/AccessTokenClaims.java
@@ -3,5 +3,5 @@ package com.mycompany.SkySong.identity.authentication.application.shared.dto;
 
 import java.util.List;
 
-public record AccessTokenClaims(int userId, String username, List<String> roles) {
+public record AccessTokenClaims(int userId, String username, List<String> roles, long sessionVersion) {
 }
