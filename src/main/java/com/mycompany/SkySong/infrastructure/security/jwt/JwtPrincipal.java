623344a1b3be391907701b2e717351dc6e0598d0
Champel Mariusz <champelmariusz@gmail.com>
2025-09-14 22:28:20 +0200

    feat(jwt): add JwtPrincipal

---
+++ src/main/java/com/mycompany/SkySong/infrastructure/security/jwt/JwtPrincipal.java
@@ -0,0 +1,6 @@
+package com.mycompany.SkySong.infrastructure.security.jwt;
+
+import java.util.List;
+
+public record JwtPrincipal(int userId, String username, List<String> roles, long sessionVersion) {
+}
