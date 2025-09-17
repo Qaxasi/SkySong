b4240acfa24326293a70d99ee5b7090837920b2e
Champel Mariusz <champelmariusz@gmail.com>
2025-09-17 21:10:29 +0200

    feat(domain): add UserId value object

---
+++ src/main/java/com/mycompany/SkySong/identity/shared/domain/UserId.java
@@ -0,0 +1,22 @@
+package com.mycompany.SkySong.identity.shared.domain;
+
+import com.mycompany.SkySong.shared.error.ErrorType;
+import com.mycompany.SkySong.shared.result.Result;
+
+public record UserId(int value) {
+    public UserId {
+        if (value <= 0) {
+            throw new IllegalArgumentException("UserId must be positive");
+        }
+    }
+
+    public int asInt() {
+        return value;
+    }
+
+    public static Result<UserId> of(int value) {
+        return (value <= 0) ?
+                Result.failure("UserId must be positive", ErrorType.DATA_INTEGRITY_ERROR)
+                : Result.success(new UserId(value));
+    }
+}
