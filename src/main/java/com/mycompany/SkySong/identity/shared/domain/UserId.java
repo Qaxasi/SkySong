1c59e7fe3e24e5f0980252ef5501d56662521fbd
Champel Mariusz <champelmariusz@gmail.com>
2025-10-02 19:56:26 +0200

    refactor(identity): replace record with final class - UserId VO

--- src/main/java/com/mycompany/SkySong/identity/shared/domain/UserId.java
+++ src/main/java/com/mycompany/SkySong/identity/shared/domain/UserId.java
@@ -3,20 +3,41 @@ package com.mycompany.SkySong.identity.shared.domain;
 import com.mycompany.SkySong.shared.error.ErrorType;
 import com.mycompany.SkySong.shared.result.Result;
 
-public record UserId(int value) {
-    public UserId {
-        if (value <= 0) {
-            throw new IllegalArgumentException("UserId must be positive");
-        }
-    }
+import java.util.Objects;
 
-    public int asInt() {
-        return value;
+public final class UserId {
+    private final int value;
+
+    private UserId(int value) {
+        this.value = value;
     }
 
     public static Result<UserId> of(int value) {
         return (value <= 0) ?
                 Result.failure("UserId must be positive", ErrorType.INVARIANT_VIOLATION)
                 : Result.success(new UserId(value));
+    }
+
+    public int asInt() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) return true;
+        if (obj == null || obj.getClass() != this.getClass()) return false;
+        var that = (UserId) obj;
+        return this.value == that.value;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
+    }
+
+    @Override
+    public String toString() {
+        return "UserId[" +
+                "value=" + value + ']';
     }
 }
