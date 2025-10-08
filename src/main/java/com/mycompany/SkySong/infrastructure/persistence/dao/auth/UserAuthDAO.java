c7a9455319aec8481af3de2581d19bfa260a510f
Champel Mariusz <champelmariusz@gmail.com>
2025-10-08 20:28:18 +0200

    feat(persistence-auth): add UserAuthDAO with findByUsername

---
+++ src/main/java/com/mycompany/SkySong/infrastructure/persistence/dao/auth/UserAuthDAO.java
@@ -0,0 +1,23 @@
+package com.mycompany.SkySong.infrastructure.persistence.dao.auth;
+
+import org.jdbi.v3.sqlobject.config.RegisterConstructorMapper;
+import org.jdbi.v3.sqlobject.customizer.Bind;
+import org.jdbi.v3.sqlobject.statement.SqlQuery;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+@RegisterConstructorMapper(UserAuthView.class)
+public interface UserAuthDAO {
+    @SqlQuery("""
+            SELECT id AS userId,
+            username,
+            passwordHash,
+            enabled,
+            locked
+            FROM users
+            WHERE username = :username
+            """)
+    Optional<UserAuthView> findByUsername(@Bind String username);
+}
