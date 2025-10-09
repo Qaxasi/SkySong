658b7c72fd947576b71bf688395e0362acb93219
Champel Mariusz <champelmariusz@gmail.com>
2025-10-09 16:03:04 +0200

    refactor(identity): move properties to infrastructure.config.token package

--- src/main/java/com/mycompany/SkySong/identity/infrastructure/config/token/RefreshTokenProperties.java
+++ src/main/java/com/mycompany/SkySong/identity/infrastructure/config/token/RefreshTokenProperties.java
@@ -1,14 +1,16 @@
-package com.mycompany.SkySong.identity.infrastructure.config;
+package com.mycompany.SkySong.identity.infrastructure.config.token;
 
 import com.mycompany.SkySong.shared.cookie.CookieProperties;
 import jakarta.validation.Valid;
 
+import jakarta.validation.constraints.NotNull;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.validation.annotation.Validated;
+import java.time.Duration;
 
 @Validated
-@ConfigurationProperties(prefix = "application.security.jwt.access-token")
-public record JwtAccessTokenProperties(
-        @Min(1) int expiration,
+@ConfigurationProperties(prefix = "application.security.refresh-token")
+public record RefreshTokenProperties(
+        @NotNull Duration duration,
         @Valid CookieProperties cookie) {
 }
