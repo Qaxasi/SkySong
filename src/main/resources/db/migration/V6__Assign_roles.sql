c4cc5d0c62579be58d66ad5417d4b764805b3275
Champel Mariusz <champelmariusz@gmail.com>
2025-10-14 19:51:18 +0200

    chore(identity-auth): remove unused scripts

--- src/main/resources/db/migration/V6__Assign_roles.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO user_roles (user_id, role_id) VALUES
-((SELECT id FROM users WHERE username = 'Admin'), (SELECT id FROM roles WHERE name = 'ROLE_ADMIN'));
-
-INSERT INTO user_roles (user_id, role_id) VALUES
-((SELECT id FROM users WHERE username = 'Admin'), (SELECT id FROM roles WHERE name = 'ROLE_USER'));
