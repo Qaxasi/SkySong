bd3ba78d29861d40883761c7280fa722b6da4fcf
Champel Mariusz <champelmariusz@gmail.com>
2025-10-17 23:54:53 +0200

    refactor(identity-domain): simplify UserId VO
    - add LOWEST_VALID constant and use it in .of()
    - simplify equals with identity match
    - use Integer.hashCode
    - simplify toString to plain value

--- src/main/java/com/mycompany/SkySong/identity/authentication/domain/UserId.java
+++ src/main/java/com/mycompany/SkySong/identity/authentication/domain/UserId.java
@@ -3,19 +3,18 @@ package com.mycompany.SkySong.identity.authentication.domain;
 import com.mycompany.SkySong.shared.error.ErrorType;
 import com.mycompany.SkySong.shared.result.Result;
 
-import java.util.Objects;
-
 public final class UserId {
+    public static final int LOWEST_VALID = 1;
     private final int value;
 
     private UserId(final int value) {
         this.value = value;
     }
 
-    public static Result<UserId> of(final int value) {
-        return (value <= 0) ?
+    public static Result<UserId> of(final int rawUserId) {
+        return (rawUserId < LOWEST_VALID) ?
                 Result.failure("UserId must be positive", ErrorType.VALIDATION_ERROR)
-                : Result.success(new UserId(value));
+                : Result.success(new UserId(rawUserId));
     }
 
     public int asInt() {
@@ -23,21 +22,17 @@ public final class UserId {
     }
 
     @Override
-    public boolean equals(final Object obj) {
-        if (obj == this) return true;
-        if (obj == null || obj.getClass() != this.getClass()) return false;
-        var that = (UserId) obj;
-        return this.value == that.value;
+    public boolean equals(final Object o) {
+        return this == o || (o instanceof UserId other && value == other.value);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(value);
+        return Integer.hashCode(value);
     }
 
     @Override
     public String toString() {
-        return "UserId[" +
-                "value=" + value + ']';
+        return String.valueOf(value);
     }
 }
