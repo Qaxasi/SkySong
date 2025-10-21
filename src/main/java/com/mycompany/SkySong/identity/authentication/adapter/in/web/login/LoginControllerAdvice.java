a6211b32061ebc36bd291c80e857562992a2298e
Champel Mariusz <champelmariusz@gmail.com>
2025-10-21 14:47:45 +0200

    feat(identity-login): add controller advice to mask login input errors

---
+++ src/main/java/com/mycompany/SkySong/identity/authentication/adapter/in/web/login/LoginControllerAdvice.java
@@ -0,0 +1,27 @@
+package com.mycompany.SkySong.identity.authentication.adapter.in.web.login;
+
+import com.mycompany.SkySong.infrastructure.web.BaseResponse;
+import com.mycompany.SkySong.infrastructure.web.HttpErrorMapper;
+import com.mycompany.SkySong.shared.error.ErrorType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice(assignableTypes = LoginController.class)
+public class LoginControllerAdvice {
+    private final HttpErrorMapper mapper;
+
+    public LoginControllerAdvice(final HttpErrorMapper mapper) {
+        this.mapper = mapper;
+    }
+
+    @ExceptionHandler({
+            MethodArgumentNotValidException.class,
+            HttpMessageNotReadableException.class
+    })
+    public ResponseEntity<BaseResponse> maskLoginInputErrors() {
+        return mapper.failure("Invalid login credentials", ErrorType.INVALID_LOGIN_CREDENTIALS);
+    }
+}
