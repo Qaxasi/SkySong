cfe47519260e86f7824a7eb69e75f1684de39517
Champel Mariusz <champelmariusz@gmail.com>
2025-10-21 14:57:52 +0200

    feat(identity-auth): refactor LoginRequest
    - password: String -> char[]
    - set password WRITE_ONLY and add validation
    - override toString()

--- src/main/java/com/mycompany/SkySong/identity/authentication/adapter/in/web/login/LoginRequest.java
+++ src/main/java/com/mycompany/SkySong/identity/authentication/adapter/in/web/login/LoginRequest.java
@@ -1,10 +1,21 @@
 package com.mycompany.SkySong.identity.authentication.adapter.in.web.login;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 public record LoginRequest(
-        @NotBlank(message = "The username field cannot be blank")
+        @NotBlank
         String username,
-        @NotBlank(message = "The password field cannot be blank")
-        String password) {
+
+        @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+        @NotNull
+        @Size(min = 1, max = 1024)
+        char[] password) {
+
+        @Override
+        public String toString() {
+                return String.format("******");
+        }
 }
