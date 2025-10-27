933aabe7c1c3d6e06e2f87dcb118cfe9576cb4e2
Champel Mariusz <champelmariusz@gmail.com>
2025-10-27 23:19:09 +0100

    chore(web): remove unused response records

--- src/main/java/com/mycompany/SkySong/infrastructure/web/SuccessResponse.java
+++
@@ -1,7 +0,0 @@
-package com.mycompany.SkySong.infrastructure.http.response;
-
-public class SuccessResponse extends BaseResponse {
-    public SuccessResponse(String message) {
-        super(message);
-    }
-}
