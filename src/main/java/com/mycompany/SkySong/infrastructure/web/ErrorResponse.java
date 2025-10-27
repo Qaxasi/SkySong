10a5a1000ee993d86903a86fc27ff72e75af5cb4
Champel Mariusz <champelmariusz@gmail.com>
2025-10-27 23:14:21 +0100

    refactor(web-api): convert ErrorResponse to record

--- src/main/java/com/mycompany/SkySong/infrastructure/web/ErrorResponse.java
+++ src/main/java/com/mycompany/SkySong/infrastructure/web/ErrorResponse.java
@@ -1,38 +1,13 @@
-package com.mycompany.SkySong.infrastructure.http.response;
+package com.mycompany.SkySong.infrastructure.web;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 
 import java.util.Map;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
-public class ErrorResponse extends BaseResponse {
-    private final String errorCode;
-    private final int status;
-    @JsonInclude(JsonInclude.Include.NON_EMPTY)
-    private final Map<String, String> errors;
-
-    public ErrorResponse(String message, String errorCode, int status) {
-        super(message);
-        this.errorCode = errorCode;
-        this.status = status;
-        this.errors = null;
-    }
-
-    public ErrorResponse(Map<String, String> errors, String errorCode, int status) {
-        super(null);
-        this.errorCode = errorCode;
-        this.status = status;
-        this.errors = errors == null || errors.isEmpty() ? null : Map.copyOf(errors);
-    }
-    public String getErrorCode() {
-        return errorCode;
-    }
-
-    public int getStatus() {
-        return status;
-    }
-
-    public Map<String, String> getErrors() {
-        return errors;
-    }
+public record ErrorResponse(
+        String message,
+        String errorCode,
+        int status,
+        @JsonInclude(JsonInclude.Include.NON_EMPTY) Map<String, String> errors)  {
 }
