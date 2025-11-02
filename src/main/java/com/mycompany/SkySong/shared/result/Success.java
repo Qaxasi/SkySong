2ec7ff75faee2bc9548b16fcb9b64d7956b22600
Champel Mariusz <champelmariusz@gmail.com>
2025-11-02 22:51:12 +0100

    feat(shared-result): add Success<T> result type

---
+++ src/main/java/com/mycompany/SkySong/shared/result/Success.java
@@ -0,0 +1,16 @@
+package com.mycompany.SkySong.shared.result;
+
+import java.util.function.Function;
+
+public record Success<T>(T data) implements Result<T> {
+
+    @Override public boolean isSuccess() {
+        return true;
+    }
+
+    @Override
+    public <R> R fold(Function<? super Failure<T>, ? extends R> onFailure,
+                      Function<? super T, ? extends R> onSuccess) {
+        return onSuccess.apply(data);
+    }
+}
