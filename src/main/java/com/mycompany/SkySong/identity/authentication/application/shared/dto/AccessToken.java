ca9b779243edac3e2e0471f7cbfb9e88462241cc
Champel Mariusz <champelmariusz@gmail.com>
2025-11-04 20:46:32 +0100

    feat(identity-domain): add method to compute access token remaining ttl

--- src/main/java/com/mycompany/SkySong/identity/authentication/application/shared/dto/AccessToken.java
+++ src/main/java/com/mycompany/SkySong/identity/authentication/application/shared/dto/AccessToken.java
@@ -1,8 +1,12 @@
 package com.mycompany.SkySong.identity.authentication.application.shared.dto;
 
+import java.time.Duration;
 import java.time.Instant;
 
 public record AccessToken(String value, Instant expiresAt) {
+    public Duration remainingTtl(final Instant referenceTime) {
+        return Duration.between(referenceTime, expiresAt);
+    }
     @Override
     public String toString() {
         return "AccessToken(*****)";
