38c004313b203cc658828f6de4196c703f6f2226
Champel Mariusz <champelmariusz@gmail.com>
2025-11-07 15:29:29 +0100

    refactor(identity-auth): remove unecessary parameter from dto

--- src/main/java/com/mycompany/SkySong/identity/authentication/application/dto/AccessGrant.java
+++ src/main/java/com/mycompany/SkySong/identity/authentication/application/dto/AccessGrant.java
@@ -1,9 +1,8 @@
-package com.mycompany.SkySong.identity.authentication.application.shared.dto;
+package com.mycompany.SkySong.identity.authentication.application.dto;
 
 import com.mycompany.SkySong.identity.authentication.domain.RefreshToken;
 
 import java.time.Duration;
 
-public record AccessGrant(AccessToken accessToken, Duration accessTokenTtl,
-                          RefreshToken refreshToken, Duration refreshTokenTtl) {
+public record AccessGrant(AccessToken accessToken, Duration accessTokenTtl, RefreshToken refreshToken) {
 }
