a57b8c82cc0b3102761327f4250bab82b7d8cecd
Champel Mariusz <champelmariusz@gmail.com>
2025-11-08 17:11:43 +0100

    refactor(identity-auth): replace Duration parameter with long and pass access token ttl as seconds, add session ttl parameter

--- src/main/java/com/mycompany/SkySong/identity/authentication/application/dto/AccessGrant.java
+++ src/main/java/com/mycompany/SkySong/identity/authentication/application/dto/AccessGrant.java
@@ -4,5 +4,6 @@ import com.mycompany.SkySong.identity.authentication.domain.RefreshToken;
 
 import java.time.Duration;
 
-public record AccessGrant(AccessToken accessToken, Duration accessTokenTtl, RefreshToken refreshToken) {
+public record AccessGrant(AccessToken accessToken, long accessTokenExpiresInSec,
+                          RefreshToken refreshToken, Duration sessionTtl) {
 }
