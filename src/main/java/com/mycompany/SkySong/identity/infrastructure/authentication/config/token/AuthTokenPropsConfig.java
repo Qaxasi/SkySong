1e6d21097cbf45bd299e5369234b88565ed40587
Champel Mariusz <champelmariusz@gmail.com>
2025-11-09 20:08:28 +0100

    feat(identity-config): add CookieProperties bean

--- src/main/java/com/mycompany/SkySong/identity/infrastructure/authentication/config/token/AuthTokenPropsConfig.java
+++ src/main/java/com/mycompany/SkySong/identity/infrastructure/authentication/config/token/AuthTokenPropsConfig.java
@@ -1,9 +1,15 @@
 package com.mycompany.SkySong.identity.infrastructure.authentication.config.token;
 
+import com.mycompany.SkySong.shared.web.cookie.CookieProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @EnableConfigurationProperties({AccessTokenProperties.class, SessionProperties.class})
 class AuthTokenPropsConfig {
+    @Bean
+    CookieProperties cookieProperties(final SessionProperties properties) {
+        return properties.cookie();
+    }
 }
