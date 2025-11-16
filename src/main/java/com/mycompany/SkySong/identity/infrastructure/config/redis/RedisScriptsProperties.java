c2a6cc2854c2828d52b5017b1edbdf39b2b61725
Champel Mariusz <champelmariusz@gmail.com>
2025-11-16 22:50:58 +0100

    refactor(identity-auth): change config key path for session save, session refresh; remove delete user sessions path

---
+++ src/main/java/com/mycompany/SkySong/identity/infrastructure/config/redis/RedisScriptsProperties.java
@@ -0,0 +1,9 @@
+package com.mycompany.SkySong.identity.infrastructure.config.redis;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.core.io.Resource;
+
+@ConfigurationProperties(prefix = "redis.script.session")
+public record RedisScriptsProperties(Resource save,
+                                     Resource refresh) {
+}
