e7162b8f71181f8449927e10e6d30c05d88344b3
Champel Mariusz <champelmariusz@gmail.com>
2025-11-16 22:57:06 +0100

    refactor(identity-auth): remove hashes index from session save script

--- src/main/resources/redis-scripts/session_save.lua
+++ src/main/resources/redis-scripts/session_save.lua
@@ -1,34 +1,24 @@
 
--- KEYS[1] = mainKey
--- KEYS[2] = hashesSetKey
+-- KEYS[1] = key
 -- ARGV[1] = payload json
 -- ARGV[2] = ttlSeconds
--- ARGV[3] = hash (Base64URL(SHA-256))
 
-local RES_OK = 1
 
-if (#KEYS ~= 2 or #ARGV ~= 3) then
-    return redis.error_reply("wrong arity")
+local RESPONSE_OK = 1
+
+if (#KEYS ~= 1 or #ARGV ~= 2) then
+    return redis.error_reply("session.save - wrong arity")
 end
 
-local mainKey = KEYS[1]
-local setKey = KEYS[2]
-
+local key = KEYS[1]
 local payload = ARGV[1]
 local ttl = tonumber(ARGV[2]) or 0
-local hash = ARGV[3]
 
 ttl = math.floor(ttl)
 if ttl <= 0 then
-    return redis.error_reply("bad ttl")
+    return redis.error_reply("session save - bad ttl")
 end
 
-redis.call('SET', mainKey, payload, 'EX', ttl)
-redis.call('SADD', setKey, hash)
+redis.call('SET', key, payload, 'EX', ttl)
 
-local curTtl = redis.call('TTL', setKey)
-if (curTtl == -1 or curTtl < ttl) then
-    redis.call('EXPIRE', setKey, ttl)
-end
-
-return RES_OK
+return RESPONSE_OK
