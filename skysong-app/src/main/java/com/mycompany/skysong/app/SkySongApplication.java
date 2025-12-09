88f74986bca03a1615b1395667651c40e687a8da
Champel Mariusz <champelmariusz@gmail.com>
2025-12-09 22:00:44 +0100

    refactor(skysong-app): move Spring Boot application entrypoint to skysong-app module, centralize configuration properties scanning

--- skysong-app/src/main/java/com/mycompany/skysong/app/SkySongApplication.java
+++ skysong-app/src/main/java/com/mycompany/skysong/app/SkySongApplication.java
@@ -1,13 +1,14 @@
-package com.mycompany.SkySong;
+package com.mycompany.skysong.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan(basePackages = "com.mycompany.skysong.app.config")
 public class SkySongApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SkySongApplication.class, args);
 	}
-
 }
