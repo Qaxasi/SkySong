87ed8f3d1582376fab654de2f8c4ca4abfcf88fc
Champel Mariusz <champelmariusz@gmail.com>
2025-12-09 18:45:01 +0100

    refactor(identity-auth): simplify user authenticator usecase

--- identity-application/src/main/java/com/mycompany/skysong/identity/application/authentication/service/UserAuthenticator.java
+++ identity-application/src/main/java/com/mycompany/skysong/identity/application/authentication/service/UserAuthenticator.java
@@ -20,7 +20,7 @@ public class UserAuthenticator {
     private final SessionStore sessionStore;
     private final UserAccessSnapshotReader snapshotReader;
     private final Clock clock;
-    private final Duration ttl;
+    private final Duration sessionLifetime;
 
     public UserAuthenticator(final Authenticator authenticator,
                              final AccessTokenGenerator accessTokenGenerator,
@@ -28,56 +28,51 @@ public class UserAuthenticator {
                              final SessionStore sessionStore,
                              final UserAccessSnapshotReader snapshotReader,
                              final Clock clock,
-                             final Duration ttl) {
+                             final Duration sessionLifetime) {
         this.authenticator = authenticator;
         this.accessTokenGenerator = accessTokenGenerator;
         this.refreshTokenGenerator = refreshTokenGenerator;
         this.sessionStore = sessionStore;
         this.snapshotReader = snapshotReader;
         this.clock = clock;
-        this.ttl = ttl;
+        this.sessionLifetime = sessionLifetime;
     }
 
-
     public Result<AccessGrant> login(final Username username, final RawPassword password) {
-       try (password) {
-           return authenticator.authenticate(username, password)
-                   .flatMap(user -> snapshotReader.load(user.id())
-                           .flatMap(userAccessSnapshot -> {
-                               final Instant now = clock.instant();
-                               final Instant expiresAt = now.plus(ttl);
+        try (password) {
+            final Instant now = clock.instant();
 
-                               return Session.issue(
-                                               user.id(),
-                                               now,
-                                               expiresAt)
-                                       .flatMap(session ->
-                                               refreshTokenGenerator.generate()
-                                                       .flatMap(refreshToken -> {
-                                                           final Duration sessionTtl = session.ttl(now);
-                                                           return sessionStore.saveSession(
-                                                                           userAccessSnapshot.userTag(),
-                                                                           refreshToken,
-                                                                           session,
-                                                                           sessionTtl)
-                                                                   .map(ignored -> {
-                                                                       final AccessTokenClaims claims = new AccessTokenClaims(
-                                                                               user.id(),
-                                                                               userAccessSnapshot.roles());
-                                                                       final AccessToken accessToken = accessTokenGenerator.generate(claims);
+            return authenticator.authenticate(username, password)
+                    .flatMap(user -> snapshotReader.load(user.id())
+                            .flatMap(userAccessSnapshot -> Session.issue(
+                                            user.id(),
+                                            now,
+                                            sessionLifetime)
+                                    .flatMap(session ->
+                                            refreshTokenGenerator.generate()
+                                                    .flatMap(refreshToken -> {
+                                                        final Duration sessionTtl = session.ttl(now);
+                                                        return sessionStore.saveSession(
+                                                                        userAccessSnapshot.userTag(),
+                                                                        refreshToken,
+                                                                        session,
+                                                                        sessionTtl)
+                                                                .map(ignored -> {
+                                                                    final AccessTokenClaims claims = userAccessSnapshot.toClaims(session.userId());
+                                                                    final AccessToken accessToken = accessTokenGenerator.generate(claims);
 
-                                                                       final long expiresIn = accessToken.expiresInSeconds(now);
-                                                                       return new AccessGrant(
-                                                                               accessToken,
-                                                                               expiresIn,
-                                                                               refreshToken,
-                                                                               sessionTtl,
-                                                                               userAccessSnapshot.userTag());
-                                                                   });
-                                                       })
-                                       );
-                           })
-                   );
-       }
+                                                                    final long expiresIn = accessToken.expiresInSeconds(now);
+                                                                    return new AccessGrant(
+                                                                            accessToken,
+                                                                            expiresIn,
+                                                                            refreshToken,
+                                                                            sessionTtl,
+                                                                            userAccessSnapshot.userTag());
+                                                                });
+                                                    })
+                                    )
+                            )
+                    );
+        }
     }
 }
