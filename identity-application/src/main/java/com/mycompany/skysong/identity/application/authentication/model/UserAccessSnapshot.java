21af51c5138771c796f36f68fc768f250cc37d3e
Champel Mariusz <champelmariusz@gmail.com>
2025-12-09 18:42:06 +0100

    refactor(identity-auth): add claims mapping

--- identity-application/src/main/java/com/mycompany/skysong/identity/application/authentication/model/UserAccessSnapshot.java
+++ identity-application/src/main/java/com/mycompany/skysong/identity/application/authentication/model/UserAccessSnapshot.java
@@ -1,9 +1,15 @@
 package com.mycompany.skysong.identity.application.authentication.model;
 
+import com.mycompany.skysong.identity.domain.UserId;
 import com.mycompany.skysong.identity.domain.UserRole;
 import com.mycompany.skysong.identity.domain.UserTag;
 
 import java.util.Set;
 
-public record UserAccessSnapshot(UserTag userTag, Set<UserRole> roles) {
+public record UserAccessSnapshot(
+        UserTag userTag,
+        Set<UserRole> roles) {
+    public AccessTokenClaims toClaims(final UserId userId) {
+        return new AccessTokenClaims(userId, roles);
+    }
 }
