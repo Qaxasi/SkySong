fc0fa163af3df77d38c0eb52fbcf579cd269dcf3
Champel Mariusz <champelmariusz@gmail.com>
2025-12-27 20:35:27 +0100

    refactor(db/schema): rename sql script and move to identity-adpater module

---
+++ identity-adapter/src/main/resources/db/migration/identity/V271220252028__identity_create_user_roles_table.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS user_roles (
+   user_id INT UNSIGNED NOT NULL,
+   role_code ENUM('USER', 'ADMIN') NOT NULL,
+   PRIMARY KEY (user_id, role_code),
+   CONSTRAINT fk_user_roles_user
+       FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+) ENGINE=InnoDB;
