6640c6979c21b0334f9741563ddad207038145fc
Champel Mariusz <champelmariusz@gmail.com>
2026-01-02 21:15:56 +0100

    feat(identity/db-schema): add index on user_roles(role_code, user_id)

--- identity-adapter/src/main/resources/db/migration/identity/V271220252028__identity_create_user_roles_table.sql
+++ identity-adapter/src/main/resources/db/migration/identity/V271220252028__identity_create_user_roles_table.sql
@@ -2,6 +2,9 @@ CREATE TABLE IF NOT EXISTS user_roles (
    user_id INT UNSIGNED NOT NULL,
    role_code ENUM('USER', 'ADMIN') NOT NULL,
    PRIMARY KEY (user_id, role_code),
+
+   KEY index_user_roles_role_user (role_code, user_id),
+
    CONSTRAINT fk_user_roles_user
        FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 ) ENGINE=InnoDB;
